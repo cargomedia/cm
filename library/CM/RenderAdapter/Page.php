b21526644ef85c1fd9ee19a6ed208e91aa67aae5
Reto Kaiser <reto@cargomedia.ch>
2011-12-21 20:03:20 +0000

    t8: Adjusting page renderadapter to prepare page

--- library/CM/RenderAdapter/Page.php
+++ library/CM/RenderAdapter/Page.php
@@ -5,18 +5,22 @@ class CM_RenderAdapter_Page extends CM_RenderAdapter_Abstract {
 	public function fetch(array $params = array()) {
 		/** @var CM_Page_Abstract $page */
 		$page = $this->_getObject();
+		/** @var CM_RequestHandler_Abstract $requestHandler */
+		$requestHandler = $params['requestHandler'];
 		$js = $this->getRender()->getJs();
-		
+
+		$page->prepare($requestHandler);
+
 		// Creates header
 		$js->onloadHeaderJs("sk.options.renderStamp = " . floor((microtime(true)) * 1000));
 		$js->onloadHeaderJs('sk.options.siteId = "' . $this->getRender()->getSite()->getId() . '"');
 		$js->onloadHeaderJs('sk.options.urlStatic = "' . URL_STATIC . '"');
 		$js->onloadHeaderJs('sk.options.stream = ' . json_encode(Config::get()->stream));
 		$js->onloadHeaderJs('WEB_SOCKET_SWF_LOCATION = "' . URL_STATIC . 'swf/WebSocketMainInsecure.swf"');
-		
+
 		if ($viewer = $page->getViewer()) {
-			$js->onloadHeaderJs('sk.options.stream.channel = ' . json_encode(CM_Stream::getStreamChannel($viewer)));
-			$js->onloadHeaderJs('sk.viewer = ' . CM_Params::encode($viewer->getProfile(), true));
+			$js->onloadHeaderJs('sk.options.stream.channel = ' . CM_Params::encode(CM_Stream::getStreamChannel($viewer), true));
+			$js->onloadHeaderJs('sk.viewer = ' . CM_Params::encode($viewer, true));
 		}
 
 		$js->onloadReadyJs('sk.component()._ready();');
@@ -28,8 +32,8 @@ class CM_RenderAdapter_Page extends CM_RenderAdapter_Abstract {
 		$this->getLayout()->assign('tracking', CM_Tracking::getInstance()->getHtml());
 		$this->getLayout()->assign('page', $page);
 		$this->getLayout()->assign('js', $js);
-		
-		// Renders header and body
-		return $this->getLayout()->fetch($this->getRender()->getLayoutPath('layout/base.tpl'));		
+
+		$tplPath = $this->getRender()->getLayoutPath('layout/base.tpl');
+		return $this->getLayout()->fetch($tplPath);
 	}
 }
--- library/CM/RequestHandler/Page.php
+++ library/CM/RequestHandler/Page.php
@@ -10,28 +10,23 @@ class CM_RequestHandler_Page extends CM_RequestHandler_Abstract {
 		try {
 			$this->_setRequest($this->getSite()->rewrite($this->_request));
 			$page = CM_Page_Abstract::factory($this->getRequest());
-			$page->prepare($this);
-			$html = $this->getRender()->render($page);
+			$html = $this->getRender()->render($page, array('requestHandler' => $this));
 		} catch (CM_Exception_Nonexistent $e) {
 			$this->_setRequest(new CM_Request_Get('/error/not-found', $this->getRequest()->getHeaders()));
 			$page = CM_Page_Abstract::factory($this->getRequest());
-			$page->prepare($this);
-			$html = $this->getRender()->render($page);
+			$html = $this->getRender()->render($page, array('requestHandler' => $this));
 		} catch (CM_Exception_InvalidParam $e) {
 			$this->_setRequest(new CM_Request_Get('/error/not-found', $this->getRequest()->getHeaders()));
 			$page = CM_Page_Abstract::factory($this->getRequest());
-			$page->prepare($this);
-			$html = $this->getRender()->render($page);
+			$html = $this->getRender()->render($page, array('requestHandler' => $this));
 		} catch (CM_Exception_AuthRequired $e) {
 			$this->_setRequest(new CM_Request_Get('/account/signup', $this->getRequest()->getHeaders()));
 			$page = CM_Page_Abstract::factory($this->getRequest());
-			$page->prepare($this);
-			$html = $this->getRender()->render($page);
+			$html = $this->getRender()->render($page, array('requestHandler' => $this));
 		} catch (SK_Exception_PremiumRequired $e) {
 			$this->_setRequest(new CM_Request_Get('/account/premium', $this->getRequest()->getHeaders()));
 			$page = CM_Page_Abstract::factory($this->getRequest());
-			$page->prepare($this);
-			$html = $this->getRender()->render($page);
+			$html = $this->getRender()->render($page, array('requestHandler' => $this));
 		}
 
 		return $html;
