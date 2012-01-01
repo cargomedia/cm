c0d1cd906bf55beda895ad9647ef446eb604dd8d
Nicolas Ruflin <nicolas.ruflin@cargomedia.ch>
2012-01-01 16:21:46 +0000

    t8: Updated config added

--- config/cm.php
+++ config/cm.php
@@ -1,18 +1,32 @@
 <?php
 
-$config = CM_Config::get();
-
 $config->modified = '0';
-
-//$config->objects_cdn = 'http://cdn.fuckbook.com/';
-//$config->content_cdn = 'http://cdn.fuckbook.com/';
-$config->debug = true;
+$config->debug = false;
+$config->urlCdnObjects = false;
+$config->urlCdnContent = false;
 $config->timeZone = 'US/Central';
-$config->testIp = '127.0.0.1';
+$config->testIp = '162.23.39.73';
+
+$config->CM_Mail = new stdClass();
+$config->CM_Mail->siteName = 'CM';
+$config->CM_Mail->siteEmailAddress = 'noreply@example.com';
+
+$config->CM_Site_Abstract = new stdClass();
+$config->CM_Site_Abstract->class = 'CM_Site_Abstract';
+$config->CM_Site_Abstract->types = array();
+
+$config->CM_Tracking_Abstract = new stdClass();
+$config->CM_Tracking_Abstract->enabled = false;
+$config->CM_Tracking_Abstract->code = '';
+
+$config->CM_Splittesting_Abstract = new stdClass();
+$config->CM_Splittesting_Abstract->enabled = false;
 
 $config->CM_Search = new stdClass();
 $config->CM_Search->enabled = true;
-$config->CM_Search->servers = array(array('host' => 'localhost', 'port' => 9200),);
+$config->CM_Search->servers = array(
+	array('host' => 'localhost', 'port' => 9200),
+);
 
 $config->CM_Cache_Runtime = new stdClass;
 $config->CM_Cache_Runtime->enabled = true;
@@ -24,7 +38,9 @@ $config->CM_Cache_Apc->lifetime = 86400;
 $config->CM_Cache_Memcache = new stdClass();
 $config->CM_Cache_Memcache->enabled = true;
 $config->CM_Cache_Memcache->lifetime = 3600;
-$config->CM_Cache_Memcache->servers = array(array('host' => 'localhost', 'port' => 11211),);
+$config->CM_Cache_Memcache->servers = array(
+	array('host' => 'localhost', 'port' => 11211),
+);
 
 $config->CM_Cache_Redis = new stdClass;
 $config->CM_Cache_Redis->enabled = true;
@@ -39,7 +55,9 @@ $config->CM_Stream->enabled = true;
 $config->CM_StreamAdapter_Abstract = new stdClass();
 $config->CM_StreamAdapter_Abstract->class = 'CM_StreamAdapter_Apache';
 $config->CM_StreamAdapter_Abstract->hostPrefix = false;
-$config->CM_StreamAdapter_Abstract->servers = array(array('host' => 'localhost', 'port' => 80),);
+$config->CM_StreamAdapter_Abstract->servers = array(
+	array('host' => 'localhost', 'port' => 80),
+);
 
 $config->CM_Mysql = new stdClass();
 $config->CM_Mysql->db = 'cm';
@@ -47,13 +65,6 @@ $config->CM_Mysql->user = '';
 $config->CM_Mysql->pass = '';
 $config->CM_Mysql->server = array('host' => 'localhost', 'port' => 3306);
 $config->CM_Mysql->servers_read = array();
-
-$config->CM_Tracking_Abstract = new stdClass();
-$config->CM_Tracking_Abstract->enabled = false;
-$config->CM_Tracking_Abstract->code = '';
-
-$config->CM_Splittesting_Abstract = new stdClass();
-$config->CM_Splittesting_Abstract->enabled = false;
 
 $config->CM_Action_Abstract = new stdClass();
 $config->CM_Action_Abstract->types = array();
@@ -64,23 +75,17 @@ $config->CM_Model_ActionLimit_Abstract->types = array();
 $config->CM_Model_Entity_Abstract = new stdClass();
 $config->CM_Model_Entity_Abstract->types = array();
 
-$config->CM_Mail = new stdClass();
-$config->CM_Mail->siteName = 'CM';
-$config->CM_Mail->siteEmailAddress = 'noreply@cargomedia.ch';
-
 $config->CM_Model_User = new stdClass();
 $config->CM_Model_User->class = 'CM_User';
 
 $config->CM_Params = new stdClass();
 $config->CM_Params->class = 'CM_Params';
 
-$config->CM_Response_RPC = new stdClass();
-$config->CM_Response_RPC->catch = array('CM_Exception_AuthRequired',);
-
 $config->CM_Response_Page = new stdClass();
 $config->CM_Response_Page->catch = array(
 	'CM_Exception_Nonexistent' => '/error/not-found',
 	'CM_Exception_InvalidParam' => '/error/not-found',
+	'CM_Exception_AuthRequired' => '/error/auth-required',
 );
 
 $config->CM_Response_Component_Abstract = new stdClass();
@@ -91,8 +96,7 @@ $config->CM_Response_Component_Abstract->catch = array(
 	'CM_Exception_Nonexistent',
 );
 
-$config->CM_Site_Abstract = new stdClass();
-$config->CM_Site_Abstract->class = 'CM_Site_Abstract';
-$config->CM_Site_Abstract->types = array();
-
-$config->CM_Tracking_Abstract->code = '';
+$config->CM_Response_RPC = new stdClass();
+$config->CM_Response_RPC->catch = array(
+	'CM_Exception_AuthRequired',
+);
