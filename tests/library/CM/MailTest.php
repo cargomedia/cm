4941454977a86ca659ce0e68c183c25df9b47b3c
Reto Kaiser <reto@cargomedia.ch>
2012-01-03 14:09:36 +0000

    t8: Fix Mailtest

--- tests/library/CM/MailTest.php
+++ tests/library/CM/MailTest.php
@@ -10,21 +10,19 @@ class CM_MailTest extends TestCase {
 	}
 
 	public function testWithTemplate() {
-
-		$user = TH::createUser();
-
+		$this->markTestIncomplete('No mail template available');
+		$user = new CM_Model_User_Mock(TH::createUser()->getId());
 		try {
-			$msg = new CM_Mail($user, 'email_verify');
+			$msg = new CM_Mail($user, 'template_test');
 			$msg->send();
 			$this->assertTrue(true);
 		} catch (Exception $ex) {
-			$this->fail("Something's wrong");
+			$this->fail('Cannot send mail: ' . $ex->getMessage());
 		}
 	}
 
 	public function testNoTemplate() {
-
-		$msg = new CM_Mail('bla@bla.ch');
+		$msg = new CM_Mail('foo@example.com');
 		try {
 			$msg->send();
 			$this->fail('Should have thrown an exception');
@@ -47,15 +45,18 @@ class CM_MailTest extends TestCase {
 	}
 
 	public function testQueue() {
-		$this->markTestIncomplete('Uses profile');
-		$user = TH::createUser();
-		$profile = TH::createProfile($user);
-		$profile->setEmailVerified();
+		$user = new CM_Model_User_Mock(TH::createUser()->getId());
 		$msg = new CM_Mail($user, null, true);
 		$msg->setSubject('testSubject');
 		$msg->setHtml('<b>hallo</b>');
 		$msg->send();
 		$this->assertRow(TBL_CM_MAIL, array('subject' => 'testSubject', 'text' => 'hallo', 'html' => '<b>hallo</b>',
-			'recipientAddress' => $profile->getEmail()));
+			'recipientAddress' => 'foo@example.com'));
+	}
+}
+
+class CM_Model_User_Mock extends CM_Model_User {
+	public function getEmail() {
+		return 'foo@example.com';
 	}
 }
