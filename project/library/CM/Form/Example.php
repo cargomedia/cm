b4bcc1239744c91d558794777fef985251fbdf68
Nicolas Ruflin <nicolas.ruflin@cargomedia.ch>
2012-01-18 06:58:35 +0000

    Form refactoring -> put actions in seperate class

--- project/library/CM/Form/Example.php
+++ project/library/CM/Form/Example.php
@@ -9,47 +9,12 @@ class CM_Form_Example extends CM_Form_Abstract {
 		$this->registerField(new CM_FormField_FileImage('image', 2));
 		$this->registerField(new CM_FormField_Color('color'));
 
-		$this->registerAction(new CM_formExample_Go());
+		$this->registerAction(new CM_FormAction_Example_Go());
 	}
 
 	public function renderStart(array $params = null) {
 		if ($locationGuess = CM_Location::findByIp(CM_Request_Abstract::getIp())) {
 			$this->getField('location')->setValue($locationGuess);
 		}
-	}
-}
-
-class CM_formExample_Go extends CM_FormAction_Abstract {
-	public function __construct() {
-		parent::__construct('go');
-	}
-
-	public function setup(CM_Form_Abstract $form) {
-		$this->required_fields = array('text', 'color');
-		parent::setup($form);
-	}
-
-	public function process(array $data, CM_Response_Component_Form $response, CM_Form_Abstract $form) {
-		//$response->reloadComponent();
-		$response->addMessage(nl2br($this->_printVar($data)));
-	}
-
-	private function _printVar($var) {
-		$str = '';
-		if (is_object($var)) {
-			$str .= get_class($var);
-		} elseif (is_array($var)) {
-			$str .= '{';
-			if (!empty($var)) {
-				$str .= PHP_EOL;
-			}
-			foreach ($var as $key => $value) {
-				$str .= $key . ': ' . $this->_printVar($value) . PHP_EOL;
-			}
-			$str .= '}';
-		} else {
-			$str .= (string) $var;
-		}
-		return $str;
 	}
 }
---
+++ project/library/CM/FormAction/Example/Go.php
@@ -0,0 +1,36 @@
+<?php
+
+class CM_FormAction_Example_Go extends CM_FormAction_Abstract {
+	public function __construct() {
+		parent::__construct('go');
+	}
+
+	public function setup(CM_Form_Abstract $form) {
+		$this->required_fields = array('text', 'color');
+		parent::setup($form);
+	}
+
+	public function process(array $data, CM_Response_Component_Form $response, CM_Form_Abstract $form) {
+		//$response->reloadComponent();
+		$response->addMessage(nl2br($this->_printVar($data)));
+	}
+
+	private function _printVar($var) {
+		$str = '';
+		if (is_object($var)) {
+			$str .= get_class($var);
+		} elseif (is_array($var)) {
+			$str .= '{';
+			if (!empty($var)) {
+				$str .= PHP_EOL;
+			}
+			foreach ($var as $key => $value) {
+				$str .= $key . ': ' . $this->_printVar($value) . PHP_EOL;
+			}
+			$str .= '}';
+		} else {
+			$str .= (string) $var;
+		}
+		return $str;
+	}
+}
