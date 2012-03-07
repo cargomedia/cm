5a0b8593253d5a909129b718036fb106f1ef297a
Alexis Peter <alexis.peter@cargomedia.ch>
2012-03-07 11:44:25 +0000

    t115: refactored CM_Wowza

--- project/library/CM/Model/StreamChannel/Message.php
+++ project/library/CM/Model/StreamChannel/Message.php
@@ -10,7 +10,7 @@ class CM_Model_StreamChannel_Message extends CM_Model_StreamChannel_Abstract {
 	public function canSubscribe(CM_Model_User $user) {
 	}
 
-	public function onPublish(CM_Params $params = null) {
+	public function onPublish(CM_Model_Stream_Publish $streamPublish, CM_Params $params = null) {
 	}
 
 	public function onSubscribe(CM_Model_Stream_Subscribe $streamSubscribe, CM_Params $params = null) {
