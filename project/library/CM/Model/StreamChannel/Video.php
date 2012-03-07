5f04ab5cb889ee7816c2f56183371f7d24cc876a
Alexis Peter <alexis.peter@cargomedia.ch>
2012-03-07 12:17:06 +0000

    t115: revert CM_Model_StreamChannel_Video

--- project/library/CM/Model/StreamChannel/Video.php
+++ project/library/CM/Model/StreamChannel/Video.php
@@ -4,15 +4,9 @@ class CM_Model_StreamChannel_Video extends CM_Model_StreamChannel_Abstract {
 	const TYPE = 17;
 
 	public function canPublish(CM_Model_User $user) {
-		if (!$user->getRoles()->contains(SK_Role::PREMIUM)) {
-			return false;
-		}
-		return true;
 	}
 
 	public function canSubscribe(CM_Model_User $user) {
-		//check wallet
-		return true;
 	}
 
 	public function onPublish(CM_Model_Stream_Publish $streamPublish, CM_Params $params = null) {
