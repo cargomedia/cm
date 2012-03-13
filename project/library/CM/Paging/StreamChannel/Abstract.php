2831e6a0e5013799afc0fe19ef146e52ebcb261e
Alexis Peter <alexis.peter@cargomedia.ch>
2012-03-13 17:40:30 +0000

    t115: cleanup

--- project/library/CM/Paging/StreamChannel/Abstract.php
+++ project/library/CM/Paging/StreamChannel/Abstract.php
@@ -3,6 +3,6 @@
 class CM_Paging_StreamChannel_Abstract extends CM_Paging_Abstract {
 
 	protected function _processItem($item) {
-		return CM_Model_StreamChannel_Abstract::factory($item);
+		return CM_Model_StreamChannel_Abstract::factory($item['id'], $item['type']);
 	}
 }
