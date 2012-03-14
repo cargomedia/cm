05c750774c1c6ae6d250f1812b68e1ff8fe495d1
Alexis Peter <alexis.peter@cargomedia.ch>
2012-03-14 14:12:23 +0000

    t115: added contains() to CM_Paging_StreamChannel_Abstract

--- project/library/CM/Paging/StreamChannel/Abstract.php
+++ project/library/CM/Paging/StreamChannel/Abstract.php
@@ -5,4 +5,12 @@ class CM_Paging_StreamChannel_Abstract extends CM_Paging_Abstract {
 	protected function _processItem($item) {
 		return CM_Model_StreamChannel_Abstract::factory($item['id'], $item['type']);
 	}
+
+	/**
+	 * @param CM_Model_StreamChannel_Abstract $streamChannel
+	 * @return boolean
+	 */
+	public function contains(CM_Model_StreamChannel_Abstract $streamChannel) {
+		return in_array(array('id' => $streamChannel->getId(), 'type' => $streamChannel->getType()), $this->getItemsRaw());
+	}
 }
