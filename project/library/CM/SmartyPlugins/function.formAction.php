15cff54bc1bd8e9e13f9af04db90e866a2b315ec
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2012-04-10 11:44:01 +0000

    t156: adjust formAction function to append & prepend content

--- project/library/CM/SmartyPlugins/function.formAction.php
+++ project/library/CM/SmartyPlugins/function.formAction.php
@@ -4,7 +4,13 @@ require_once 'function.button.php';
 
 function smarty_function_formAction(array $params, Smarty_Internal_Template $template) {
 	$html = '<div class="formField clearfix submit">';
+	if (isset($params['prepend'])) {
+		$html .= (string) $params['prepend'];
+	}
 	$html .= smarty_function_button($params, $template);
+	if (isset($params['append'])) {
+		$html .= (string) $params['append'];
+	}
 	$html .= '</div>';
 	return $html;
 }
