3af402121894928829dae134d103c2239a814146
Reto Kaiser <reto@cargomedia.ch>
2012-04-15 14:40:41 +0000

    Formatting

--- project/library/CM/Paging/Transgression/Ip.php
+++ project/library/CM/Paging/Transgression/Ip.php
@@ -1,16 +1,15 @@
 <?php
 
-class CM_Paging_Transgression_Ip extends CM_Paging_Transgression_Abstract
- {
-	
+class CM_Paging_Transgression_Ip extends CM_Paging_Transgression_Abstract {
+
 	private $_ip;
-	
+
 	/**
 	 * @param int $ip
-	 * @param int $modelType OPTIONAL
+	 * @param int $modelType  OPTIONAL
 	 * @param int $actionType OPTIONAL
-	 * @param int $limitType OPTIONAL
-	 * @param int $period OPTIONAL
+	 * @param int $limitType  OPTIONAL
+	 * @param int $period     OPTIONAL
 	 */
 	public function __construct($ip = null, $modelType = null, $actionType = null, $limitType = null, $period = null) {
 		if ($ip) {
@@ -41,10 +40,10 @@ class CM_Paging_Transgression_Ip extends CM_Paging_Transgression_Abstract
 		$source = new CM_PagingSource_Sql_Deferred('modelType, actionType, createStamp', TBL_CM_ACTION, $where, '`createStamp` DESC');
 		parent::__construct($source);
 	}
-	
+
 	public function add(CM_Action_Abstract $action, $limitType) {
 		$limitType = (int) $limitType;
-		CM_Mysql::insertDelayed(TBL_CM_ACTION,
-				array('ip' => $this->_ip, 'actionType' => $action->getType(), 'modelType' => $action->getModelType(), 'actionLimitType' => $limitType, 'createStamp' => time()));
+		CM_Mysql::insertDelayed(TBL_CM_ACTION, array('ip' => $this->_ip, 'actionType' => $action->getType(), 'modelType' => $action->getModelType(),
+				'actionLimitType' => $limitType, 'createStamp' => time()));
 	}
 }
