65d06e28f8ead15d53e7d15ab0a838f48b2afdd3
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2012-04-17 13:10:36 +0000

    t162: delete -webkit-gradient legacy rule

--- project/library/CM/Css.php
+++ project/library/CM/Css.php
@@ -55,7 +55,6 @@ class CM_Css {
 	background-image: -webkit-linear-gradient(left,@color1,@color2);
 	background-image: -o-linear-gradient(left,@color1,@color2);
 	background-image: -ms-linear-gradient(left,@color1,@color2);
-	background-image: -webkit-gradient(linear,left top,right top,from(@color1),to(@color2));
 }
 .gradient(@direction, @color1, @color2) when (@direction = vertical) {
 	filter: progid:DXImageTransform.Microsoft.gradient(GradientType=0,startColorstr=rgbahex(@color1),endColorstr=rgbahex(@color2));
@@ -64,7 +63,6 @@ class CM_Css {
 	background-image: -webkit-linear-gradient(top,@color1,@color2);
 	background-image: -o-linear-gradient(top,@color1,@color2);
 	background-image: -ms-linear-gradient(top,@color1,@color2);
-	background-image: -webkit-gradient(linear,left top,left bottom,from(@color1),to(@color2));
 }
 .background-color(@color) when (isColor(@color)) {
 	filter: progid:DXImageTransform.Microsoft.gradient(GradientType=0,startColorstr=rgbahex(@color),endColorstr=rgbahex(@color));
