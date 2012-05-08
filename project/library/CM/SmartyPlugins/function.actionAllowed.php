0ddcd800ada7acf526ded99077142a32cf72a15c
Marcel Colomb <marcel.colomb@cargomedia.ch>
2012-05-08 15:55:06 +0000

    t198: refactoring params

--- project/library/CM/SmartyPlugins/function.actionAllowed.php
+++ project/library/CM/SmartyPlugins/function.actionAllowed.php
@@ -6,11 +6,11 @@
  */
 function smarty_function_actionAllowed(array $params, Smarty_Internal_Template $template) {
 
-	if (!isset($params['action'])) {
-		trigger_error('Param `action` missing.');
+	if (!isset($params['modelType'])) {
+		trigger_error('Param `modelType` missing.');
 	}
-	if (!isset($params['type'])) {
-		trigger_error('Param `type` missing.');
+	if (!isset($params['actionType'])) {
+		trigger_error('Param `actionType` missing.');
 	}
 	if (isset($params['forceAllow']) && $params['forceAllow'] == true) {
 		return true;
@@ -19,7 +19,7 @@ function smarty_function_actionAllowed(array $params, Smarty_Internal_Template $
 	/** @var CM_Model_User $viewer */
 	$viewer = $template->smarty->getTemplateVars('viewer');
 	if (isset($viewer)) {
-		$action = CM_Action_Abstract::factory($viewer, (int) $params['type'], (int) $params['action']);
+		$action = CM_Action_Abstract::factory($viewer, (int) $params['actionType'], (int) $params['modelType']);
 
 		/** @var $action SK_Action_Abstract */
 		return ($action->getActionLimit() == NULL);
