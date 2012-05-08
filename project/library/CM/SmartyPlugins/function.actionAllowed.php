21854cbf6a6c2dfa0e99da9a1576235241a57e27
Marcel Colomb <marcel.colomb@cargomedia.ch>
2012-05-08 15:39:18 +0000

    t198: Code review and bug fixes

--- project/library/CM/SmartyPlugins/function.actionAllowed.php
+++ project/library/CM/SmartyPlugins/function.actionAllowed.php
@@ -1,19 +1,30 @@
 <?php
-
+/**
+ * @param array                    $params
+ * @param Smarty_Internal_Template $template
+ * @return bool
+ */
 function smarty_function_actionAllowed(array $params, Smarty_Internal_Template $template) {
 
-	if($params['forceAllow'] == TRUE) {
-		return TRUE;
+	if (!isset($params['action'])) {
+		trigger_error('Param `action` missing.');
+	}
+	if (!isset($params['type'])) {
+		trigger_error('Param `type` missing.');
+	}
+	if (isset($params['forceAllow']) && $params['forceAllow'] == true) {
+		return true;
 	}
 
 	/** @var CM_Model_User $viewer */
 	$viewer = $template->smarty->getTemplateVars('viewer');
+	if (isset($viewer)) {
+		$action = CM_Action_Abstract::factory($viewer, (int) $params['type'], (int) $params['action']);
 
-	$action = new $params['action']($params['type'], $viewer);
-	//$action = new SK_Action_ConversationMessage_Gift(SK_Action_Abstract::VIEW, $viewer);
-
-
-	return ($action->getActionLimit() == NULL);
-
+		/** @var $action SK_Action_Abstract */
+		return ($action->getActionLimit() == NULL);
+	} else{
+		return false;
+	}
 
 }
