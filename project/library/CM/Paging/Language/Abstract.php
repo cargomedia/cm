5029b1795d77587e38dc06eeff5952e33c6ef718
Tomasz Durka <tomasz.durka@cargomedia.ch>
2012-07-05 17:25:30 +0000

    t257: class should be abstract

--- project/library/CM/Paging/Language/Abstract.php
+++ project/library/CM/Paging/Language/Abstract.php
@@ -1,6 +1,6 @@
 <?php
 
-class CM_Paging_Language_Abstract extends CM_Paging_Abstract {
+abstract class CM_Paging_Language_Abstract extends CM_Paging_Abstract {
 
 	protected function _processItem($item) {
 		return new CM_Model_Language($item);
