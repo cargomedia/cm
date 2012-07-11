6c2cf830ba8d6625a67f8aa9749b9a335a4f964b
Tomasz Durka <tomasz.durka@cargomedia.ch>
2012-07-11 08:43:52 +0000

    t268: where param fix

--- project/library/CM/Paging/Language/Enabled.php
+++ project/library/CM/Paging/Language/Enabled.php
@@ -3,7 +3,7 @@
 class CM_Paging_Language_Enabled extends CM_Paging_Language_Abstract {
 
 	public function __construct() {
-		$source = new CM_PagingSource_Sql('id, abbreviation', TBL_CM_LANGUAGE, array('enabled' => true));
+		$source = new CM_PagingSource_Sql('id, abbreviation', TBL_CM_LANGUAGE, 'enabled = 1');
 		$source->enableCacheLocal();
 		parent::__construct($source);
 	}
