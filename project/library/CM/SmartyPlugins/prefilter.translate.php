c35ea70fa19387eb7b59b96d3c8f4540588f1c50
Alexis Peter <alexis.peter@cargomedia.ch>
2012-07-19 15:35:07 +0000

    changed translate prefilter translateStatic-regexp to not take translate statements where variables are passed

--- project/library/CM/SmartyPlugins/prefilter.translate.php
+++ project/library/CM/SmartyPlugins/prefilter.translate.php
@@ -1,7 +1,7 @@
 <?php
 
 function smarty_prefilter_translate($source, Smarty_Internal_Template $template) {
-	$source = preg_replace('#{translate\s+([^}${]+})#', '{translateStatic key=$1', $source);
+	$source = preg_replace('#{translate\s+([^}${=]+})#', '{translateStatic key=$1', $source);
 	$source = preg_replace('#{translate\s+#', '{translateVariable key=', $source);
 	return $source;
 }
