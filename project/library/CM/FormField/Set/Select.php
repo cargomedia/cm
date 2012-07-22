24dd0b49e551faac45d16d7755652c3b8cae22c6
Tomasz Durka <tomasz.durka@cargomedia.ch>
2012-07-22 19:32:37 +0000

    t289: CM_FormField_Select now inherits fully from CM_FormField_Set; Removed the same methods' declarations

--- project/library/CM/FormField/Set/Select.php
+++ project/library/CM/FormField/Set/Select.php
@@ -1,23 +1,9 @@
 <?php
 
-class CM_FormField_Set_Select extends CM_FormField_Abstract {
-
-	private $_values = array();
-	private $_labelsInValues = false;
+class CM_FormField_Set_Select extends CM_FormField_Set {
 
 	const DISPLAY_SELECT = 'select';
 	const DISPLAY_RADIOS = 'radios';
-
-	/**
-	 * @param string       $name
-	 * @param array|null   $values
-	 * @param bool|null    $labelsInValues
-	 */
-	public function __construct($name, array $values = null, $labelsInValues = null) {
-		$this->_values = (array) $values;
-		$this->_labelsInValues = (bool) $labelsInValues;
-		parent::__construct($name);
-	}
 
 	public function validate($userInput, CM_Response_Abstract $response) {
 		if (!in_array($userInput, $this->_getValues())) {
@@ -42,27 +28,5 @@ class CM_FormField_Set_Select extends CM_FormField_Abstract {
 		$this->setTplParam('translate', !empty($params['translate']) || !empty($params['translatePrefix']));
 		$this->setTplParam('translatePrefix', !empty($params['translatePrefix']) ? $params['translatePrefix'] : '');
 		$this->setTplParam('colSize', !empty($params['colSize']) ? $params['colSize'] : '');
-	}
-
-	/**
-	 * @return array
-	 */
-	protected function _getOptionList() {
-		if ($this->_labelsInValues || !$this->_values) {
-			return $this->_values;
-		} else {
-			return array_combine($this->_values, $this->_values);
-		}
-	}
-
-	/**
-	 * @return array
-	 */
-	protected function _getValues() {
-		if ($this->_labelsInValues) {
-			return array_keys($this->_values);
-		} else {
-			return $this->_values;
-		}
 	}
 }
