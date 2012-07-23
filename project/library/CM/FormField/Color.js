54c780ae18b9d2603b193e6f423c7e5dfa3ed9ac
Reto Kaiser <reto@cargomedia.ch>
2012-07-23 18:17:50 +0000

    t307: Fix getUrlStatic() usage

--- project/library/CM/FormField/Color.js
+++ project/library/CM/FormField/Color.js
@@ -8,7 +8,7 @@ ready: function() {
 	$.fn.mColorPicker.init.showLogo = false;
 	$.fn.mColorPicker.init.enhancedSwatches = false;
 	this.$('input').mColorPicker({
-		"imageFolder": cm.getUrlStatic("img/jquery.mColorPicker/")
+		"imageFolder": cm.getUrlStatic('/img/jquery.mColorPicker/')
 	});
 	$("#mColorPickerFooter").remove();
 	$("#mColorPicker").height(158);
