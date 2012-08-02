5da1cb676b662e582e204b7f6c358b71fd3c7fe9
Tomasz Durka <tomasz.durka@cargomedia.ch>
2012-08-02 15:51:29 +0000

    t338: Refactor

--- project/library/CM/Response/Resource/CSS.php
+++ project/library/CM/Response/Resource/CSS.php
@@ -32,35 +32,30 @@ class CM_Response_Resource_CSS extends CM_Response_Resource_Abstract {
 				}
 			}
 
-			foreach (array('Component', 'Page', 'FormField') as $viewType) {
-				$viewClasses = array();
-				foreach ($this->getSite()->getNamespaces() as $namespace) {
-					$viewClasses = array_merge($viewClasses, CM_Util::rglob('*.php', DIR_LIBRARY . $namespace . '/' . $viewType . '/'));
-				}
-				foreach (CM_Util::getClasses($viewClasses) as $viewClass) {
-					if (!preg_match('#^(\w+)_' . $viewType . '_(.+)$#', $viewClass['classNames'][0], $matches)) {
-						throw new CM_Exception("Cannot detect namespace from component's class-name");
-					}
-					$namespace = $matches[1];
-					$viewName = $matches[2];
-					$relativePaths = array();
-					foreach ($this->getRender()->getSite()->getThemes() as $theme) {
-						$basePath = $this->getRender()->getThemeDir(true, $theme, $namespace) . $viewType . '/' . $viewName . '/';
-						foreach (CM_Util::rglob('*.less', $basePath) as $path) {
-							$relativePaths[] = preg_replace('#^' . $basePath . '#', '', $path);
-						}
-					}
-					foreach (array_unique($relativePaths) as $path) {
-						$prefix = '.' . $viewClass['classNames'][0];
-						if ('Component' == $viewType) {
-							if ($path != 'default.less' && strpos($path, '/') === false) {
-								$prefix .= '.' . preg_replace('#.less$#', '', $path);
-							}
-						}
 
-						$file = $this->getRender()->getLayoutFile($viewType . '/' . $viewName . '/' . $path, $namespace);
-						$css->add(new CM_Css($file->read(), $prefix));
+			$viewClasses = CM_View_Abstract::getClasses($this->getSite()->getNamespaces(), CM_View_Abstract::CONTEXT_CSS);
+			foreach ($viewClasses as $viewClass) {
+				if (!preg_match('#^([^_]+)_([^_]+)_(.+)$#', $viewClass['classNames'][0], $matches)) {
+					throw new CM_Exception("Cannot detect namespace from component's class-name");
+				}
+				list($className, $namespace, $viewType, $viewName) = $matches;
+				$relativePaths = array();
+				foreach ($this->getRender()->getSite()->getThemes() as $theme) {
+					$basePath = $this->getRender()->getThemeDir(true, $theme, $namespace) . $viewType . '/' . $viewName . '/';
+					foreach (CM_Util::rglob('*.less', $basePath) as $path) {
+						$relativePaths[] = preg_replace('#^' . $basePath . '#', '', $path);
 					}
+				}
+				foreach (array_unique($relativePaths) as $path) {
+					$prefix = '.' . $className;
+					if ('Component' == $viewType) {
+						if ($path != 'default.less' && strpos($path, '/') === false) {
+							$prefix .= '.' . preg_replace('#.less$#', '', $path);
+						}
+					}
+
+					$file = $this->getRender()->getLayoutFile($viewType . '/' . $viewName . '/' . $path, $namespace);
+					$css->add(new CM_Css($file->read(), $prefix));
 				}
 			}
 			$content = $css->compile($this->getRender());
