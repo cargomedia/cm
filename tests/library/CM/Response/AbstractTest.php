7021679d5c9245f4f46c033897d91fbfe88ab4d1
Tomasz Durka <tomasz.durka@cargomedia.ch>
2012-08-08 14:44:22 +0000

    t347: Response::match tests

---
+++ tests/library/CM/Response/AbstractTest.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/../../../TestCase.php';
+
+class CM_Response_AbstractTest extends TestCase {
+
+	public function tearDown() {
+		TH::clearEnv();
+	}
+
+	public function testFactory() {
+		$responses = array(
+			'/captcha/'       => 'CM_Response_Captcha',
+			'/emailtracking/' => 'CM_Response_EmailTracking',
+			'/longpolling/'   => 'CM_Response_Longpolling',
+			'/rpc/'           => 'CM_Response_RPC',
+			'/upload/'        => 'CM_Response_Upload',
+			'/css/'           => 'CM_Response_Resource_CSS',
+			'/img/'           => 'CM_Response_Resource_Img',
+			'/js/'            => 'CM_Response_Resource_JS',
+			'/ajax/'          => 'CM_Response_View_Ajax',
+			'/form/'          => 'CM_Response_View_Form',
+			// TODO: '/homepage/'      => 'CM_Response_Page',
+		);
+		foreach ($responses as $path => $expectedResponse) {
+			$request = new CM_Request_Post($path, null, null, '');
+			$this->assertInstanceOf($expectedResponse, CM_Response_Abstract::factory($request));
+		}
+	}
+
+}
