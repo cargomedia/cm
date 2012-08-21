31f08f006f1ce4613362d34937e941a2facf8a7a
Alexis Peter <alexis.peter@cargomedia.ch>
2012-08-21 12:17:11 +0000

    t370: expanded tests

--- tests/library/CM/PagingSource/ArrayTest.php
+++ tests/library/CM/PagingSource/ArrayTest.php
@@ -7,6 +7,10 @@ class CM_PagingSource_ArrayTest extends TestCase {
 		$pagingSource = new CM_PagingSource_Array(range(1, 10));
 		$this->assertSame(10, $pagingSource->getCount());
 		$this->assertSame(10, $pagingSource->getCount(5));
+
+		$pagingSource = new CM_PagingSource_Array(array());
+		$this->assertSame(0, $pagingSource->getCount());
+
 	}
 
 	public function testGetItems() {
@@ -15,6 +19,10 @@ class CM_PagingSource_ArrayTest extends TestCase {
 		$this->assertSame(range(6,10), $pagingSource->getItems(5));
 		$this->assertSame(range(1,5), $pagingSource->getItems(null, 5));
 		$this->assertSame(range(4,8), $pagingSource->getItems(3,5));
+
+		$pagingSource = new CM_PagingSource_Array(array());
+		$this->assertSame(array(), $pagingSource->getItems());
+		$this->assertSame(array(), $pagingSource->getItems(2));
 	}
 
 }
