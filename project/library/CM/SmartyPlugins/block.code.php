f327e29392de9bcb2036df4cd1e3c790354b00a9
Tomasz Durka <tomasz.durka@cargomedia.ch>
2012-08-21 14:46:06 +0000

    t344: removed keyword links

--- project/library/CM/SmartyPlugins/block.code.php
+++ project/library/CM/SmartyPlugins/block.code.php
@@ -16,6 +16,7 @@ function smarty_block_code($params, $content, Smarty_Internal_Template $template
 
 		$content = trim($content, "\n\r");
 		$geshi = new GeSHi($content, $language);
+		$geshi->keyword_links = false;
 		$geshi->set_tab_width(4);
 		$geshi->set_header_type(GESHI_HEADER_NONE);
 
