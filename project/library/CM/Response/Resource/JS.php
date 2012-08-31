c1ec009a7fd74d7227555a67d117088bfad41fa8
Reto Kaiser <reto@cargomedia.ch>
2012-08-31 12:00:17 +0000

    t384: Raise lock timeout

--- project/library/CM/Response/Resource/JS.php
+++ project/library/CM/Response/Resource/JS.php
@@ -83,7 +83,7 @@ class CM_Response_Resource_JS extends CM_Response_Resource_Abstract {
 				}
 
 				if (false === ($contentMinified = CM_CacheLocal::get($cacheKey))) {
-					CM_CacheLocal::set($cacheKeyLock, true, 10);
+					CM_CacheLocal::set($cacheKeyLock, true, 30);
 					$contentMinified = CM_Util::exec('uglifyjs --no-copyright', null, $content);
 					CM_CacheLocal::set($cacheKey, $contentMinified);
 					CM_CacheLocal::delete($cacheKeyLock);
