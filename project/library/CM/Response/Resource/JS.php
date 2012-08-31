16271dc0df5d52c9c470de82b998e68fb1af2164
Tomasz Durka <tomasz.durka@cargomedia.ch>
2012-08-31 13:40:39 +0000

    t384: Reto - shame on you!

--- project/library/CM/Response/Resource/JS.php
+++ project/library/CM/Response/Resource/JS.php
@@ -72,7 +72,7 @@ class CM_Response_Resource_JS extends CM_Response_Resource_Abstract {
 	 * @return string
 	 */
 	private function _process($content) {
-		if (true || !$this->getRender()->isDebug()) {
+		if (!$this->getRender()->isDebug()) {
 			$md5 = md5($content);
 			$cacheKey = CM_CacheConst::Response_Resource_JS . '_md5:' . $md5;
 			$cacheKeyLock = CM_CacheConst::Response_Resource_JS_Lock . '_md5:' . $md5;
