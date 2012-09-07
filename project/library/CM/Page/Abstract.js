e9154c4f762b2062994ab14566769f165e3683d2
Alexis Peter <alexis.peter@cargomedia.ch>
2012-09-07 09:22:19 +0000

    398: page js

---
+++ project/library/CM/Page/Abstract.js
@@ -0,0 +1,9 @@
+/**
+ * @class CM_Page_Abstract
+ * @extends CM_Component_Abstract
+ */
+var CM_Page_Abstract = CM_Component_Abstract.extend({
+
+	/** @type String */
+	_class: 'CM_Page_Abstract'
+});
