72c5efc97483e399808858857e2c53e84ec56d5f
Tomasz Durka <tomasz.durka@cargomedia.ch>
2012-09-30 17:00:27 +0000

    t406: Kiss metrics adjustments

--- project/library/CM/KissTracking.php
+++ project/library/CM/KissTracking.php
@@ -1,10 +1,10 @@
 <?php
 
-class CM_KissTracking_Api extends CM_Class_Abstract {
+class CM_KissTracking extends CM_Class_Abstract {
 
 	const setName = 'kisstracking';
 
-	/** @var CM_KissTracking_Api */
+	/** @var CM_KissTracking */
 	private static $_instance;
 
 	/** @var string */
@@ -14,44 +14,37 @@ class CM_KissTracking_Api extends CM_Class_Abstract {
 	private $_set;
 
 	public function __construct() {
-		$this->_dirData = DIR_TMP . 'kisstracking/';
-		CM_Util::mkDir($this->_dirData);
+		$this->_dirData = DIR_TMP;
 	}
 
 	/**
-	 * @param String        $event
+	 * @param string        $event
 	 * @param CM_Model_User $user
-	 * @param null|String   $alias
+	 * @param null|string   $alias
 	 * @param array         $properties
-	 * @return CM_KissTracking_Api
+	 * @return CM_KissTracking
 	 */
 	public function trackUser($event, CM_Model_User $user, $alias = null, array $properties = null) {
 		return $this->track($event, $user->getId(), $alias, $properties);
 	}
 
 	/**
-	 * @param       $event
-	 * @param       $identity
-	 * @param null  $alias
-	 * @param array $properties
-	 * @return CM_KissTracking_Api
+	 * @param string        $event
+	 * @param string        $identity
+	 * @param null|string   $alias
+	 * @param array         $properties
 	 */
 	public function track($event, $identity, $alias = null, array $properties = null) {
 		$event = (string) $event;
 		$identity = (string) $identity;
-		if (null !== $alias) {
-			$alias = (string) $alias;
-		}
+		$alias = (string) $alias;
 		$properties = (array) $properties;
-		$record = array('Identity' => $identity, 'Alias' => $alias);
-		$record['Timestamp'] = (string) time();
-		$record['Event'] = $event;
+		$record = array('Identity' => $identity, 'Alias' => $alias, 'Timestamp' => time(), 'Event' => $event);
 		foreach ($properties as $propName => &$propValue) {
 			$record['Prop:' . $propName] = (string) $propValue;
 		}
 
 		$this->_getSet()->add($record);
-		return $this;
 	}
 
 	public function exportEvents() {
@@ -59,7 +52,7 @@ class CM_KissTracking_Api extends CM_Class_Abstract {
 		if (count($files) > 1) {
 			sort($files);
 			$file = new CM_File_Csv($files[0]);
-			$this->uploadCsv($file);
+			$this->_uploadCsv($file);
 			$file->delete();
 		}
 	}
@@ -78,6 +71,7 @@ class CM_KissTracking_Api extends CM_Class_Abstract {
 		}
 
 		if (!file_exists($filename) || !is_readable($filename)) {
+			/** @var $file CM_File_Csv */
 			$file = CM_File_Csv::create($filename);
 			$file->appendRow($header);
 		} else {
@@ -94,22 +88,21 @@ class CM_KissTracking_Api extends CM_Class_Abstract {
 	}
 
 	/**
-	 * @param CM_File_Csv $file
-	 * @throws CM_Exception_Invalid
-	 */
-	public function uploadCsv(CM_File_Csv $file) {
-		$bucketName = self::_getConfig()->awsBucketName;
-		$targetFilename = self::_getConfig()->awsFilePrefix . '.' . date('YmdHis') . '.csv';
-
-		$amazonS3 = new CM_Amazon_S3();
-		$amazonS3->createObject($file, $bucketName, $targetFilename, array('6acb81d7742ac437833f51ecb2a40c74cd831ce26909e5f72354fa6af42cfb1f' => 'full-control'));
-	}
-
-	/**
 	 * @return string
 	 */
 	protected function _getFileName() {
 		return $this->_dirData . self::_getConfig()->awsFilePrefix . '.' . date('YmdH') . '.csv';
+	}
+
+	/**
+	 * @param CM_File_Csv $file
+	 */
+	private function _uploadCsv(CM_File_Csv $file) {
+		$bucketName = self::_getConfig()->awsBucketName;
+		$targetFilename = self::_getConfig()->awsFilePrefix . '.' . date('YmdHis') . '.csv';
+
+		$amazonS3 = new CM_Amazon_S3();
+		$amazonS3->upload($file, $bucketName, $targetFilename, array('6acb81d7742ac437833f51ecb2a40c74cd831ce26909e5f72354fa6af42cfb1f' => 'full-control'));
 	}
 
 	/**
@@ -125,12 +118,12 @@ class CM_KissTracking_Api extends CM_Class_Abstract {
 	/**
 	 * @return mixed[]
 	 */
-	private function _getEvents() {
+	public function _getEvents() {
 		return $this->_getSet()->flush();
 	}
 
 	/**
-	 * @return CM_KissTracking_Api
+	 * @return CM_KissTracking
 	 */
 	public static function getInstance() {
 		if (!self::$_instance) {
