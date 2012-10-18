71f15b4f240fd55986115127278696027acc885a
Alexis Peter <alexis.peter@cargomedia.ch>
2012-10-18 16:50:41 +0000

    Bug: Ads not working with ajax-navigation: created a jQuery-plugin to inject ads after page is loaded, created a proxy-script for the adserver to handle document.write

--- library/CM/AdproviderAdapter/Openx.php
+++ library/CM/AdproviderAdapter/Openx.php
@@ -15,24 +15,8 @@ class CM_AdproviderAdapter_Openx extends CM_AdproviderAdapter_Abstract {
 		}
 		$zoneId = $zoneData['zoneId'];
 		$host = $this->_getHost();
-		$rand = rand(1, 999999);
-		$uniqid = 'a' . rand(1, 999999);
 		$html = <<<EOF
-<script type='text/javascript'><!--//<![CDATA[
-   var m3_u = (location.protocol=='https:'?'https://$host/delivery/ajs.php':'http://$host/delivery/ajs.php');
-   var m3_r = Math.floor(Math.random()*99999999999);
-   if (!document.MAX_used) document.MAX_used = ',';
-   document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
-   document.write ("?zoneid=$zoneId");
-   document.write ('&amp;cb=' + m3_r);
-   if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
-   document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
-   document.write ("&amp;loc=" + escape(window.location));
-   if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
-   if (document.context) document.write ("&context=" + escape(document.context));
-   if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
-   document.write ("'><\/scr"+"ipt>");
-//]]>--></script><noscript><a href='http://$host/delivery/ck.php?n=$uniqid&amp;cb=$rand' target='_blank'><img src='http://$host/delivery/avw.php?zoneid=$zoneId&amp;cb=$rand&amp;n=$uniqid' border='0' alt='' /></a></noscript>
+<div class="adSpace" data-zone-id=$zoneId data-host=$host></div>
 
 EOF;
 		return $html;
