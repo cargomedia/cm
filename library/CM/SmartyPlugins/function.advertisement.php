cfe109ffd8c61ec1304e64579d634b63b5d514d8
Reto Kaiser <reto@cargomedia.ch>
2012-10-18 12:44:37 +0000

    Fix: ad smarty function

--- library/CM/SmartyPlugins/function.advertisement.php
+++ library/CM/SmartyPlugins/function.advertisement.php
@@ -4,7 +4,7 @@
  * @param Smarty_Internal_Template $template
  * @return bool
  */
-function smarty_function_advertisment(array $params, Smarty_Internal_Template $template) {
+function smarty_function_advertisement(array $params, Smarty_Internal_Template $template) {
 	if (!isset($params['zone'])) {
 		trigger_error('Param `zone` missing.');
 	}
