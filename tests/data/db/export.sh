87af6d16c1a180f926584d7d55335fab14b5080e
Reto Kaiser <reto@cargomedia.ch>
2012-10-29 20:22:31 +0000

    t493: This perl 'and' did not work

--- tests/data/db/export.sh
+++ tests/data/db/export.sh
@@ -10,4 +10,5 @@ echo 'SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";' >> dump.sql
 echo '/*!40101 SET NAMES utf8 */;' >> dump.sql
 
 mysqldump --no-data --compact --add-drop-table -u$DB_USER -p$DB_PASS $DB_NAME \
-	| perl -pe 's#\s*AUTO_INCREMENT=\d*\s*# # and s#/\*.*\*/;##' >> dump.sql
+	| perl -pe 's#\s*AUTO_INCREMENT=\d*\s*# #' \
+	| perl -pe 's#/\*.*\*/;##' >> dump.sql
