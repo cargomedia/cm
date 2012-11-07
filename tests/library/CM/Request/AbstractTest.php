3b9b328c8be1028d80784d25e994fd34dfc3ef52
Marcel Colomb <marcel.colomb@cargomedia.ch>
2012-11-07 14:26:29 +0000

    t499: Review Request

--- tests/library/CM/Request/AbstractTest.php
+++ tests/library/CM/Request/AbstractTest.php
@@ -146,7 +146,7 @@ class CM_Request_AbstractTest extends TestCase {
 
 
 	public function testGetClientIdSetCookie() {
-		$request = new CM_Request_Post('/foo/bar');
+		$request = new CM_Request_Post('/foo/' . CM_Site_CM::TYPE);
 		$clientId = $request->getClientId();
 		/** @var CM_Response_Abstract $response */
 		$response = $this->getMock('CM_Response_Abstract', array('_process', 'setCookie'), array($request));
