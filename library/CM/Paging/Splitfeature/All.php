a2952ee7c9c7d5e80ae17763e2dc12b5a7ba8d5c
Marcel Colomb <marcel.colomb@cargomedia.ch>
2012-11-12 18:01:07 +0000

    t509: Bugfix paging

--- library/CM/Paging/Splitfeature/All.php
+++ library/CM/Paging/Splitfeature/All.php
@@ -1,6 +1,6 @@
 <?php
 
-class CM_Paging_Splitfeature_All extends CM_Paging_Splittest_Abstract {
+class CM_Paging_Splitfeature_All extends CM_Paging_Splitfeature_Abstract {
 
 	public function __construct() {
 		$source = new CM_PagingSource_Sql('name', TBL_CM_SPLITFEATURE);
