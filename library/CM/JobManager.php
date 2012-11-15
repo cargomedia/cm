118715f70d5e5d37f964f636d1d553e9747754eb
Alexis Peter <alexis.peter@cargomedia.ch>
2012-11-15 20:52:46 +0000

    t502: make JobManager kill his own children before he gets killed

--- library/CM/JobManager.php
+++ library/CM/JobManager.php
@@ -2,13 +2,20 @@
 
 final class CM_JobManager extends CM_Class_Abstract {
 
-	private $_childCount;
+	/** @var array */
+	private $_children;
+
+	public function __construct() {
+		declare(ticks = 1);
+		pcntl_signal(SIGTERM, array($this, '_handleSignal'));
+		pcntl_signal(SIGINT, array($this, '_handleSignal'));
+	}
 
 	public function start() {
 		while (true) {
-			if ($this->_childCount == $this->_getConfig()->workerCount) {
-				if (pcntl_wait($status, WNOHANG) > 0) {
-					$this->_childCount--;
+			if (count($this->_children) == $this->_getConfig()->workerCount) {
+				if ($pid = pcntl_wait($status, WNOHANG) > 0) {
+					unset($this->_children[$pid]);
 					$this->_startWorker();
 				}
 				usleep(50000);
@@ -29,7 +36,19 @@ final class CM_JobManager extends CM_Class_Abstract {
 			case -1: //failure
 				throw new CM_Exception('Could not fork Gearman Job Manager');
 			default: //parent
-				$this->_childCount++;
+				$this->_children[$pid] = $pid;
 		}
 	}
+
+	private function _handleSignal($signal) {
+		switch ($signal) {
+			case SIGTERM:
+			case SIGINT:
+				foreach ($this->_children as $child) {
+					posix_kill($child, SIGKILL);
+				}
+				exit;
+		}
+	}
+
 }
