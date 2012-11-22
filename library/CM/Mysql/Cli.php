5825d7ddee4b3e23dc3ce0a7aec04e237a941898
Reto Kaiser <reto@cargomedia.ch>
2012-11-22 14:46:06 +0000

    t527: Add underscore after namespace in table filter

--- library/CM/Mysql/Cli.php
+++ library/CM/Mysql/Cli.php
@@ -7,7 +7,7 @@ class CM_Mysql_Cli extends CM_Cli_Runnable_Abstract {
 	 */
 	public function dump($namespace) {
 		$namespace = (string) $namespace;
-		$tables = CM_Mysql::exec("SHOW TABLES LIKE '?'", strtolower($namespace) . '%')->fetchCol();
+		$tables = CM_Mysql::exec("SHOW TABLES LIKE '?'", strtolower($namespace) . '_%')->fetchCol();
 		sort($tables);
 		$dump = CM_Mysql::getDump($tables, true);
 		CM_File::create(CM_Util::getNamespacePath($namespace) . '/resources/db/structure.sql', $dump);
