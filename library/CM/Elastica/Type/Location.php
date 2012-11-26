6ff4d9798c2253f5e7116d6bacab3ca50ba6fd91
Tomasz Durka <tomasz.durka@cargomedia.ch>
2012-11-26 20:09:47 +0000

    t537: Move search type for Loction to CM

---
+++ library/CM/Elastica/Type/Location.php
@@ -0,0 +1,70 @@
+<?php
+
+class CM_Elastica_Type_Location extends CM_Elastica_Type_Abstract {
+
+	const INDEX_NAME = 'location';
+
+	protected $_mapping = array(
+		'level' => array('type' => 'integer', 'store' => 'yes'),
+		'id' => array('type' => 'integer', 'store' => 'yes'),
+		'ids' => array('type' => 'object', 'properties' => array(
+			'1' => array('type' => 'integer'),
+			'2' => array('type' => 'integer'),
+			'3' => array('type' => 'integer'),
+			'4' => array('type' => 'integer'),			
+		)),
+		'name' => array('type' => 'string', 'analyzer' => 'lw'),
+		'coordinates' => array('type' => 'geo_point'),
+	);
+
+	protected $_indexParams = array(
+		'index' => array(
+			'number_of_shards' => 1,
+			'number_of_replicas' => 0
+		),
+		'analysis' => array(
+			'analyzer' => array(
+				'lw' => array(
+					'type' => 'custom',
+					'tokenizer' => 'keyword',
+					'filter' => array('lowercase')
+				)
+			),
+        )
+	);
+
+	protected function _getQuery($ids = null, $limit = null) {
+		$query = 'SELECT * FROM `cm_tmp_location`';
+		if (($limit = (int) $limit) > 0) {
+			$query .= ' LIMIT ' . $limit;
+		}
+		return $query;
+	}
+
+	protected function _getDocument(array $data) {
+		$doc = new Elastica_Document(null, array(
+			'level' => (int) $data['level'],
+			'id' => (int) $data['id'],
+			'name' => $data['name'],
+			'ids' => array(
+				'1' => $data['1Id'],
+				'2' => $data['2Id'],
+				'3' => $data['3Id'],
+				'4' => $data['4Id'],		
+			),
+		));
+
+		if (isset($data['lat']) && isset($data['lon'])) {
+			$doc->addGeoPoint('coordinates', $data['lat'], $data['lon']);	
+		}
+		
+		return $doc;
+	}
+
+	/**
+	 * @throws CM_Exception_NotImplemented
+	 */
+	public static function updateItem() {
+		throw new CM_Exception_NotImplemented();
+	}
+}
--- library/CM/PagingSource/Search/Location.php
+++ library/CM/PagingSource/Search/Location.php
@@ -3,6 +3,6 @@
 class CM_PagingSource_Search_Location extends CM_PagingSource_Search {
 
 	function __construct(CM_SearchQuery_Abstract $query) {
-		parent::__construct(CM_Search::INDEX_LOCATION, CM_Search::INDEX_LOCATION, $query, array('level', 'id'));
+		parent::__construct(CM_Elastica_Type_Location::INDEX_NAME, CM_Elastica_Type_Location::INDEX_NAME, $query, array('level', 'id'));
 	}
 }
