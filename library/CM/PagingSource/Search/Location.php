a20ead36bb715d861f2ef7b9d40a2466734dc80d
Reto Kaiser <reto@cargomedia.ch>
2012-11-29 13:14:24 +0000

    Fix: Use CM's location index

--- library/CM/PagingSource/Search/Location.php
+++ library/CM/PagingSource/Search/Location.php
@@ -3,6 +3,6 @@
 class CM_PagingSource_Search_Location extends CM_PagingSource_Search {
 
 	function __construct(CM_SearchQuery_Abstract $query) {
-		parent::__construct(new SK_Elastica_Type_Location(), $query, array('level', 'id'));
+		parent::__construct(new CM_Elastica_Type_Location(), $query, array('level', 'id'));
 	}
 }
