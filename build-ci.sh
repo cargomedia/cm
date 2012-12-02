9030d10348ef9cf99ff20e49765a12b2bbde8efd
Reto Kaiser <reto@cargomedia.ch>
2012-12-02 20:41:51 +0000

    CI; Add build script and ci config

---
+++ build-ci.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+cd $(dirname $0)/../..
+
+USER="root"
+HOST="172.10.1.100"
+DIR=$(ssh ${USER}@${HOST} "mktemp -d /tmp/build.XXXXXX")
+
+scp -qr . ${USER}@${HOST}:${DIR}
+ssh ${USER}@${HOST} "
+ cd ${DIR} &&
+ cp resources/config/local.ci.php resources/config/local.php
+ composer -q -n install &&
+ phpunit -d display_errors=1 tests/
+"
+ssh ${USER}@${HOST} "rm -rf ${DIR}"
---
+++ resources/config/local.ci.php
@@ -0,0 +1,22 @@
+<?php
+
+$config->CM_Cache_Redis->server = array('host' => '172.10.1.100', 'port' => 6379);
+
+$config->CM_Search->servers = array(
+	array('host' => '172.10.1.105', 'port' => 9200),
+);
+
+$config->CM_Cache_Memcache->servers = array(
+	array('host' => '172.10.1.100', 'port' => 11211),
+);
+
+$config->CM_Mysql->db = 'skadate_test';
+$config->CM_Mysql->user = 'skadate';
+$config->CM_Mysql->pass = 'k290DSkjsfiE02sDjj';
+$config->CM_Mysql->server = array('host' => '172.10.1.101', 'port' => 3306);
+$config->CM_Mysql->servers_read = array(
+	array('host' => '127.0.0.1', 'port' => 4040),
+);
+
+$config->CM_Jobdistribution_JobWorker->servers = array(array('host' => 'localhost', 'port' => 4730));
+$config->CM_Jobdistribution_Job_Abstract->servers = array(array('host' => 'localhost', 'port' => 4730));
