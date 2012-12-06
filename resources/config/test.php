d0dea26204b128a18e672d0926a8d54f85934539
Reto Kaiser <reto@cargomedia.ch>
2012-12-06 13:42:20 +0000

    t571: Disable gearman for tests

--- resources/config/test.php
+++ resources/config/test.php
@@ -17,3 +17,5 @@ $config->CM_Model_Splittest->withoutPersistence = true;
 $config->CM_Model_Splitfeature->withoutPersistence = true;
 
 $config->CM_Model_Location->loadFromSlave = false;
+
+$config->CM_Jobdistribution_Job_Abstract->gearmanEnabled = false;
