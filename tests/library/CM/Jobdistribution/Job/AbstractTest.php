a0a20c91bd984f40f8954ca6209ceaf4ae499ab1
Reto Kaiser <reto@cargomedia.ch>
2012-12-06 17:15:58 +0000

    t571: Adjust jobdistribution to cover gearman enabled+disabled

--- tests/library/CM/Jobdistribution/Job/AbstractTest.php
+++ tests/library/CM/Jobdistribution/Job/AbstractTest.php
@@ -4,16 +4,11 @@ require_once __DIR__ . '/../../../../TestCase.php';
 
 class CM_Jobdistribution_Job_AbstractTest extends TestCase {
 
-	public static function setUpBeforeClass() {
-	}
-
-	public static function tearDownAfterClass() {
-	}
-
 	public function testRun() {
 		if (!extension_loaded('gearman')) {
 			$this->markTestSkipped('Gearman Pecl Extension not installed.');
 		}
+		CM_Config::get()->CM_Jobdistribution_Job_Abstract->gearmanEnabled = true;
 
 		/** @var CM_Jobdistribution_Job_Abstract $job  */
 		$job = $this->getMockForAbstractClass('CM_Jobdistribution_Job_Abstract', array(), '', true, true, true, array('_getGearmanClient', '_run'));
@@ -35,7 +30,7 @@ class CM_Jobdistribution_Job_AbstractTest extends TestCase {
 		$result = $job->run(array('foo' => 'bar'));
 		$this->assertSame(array('bar' => 'foo'), $result);
 
-		//exception thrown in worker
+		// Exception thrown in worker
 		$job->expects($this->any())->method('_run')->will($this->returnCallback(function (CM_Params $params) {
 			throw new Exception();
 		}));
@@ -45,6 +40,44 @@ class CM_Jobdistribution_Job_AbstractTest extends TestCase {
 		} catch (CM_Exception $ex) {
 			$this->assertContains('Job `' . get_class($job) . '` failed', $ex->getMessage());
 		}
+
+		TH::clearConfig();
+	}
+
+	public function testRunGearmanDisabled() {
+		CM_Config::get()->CM_Jobdistribution_Job_Abstract->gearmanEnabled = false;
+
+		$job = $this->getMockForAbstractClass('CM_Jobdistribution_Job_Abstract', array(), '', true, true, true, array('_run'));
+		$job->expects($this->exactly(2))->method('_run')->will($this->returnCallback(function (CM_Params $params) {
+			return array_flip($params->getAllOriginal());
+		}));
+
+		/** @var CM_Jobdistribution_Job_Abstract $job */
+		$result = $job->run(array('foo' => 'bar'));
+		$this->assertSame(array('bar' => 'foo'), $result);
+
+		$job->queue(array('foo' => 'bar'));
+
+		TH::clearConfig();
+	}
+
+	public function testRunGearmanDisabledThrows() {
+		CM_Config::get()->CM_Jobdistribution_Job_Abstract->gearmanEnabled = false;
+
+		$job = $this->getMockForAbstractClass('CM_Jobdistribution_Job_Abstract', array(), '', true, true, true, array('_run'));
+		$job->expects($this->exactly(1))->method('_run')->will($this->returnCallback(function (CM_Params $params) {
+			throw new Exception('Job failed');
+		}));
+
+		/** @var CM_Jobdistribution_Job_Abstract $job */
+		try {
+			$job->run(array('foo' => 'bar'));
+			$this->fail('Job should have thrown an exception');
+		} catch (Exception $ex) {
+			$this->assertSame('Job failed', $ex->getMessage());
+		}
+
+		TH::clearConfig();
 	}
 
 }
