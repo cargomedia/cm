0604c7cab7d5fbc7e44b4aa47609f80631e4a8dd
Marcel Colomb <marcel.colomb@cargomedia.ch>
2012-12-12 21:09:36 +0100

    implemented lazy loading on PagingGroup, use getMockForAbstract in test, test whole array. closes #3

--- library/CM/PagingSource/PagingGroup.php
+++ library/CM/PagingSource/PagingGroup.php
@@ -2,30 +2,47 @@
 
 class CM_PagingSource_PagingGroup extends CM_PagingSource_Abstract {
 
-	/**
-	 * @var array
-	 */
-	private $_data;
+	/** @var array */
+	private $_groupedItems;
+
+	/**    @var CM_Paging_Abstract */
+	private $_paging;
+
+	/** @var Closure */
+	private $_getGroupKey;
 
 	/**
-	 * @param array $paging
+	 * @param CM_Paging_Abstract $paging
+	 * @param Closure            $getGroupKey
 	 */
 	public function __construct(CM_Paging_Abstract $paging, Closure $getGroupKey) {
-		$this->_data = array();
-		foreach ($paging as $item) {
-			$this->_data[$getGroupKey($item)][] = $item;
-		}
+		$this->_paging = $paging;
+		$this->_getGroupKey = $getGroupKey;
 	}
 
 	public function getCount($offset = null, $count = null) {
-		return count($this->_data);
+		return count($this->_getGroupedItems());
 	}
 
 	public function getItems($offset = null, $count = null) {
-		return array_slice($this->_data, $offset, $count);
+		return array_slice($this->_getGroupedItems(), $offset, $count);
 	}
 
 	protected function _cacheKeyBase() {
 		throw new CM_Exception_Invalid('`' . __CLASS__ . '` does not support caching.');
+	}
+
+	/**
+	 * return array
+	 */
+	private function _getGroupedItems() {
+		if (null === $this->_groupedItems) {
+			$this->_groupedItems = array();
+			$getGroupKey = $this->_getGroupKey;
+			foreach ($this->_paging as $item) {
+				$this->_groupedItems[$getGroupKey($item)][] = $item;
+			}
+		}
+		return $this->_groupedItems;
 	}
 }
--- tests/library/CM/PagingSource/PagingGroupTest.php
+++ tests/library/CM/PagingSource/PagingGroupTest.php
@@ -3,43 +3,33 @@ require_once __DIR__ . '/../../../TestCase.php';
 
 class CM_PagingSource_PagingGroupTest extends TestCase {
 
-	/** @var CM_PagingSource_PagingGroup */
-	private static $_pagingSource;
-
-	public static function setUpBeforeClass() {
-		$pagingSourceValues = array();
-		for ($i = 0; $i < 100; $i++) {
-			$pagingSourceValues[] = $i;
-		}
-
-		$paging = new CM_Paging_MockPagingGroup(new CM_PagingSource_Array($pagingSourceValues));
-		self::$_pagingSource = new CM_PagingSource_PagingGroup($paging, function($value) {
-			return $value % 10;
-		});
-	}
-
-	public static function tearDownAfterClass() {
-		TH::clearEnv();
-	}
-
 	public function testGetCount() {
-		$this->assertSame(10, self::$_pagingSource->getCount());
-		$this->assertSame(10, self::$_pagingSource->getCount(5));
+		$pagingSource = $this->_getPagingSource();
+
+		$this->assertSame(20, $pagingSource->getCount());
+		$this->assertSame(20, $pagingSource->getCount(5));
 	}
 
 	public function testGetItems() {
-		$itemList = self::$_pagingSource->getItems();
-		$this->assertSame(10, count($itemList));
-		$this->assertSame(1, $itemList[1][0]);
+		$pagingSource = $this->_getPagingSource();
 
-		$itemList = self::$_pagingSource->getItems(5);
-		$this->assertSame(5, count($itemList));
-		$this->assertSame(25, $itemList[0][2]);
+		$itemList = $pagingSource->getItems();
+		$this->assertSame(20, count($itemList));
+		$this->assertSame(array(1,21,41,61,81), $itemList[1]);
+
+		$itemList = $pagingSource->getItems(5);
+		$this->assertSame(15, count($itemList));
+		$this->assertSame(array(5,25,45,65,85), $itemList[0]);
+	}
+
+	private function _getPagingSource() {
+		$paging = $this->getMockForAbstractClass('CM_Paging_Abstract', array(new CM_PagingSource_Array(range(0, 100))));
+		$pagingSource = new CM_PagingSource_PagingGroup($paging, function($value) {
+			return $value % 20;
+		});
+
+		return $pagingSource;
 	}
 }
 
-class CM_Paging_MockPagingGroup extends CM_Paging_Abstract {
-	protected function _processItem($itemRaw) {
-		return (int) $itemRaw;
-	}
-}
+
