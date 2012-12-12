b70e9d14fe146b36bbf39aa19096fb263dbc711c
Reto Kaiser <reto@cargomedia.ch>
2012-12-12 14:20:35 +0000

    Fix CM_Site_CM hack

--- library/CM/Site/CM.php
+++ library/CM/Site/CM.php
@@ -5,6 +5,7 @@ class CM_Site_CM extends CM_Site_Abstract {
 
 	public static function match(CM_Request_Abstract $request) {
 		// @todo: Remove this class
-		return IS_TEST && !strpos(DIR_ROOT, 'www');
+		$isNotCmProject = false === strpos(DIR_ROOT, 'www') && false === strpos(DIR_ROOT, '/home/fuckbook/releases');
+		return IS_TEST && $isNotCmProject;
 	}
 }
