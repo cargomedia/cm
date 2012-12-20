cd902b82a4d6f625e04b2504073f182704ec665e
Marcel Colomb <marcel.colomb@cargomedia.ch>
2012-12-20 17:29:32 +0100

    Added FormField_Float and test

---
+++ tests/library/CM/FormField/FloatTest.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/../../../TestCaseRender.php';
+
+class CM_FormField_FloatTest extends TestCaseRender {
+
+	public function testValidate() {
+		$field = new CM_FormField_Float('foo');
+		$response = $this->getMockForAbstractClass('CM_Response_Abstract', array(), '', false);
+
+		$validationResult = $field->validate(1.3, $response);
+		$this->assertSame(1.3, $validationResult);
+		try {
+			$field->validate('foo', $response);
+			$this->fail('Could insert text in float formfield');
+		} catch (CM_Exception_FormFieldValidation $e) {
+			$this->assertTrue(true);
+		}
+	}
+}
