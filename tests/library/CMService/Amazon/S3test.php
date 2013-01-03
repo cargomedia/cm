c29f8a47c436982555b2767f69e217a14e140dea
Marcel Colomb <marcel.colomb@cargomedia.ch>
2013-01-03 14:03:52 +0100

    Added correct formation

--- tests/library/CMService/Amazon/S3test.php
+++ tests/library/CMService/Amazon/S3test.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../../../TestCase.php';
 
 class CMService_Amazon_S3Test extends TestCase {
 
-	public function testConstructor(){
+	public function testConstructor() {
 		$amazonS3 = new CMService_Amazon_S3();
 		$this->assertTrue(true);
 	}
