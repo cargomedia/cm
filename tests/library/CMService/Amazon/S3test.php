6b6a0e4ef31144c0892cedc0e26094f90ab2104d
Marcel Colomb <marcel.colomb@cargomedia.ch>
2013-01-03 13:34:35 +0100

    test amazon constructor

--- tests/library/CMService/Amazon/S3test.php
+++ tests/library/CMService/Amazon/S3test.php
@@ -3,8 +3,9 @@ require_once __DIR__ . '/../../../TestCase.php';
 
 class CMService_Amazon_S3Test extends TestCase {
 
-	public function testisItThere(){
+	public function testConstructor(){
 		$amazonS3 = new CMService_Amazon_S3();
+		$this->assertTrue(true);
 	}
 
 }
