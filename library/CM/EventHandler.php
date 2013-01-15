fb1311bd61c4bec99fc89b0b1f62f0284b5e913e
Alexis Peter <alexis.peter@cargomedia.ch>
2013-01-15 14:59:45 +0100

    use Jobs to execute eventhandler-bindings

--- library/CM/EventHandler.php
+++ library/CM/EventHandler.php
@@ -3,40 +3,46 @@
 final class CM_EventHandler {
 
 	/**
-	 * @var array $_callbacks
+	 * @var array $_jobs
 	 */
-	private $_callbacks = array();
+	private $_jobs = array();
 
 	/**
-	 * @param string $event
-	 * @param Closure $callback
-	 * @param array|null $params
+	 * @param string                          $event
+	 * @param CM_Jobdistribution_Job_Abstract $job
+	 * @param array|null                      $params
 	 */
-	public function bind($event, Closure $callback, array $params = null) {
-		$this->_callbacks[$event][] = array('callback' => $callback, 'params' => $params);
+	public function bind($event, CM_Jobdistribution_Job_Abstract $job, array $params = null) {
+		$event = (string) $event;
+		$this->_jobs[$event][] = array('job' => $job, 'params' => $params);
 	}
 
 	/**
 	 * @param string $event
 	 */
 	public function unbind($event) {
-		unset($this->_callbacks[$event]);
+		$event = (string) $event;
+		unset($this->_jobs[$event]);
 	}
 
 	/**
-	 * @param string $event
+	 * @param string     $event
 	 * @param array|null $params
 	 */
 	public function trigger($event, array $params = null) {
+		$event = (string) $event;
 		if (!$params) {
 			$params = array();
 		}
-		if (!empty($this->_callbacks[$event])) {
-			foreach ($this->_callbacks[$event] as $callback) {
-				if (!empty($callback['params'])) {
-					$params = array_merge($callback['params'], $params);
+		if (!empty($this->_jobs[$event])) {
+			foreach ($this->_jobs[$event] as $job) {
+				$jobParams = $params;
+				if (!empty($job['params'])) {
+					$jobParams = array_merge($job['params'], $jobParams);
 				}
-				$callback['callback'](CM_Params::factory($params));
+				/** @var CM_Jobdistribution_Job_Abstract $job */
+				$job = $job['job'];
+				$job->queue($jobParams);
 			}
 		}
 	}
