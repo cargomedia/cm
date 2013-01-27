0730636e5e5e91d64f63bbd46e50fccd41c76742
tomaszdurka <tomasz@durka.pl>
2013-01-27 20:11:58 +0100

    resetNamespacePaths => reloadNamespacePaths; clear apc cache within reloadNamespacePaths; add getNamespacePathsCacheKey, add DIR_ROOT prefix for the cacheKey

--- library/CM/Generator/Cli.php
+++ library/CM/Generator/Cli.php
@@ -27,7 +27,7 @@ class CM_Generator_Cli extends CM_Cli_Runnable_Abstract {
 	 */
 	public function createNamespace($namespace) {
 		$this->_createNamespaceDirectories($namespace);
-		CM_Bootloader::getInstance()->resetNamespacePaths();
+		CM_Bootloader::getInstance()->reloadNamespacePaths();
 		$this->_generateClassFilePhp($namespace . '_Site', 'CM_Site_Abstract');
 		$bootloaderFile = $this->_generateClassFilePhp($namespace . '_Bootloader', 'CM_Bootloader');
 		$namespaces = array_merge(CM_Bootloader::getInstance()->getNamespaces(), array($namespace));
