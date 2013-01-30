0ce862ca6ff677ee83a59410c11b4e8c502a2e49
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2013-01-30 14:46:11 +0100

    rename class name  ad to advertismement

--- library/CM/SmartyPlugins/function.advertisement.php
+++ library/CM/SmartyPlugins/function.advertisement.php
@@ -8,5 +8,5 @@ function smarty_function_advertisement(array $params, Smarty_Internal_Template $
 	if (!isset($params['zone'])) {
 		trigger_error('Param `zone` missing.');
 	}
-	return '<div class="ad">' . CM_Adprovider::getInstance()->getHtml($params['zone']) . '</div>';
+	return '<div class="advertisement">' . CM_Adprovider::getInstance()->getHtml($params['zone']) . '</div>';
 }
