cd6f69c6bd26f420bbcacab012b599ca57199926
Reto Kaiser <reto@cargomedia.ch>
2013-02-15 11:28:41 +0100

    Always print json *object* to DOM for openx

--- tests/library/CM/AdproviderAdapter/OpenxTest.php
+++ tests/library/CM/AdproviderAdapter/OpenxTest.php
@@ -6,9 +6,12 @@ class CM_AdproviderAdapter_OpenxTest extends CMTest_TestCase {
 		CM_Config::get()->CM_Adprovider->enabled = true;
 		CM_Config::get()->CM_AdproviderAdapter_Openx->host = 'www.foo.org';
 		$openx = new CM_AdproviderAdapter_Openx();
-		$html = $openx->getHtml(array('zoneId' => 'zone1'), array('foo' => 'bar'));
 
+		$html = $openx->getHtml(array('zoneId' => 'zone1'), array('foo' => 'bar'));
 		$this->assertContains('<div class="openx-ad" data-zone-id="zone1" data-host="www.foo.org" data-variables="{&quot;foo&quot;:&quot;bar&quot;}"', $html);
+
+		$html = $openx->getHtml(array('zoneId' => 'zone1'), array());
+		$this->assertContains('<div class="openx-ad" data-zone-id="zone1" data-host="www.foo.org" data-variables="{}"', $html);
 
 		CMTest_TH::clearConfig();
 	}
