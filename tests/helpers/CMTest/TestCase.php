c531ed9a40f40da895769e9a44e8440e0dbb6d83
Alexis Peter <alexis.peter@cargomedia.ch>
2013-02-19 15:43:01 +0100

    Move TestCaseRender-functionality to TestCase

--- tests/helpers/CMTest/TestCase.php
+++ tests/helpers/CMTest/TestCase.php
@@ -1,6 +1,35 @@
 <?php
 
 abstract class CMTest_TestCase extends PHPUnit_Framework_TestCase {
+
+	/** @var CM_Render */
+	private $_render;
+
+	/**
+	 * @return CM_Render
+	 */
+	protected function _getRender() {
+		if (!$this->_render) {
+			$this->_render = new CM_Render($this->_getSite());
+		}
+		return $this->_render;
+	}
+
+	/**
+	 * @param CM_Form_Abstract           $form
+	 * @param CM_FormField_Abstract      $formField
+	 * @param array|null                 $params
+	 * @return CMTest_TH_Html
+	 */
+	protected function _renderFormField(CM_Form_Abstract $form, CM_FormField_Abstract $formField, array $params = null) {
+		if (null === $params) {
+			$params = array();
+		}
+		$formField->prepare($params);
+		$html = $this->_getRender()->render($formField, array('form' => $form));
+		return new CMTest_TH_Html($html);
+	}
+
 	/**
 	 * @param string $table
 	 * @param array  $where WHERE conditions: ('attr' => 'value', 'attr2' => 'value')
