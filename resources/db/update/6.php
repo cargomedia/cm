b3ee7e2b69dd9fd250a184f894913745f228440e
tomaszdurka <tomasz@durka.pl>
2013-02-19 20:43:56 +0100

    Remove script 6

--- resources/db/update/6.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!CM_Mysql::exists('cm_stream_publish', null, 'channelId-key')) {
-	CM_Mysql::exec('ALTER TABLE  `cm_stream_publish` DROP INDEX `key`');
-	CM_Mysql::exec('ALTER TABLE  `cm_stream_publish` DROP INDEX `channelId`');
-	CM_Mysql::exec('ALTER TABLE  `cm_stream_publish` ADD UNIQUE `channelId-key` (`channelId`, `key`)');
-}
