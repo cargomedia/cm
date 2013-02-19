edb62d79b3c0f53833920e9fd9e5783313e23f26
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-19 19:03:05 +0100

    * update smarty modifier

--- library/CM/SmartyPlugins/modifier.usertext2.php
+++ library/CM/SmartyPlugins/modifier.usertext2.php
@@ -6,25 +6,26 @@
  * @param int     $lengthMax
  * @param boolean $stripEmoji          cutout all emoji
  * @param boolean $preserveParagraph   allow <p> in plain
+ * @param boolean $preserveEmoji
  */
 
-function smarty_modifier_usertext($text, $mode = null, $lengthMax = null, $stripEmoji = null, $preserveParagraph = null) {
+function smarty_modifier_usertext($text, $mode = null, $lengthMax = null, $stripEmoji = null, $preserveParagraph = null, $preserveEmoji = null) {
 
 	$userText = new CM_Usertext_Usertext($text);
 
-	if (null===$mode){
+	if (null === $mode) {
 		$mode = 'markdown';
 	}
 
 	switch ($mode) {
 		case 'plain':
-			$text = $userText->getPlain($lengthMax, $stripEmoji, $preserveParagraph);
+			$text = $userText->getPlain($lengthMax, $preserveParagraph, $preserveEmoji);
 			break;
 		case 'markdown':
 		default:
 			$text = $userText->getMarkdown($lengthMax, $stripEmoji);
 	}
 
-	$text = '<span class="usertext2">'.$text.'</span>';
+	$text = '<span class="usertext2">' . $text . '</span>';
 	return $text;
 }
