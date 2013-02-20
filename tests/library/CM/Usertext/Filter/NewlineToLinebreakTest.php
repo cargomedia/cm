859db90c2945dbeb8e0dfb8afb2f8251dc410706
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-20 18:48:38 +0100

    add CM_Usertext_Filter_NewlineToLinebreakeTest

---
+++ tests/library/CM/Usertext/Filter/NewlineToLinebreakTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class CM_Usertext_Filter_NewlineToLinebreakeTest extends CMTest_TestCase {
+
+	public static function tearDownAfterClass() {
+		CMTest_TH::clearEnv();
+	}
+
+	public function testProcess() {
+		$text = "foo\n\rbar\n\n\n\n\n\n\n\n\nfoo\n\nbar\n\r\r\r";
+		$expected = "foo<br />\nbar<br />\n<br />\n<br />\nfoo<br />\n<br />\nbar";
+		$filter = new CM_Usertext_Filter_NewlineToLinebreak(3);
+
+		$this->assertEquals($expected,$filter->transform($text));
+	}
+
+}
