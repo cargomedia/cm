2f7bf97c7827379444559097d10298ea565fae6f
tomaszdurka <tomasz@durka.pl>
2013-02-20 16:13:06 +0100

    Adjust Stream_Subscribe Paging

--- library/CM/Paging/StreamSubscribe/AdapterType.php
+++ library/CM/Paging/StreamSubscribe/AdapterType.php
@@ -6,23 +6,9 @@ class CM_Paging_StreamSubscribe_AdapterType extends CM_Paging_StreamSubscribe_Ab
 	 * @param int $adapterType
 	 */
 	public function __construct($adapterType) {
+		$adapterType = (int) $adapterType;
 		$source = new CM_PagingSource_Sql(TBL_CM_STREAM_SUBSCRIBE . '.`id`', TBL_CM_STREAM_SUBSCRIBE, '`adapterType` = ' . $adapterType, null, 'JOIN ' . TBL_CM_STREAMCHANNEL . ' ON ' . TBL_CM_STREAM_SUBSCRIBE . '.channelId = ' . TBL_CM_STREAMCHANNEL . '.id');
 		$source->enableCache();
 		return parent::__construct($source);
-	}
-
-	/**
-	 * @param string $key
-	 * @return CM_Model_Stream_Subscribe|null
-	 */
-	public function findKey($key) {
-		$key = (string) $key;
-		/** @var CM_Model_Stream_Subscribe $streamSubscribe */
-		foreach ($this as $streamSubscribe) {
-			if ($streamSubscribe->getKey() == $key) {
-				return $streamSubscribe;
-			}
-		}
-		return null;
 	}
 }
