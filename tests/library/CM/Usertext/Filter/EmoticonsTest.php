c302fc85078b687518a320adc8b912cc4ecb0fd2
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-20 18:24:25 +0100

    add CM_Usertext_Filter_EmoticonsTest

---
+++ tests/library/CM/Usertext/Filter/EmoticonsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+class CM_Usertext_Filter_EmoticonsTest extends CMTest_TestCase {
+
+	public static function tearDownAfterClass() {
+		CMTest_TH::clearEnv();
+	}
+
+	public function testProcess() {
+
+		CM_Mysql::insert(TBL_CM_SMILEY, array('code' => ':),:-),:smiley:', 'file' => '1.png'));
+
+		$text = 'foo :) bar :smiley:';
+		$expected = 'foo <img class="emoticon" title=":)" alt=":)" src="/img/emoticons/1.png" /> bar <img class="emoticon" title=":smiley:" alt=":smiley:" src="/img/emoticons/1.png" />';
+		$filter = new CM_Usertext_Filter_Emoticons();
+		$actual = $filter->transform($text);
+
+		$this->assertEquals($expected, $actual);
+	}
+
+}
