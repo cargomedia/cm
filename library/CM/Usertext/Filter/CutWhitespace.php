6956a9d088b51e87dd35210c09102586901eef5c
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-20 18:17:18 +0100

    add CM_Usertext_Filter_CutWhitespaceTest

--- library/CM/Usertext/Filter/CutWhitespace.php
+++ library/CM/Usertext/Filter/CutWhitespace.php
@@ -4,10 +4,11 @@ class CM_Usertext_Filter_CutWhitespace extends CM_Usertext_Filter_Abstract {
 
 	public function transform($text) {
 		$text = (string) $text;
+		$text = str_replace("\t", " ", $text);
 		$text = preg_replace('/([\s])\1+/', ' ', $text);
 		$text = str_replace(" \n", "\n", $text);
 		$text = str_replace(' </p>', '</p>', $text);
-		$text = trim($text, " \n\t");
+		$text = trim($text);
 		return $text;
 	}
 
---
+++ tests/library/CM/Usertext/Filter/CutWhitespaceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class CM_Usertext_Filter_CutWhitespaceTest extends CMTest_TestCase {
+
+	public static function tearDownAfterClass() {
+		CMTest_TH::clearEnv();
+	}
+
+	public function testProcess() {
+		$text = "\n\n \t   foo  \nbar     \n \r \t";
+		$expected = "foo\nbar";
+		$filter = new CM_Usertext_Filter_CutWhitespace();
+		$actual = $filter->transform($text);
+
+		$this->assertEquals($expected, $actual);
+	}
+
+}
