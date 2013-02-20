e746729b3d5c3633b9b82ce81213fc9098df4505
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-20 17:13:28 +0100

    add new Filter NewlineToLinebreak

---
+++ library/CM/Usertext/Filter/NewlineToLinebreak.php
@@ -0,0 +1,28 @@
+<?php
+
+class CM_Usertext_Filter_NewlineToLinebreak extends CM_Usertext_Filter_Abstract {
+
+	/** @var int|null */
+	private $_breaksMax = null;
+
+	/**
+	 * @param int|null $breaksMax
+	 */
+	function __construct($breaksMax = null) {
+		if (null !== $breaksMax) {
+			$this->_breaksMax = (int) $breaksMax;
+		}
+	}
+
+	public function transform($text) {
+		$text = (string) $text;
+		$text = str_replace("\r", '', $text);
+		if (null !== $this->_breaksMax) {
+			$text = preg_replace('#(\n{' . $this->_breaksMax . '})\n+#', '$1', $text);
+		}
+		$text = trim($text, "\n");
+		$text = str_replace("\n", "<br />\n", $text);
+		return $text;
+	}
+
+}
