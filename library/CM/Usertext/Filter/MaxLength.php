2c9b3a28e0d287756645fbd73440784515325ffe
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-20 17:12:05 +0100

    update construct in CM_Usertext_Filter_MaxLength

--- library/CM/Usertext/Filter/MaxLength.php
+++ library/CM/Usertext/Filter/MaxLength.php
@@ -2,17 +2,23 @@
 
 class CM_Usertext_Filter_MaxLength extends CM_Usertext_Filter_Abstract {
 
-	private $_lengthMax;
+	/** @var int|null */
+	private $_lengthMax = null;
 
 	/**
 	 * @param int|null $lengthMax
 	 */
 	function __construct($lengthMax = null) {
-		$this->_lengthMax = (int) $lengthMax;
+		if (null !== $lengthMax) {
+			$this->_lengthMax = (int) $lengthMax;
+		}
 	}
 
 	public function transform($text) {
 		$text = (string) $text;
+		if (null === $this->_lengthMax) {
+			return $text;
+		}
 		if (strlen($text) > $this->_lengthMax) {
 			$text = substr($text, 0, $this->_lengthMax);
 			$lastBlank = strrpos($text, ' ');
