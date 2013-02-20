c70e9f34165e661ba7458ada21b588e7ee269d4c
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-20 18:51:44 +0100

    add CM_Usertext_Filter_StriptagsTest

---
+++ tests/library/CM/Usertext/Filter/StriptagsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class CM_Usertext_Filter_StriptagsTest extends CMTest_TestCase {
+
+	public static function tearDownAfterClass() {
+		CMTest_TH::clearEnv();
+	}
+
+	public function testProcess() {
+		$text = "<p>foo<br><br/></p>";
+
+		$filter = new CM_Usertext_Filter_Striptags();
+		$this->assertEquals('foo',$filter->transform($text));
+
+		$filter = new CM_Usertext_Filter_Striptags(array('p'));
+		$this->assertEquals('<p>foo</p>',$filter->transform($text));
+	}
+
+}
