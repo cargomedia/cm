6c99684fd45e60331c76205a085ba273f10f7fb6
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-20 14:11:37 +0100

    removed optional charset in Filter_Escape

--- library/CM/Usertext/Filter/Escape.php
+++ library/CM/Usertext/Filter/Escape.php
@@ -2,18 +2,9 @@
 
 class CM_Usertext_Filter_Escape extends CM_Usertext_Filter_Abstract {
 
-	private $_char_set;
-
-	/**
-	 * @param string $char_set
-	 */
-	function __construct($char_set = 'UTF-8') {
-		$this->_char_set = (string) $char_set;
-	}
-
 	public function transform($text) {
 		$text = (string) $text;
-		return htmlspecialchars($text, ENT_QUOTES, $this->_char_set);
+		return htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
 	}
 
 }
