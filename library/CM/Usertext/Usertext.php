6ac8f2f20672f05d3e35502db79a127238c290c1
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-20 14:54:48 +0100

    modified CM_Usertext_Usertext

--- library/CM/Usertext/Usertext.php
+++ library/CM/Usertext/Usertext.php
@@ -2,22 +2,32 @@
 
 class CM_Usertext_Usertext {
 
-	private $_filter = array();
+	/** @var CM_Usertext_Filter_Abstract[] */
+	private $_filterList = array();
 
+	/**
+	 * @param CM_Usertext_Filter_Abstract $filter
+	 */
 	public function addFilter(CM_Usertext_Filter_Abstract $filter) {
-		$this->_filter[] = $filter;
+		$this->_filterList[] = $filter;
 	}
 
+	/**
+	 * @param string $text
+	 * @return string
+	 */
 	public function transform($text) {
 		foreach ($this->_getFilters() as $filter) {
-			/** @var $filter CM_Usertext_Filter_Abstract */
 			$text = $filter->transform($text);
 		}
 		return $text;
 	}
 
+	/**
+	 * @return CM_Usertext_Filter_Abstract[]
+	 */
 	private function _getFilters() {
-		return $this->_filter;
+		return $this->_filterList;
 	}
 
 }
