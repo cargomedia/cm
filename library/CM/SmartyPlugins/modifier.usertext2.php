ef9a67baf3866b1b747f4733620dcdca9de6e3a8
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-20 17:12:46 +0100

    updated smarty_modifier (4 modes)

--- library/CM/SmartyPlugins/modifier.usertext2.php
+++ library/CM/SmartyPlugins/modifier.usertext2.php
@@ -1,25 +1,49 @@
 <?php
 
 /**
- * @param string       $text
- * @param string|null  $mode                markdown | plain
- * @param int|null     $lengthMax
- * @param boolean|null $stripEmoji          cutout all emoji
- * @param boolean|null $preserveParagraph   allow <p> in plain
+ * Supported modes:
+ * =====================================================
+ * oneline = escape, remove badwords, add emoticons
+ * simple = escape, remove badwords, nl2br, add emoticons
+ * format = escape, remove badwords, create html markdown, add emoticons
+ * plain = escape, remove badwords, create html markdown, strip all tags, add emoticons
+ *
+ * @param string   $text
+ * @param string   $mode
+ * @param int|null $maxLength
+ * @return string
+ * @throws CM_Exception_Invalid
  */
+function smarty_modifier_usertext2($text, $mode, $maxLength = null) {
+	$usertext = new CM_Usertext_Usertext();
 
-function smarty_modifier_usertext2($text, $mode = null, $lengthMax = null, $stripEmoji = null, $preserveParagraph = null) {
-	$userText = new CM_Usertext_Usertext($text);
-
+	$usertext->addFilter(new CM_Usertext_Filter_Escape());
+	$usertext->addFilter(new CM_Usertext_Filter_Badwords());
+	$usertext->addFilter(new CM_Usertext_Filter_MaxLength($maxLength));
 	switch ($mode) {
-		case 'plain':
-			$text = $userText->getPlain($lengthMax, $preserveParagraph, $stripEmoji);
+		case 'oneline':
 			break;
-		case 'markdown':
+		case 'simple':
+			$usertext->addFilter(new CM_Usertext_Filter_NewlineToLinebreak(3));
+			break;
+		case 'format':
+			$usertext->addFilter(new CM_Usertext_Filter_Markdown());
+			break;
+		case 'plain':
+			$usertext->addFilter(new CM_Usertext_Filter_Markdown());
+			$usertext->addFilter(new CM_Usertext_Filter_Striptags());
+			break;
 		default:
-			$text = $userText->getMarkdown($lengthMax, $stripEmoji);
+			throw new CM_Exception_Invalid('Must define `mode` in Usertext.');
 	}
+	$usertext->addFilter(new CM_Usertext_Filter_Emoticons());
+	$usertext->addFilter(new CM_Usertext_Filter_CutWhitespace());
 
-	$text = '<span class="usertext2">' . $text . '</span>';
+	$text = $usertext->transform($text);
+
+	if ('format' == $mode) {
+		$text = '<span class="usertext2">' . $text . '</span>';
+	}
 	return $text;
+
 }
