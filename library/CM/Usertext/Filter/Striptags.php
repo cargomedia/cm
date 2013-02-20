8020b9ec6227ab174a2ed21dd50716fce48a8e05
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-20 14:49:53 +0100

    CM_Usertext_Filter_Striptags:  configurable as array of tags

--- library/CM/Usertext/Filter/Striptags.php
+++ library/CM/Usertext/Filter/Striptags.php
@@ -2,20 +2,22 @@
 
 class CM_Usertext_Filter_Striptags extends CM_Usertext_Filter_Abstract {
 
-	private $_preserveParagraph;
+	private $_allowedTags;
 
 	/**
-	 * @param boolean $preserveParagraph
+	 * @param array $allowedTags
 	 */
-	function __construct($preserveParagraph = null) {
-		$this->_preserveParagraph = (boolean) $preserveParagraph;
+	function __construct($allowedTags = null) {
+		$this->_allowedTags = (array) $allowedTags;
 	}
 
 	public function transform($text) {
 		$text = (string) $text;
-		$allowedTags = null;
-		if ($this->_preserveParagraph) {
-			$allowedTags = '<p>';
+		$allowedTags = '';
+		if ($this->_allowedTags){
+			foreach($this->_allowedTags as $tag){
+				$allowedTags .= '<'.$tag.'>';
+			}
 		}
 		return strip_tags($text, $allowedTags);
 	}
