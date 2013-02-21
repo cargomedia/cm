ad90461dc4a93435ce58a7ce26c257d3728db101
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-21 18:10:17 +0100

    docu and formating

--- library/CM/Usertext/Filter/Striptags.php
+++ library/CM/Usertext/Filter/Striptags.php
@@ -2,6 +2,7 @@
 
 class CM_Usertext_Filter_Striptags extends CM_Usertext_Filter_Abstract {
 
+	/** @var array */
 	private $_allowedTags;
 
 	/**
@@ -14,10 +15,8 @@ class CM_Usertext_Filter_Striptags extends CM_Usertext_Filter_Abstract {
 	public function transform($text) {
 		$text = (string) $text;
 		$allowedTags = '';
-		if ($this->_allowedTags){
-			foreach($this->_allowedTags as $tag){
-				$allowedTags .= '<'.$tag.'>';
-			}
+		foreach ($this->_allowedTags as $tag) {
+			$allowedTags .= '<' . $tag . '>';
 		}
 		return strip_tags($text, $allowedTags);
 	}
