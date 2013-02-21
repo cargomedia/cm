73972a6025220fe5b61987ac5122228690ed9cd1
tomaszdurka <tomasz@durka.pl>
2013-02-21 17:28:23 +0100

    Cover (partly) CM_Util

--- tests/library/CM/UtilTest.php
+++ tests/library/CM/UtilTest.php
@@ -9,7 +9,7 @@ class CM_UtilTest extends CMTest_TestCase {
 		$classPaths['CM_Paging_Action_Abstract'] = 'CM/Paging/Action/Abstract.php';
 		$classPaths['CM_Paging_Action_User'] = 'CM/Paging/Action/User.php';
 
-		foreach($classPaths as $className => &$path){
+		foreach ($classPaths as $className => &$path) {
 			$path = CM_Util::getNamespacePath(CM_Util::getNamespace($className)) . 'library/' . $path;
 		}
 
@@ -30,4 +30,42 @@ class CM_UtilTest extends CMTest_TestCase {
 		}
 	}
 
+	public function testTitleize() {
+		$testCases = array(
+			'foo'     => 'Foo',
+			'Foo'     => 'Foo',
+			'foo bar' => 'Foo bar',
+			'foo-bar' => 'Foo Bar',
+			'foo.bar' => 'Foo.bar',
+		);
+		foreach ($testCases as $actual => $expected) {
+			$this->assertSame($expected, CM_Util::titleize($actual));
+		}
+	}
+
+	public function testGetResourceFiles() {
+		$files = CM_Util::getResourceFiles('config/default.php');
+		if (!count($files)) {
+			$this->markTestSkipped('There are no files to test this functionality');
+		}
+		foreach ($files as $file) {
+			$this->assertInstanceOf('CM_File', $file);
+			$this->assertSame('default.php', $file->getFileName());
+		}
+	}
+
+	public function testBenchmark() {
+		$reverse = function ($timeString) {
+			$results = sscanf($timeString, '%f ms');
+			return $results[0];
+		};
+		$this->assertEquals(0, $reverse(CM_Util::benchmark()));
+		$this->assertGreaterThan(0, $reverse(CM_Util::benchmark()));
+
+		CM_Util::benchmark('foo');
+		usleep(100);
+		CM_Util::benchmark('bar');
+		$this->assertGreaterThan($reverse(CM_Util::benchmark('bar')), $reverse(CM_Util::benchmark('foo')));
+
+	}
 }
