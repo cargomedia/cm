546c13ce16e84e3705cfbd4b4d3b7ea3f26adca7
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-21 17:13:33 +0100

    updated order CM_Usertext_Filter_CutWhitespace

--- library/CM/Usertext/Filter/CutWhitespace.php
+++ library/CM/Usertext/Filter/CutWhitespace.php
@@ -4,10 +4,10 @@ class CM_Usertext_Filter_CutWhitespace extends CM_Usertext_Filter_Abstract {
 
 	public function transform($text) {
 		$text = (string) $text;
+		$text = str_replace("\r", '', $text);
 		$text = str_replace("\t", " ", $text);
-		$text = preg_replace('/([\s])\1+/', ' ', $text);
-		$text = str_replace(" \n", "\n", $text);
-		$text = str_replace(array(' </p>', ' <br />'), array('</p>', '<br />'), $text);
+		$text = preg_replace('#\s{1,}#', ' ', $text);
+		$text = str_replace(array(" \n", ' </p>', ' <br />'), array("\n", '</p>', '<br />'), $text);
 		$text = trim($text);
 		return $text;
 	}
