c5e1915993670653c4f73446e6c61574909fa0f8
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-21 11:28:04 +0100

    updated CM_Usertext_Filter_CutWhitespace

--- library/CM/Usertext/Filter/CutWhitespace.php
+++ library/CM/Usertext/Filter/CutWhitespace.php
@@ -7,7 +7,7 @@ class CM_Usertext_Filter_CutWhitespace extends CM_Usertext_Filter_Abstract {
 		$text = str_replace("\t", " ", $text);
 		$text = preg_replace('/([\s])\1+/', ' ', $text);
 		$text = str_replace(" \n", "\n", $text);
-		$text = str_replace(' </p>', '</p>', $text);
+		$text = str_replace(array(' </p>', ' <br />'), array('</p>', '<br />'), $text);
 		$text = trim($text);
 		return $text;
 	}
