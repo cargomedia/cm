70650d3cf83cc74d09d967ef7949383a4714348d
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-25 12:47:53 +0100

    changed background url in _getCssEmoticon

--- library/CM/Response/Resource/CSS.php
+++ library/CM/Response/Resource/CSS.php
@@ -78,9 +78,10 @@ class CM_Response_Resource_CSS extends CM_Response_Resource_Abstract {
 	 */
 	private function _getCssEmoticon() {
 		$css = '';
+		$url = $this->getRender()->getThemeDir();
 		foreach (new CM_Paging_Emoticon_All() as $emoticon) {
 			$css .= '.emoticon.emoticon-' . $emoticon['id'] . '{';
-			$css .= 'background-image: url(' . $this->getRender()->getUrlStatic('/img/emoticon/' . $emoticon['file']) . ')';
+			$css .= 'background-image: url(' . $this->getRender()->getUrlResource('img', 'emoticon/' . $emoticon['file']) . ');';
 			$css .= '}' . PHP_EOL;
 		}
 		return $css;
