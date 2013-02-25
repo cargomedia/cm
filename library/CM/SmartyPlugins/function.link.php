6b47f2c2fa0eb8ed50aaa47d160f97029b9da7d7
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2013-02-25 09:21:05 +0100

    - Link URL not used as fallback for the missing label any more if an icon is set - Bugfix: Javascript code not displayed any more if no URL, no label and no title are set - Bugfix: Opening span added before the fallback label if no label and no title are set - Added tests

--- library/CM/SmartyPlugins/function.link.php
+++ library/CM/SmartyPlugins/function.link.php
@@ -2,18 +2,15 @@
 require_once 'function.linkUrl.php';
 
 function smarty_function_link(array $params, Smarty_Internal_Template $template) {
-	/** @var CM_Render $render */
-	$render = $template->smarty->getTemplateVars('render');
-
 	$label = '';
 	if (isset($params['label'])) {
 		$label = $params['label'];
 	}
 	unset($params['label']);
 
-	$class = '';
+	$class = 'link';
 	if (isset($params['class'])) {
-		$class = $params['class'];
+		$class .= ' ' . $params['class'];
 	}
 	unset($params['class']);
 
@@ -33,27 +30,22 @@ function smarty_function_link(array $params, Smarty_Internal_Template $template)
 		$href = smarty_function_linkUrl($params, $template);
 	}
 
-	$html = '<a href="' . $href . '" class="link ' . $class . '"';
-	if (!empty($title)) {
-		$html .= ' title="' . $title . '"';
-	}
-	$html .= '>';
-
-	if (!empty($icon)) {
-		$html .= '<span class="icon ' . $icon . '"></span>';
-	}
-	if (!empty($label)) {
-		$html .= '<span class="label">';
-	}
-	if (empty($title) && empty($label)) {
+	if (empty($label) && empty($icon) && empty($title) && (0 !== strpos($href, 'javascript:'))) {
 		$label = $href;
 	}
-	$html .= CM_Util::htmlspecialchars($label);
 
+	$html = '';
 	if (!empty($label)) {
-		$html .= '</span>';
+		$html = '<span class="label">' . CM_Util::htmlspecialchars($label) . '</span>';
 	}
-	$html .= '</a>';
+	if (!empty($icon)) {
+		$html = '<span class="icon ' . $icon . '"></span>' . $html;
+	}
+	$titleAttr = '';
+	if (!empty($title)) {
+		$titleAttr = ' title="' . $title . '"';
+	}
+	$html = '<a href="' . $href . '" class="' . $class . '"' . $titleAttr . '>' . $html . '</a>';
 
 	return $html;
 }
