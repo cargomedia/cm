b0028aeb7fbc7f116bae7394558fbf3137a6d0ae
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-25 13:55:38 +0100

    fixed bug in Emoticon Paging

--- library/CM/Paging/Emoticon/Abstract.php
+++ library/CM/Paging/Emoticon/Abstract.php
@@ -5,7 +5,8 @@ abstract class CM_Paging_Emoticon_Abstract extends CM_Paging_Abstract {
 	protected function _processItem($itemRaw) {
 		$item = array();
 		$item['id'] =  (int) $itemRaw['id'];
-		$item['codes'] = array($itemRaw['code'], explode(',', $itemRaw['codeAdditional']));
+		$item['codes'] = explode(',', $itemRaw['codeAdditional']);
+		$item['codes'][] = $itemRaw['code'];
 		$item['file'] = $itemRaw['file'];
 		return $item;
 	}
