f0c7684c35b73c0e73a5ba61440152644d38f651
Constantin7 <marian.sollmann@cargomedia.ch>
2013-02-28 15:31:50 +0100

    removed not needed stuff in CM_Usertext_Filter_EmoticonTest

--- tests/library/CM/Usertext/Filter/EmoticonTest.php
+++ tests/library/CM/Usertext/Filter/EmoticonTest.php
@@ -2,21 +2,12 @@
 
 class CM_Usertext_Filter_EmoticonTest extends CMTest_TestCase {
 
-	private static $_emoticonIdList = array();
-
-	public static function setUpBeforeClass() {
-		self::$_emoticonIdList[] = CM_Mysql::insert(TBL_CM_EMOTICON, array('code' => ':smiley:', 'codeAdditional' => ':),:-)', 'file' => '1.png'));
-		self::$_emoticonIdList[] = CM_Mysql::insert(TBL_CM_EMOTICON, array('code' => ':<', 'file' => '2.png'));
-
-		CMTest_TH::clearCache();
-	}
-
-	public static function tearDownAfterClass() {
+	public function tearDown() {
 		CMTest_TH::clearEnv();
 	}
 
 	public function testProcess() {
-		$emoticonId = self::$_emoticonIdList[0];
+		$emoticonId = CM_Mysql::insert(TBL_CM_EMOTICON, array('code' => ':smiley:', 'codeAdditional' => ':),:-)', 'file' => '1.png'));
 		$text = 'foo :) bar :smiley:';
 		$expected =
 				'foo <span class="emoticon emoticon-' . $emoticonId . '" title="smiley"></span> bar <span class="emoticon emoticon-' .
