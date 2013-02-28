5a09a5b46badf8a64b2bd3d8eb2bf27a65c40e0b
Alexis Peter <alexis.peter@cargomedia.ch>
2013-02-28 15:23:31 +0100

    test Jobworker

--- library/CM/Jobdistribution/JobWorker.php
+++ library/CM/Jobdistribution/JobWorker.php
@@ -1,15 +1,15 @@
 <?php
 
-final class CM_Jobdistribution_JobWorker extends CM_Class_Abstract {
+class CM_Jobdistribution_JobWorker extends CM_Class_Abstract {
 
 	/** @var GearmanWorker */
 	private $_gearmanWorker;
 
-	public function __construct() {
-		$this->_gearmanWorker = new GearmanWorker();
+	final public function __construct() {
+		$worker = $this->_getGearmanWorker();
 		$config = self::_getConfig();
 		foreach ($config->servers as $server) {
-			$this->_gearmanWorker->addServer($server['host'], $server['port']);
+			$worker->addServer($server['host'], $server['port']);
 		}
 		//use non-blocking IO mode to enable signal processing in worker processes as soon as libgearman/pecl gearman is fixed
 		//see https://bugs.php.net/bug.php?id=60764
@@ -17,11 +17,11 @@ final class CM_Jobdistribution_JobWorker extends CM_Class_Abstract {
 		$this->_registerJobs();
 	}
 
-	public function run() {
+	final public function run() {
 		while (true) {
 			$workFailed = false;
 			try {
-				$workFailed = !$this->_gearmanWorker->work();
+				$workFailed = !$this->_getGearmanWorker()->work();
 			} catch (Exception $ex) {
 				CM_Bootloader::handleException($ex);
 			}
@@ -31,7 +31,14 @@ final class CM_Jobdistribution_JobWorker extends CM_Class_Abstract {
 		}
 	}
 
-	private function _registerJobs() {
+	protected function _getGearmanWorker() {
+		if (!$this->_gearmanWorker) {
+			$this->_gearmanWorker = new GearmanWorker();
+		}
+		return $this->_gearmanWorker;
+	}
+
+	final private function _registerJobs() {
 		foreach (CM_Jobdistribution_Job_Abstract::getClassChildren() as $jobClassName) {
 			$job = new $jobClassName();
 			$this->_gearmanWorker->addFunction($jobClassName, array($job, '__run'));
