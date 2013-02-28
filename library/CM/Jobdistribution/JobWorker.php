ea344d582baa10c6051bedf26515f21cf6a0913c
Alexis Peter <alexis.peter@cargomedia.ch>
2013-02-28 10:39:56 +0100

    catch exceptions in gearmanWorker to keep the process running

--- library/CM/Jobdistribution/JobWorker.php
+++ library/CM/Jobdistribution/JobWorker.php
@@ -11,14 +11,23 @@ final class CM_Jobdistribution_JobWorker extends CM_Class_Abstract {
 		foreach ($config->servers as $server) {
 			$this->_gearmanWorker->addServer($server['host'], $server['port']);
 		}
-//		use non-blocking IO mode to enable signal processing in worker processes as soon as libgearman/pecl gearman is fixed
-//		see https://bugs.php.net/bug.php?id=60764
-//		$this->_gearmanWorker->addOptions(GEARMAN_WORKER_NON_BLOCKING);
+		//use non-blocking IO mode to enable signal processing in worker processes as soon as libgearman/pecl gearman is fixed
+		//see https://bugs.php.net/bug.php?id=60764
+		//$this->_gearmanWorker->addOptions(GEARMAN_WORKER_NON_BLOCKING);
 		$this->_registerJobs();
 	}
 
 	public function run() {
-		while ($this->_gearmanWorker->work()) {
+		while (true) {
+			$workFailed = false;
+			try {
+				$workFailed = !$this->_gearmanWorker->work();
+			} catch (Exception $ex) {
+				CM_Bootloader::handleException($ex);
+			}
+			if ($workFailed) {
+				throw new CM_Exception_Invalid('Worker failed');
+			}
 		}
 	}
 
@@ -28,5 +37,4 @@ final class CM_Jobdistribution_JobWorker extends CM_Class_Abstract {
 			$this->_gearmanWorker->addFunction($jobClassName, array($job, '__run'));
 		}
 	}
-
 }
