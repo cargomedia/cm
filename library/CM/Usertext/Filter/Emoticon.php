5ee0866242a8f919fa7d9d71a1e2b2bb29533238
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-01 17:37:40 +0100

    add title tag in emoticon-img-tag and removed comments

--- library/CM/Usertext/Filter/Emoticon.php
+++ library/CM/Usertext/Filter/Emoticon.php
@@ -10,24 +10,23 @@ class CM_Usertext_Filter_Emoticon implements CM_Usertext_Filter_Interface {
 	}
 
 	/**
+	 * @param CM_Render $render
 	 * @return array
 	 */
 	private function _getEmoticonData(CM_Render $render) {
 		$cacheKey = CM_CacheConst::Usertext_Filter_EmoticonList;
-		//if (($emoticons = CM_CacheLocal::get($cacheKey)) === false) {
+		if (($emoticons = CM_CacheLocal::get($cacheKey)) === false) {
 			$emoticons = array('codes' => array(), 'htmls' => array());
 			foreach (new CM_Paging_Emoticon_All() as $emoticon) {
 				foreach ($emoticon['codes'] as $code) {
 					$emoticons['codes'][] = $code;
-//					$emoticons['htmls'][] =
-//							'<span class="emoticon emoticon-' . $emoticon['id'] . '" title="' .
-//									CM_Util::htmlspecialchars($emoticon['code']) . '"></span>';
-					$emoticons['htmls'][] = '<img src="'.$render->getUrlResource('img', 'emoticon/' . $emoticon['file']).'" class="emoticon emoticon-' . $emoticon['id'] . '" />';
-
+					$emoticons['htmls'][] =
+							'<img src="' . $render->getUrlResource('img', 'emoticon/' . $emoticon['file']) . '" class="emoticon emoticon-' .
+									$emoticon['id'] . '" title="' . $emoticon['code'] . '" />';
 				}
 			}
 			CM_CacheLocal::set($cacheKey, $emoticons);
-		//}
+		}
 		return $emoticons;
 	}
 }
