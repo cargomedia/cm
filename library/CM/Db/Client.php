3dc3a9f9f5a65aac95df9a60ae32e8c21c52f007
Reto Kaiser <reto@cargomedia.ch>
2013-03-03 16:54:19 +0100

    Remove copied code from cm_mysql

--- library/CM/Db/Client.php
+++ library/CM/Db/Client.php
@@ -14,7 +14,7 @@ class CM_Db_Client {
 	/** @var string */
 	private $_password;
 
-	/** @var mysqli */
+	/** @var PDO */
 	private $_link;
 
 	/**
@@ -29,114 +29,57 @@ class CM_Db_Client {
 		$this->_port = (int) $port;
 		$this->_username = (string) $username;
 		$this->_password = (string) $password;
-		$this->_link = $this->_connect($db);
-	}
-
-	/**
-	 * @param string $db
-	 * @throws CM_Exception
-	 */
-	public function selectDb($db) {
-		if (!$this->_link->select_db($db)) {
-			throw new CM_Exception('Cannot select database `' . $db . '`');
-		}
-	}
-
-	/**
-	 * @param string    $query
-	 * @throws CM_Exception
-	 * @return CM_MysqlResult|bool
-	 */
-	public function query($query) {
-		$result = self::_callRetried(function (mysqli $link) use ($query) {
-			return @$link->query($query);
-		});
-		if (true === $result) {
-			return true;
-		}
-		return new CM_MysqlResult($result);
-	}
-
-	/**
-	 * @return int|string|bool Last insert query autoincrement id or FALSE if none available
-	 */
-	public function getInsertId() {
-		$insertId = $this->_link->insert_id;
-		if (0 === $insertId) {
-			return false;
-		}
-		return $insertId;
-	}
-
-	/**
-	 * @return int|string|bool Number of affected rows or FALSE on error
-	 */
-	public function getAffectedRows() {
-		$affectedRows = $this->_link->affected_rows;
-		if ($affectedRows < 0) {
-			return false;
-		}
-		return $affectedRows;
-	}
-
-	/**
-	 * @param $string
-	 * @return string
-	 */
-	public function escapeString($string) {
-		return self::_callRetried(function (mysqli $link) use ($string) {
-			return @$link->real_escape_string($string);
-		}, false);
+		$this->connect($db);
 	}
 
 	/**
 	 * @param string|null $db
-	 * @throws CM_Exception
-	 * @return mysqli
+	 * @throws CM_Db_Exception
 	 */
-	private function _connect($db = null) {
-		$link = @new mysqli($this->_host, $this->_username, $this->_password, $db, $this->_port);
-		if ($link->connect_error) {
-			throw new CM_Exception('Database connection failed: ' . $link->connect_error);
+	public function connect($db = null) {
+		if ($this->isConnected()) {
+			return;
 		}
+		$dsnOptions = array('host=' . $this->_host, 'port=' . $this->_port);
+		if (null !== $db) {
+			$dsnOptions[] = 'dbname=' . $db;
+		}
+		$dsn = 'mysql:' . implode(';', $dsnOptions);
+		try {
+			$this->_link = new PDO($dsn, $this->_username, $this->_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES "UTF8"'));
+			$this->_link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch(PDOException $e) {
+			throw new CM_Db_Exception('Database connection failed: ' . $e->getMessage());
+		}
+	}
 
-		if (!$link->set_charset('utf8')) {
-			throw new CM_Exception('Cannot set database charset to utf-8');
+	public function disconnect() {
+		if (!$this->isConnected()) {
+			return;
 		}
-		return $link;
+		unset($this->_link);
 	}
 
 	/**
 	 * @return bool
 	 */
-	private function _getLastCallShouldRetry() {
-		if (0 === $this->_link->errno) {
-			return false;
-		}
-		$retryErrnoList = array(
-			1317, // Query execution was interrupted
-			2006, // MySQL server has gone away
-		);
-		return in_array($this->_link->errno, $retryErrnoList);
+	public function isConnected() {
+		return isset($this->_link);
 	}
 
 	/**
-	 * @param Closure $callback fn(mysqli)
-	 * @throws CM_Exception
-	 * @return mixed
+	 * @param string $db
 	 */
-	private function _callRetried(Closure $callback) {
-		$retryCount = 1;
-		$try = 0;
-		do {
-			$result = $callback($this->_link);
-			$try++;
-		} while ($this->_getLastCallShouldRetry() && $try <= $retryCount);
+	public function setDb($db) {
+		$this->_link->exec('USE ' . $db);
+	}
 
-		if (0 !== $this->_link->errno) {
-			throw new CM_Exception('Mysql error `' . $this->_link->errno . '` with message `' . $this->_link->error . '`');
-		}
-
-		return $result;
+	/**
+	 * @param string $sqlTemplate
+	 * @return CM_Db_Statement
+	 */
+	public function createStatement($sqlTemplate) {
+		$statement = $this->_link->prepare($sqlTemplate);
+		return new CM_Db_Statement($statement);
 	}
 }
--- library/CM/Db/Db.php
+++ library/CM/Db/Db.php
@@ -2,12 +2,6 @@
 
 class CM_Db_Db extends CM_Class_Abstract {
 
-	const STMT_INSERT = 'INSERT';
-	const STMT_INSERT_IGNORE = 'INSERT IGNORE';
-	const STMT_INSERT_DELAYED = 'INSERT DELAYED';
-	const STMT_REPLACE = 'REPLACE';
-	const STMT_REPLACE_DELAYED = 'REPLACE DELAYED';
-
 	/** @var CM_Db_Client */
 	private static $_client;
 
@@ -15,177 +9,8 @@ class CM_Db_Db extends CM_Class_Abstract {
 	private static $_clientReadOnly;
 
 	/**
-	 * @param string $query
-	 * @param mixed  $arg1,...
-	 * @return CM_MysqlResult|int|string|bool Mysql-result, last insert id, affected rows or FALSE if none affected
-	 */
-	public static function exec($query) {
-		$client = self::_getClient(false);
-		$query = call_user_func_array(array('self', 'placeholder'), func_get_args());
-		$result = $client->query($query);
-		if ($result instanceof CM_MysqlResult) {
-			return $result;
-		} elseif ($insertId = $client->getInsertId()) {
-			return $insertId;
-		} else {
-			return $client->getAffectedRows();
-		}
-	}
-
-	/**
-	 * Select fields from a table
-	 *
-	 * @param string            $table
-	 * @param string|array      $attrs Column-name OR Column-names array
-	 * @param string|array|null $where Associative array field=>value OR string
-	 * @param string|null       $order
-	 * @return CM_MysqlResult|bool
-	 */
-	public static function select($table, $attrs, $where = null, $order = null) {
-		$client = self::_getClient(false);
-		$attrs = (array) $attrs;
-		foreach ($attrs as &$attr) {
-			if ($attr == '*') {
-				$attr = '*';
-			} else {
-				$attr = self::placeholder("`?`", $attr);
-			}
-		}
-		$query = 'SELECT ' . implode(',', $attrs) . ' FROM `' . $table . '` ' . self::_queryWhere($where);
-		if ($order) {
-			$query .= ' ORDER BY ' . $order;
-		}
-		return $client->query($query);
-	}
-
-	/**
-	 * Insert one/multiple rows
-	 *
-	 * @param string            $table
-	 * @param string|array      $attr           Column-name OR Column-names array OR associative field=>value pair
-	 * @param string|array|null $value          Column-value OR Column-values array OR Multiple Column-values array(array)
-	 * @param array|null        $onDuplicateKeyValues
-	 * @param string            $statement
-	 * @throws CM_Exception
-	 * @return int|string Insert Id
-	 */
-	public static function insert($table, $attr, $value = null, array $onDuplicateKeyValues = null, $statement = self::STMT_INSERT) {
-		$client = self::_getClient(false);
-		if ($value === null && is_array($attr)) {
-			$value = array_values($attr);
-			$attr = array_keys($attr);
-		}
-		$values = (array) $value;
-		$attrs = (array) $attr;
-		if (!is_array(reset($values))) {
-			if (count($attrs) == 1) {
-				foreach ($values as &$value) {
-					$value = array($value);
-				}
-			} else {
-				$values = array($values);
-			}
-		}
-		$attrsEscaped = array();
-		foreach ($attrs as $attr) {
-			$attrsEscaped[] = self::placeholder("`?`", $attr);
-		}
-		$rowsEscaped = array();
-		foreach ($values as &$value) {
-			$row = (array) $value;
-			if (count($row) != count($attrs)) {
-				throw new CM_Exception('Row size does not match number of fields');
-			}
-			$rowEscaped = array();
-			foreach ($row as $rowValue) {
-				if ($rowValue === null) {
-					$rowEscaped[] = 'NULL';
-				} else {
-					$rowEscaped[] = self::placeholder("'?'", $rowValue);
-				}
-			}
-			$rowsEscaped[] = '(' . implode(',', $rowEscaped) . ')';
-		}
-
-		$query = $statement . ' INTO `' . $table . '` (' . implode(',', $attrsEscaped) . ') VALUES ' . implode(',', $rowsEscaped);
-		if ($onDuplicateKeyValues) {
-			$valuesEscaped = array();
-			foreach ($onDuplicateKeyValues as $attr => $value) {
-				if ($value === null) {
-					$valuesEscaped[] = self::placeholder("`?`=NULL", $attr);
-				} else {
-					$valuesEscaped[] = self::placeholder("`?`='?'", $attr, $value);
-				}
-			}
-			$query .= ' ON DUPLICATE KEY UPDATE ' . implode(',', $valuesEscaped);
-		}
-		$client->query($query);
-		return $client->getInsertId();
-	}
-
-	/**
-	 * @param string $queryTemplate
-	 * @param mixed  $arg,...
-	 * @return string
-	 */
-	public static function placeholder($queryTemplate) {
-		$client = self::_getClient(false);
-		$arguments = func_get_args();
-		$c_query = array_shift($arguments);
-		if (!is_array($c_query)) {
-			$c_query = self::_placeholderCompile($c_query);
-		}
-
-		$query = '';
-
-		foreach ($c_query as $piece) {
-			if (!is_array($piece)) {
-				$query .= $piece;
-				continue;
-			}
-
-			list($index, $type) = $piece;
-
-			if (isset($piece[2])) {
-				// array value
-				$array = $arguments[$index];
-
-				switch ($type) {
-					case '"':
-					case "'":
-					case '`':
-						foreach ($array as &$var) {
-							$var = $client->escapeString($var);
-						}
-						$query .= implode("$type,$type", $array);
-						break;
-					default:
-						$query .= implode(",", array_map('intval', $array));
-						break;
-				}
-			} else {
-				// scalar value
-				$var = $arguments[$index];
-
-				switch ($type) {
-					case '"':
-					case "'":
-					case '`':
-						$query .= $client->escapeString($var);
-						break;
-					default:
-						$query .= round($var, 0);
-						break;
-				}
-			}
-		}
-
-		return $query;
-	}
-
-	/**
 	 * @param bool $readOnly
-	 * @throws CM_Exception_Invalid
+	 * @throws CM_Db_Exception
 	 * @return CM_Db_Client
 	 */
 	private static function _getClient($readOnly) {
@@ -198,7 +23,7 @@ class CM_Db_Db extends CM_Class_Abstract {
 			$config = self::_getConfig();
 			if ($readOnly && $config->serversReadEnabled) {
 				if (empty($config->serversRead)) {
-					throw new CM_Exception_Invalid('No read servers configured');
+					throw new CM_Db_Exception('No read servers configured');
 				}
 				$server = $config->serversRead[array_rand($config->serversRead)];
 			} else {
@@ -208,76 +33,4 @@ class CM_Db_Db extends CM_Class_Abstract {
 		}
 		return $client;
 	}
-
-	/**
-	 * @param string|array|null $where Associative array field=>value OR string
-	 * @return string WHERE-query
-	 */
-	private static function _queryWhere($where) {
-		if (empty($where)) {
-			return '';
-		}
-		if (is_array($where)) {
-			$valuesEscaped = array();
-			foreach ($where as $attr => $value) {
-				if ($value === null) {
-					$valuesEscaped[] = self::placeholder("`?` IS NULL", $attr);
-				} else {
-					$valuesEscaped[] = self::placeholder("`?`='?'", $attr, $value);
-				}
-			}
-			$where = implode(' AND ', $valuesEscaped);
-		}
-		return 'WHERE ' . $where;
-	}
-
-	/**
-	 * @param string $queryTemplate
-	 * @return string
-	 */
-	private static function _placeholderCompile($queryTemplate) {
-		// Replace TBL_* constants
-		$queryTemplate = preg_replace_callback('/(TBL_.+?)\b/', function ($matches) {
-			return '`' . constant($matches[1]) . '`';
-		}, $queryTemplate);
-
-		$compiled = array();
-		$i = 0; // placeholders counter
-		$p = 0; // current position
-		$prev_p = 0; // previous position
-
-		while (false !== ($p = strpos($queryTemplate, '?', $p))) {
-			$compiled[] = substr($queryTemplate, $prev_p, $p - $prev_p);
-
-			$type_char = $char = $queryTemplate{$p - 1};
-
-			switch ($type_char) {
-				case '"':
-				case "'":
-				case '`':
-					$type = $type_char; // string
-					break;
-				default:
-					$type = ''; // integer
-					break;
-			}
-
-			$next_char = isset($queryTemplate{$p + 1}) ? $queryTemplate{$p + 1} : null;
-			if ($next_char === '@') { // array list
-				$compiled[] = array($i++, $type, '@');
-				$prev_p = ($p = $p + 2);
-			} else {
-				$compiled[] = array($i++, $type);
-				$prev_p = ($p = $p + 1);
-			}
-		}
-
-		$tail_length = (strlen($queryTemplate) - $prev_p);
-		if ($tail_length) {
-			$compiled[] = substr($queryTemplate, -$tail_length);
-		}
-
-		return $compiled;
-	}
-
 }
