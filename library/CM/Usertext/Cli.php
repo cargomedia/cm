c16826565b5904f9c16a2cb8d7024e13a341a31b
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-04 12:44:56 +0100

    moved emoticon intersection test to Cli-file

--- library/CM/Usertext/Cli.php
+++ library/CM/Usertext/Cli.php
@@ -22,6 +22,27 @@ class CM_Usertext_Cli extends CM_Cli_Runnable_Abstract {
 
 		CM_Mysql::insertIgnore(TBL_CM_EMOTICON, array('code', 'file'), $insertList);
 		$this->_getOutput()->writeln('Updated ' . count($insertList) . ' emoticons.');
+
+		$this->_checkEmoticonNoIntersection();
+	}
+
+	private function _checkEmoticonNoIntersection() {
+		$paging = new CM_Paging_Emoticon_All();
+		$codes = array();
+		foreach ($paging as $emoticon) {
+			if (false !== array_search('', $emoticon['codes'])) {
+				$this->_getOutput()->writeln('WARNING: Empty emoticon with ID `' . $emoticon['id'] . '`.');
+				return;
+			}
+			$codes = array_merge($codes, $emoticon['codes']);
+		}
+		for ($i = 0; $i < count($codes); $i++) {
+			for ($j = $i + 1; $j < count($codes); $j++) {
+				if (false !== strpos($codes[$i], $codes[$j]) || false !== strpos($codes[$j], $codes[$i])) {
+					$this->_getOutput()->writeln('WARNING: Emoticon intersection: `' . $codes[$i] . '` <-> `' . $codes[$j] . '`.');
+				}
+			}
+		}
 	}
 
 	public static function getPackageName() {
--- tests/library/CM/Paging/Emoticon/AllTest.php
+++ tests/library/CM/Paging/Emoticon/AllTest.php
@@ -16,19 +16,4 @@ class CM_Paging_Emoticon_AllTest extends CMTest_TestCase {
 		$this->assertEquals(array(':smiley:', ':)', ':-)'), $emoticonList[0]['codes']);
 		$this->assertEquals(array(':<'), $emoticonList[1]['codes']);
 	}
-
-	public function testNoIntersection() {
-		$paging = new CM_Paging_Emoticon_All();
-		$codes = array();
-		foreach ($paging as $emoticon) {
-			$codes = array_merge($codes, $emoticon['codes']);
-		}
-		for ($i = 0; $i < count($codes); $i++) {
-			for ($j = $i + 1; $j < count($codes); $j++) {
-				if (false !== strpos($codes[$i], $codes[$j])) {
-					$this->fail('Emoticon `' . $codes[$i] . '` in `' . $codes[$j] . '`');
-				}
-			}
-		}
-	}
 }
