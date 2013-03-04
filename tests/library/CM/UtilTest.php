2a98c3b2d905d05392c25934c9d4fc4fc5a4f582
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-04 16:27:51 +0100

    add super simple test for benchmark + codestyling

--- tests/library/CM/UtilTest.php
+++ tests/library/CM/UtilTest.php
@@ -2,6 +2,11 @@
 
 class CM_UtilTest extends CMTest_TestCase {
 
+	public function testBenchmark() {
+		CM_Util::benchmark();
+		CM_Util::benchmark('CM');
+	}
+
 	public function testGetClasses() {
 		$classPaths = array();
 		$classPaths['CM_Class_Abstract'] = 'CM/Class/Abstract.php';
@@ -35,13 +40,13 @@ class CM_UtilTest extends CMTest_TestCase {
 			array('id' => 3, 'type' => 1, 'amount' => 3), array('id' => 4, 'type' => 1, 'amount' => 4));
 
 		$this->assertSame(array(1 => array('type' => 1, 'amount' => 1), 2 => array('type' => 1, 'amount' => 2),
-			3 => array('type' => 1, 'amount' => 3), 4 => array('type' => 1, 'amount' => 4)), CM_Util::getArrayTree($array));
+								3 => array('type' => 1, 'amount' => 3), 4 => array('type' => 1, 'amount' => 4)), CM_Util::getArrayTree($array));
 
 		$this->assertSame(array(1 => array('id' => 1, 'type' => 1), 2 => array('id' => 2, 'type' => 1), 3 => array('id' => 3, 'type' => 1),
-			4 => array('id' => 4, 'type' => 1)), CM_Util::getArrayTree($array, 1, true, 'amount'));
+								4 => array('id' => 4, 'type' => 1)), CM_Util::getArrayTree($array, 1, true, 'amount'));
 
 		$this->assertSame(array(1 => array(1 => 1), 2 => array(1 => 2), 3 => array(1 => 3),
-			4 => array(1 => 4)), CM_Util::getArrayTree($array, 2, true, array('amount', 'type')));
+								4 => array(1 => 4)), CM_Util::getArrayTree($array, 2, true, array('amount', 'type')));
 
 		try {
 			CM_Util::getArrayTree($array, 1, true, 'foo');
@@ -77,5 +82,4 @@ class CM_UtilTest extends CMTest_TestCase {
 			$this->assertTrue(true);
 		}
 	}
-
 }
