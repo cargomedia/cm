3d9cd4da7367d61e99e69d0a64f795e4ca473e73
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-05 12:16:52 +0100

    add function setMode() in Usertext

--- library/CM/Usertext/Usertext.php
+++ library/CM/Usertext/Usertext.php
@@ -24,6 +24,47 @@ class CM_Usertext_Usertext {
 	}
 
 	/**
+	 * @param (string) $mode
+	 * @throws CM_Exception_Invalid
+	 */
+	public function setMode($mode) {
+		$mode = (string) $mode;
+		$this->_clearFilters();
+		$this->addFilter(new CM_Usertext_Filter_Escape());
+		$this->addFilter(new CM_Usertext_Filter_Badwords());
+		switch ($mode) {
+			case 'oneline':
+				$this->addFilter(new CM_Usertext_Filter_MaxLength($this->_maxLength));
+				break;
+			case 'simple':
+				$this->addFilter(new CM_Usertext_Filter_MaxLength($this->_maxLength));
+				$this->addFilter(new CM_Usertext_Filter_NewlineToLinebreak(3));
+				break;
+			case 'markdown':
+				if (null !== $this->_maxLength) {
+					throw new CM_Exception_Invalid('MaxLength is not allowed in mode markdown.');
+				}
+				$this->addFilter(new CM_Usertext_Filter_Emoticon_EscapeMarkdown());
+				$this->addFilter(new CM_Usertext_Filter_Markdown(true));
+				$this->addFilter(new CM_Usertext_Filter_Emoticon_UnescapeMarkdown());
+				break;
+			case 'markdownPlain':
+				$this->addFilter(new CM_Usertext_Filter_Emoticon_EscapeMarkdown());
+				$this->addFilter(new CM_Usertext_Filter_Markdown(true));
+				$this->addFilter(new CM_Usertext_Filter_Emoticon_UnescapeMarkdown());
+				$this->addFilter(new CM_Usertext_Filter_Striptags());
+				$this->addFilter(new CM_Usertext_Filter_MaxLength($this->_maxLength));
+				break;
+			default:
+				throw new CM_Exception_Invalid('Must define mode in Usertext.');
+		}
+		$this->addFilter(new CM_Usertext_Filter_Emoticon());
+		if ('markdownPlain' != $mode) {
+			$this->addFilter(new CM_Usertext_Filter_CutWhitespace());
+		}
+	}
+
+	/**
 	 * @param string $text
 	 * @return string
 	 */
