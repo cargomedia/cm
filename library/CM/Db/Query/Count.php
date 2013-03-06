8715fc16389c21a5473eefce33e4019b0182c8f6
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-06 09:49:17 +0100

    Write SELECT and FROM in two _addSql()-calls for consistency

--- library/CM/Db/Query/Count.php
+++ library/CM/Db/Query/Count.php
@@ -7,7 +7,8 @@ class CM_Db_Query_Count extends CM_Db_Query_Abstract {
 	 * @param string|array|null $where  Associative array field=>value OR string
 	 */
 	public function __construct($table, $where = null) {
-		$this->_addSql('SELECT COUNT(*) FROM ' . $this->_quoteIdentifier($table));
+		$this->_addSql('SELECT COUNT(*)');
+		$this->_addSql('FROM ' . $this->_quoteIdentifier($table));
 		$this->_addWhere($where);
 	}
 }
