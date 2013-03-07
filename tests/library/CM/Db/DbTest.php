70a6a1d4a2937fadecaf3e0be0f0ce4f019f0557
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-07 15:57:35 +0100

    add test CM_Db_DbTest::existsColumn() and CM_Db_DbTest::existsIndex()

--- tests/library/CM/Db/DbTest.php
+++ tests/library/CM/Db/DbTest.php
@@ -2,7 +2,7 @@
 
 class CM_Db_DbTest extends CMTest_TestCase {
 
-	public function setUp() {
+	public static function setUpBeforeClass() {
 		CM_Db_Db::exec(
 			'CREATE TABLE `test` (
 					`id` INT(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -13,8 +13,18 @@ class CM_Db_DbTest extends CMTest_TestCase {
 				)');
 	}
 
-	public function tearDown() {
+	public static function tearDownAfterClass() {
 		CM_Db_Db::exec('DROP TABLE `test`');
+	}
+
+	public function testExistsColumn() {
+		$this->assertSame(true, CM_Db_Db::existsColumn('test', 'foo'));
+		$this->assertSame(false, CM_Db_Db::existsColumn('test', 'test'));
+	}
+
+	public function testExistsIndex() {
+		$this->assertSame(true, CM_Db_Db::existsIndex('test', 'foo'));
+		$this->assertSame(false, CM_Db_Db::existsIndex('test', 'test'));
 	}
 
 	public function testExistsTable() {
