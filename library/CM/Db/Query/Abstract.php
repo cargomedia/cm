d16a2b93510a91a0353c120eb3d91afc576423dc
Reto Kaiser <reto@cargomedia.ch>
2013-03-07 19:26:36 +0000

    Allow to pass array for ordering

--- library/CM/Db/Query/Abstract.php
+++ library/CM/Db/Query/Abstract.php
@@ -80,17 +80,32 @@ abstract class CM_Db_Query_Abstract {
 	}
 
 	/**
-	 * @param string|null $orderBy
+	 * @param string|array|null $orderBy
 	 * @throws CM_Exception_Invalid
 	 */
 	protected function _addOrderBy($orderBy) {
-		if (!is_string($orderBy) && !is_null($orderBy)) {
-			throw new CM_Exception_Invalid('Invalid order type');
-		}
 		if (null === $orderBy) {
 			return;
 		}
-		$this->_addSql('ORDER BY ' . $orderBy);
+		if (!is_string($orderBy) && !is_array($orderBy)) {
+			throw new CM_Exception_Invalid('Invalid order type');
+		}
+		if (is_array($orderBy)) {
+			$sqlParts = array();
+			foreach ($orderBy as $field => $direction) {
+				$direction = strtoupper($direction);
+				if (!is_string($field)) {
+					throw new CM_Exception_Invalid('Order field name is not string');
+				}
+				if ('ASC' !== $direction && 'DESC' !== $direction) {
+					throw new CM_Exception_Invalid('Invalid order direction `' . $direction . '`.');
+				}
+				$sqlParts[] = $this->_quoteIdentifier($field) . ' ' . $direction;
+			}
+			$this->_addSql('ORDER BY ' . implode(', ', $sqlParts));
+		} elseif (is_string($orderBy)) {
+			$this->_addSql('ORDER BY ' . $orderBy);
+		}
 	}
 
 	/**
