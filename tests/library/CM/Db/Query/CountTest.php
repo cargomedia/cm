970b1ef91d4598f0969e9eced25bc67df3324bc5
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-08 15:13:12 +0100

    add CMTest_TH::createClient in CM_Db_Query_CountTest

--- tests/library/CM/Db/Query/CountTest.php
+++ tests/library/CM/Db/Query/CountTest.php
@@ -3,7 +3,8 @@
 class CM_Db_Query_CountTest extends CMTest_TestCase {
 
 	public function testAll() {
-		$query = new CM_Db_Query_Count('t`est', array('foo' => 'foo1', 'bar' => 'bar1'));
+		$client = CMTest_TH::createClient(false);
+		$query = new CM_Db_Query_Count($client, 't`est', array('foo' => 'foo1', 'bar' => 'bar1'));
 		$this->assertSame('SELECT COUNT(*) FROM `t``est` WHERE `foo` = ? AND `bar` = ?', $query->getSqlTemplate());
 		$this->assertEquals(array('foo1', 'bar1'), $query->getParameters());
 	}
