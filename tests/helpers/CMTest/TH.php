fd6fb706629097a93d78c0edc7dd85e449904ddd
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-11 10:27:20 +0100

    removed CMTest_TH::createClient() and updated CM_Db_Query_CountTest

--- tests/helpers/CMTest/TH.php
+++ tests/helpers/CMTest/TH.php
@@ -249,25 +249,6 @@ class CMTest_TH {
 	}
 
 	/**
-	 * @param bool $readOnly
-	 * @return CM_Db_Client
-	 * @throws CM_Db_Exception
-	 */
-	public static function createClient($readOnly) {
-		$config = CM_Config::get()->CM_Db_Db;
-		if ($readOnly && $config->serversReadEnabled) {
-			if (empty($config->serversRead)) {
-				throw new CM_Db_Exception('No read servers configured');
-			}
-			$server = $config->serversRead[array_rand($config->serversRead)];
-		} else {
-			$server = $config->server;
-		}
-		$client = new CM_Db_Client($server['host'], $server['port'], $config->username, $config->password, $config->db);
-		return $client;
-	}
-
-	/**
 	 * @param int    $length
 	 * @param string $charset
 	 * @return string
--- tests/library/CM/Db/Query/CountTest.php
+++ tests/library/CM/Db/Query/CountTest.php
@@ -2,9 +2,16 @@
 
 class CM_Db_Query_CountTest extends CMTest_TestCase {
 
+	/** @var CM_Db_Client */
+	private static $_client;
+
+	public static function setUpBeforeClass() {
+		$config = CM_Config::get()->CM_Db_Db;
+		self::$_client = new CM_Db_Client($config->server['host'], $config->server['port'], $config->username, $config->password, $config->db);
+	}
+
 	public function testAll() {
-		$client = CMTest_TH::createClient(false);
-		$query = new CM_Db_Query_Count($client, 't`est', array('foo' => 'foo1', 'bar' => 'bar1'));
+		$query = new CM_Db_Query_Count(self::$_client, 't`est', array('foo' => 'foo1', 'bar' => 'bar1'));
 		$this->assertSame('SELECT COUNT(*) FROM `t``est` WHERE `foo` = ? AND `bar` = ?', $query->getSqlTemplate());
 		$this->assertEquals(array('foo1', 'bar1'), $query->getParameters());
 	}
