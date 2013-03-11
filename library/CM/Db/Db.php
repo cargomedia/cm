e6ca1c4c440889e3e661dc0179dbeee36fc2e252
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-11 14:07:40 +0100

    add CM_Db_Db::insertDelayed()

--- library/CM/Db/Db.php
+++ library/CM/Db/Db.php
@@ -96,6 +96,18 @@ class CM_Db_Db extends CM_Class_Abstract {
 
 	/**
 	 * @param string            $table
+	 * @param string|array      $attr           Column-name OR Column-names array OR associative field=>value pair
+	 * @param string|array|null $value          Column-value OR Column-values array OR Multiple Column-values array(array)
+	 * @param array|null        $onDuplicateKeyValues
+	 * @return string|null
+	 */
+	public static function insertDelayed($table, $attr, $value = null, array $onDuplicateKeyValues = null) {
+		$statement = (self::_getConfig()->delayedEnabled) ? 'INSERT DELAYED' : 'INSERT';
+		return self::insert($table, $attr, $value, $onDuplicateKeyValues, $statement);
+	}
+
+	/**
+	 * @param string            $table
 	 * @param string|array      $fields Column-name OR Column-names array
 	 * @param string|array|null $where  Associative array field=>value OR string
 	 * @param string|null       $order
