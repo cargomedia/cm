e452237deba971872935e91d1b059df8c546f95e
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2013-03-14 12:48:05 +0100

    Migrated `CM_Mysql::getDump` and `CM_Mysql::runDump` to `CM_Db_Db::getDump` and `CM_Db_Db::runDump` respectively (straightforward)
    Based on #263

--- library/CM/Mysql/Cli.php
+++ library/CM/Mysql/Cli.php
@@ -9,7 +9,7 @@ class CM_Mysql_Cli extends CM_Cli_Runnable_Abstract {
 		$namespace = (string) $namespace;
 		$tables = CM_Mysql::exec("SHOW TABLES LIKE '?'", strtolower($namespace) . '_%')->fetchCol();
 		sort($tables);
-		$dump = CM_Mysql::getDump($tables, true);
+		$dump = CM_Db_Db::getDump($tables, true);
 		CM_File::create(CM_Util::getNamespacePath($namespace) . '/resources/db/structure.sql', $dump);
 	}
 
