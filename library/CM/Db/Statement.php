6b0cd57ebd88cc5af5382b40e65aae125975aba3
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-18 13:33:44 +0100

    Add retried count in CM_Db_Exception

--- library/CM/Db/Statement.php
+++ library/CM/Db/Statement.php
@@ -35,7 +35,9 @@ class CM_Db_Statement {
 					$this->_reCreatePdoStatement();
 					continue;
 				}
-				throw new CM_Db_Exception('Cannot execute statement: ' . $e->getMessage() . ' (query: `' . $this->_pdoStatement->queryString . '`)');
+				throw new CM_Db_Exception(
+					'Cannot execute statement (retried ' . $try . 'x): ' . $e->getMessage() .
+							' (query: `' . $this->_pdoStatement->queryString . '`)');
 			}
 		}
 		throw new CM_Db_Exception('Line should never be reached');
