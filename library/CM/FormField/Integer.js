a9787a559bf3991b33ca3407ef88e4fabf18d353
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-18 19:54:57 +0100

    unbind events

--- library/CM/FormField/Integer.js
+++ library/CM/FormField/Integer.js
@@ -9,6 +9,7 @@ var CM_FormField_Integer = CM_FormField_Abstract.extend({
 		var field = this;
 		var $slider = this.$(".slider");
 		var $input = this.$("input");
+		var input = $input.get(0);
 		$slider.slider({
 			value: $input.val(),
 			min: field.getOption("min"),
@@ -16,22 +17,35 @@ var CM_FormField_Integer = CM_FormField_Abstract.extend({
 			step: field.getOption("step"),
 			slide: function(event, ui) {
 				var value = ui.value + 0;
-				$input.val(value);
+				if (value != input.value) {
+					input.value = value;
+				}
 				$(this).children(".ui-slider-handle").text(value);
 			},
 			change: function(event, ui) {
 				var value = ui.value + 0;
-				$input.val(value);
+				if (value != input.value) {
+					input.value = value;
+				}
 				$(this).children(".ui-slider-handle").text(value);
 			}
 		});
 		$slider.children(".ui-slider-handle").text($input.val());
 		$input.watch("disabled", function(propName, oldVal, newVal) {
 			$slider.slider("option", "disabled", newVal);
+			return newVal;
 		});
 		$input.watch("value", function(propName, oldVal, newVal) {
-			$slider.slider("option", "value", newVal);
+			if (newVal != $slider.slider("option", "value")) {
+				$slider.slider("option", "value", newVal);
+			}
 			field.trigger('change');
+			return newVal;
+		});
+
+		this.on('destruct', function() {
+			$input.unwatch('disabled');
+			$input.unwatch('value');
 		});
 	}
 });
