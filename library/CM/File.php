fa165c36d71ce89a07cea813ff9816a989985c55
Marcel Colomb <marcel.colomb@cargomedia.ch>
2013-03-19 18:06:34 +0100

    supress E_WARNING on file_put_contents in CM_File::create()

--- library/CM/File.php
+++ library/CM/File.php
@@ -203,7 +203,7 @@ class CM_File extends CM_Class_Abstract {
 	 */
 	public static function create($path, $content = null) {
 		$content = (string) $content;
-		if (false === file_put_contents($path, $content)) {
+		if (false === @file_put_contents($path, $content)) {
 			throw new CM_Exception('Cannot write to `' . $path . '`.');
 		}
 		$file = new static($path);
