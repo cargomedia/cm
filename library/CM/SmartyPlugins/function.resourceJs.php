ce4950e2208a3d2aa0b08480ae1d072ef3c0d971
Tomasz Durka <tomasz@durka.pl>
2013-03-24 03:47:01 +0100

    Add function params types

--- library/CM/SmartyPlugins/function.resourceJs.php
+++ library/CM/SmartyPlugins/function.resourceJs.php
@@ -3,8 +3,8 @@
 function smarty_function_resourceJs(array $params, Smarty_Internal_Template $template) {
 	/** @var $render CM_Render */
 	$render = $template->smarty->getTemplateVars('render');
-	$type = $params['type'];
-	$file = $params['file'];
+	$type = (string) $params['type'];
+	$file = (string) $params['file'];
 	if (!in_array($type, array('vendor', 'library'))) {
 		throw new CM_Exception_Invalid();
 	}
