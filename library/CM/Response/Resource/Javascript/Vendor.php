2b936c3e03144b24d1d6fc54ad973a4d9e89fbf9
Tomasz Durka <tomasz@durka.pl>
2013-03-24 04:25:25 +0100

    Load libraries from all namespaces' client-vendor directories (not static anymore)

--- library/CM/Response/Resource/Javascript/Vendor.php
+++ library/CM/Response/Resource/Javascript/Vendor.php
@@ -6,15 +6,21 @@ class CM_Response_Resource_Javascript_Vendor extends CM_Response_Resource_Javasc
 		switch ($this->getRequest()->getPath()) {
 			case '/library.js':
 				$content = '';
-				foreach (CM_Util::rglob('*.js', DIR_PUBLIC . 'static/js/library/') as $path) {
-					$content .= new CM_File($path) . ';' . PHP_EOL;
+				foreach ($this->getSite()->getNamespaces() as $namespace) {
+					$libraryPath = DIR_ROOT . CM_Bootloader::getInstance()->getNamespacePath($namespace) . 'client-vendor/js/library';
+					foreach (CM_Util::rglob('*.js', $libraryPath) as $path) {
+						$content .= new CM_File($path) . ';' . PHP_EOL;
+					}
 				}
 				$this->_setContent($content);
 				break;
 			case '/init.js':
 				$content = '';
-				foreach (CM_Util::rglob('*.js', DIR_PUBLIC . 'static/js/init/') as $path) {
-					$content .= new CM_File($path) . ';' . PHP_EOL;
+				foreach ($this->getSite()->getNamespaces() as $namespace) {
+					$initPath = DIR_ROOT . CM_Bootloader::getInstance()->getNamespacePath($namespace) . 'client-vendor/js/init';
+					foreach (CM_Util::rglob('*.js', $initPath) as $path) {
+						$content .= new CM_File($path) . ';' . PHP_EOL;
+					}
 				}
 				$this->_setContent($content);
 				break;
@@ -24,7 +30,8 @@ class CM_Response_Resource_Javascript_Vendor extends CM_Response_Resource_Javasc
 		}
 	}
 
-	public static function match(CM_Request_Abstract $request) {
+	public
+	static function match(CM_Request_Abstract $request) {
 		return $request->getPathPart(0) === 'vendor-js';
 	}
 }
