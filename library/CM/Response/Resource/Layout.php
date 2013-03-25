81a26dc380a48f0e7187c570a77bf5f806a1ceaf
Tomasz Durka <tomasz@durka.pl>
2013-03-25 11:23:36 +0100

    Rollback Response_Layout

--- library/CM/Response/Resource/Layout.php
+++ library/CM/Response/Resource/Layout.php
@@ -3,22 +3,19 @@
 class CM_Response_Resource_Layout extends CM_Response_Resource_Abstract {
 
 	protected function _process() {
+		$file = null;
 		foreach ($this->getSite()->getNamespaces() as $namespace) {
 			if ($path = $this->getRender()->getLayoutPath('resource/' . $this->getRequest()->getPath(), $namespace, true, false)) {
-				$this->_setContent(new CM_File($path));
-				return;
+				$file = new CM_File($path);
+				break;
 			}
 		}
-		throw new CM_Exception_Nonexistent('Invalid filename: `' . $this->getRequest()->getPath() . '`');
-	}
-
-	/**
-	 * @param CM_File $file
-	 */
-	protected function _setContent(CM_File $file) {
+		if (!$file) {
+			throw new CM_Exception_Nonexistent('Invalid filename: `' . $this->getRequest()->getPath() . '`');
+		}
 		$this->enableCache();
 		$this->setHeader('Content-Type', $file->getMimeType());
-		parent::_setContent($file->read());
+		$this->_setContent($file->read());
 	}
 
 	public static function match(CM_Request_Abstract $request) {
