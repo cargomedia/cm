3593495a9acdc9efc0815cc626d4b9059cdb5afd
Tomasz Durka <tomasz@durka.pl>
2013-03-25 11:16:40 +0100

    resourceLayoutUrl => resourceUrl - add type param

--- library/CM/SmartyPlugins/function.resourceLayoutUrl.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function smarty_function_resourceLayoutUrl(array $params, Smarty_Internal_Template $template) {
-	/** @var CM_Render $render */
-	$render = $template->smarty->getTemplateVars('render');
-	$url = $render->getUrlResource('layout', $params['path']);
-	return $url;
-}
