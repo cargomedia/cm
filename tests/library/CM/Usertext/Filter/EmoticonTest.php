8b44f7d583c77f4067829b837d2f270e48475c37
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-26 15:38:08 +0100

    fixed tests

--- tests/library/CM/Usertext/Filter/EmoticonTest.php
+++ tests/library/CM/Usertext/Filter/EmoticonTest.php
@@ -10,8 +10,8 @@ class CM_Usertext_Filter_EmoticonTest extends CMTest_TestCase {
 		$emoticonId = CM_Db_Db::insert(TBL_CM_EMOTICON, array('code' => ':smiley:', 'codeAdditional' => ':),:-)', 'file' => '1.png'));
 		$text = 'foo :) bar :smiley:';
 		$expected =
-				'foo <img src="/img/1/0/emoticon/1.png" class="emoticon emoticon-' . $emoticonId .
-						'" title=":smiley:" /> bar <img src="/img/1/0/emoticon/1.png" class="emoticon emoticon-' . $emoticonId .
+				'foo <img src="/layout/1/0/img/emoticon/1.png" class="emoticon emoticon-' . $emoticonId .
+						'" title=":smiley:" /> bar <img src="/layout/1/0/img/emoticon/1.png" class="emoticon emoticon-' . $emoticonId .
 						'" title=":smiley:" />';
 		$filter = new CM_Usertext_Filter_Emoticon();
 		$actual = $filter->transform($text, $this->_getRender());
