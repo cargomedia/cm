f157d187461459ac3eea771592ad63ce4e9a16da
Tomasz Durka <tomasz@durka.pl>
2013-03-27 17:38:48 +0100

    CM_Usertext_Filter_BadwordRegexp: Improve performance

--- library/CM/Usertext/Filter/Badwords.php
+++ library/CM/Usertext/Filter/Badwords.php
@@ -4,17 +4,17 @@ class CM_Usertext_Filter_Badwords implements CM_Usertext_Filter_Interface {
 
 	public function transform($text, CM_Render $render) {
 		$text = (string) $text;
-		$cacheKey = CM_CacheConst::Usertext_Filter_BadwordList;
-		if (($badwords = CM_CacheLocal::get($cacheKey)) === false) {
-			$badwords = array('search' => array(), 'replace' => '…');
+		$cacheKey = CM_CacheConst::Usertext_Filter_BadwordRegexp;
+		if (($badwordsRegexp = CM_CacheLocal::get($cacheKey)) === false) {
+			$badwords = array();
 			foreach (new CM_Paging_ContentList_Badwords() as $badword) {
 				$badword = preg_quote($badword, '#');
 				$badword = str_replace('\*', '[^\s]*', $badword);
-				$badwords['search'][] = '#(\b' . $badword . '\b)#i';
+				$badwords[] = '\b' . $badword . '\b';
 			}
-			CM_CacheLocal::set($cacheKey, $badwords);
+			$badwordsRegexp = '#(' . implode('|', $badwords) . ')#i';
+			CM_CacheLocal::set($cacheKey, $badwordsRegexp);
 		}
-		return preg_replace($badwords['search'], $badwords['replace'], $text);
+		return preg_replace($badwordsRegexp, '…', $text);
 	}
-
 }
