803cb6af1f03092585b48125457cabc9f4f65bb0
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-28 12:40:35 +0100

    add more specific permit ">"

--- library/CM/Usertext/Filter/Escape.php
+++ library/CM/Usertext/Filter/Escape.php
@@ -5,7 +5,7 @@ class CM_Usertext_Filter_Escape implements CM_Usertext_Filter_Interface {
 	public function transform($text, CM_Render $render) {
 		$text = (string) $text;
 		$text = htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
-		$text = str_replace('&gt;','>',$text);
+		$text = preg_replace(array('#\\n&gt;\s#', '#\\n {4}&gt;\s#'), array("\n> ", "\n    > "), $text);
 		return $text;
 	}
 }
