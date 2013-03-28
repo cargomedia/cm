9459105f780a7629247e524d48443a5718ee4ffa
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-28 18:02:45 +0100

    Fixed Test Bugs
    - renamed files
    - add render

--- tests/library/CM/Usertext/Filter/Emoticon/UnescapeMarkdownTest.php
+++ tests/library/CM/Usertext/Filter/Emoticon/UnescapeMarkdownTest.php
@@ -10,7 +10,7 @@ class CM_Usertext_Filter_Emoticon_UnescapeMarkdownTest extends CMTest_TestCase {
 		$text = 'foo-bar foo---bar--foo :foo-bar: -foo- :foo-bar-foo:';
 		$expected = 'foo-bar foo---bar--foo :foo_bar: -foo- :foo_bar_foo:';
 		$filter = new CM_Usertext_Filter_Emoticon_UnescapeMarkdown();
-		$actual = $filter->transform($text);
+		$actual = $filter->transform($text, $this->_getRender());
 
 		$this->assertSame($expected, $actual);
 	}
