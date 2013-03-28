ece96079bda52e17880b344823188080aeaf887e
Constantin7 <marian.sollmann@cargomedia.ch>
2013-03-28 19:10:41 +0100

    add more "<" to the test

--- tests/library/CM/Usertext/Filter/Markdown/EscapeBlockquoteTest.php
+++ tests/library/CM/Usertext/Filter/Markdown/EscapeBlockquoteTest.php
@@ -8,11 +8,11 @@ class CM_Usertext_Filter_Markdown_UnescapeBlockquoteTest extends CMTest_TestCase
 
 	public function testProcess() {
 		$text = <<<'EOD'
-&gt; blockquote
+&gt; blockquote &gt; foo&gt;bar&gt;
     &gt; blockquote
 EOD;
 		$expected = <<<'EOD'
-> blockquote
+> blockquote &gt; foo&gt;bar&gt;
     > blockquote
 EOD;
 		$filter = new CM_Usertext_Filter_Markdown_UnescapeBlockquote();
