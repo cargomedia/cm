af6ad8a427d76d75934c6e45fc3ec8bb2250a934
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2013-04-05 14:25:44 +0200

    simplify time_duration function

--- library/CM/SmartyPlugins/function.time_duration.php
+++ library/CM/SmartyPlugins/function.time_duration.php
@@ -3,12 +3,10 @@
 function smarty_function_time_duration(array $params, Smarty_Internal_Template $template) {
 
 	$seconds = (int) $params['duration'];
-	$format = null;
+	$format = 'i:s';
 
 	if (($seconds / 3600) >= 1) {
 		$format = 'H:i:s';
-	} else {
-		$format = 'i:s';
 	}
 
 	return '<span class="time-duration">' . gmdate($format, $seconds) . '</span>';
