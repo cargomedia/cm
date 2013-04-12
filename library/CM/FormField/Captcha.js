0546c3c44e606bb4d310f4230d001348e33e2416
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2013-04-12 12:16:45 +0200

    fix & refactor captcha formfield

--- library/CM/FormField/Captcha.js
+++ library/CM/FormField/Captcha.js
@@ -7,19 +7,19 @@ var CM_FormField_Captcha = CM_FormField_Abstract.extend({
 
 	ready: function() {
 		var field = this;
-		this.$(".captcha_container:eq(0)").find(".reload").click(function(){
+		this.$(".captcha:eq(0)").find(".refreshCaptcha").click(function(){
 			field.refresh();
 		});
 		this.getForm().bind("error", function(){
 			field.refresh();
 		});
-		
+
 	},
-	
+
 	refresh: function(){
 		this.ajax('createNumber', {}, {
 			success: function(id) {
-				var $container = this.$(".captcha_container:eq(0)");
+				var $container = this.$(".captcha:eq(0)");
 				var $img = $container.find("img");
 				$img.attr("src", $img.attr("src").replace(/\?[^\?]+$/, '?id=' + id));
 				$container.find("input[name=\'captcha[id]\']").val(id);
