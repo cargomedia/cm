14650478c21598f7e83b6a6f834fa074f1fadc72
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-04-23 18:44:58 +0200

    add fixedHeight to emoticon filter

--- library/CM/Usertext/Filter/Emoticon.php
+++ library/CM/Usertext/Filter/Emoticon.php
@@ -2,6 +2,16 @@
 
 class CM_Usertext_Filter_Emoticon implements CM_Usertext_Filter_Interface {
 
+	/** @var int $_fixedHeight */
+	private $_fixedHeight;
+
+	/**
+	 * @param int|null $fixedHeight
+	 */
+	function __construct($fixedHeight = null) {
+		$this->_fixedHeight = (int) $fixedHeight;
+	}
+
 	public function transform($text, CM_Render $render) {
 		$text = (string) $text;
 		$emoticons = $this->_getEmoticonData($render);
@@ -14,15 +24,16 @@ class CM_Usertext_Filter_Emoticon implements CM_Usertext_Filter_Interface {
 	 * @return array
 	 */
 	private function _getEmoticonData(CM_Render $render) {
-		$cacheKey = CM_CacheConst::Usertext_Filter_EmoticonList;
+		$cacheKey = CM_CacheConst::Usertext_Filter_EmoticonList . '_fixedHeight:' . $this->_fixedHeight;
 		if (($emoticons = CM_CacheLocal::get($cacheKey)) === false) {
 			$emoticons = array('codes' => array(), 'htmls' => array());
+			$fixedHeight = $this->_fixedHeight ? ' height="' . $this->_fixedHeight . '"' : null;
 			foreach (new CM_Paging_Emoticon_All() as $emoticon) {
 				foreach ($emoticon['codes'] as $code) {
 					$emoticons['codes'][] = $code;
 					$emoticons['htmls'][] =
 							'<img src="' . $render->getUrlResource('layout', 'img/emoticon/' . $emoticon['file']) . '" class="emoticon emoticon-' .
-									$emoticon['id'] . '" title="' . $emoticon['code'] . '" />';
+									$emoticon['id'] . '" title="' . $emoticon['code'] . '"' . $fixedHeight . ' />';
 				}
 			}
 			CM_CacheLocal::set($cacheKey, $emoticons);
--- tests/library/CM/Usertext/Filter/EmoticonTest.php
+++ tests/library/CM/Usertext/Filter/EmoticonTest.php
@@ -7,16 +7,31 @@ class CM_Usertext_Filter_EmoticonTest extends CMTest_TestCase {
 	}
 
 	public function testProcess() {
-		$siteType = CM_Site_Abstract::factory()->getType();
+		$site = $this->getMockSite(24, null, 'http://www.default.dev');
+
 		$emoticonId = CM_Db_Db::insert(TBL_CM_EMOTICON, array('code' => ':smiley:', 'codeAdditional' => ':),:-)', 'file' => '1.png'));
 		$text = 'foo :) bar :smiley:';
 		$expected =
-				'foo <img src="http://www.default.dev/layout/' . $siteType . '/0/img/emoticon/1.png" class="emoticon emoticon-' . $emoticonId .
-						'" title=":smiley:" /> bar <img src="http://www.default.dev/layout/' . $siteType . '/0/img/emoticon/1.png" class="emoticon emoticon-' .
-						$emoticonId .
-						'" title=":smiley:" />';
+				'foo <img src="http://www.default.dev/layout/24/0/img/emoticon/1.png" class="emoticon emoticon-' . $emoticonId .
+						'" title=":smiley:" /> bar <img src="http://www.default.dev/layout/24/0/img/emoticon/1.png" class="emoticon emoticon-' .
+						$emoticonId . '" title=":smiley:" />';
 		$filter = new CM_Usertext_Filter_Emoticon();
-		$actual = $filter->transform($text, new CM_Render());
+		$actual = $filter->transform($text, new CM_Render($site));
+
+		$this->assertSame($expected, $actual);
+	}
+
+	public function testFixedHeight() {
+		$site = $this->getMockSite(24, null, 'http://www.default.dev');
+
+		$emoticonId = CM_Db_Db::insert(TBL_CM_EMOTICON, array('code' => ':smiley:', 'codeAdditional' => ':),:-)', 'file' => '1.png'));
+		$text = 'foo :) bar :smiley:';
+		$expected =
+				'foo <img src="http://www.default.dev/layout/24/0/img/emoticon/1.png" class="emoticon emoticon-' . $emoticonId .
+						'" title=":smiley:" height="16" /> bar <img src="http://www.default.dev/layout/24/0/img/emoticon/1.png" class="emoticon emoticon-' .
+						$emoticonId . '" title=":smiley:" height="16" />';
+		$filter = new CM_Usertext_Filter_Emoticon(16);
+		$actual = $filter->transform($text, new CM_Render($site));
 
 		$this->assertSame($expected, $actual);
 	}
