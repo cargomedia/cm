4b9f06b03a3afad328e914624e7174d25b818d2d
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2013-05-04 18:07:41 +0200

    Add blur/focus event to text field, add methods to focus and set value

--- library/CM/FormField/Text.js
+++ library/CM/FormField/Text.js
@@ -3,5 +3,25 @@
  * @extends CM_FormField_Abstract
  */
 var CM_FormField_Text = CM_FormField_Abstract.extend({
-	_class: 'CM_FormField_Text'
+	_class: 'CM_FormField_Text',
+
+	events: {
+		'blur input': function() {
+			this.trigger('blur');
+		},
+		'focus input': function() {
+			this.trigger('focus');
+		}
+	},
+
+	/**
+	 * @param {String} value
+	 */
+	setValue: function(value) {
+		this.$('input').val(value);
+	},
+
+	setFocus: function() {
+		this.$('input').focus();
+	}
 });
