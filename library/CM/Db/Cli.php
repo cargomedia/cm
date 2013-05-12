7267a0b235eaaf5b9b28d2524a0c0c43316ab3ae
Tomasz Durka <tomasz@durka.pl>
2013-05-13 01:24:38 +0200

    Add fileToDb, rename dump to dbToFile

--- library/CM/Db/Cli.php
+++ library/CM/Db/Cli.php
@@ -5,12 +5,25 @@ class CM_Db_Cli extends CM_Cli_Runnable_Abstract {
 	/**
 	 * @param string $namespace
 	 */
-	public function dump($namespace) {
+	public function dbToFile($namespace) {
 		$namespace = (string) $namespace;
 		$tables = CM_Db_Db::exec("SHOW TABLES LIKE ?", array(strtolower($namespace) . '_%'))->fetchAllColumn();
 		sort($tables);
 		$dump = CM_Db_Db::getDump($tables, true);
 		CM_File::create(CM_Util::getNamespacePath($namespace) . '/resources/db/structure.sql', $dump);
+	}
+
+	public function fileToDb() {
+		$configDb = CM_Config::get()->CM_Db_Db;
+		$client = new CM_Db_Client($configDb->server['host'], $configDb->server['port'], $configDb->username, $configDb->password);
+
+		$databaseExists = (bool) $client->createStatement('SHOW DATABASES LIKE ?')->execute(array($configDb->db))->fetch();
+		if (!$databaseExists) {
+			$client->createStatement('CREATE DATABASE ' . $client->quoteIdentifier($configDb->db))->execute();
+		}
+		foreach (CM_Util::getResourceFiles('db/structure.sql') as $dump) {
+			CM_Db_Db::runDump($configDb->db, $dump);
+		}
 	}
 
 	public function runUpdates() {
