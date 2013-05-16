ec56e7f9be26ffa45b594d4a359fa383be2de1ce
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2013-05-16 13:32:25 +0200

    Docu

--- library/CM/Model/Splittest/User.php
+++ library/CM/Model/Splittest/User.php
@@ -17,7 +17,7 @@ class CM_Model_Splittest_User extends CM_Model_Splittest {
 	 * @param CM_Model_User $user
 	 * @param float|null    $weight
 	 */
-	public function setConversion($user, $weight = null) {
+	public function setConversion(CM_Model_User $user, $weight = null) {
 		$this->_setConversion(new CM_Splittest_Fixture($user), $weight);
 	}
 }
