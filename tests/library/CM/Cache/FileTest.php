1cc9bb598b7c508e1f0991311d956b754eebe958
Tomasz Durka <tomasz@durka.pl>
2013-05-17 04:40:03 +0200

    Add CM_Cache_FileTest

---
+++ tests/library/CM/Cache/FileTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class CM_Cache_FileTest extends CMTest_TestCase {
+
+	public function testGetSet() {
+		$this->assertFalse(CM_Cache_File::get('foo'));
+
+		CM_Cache_File::set('foo', 'bar');
+		$this->assertSame('bar', CM_Cache_File::get('foo'));
+	}
+
+	public function testDelete() {
+		CM_Cache_File::set('foo', 'bar');
+		$this->assertSame('bar', CM_Cache_File::get('foo'));
+
+		CM_Cache_File::delete('foo');
+		$this->assertFalse(CM_Cache_File::get('foo'));
+	}
+
+	public function testFlush() {
+		CM_Cache_File::set('foo', 'bar');
+		$this->assertSame('bar', CM_Cache_File::get('foo'));
+
+		CM_Cache_File::flush();
+		$this->assertFalse(CM_Cache_File::get('foo'));
+	}
+}
