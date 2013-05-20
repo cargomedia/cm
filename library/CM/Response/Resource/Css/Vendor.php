082bc7a9cbd8162d2e1693cf7efd8bc5af20a2ed
Tomasz Durka <tomasz@durka.pl>
2013-05-20 17:37:17 +0200

    Refactor CM_Response_Resource_Css_* to use newly added CM_Asset_Css_*

--- library/CM/Response/Resource/Css/Vendor.php
+++ library/CM/Response/Resource/Css/Vendor.php
@@ -5,18 +5,7 @@ class CM_Response_Resource_Css_Vendor extends CM_Response_Resource_Css_Abstract 
 	protected function _process() {
 		switch ($this->getRequest()->getPath()) {
 			case '/all.css':
-				$content = '';
-				foreach (array_reverse($this->getSite()->getNamespaces()) as $namespace) {
-					$libraryPath = DIR_ROOT . CM_Bootloader::getInstance()->getNamespacePath($namespace) . 'client-vendor/';
-					foreach (CM_Util::rglob('*.css', $libraryPath) as $path) {
-						$content .= new CM_File($path);
-					}
-					foreach (CM_Util::rglob('*.less', $libraryPath) as $path) {
-						$css = new CM_Css(new CM_File($path));
-						$content .= $css->compile($this->getRender());
-					}
-				}
-				$this->_setContent($content);
+				$this->_setAsset(new CM_Asset_Css_Vendor($this->getRender(), $this->getSite()));
 				break;
 			default:
 				throw new CM_Exception_Invalid('Invalid path `' . $this->getRequest()->getPath() . '` provided', null, null, CM_Exception::WARN);
