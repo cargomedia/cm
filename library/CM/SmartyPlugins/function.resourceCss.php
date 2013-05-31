ebb1997cf1dc9f217a15e9a573b400cd2ec71e1f
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-05-31 10:36:47 +0200

    added "$urlOnly" param

--- library/CM/SmartyPlugins/function.resourceCss.php
+++ library/CM/SmartyPlugins/function.resourceCss.php
@@ -5,11 +5,15 @@ function smarty_function_resourceCss(array $params, Smarty_Internal_Template $te
 	$render = $template->smarty->getTemplateVars('render');
 	$type = (string) $params['type'];
 	$file = (string) $params['file'];
+	$urlOnly = isset($params['urlOnly']) ? (bool) $params['urlOnly'] : false;
 
 	if (!in_array($type, array('vendor', 'library'))) {
 		throw new CM_Exception_Invalid('Invalid type `' . $type . '` provided');
 	}
 
 	$url = $render->getUrlResource($type . '-css', $file);
+	if ($urlOnly) {
+		return $url;
+	}
 	return '<link rel="stylesheet" type="text/css" href="' . $url . '" />';
 }
