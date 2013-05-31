462120bbe3728b4857e4aefa44a4b47d6bfc9c32
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-05-31 15:11:13 +0200

    add param "$round"

--- tests/library/CM/SmartyPlugins/function.moneyTest.php
+++ tests/library/CM/SmartyPlugins/function.moneyTest.php
@@ -30,10 +30,27 @@ class smarty_function_moneyTest extends CMTest_TestCase {
 
 	public function testNiceDiscount() {
 		$this->_assertSame("$99.95", array('amount' => 96, 'format' => 'discount'));
-		$this->_assertSame("$17.95", array('amount' => 17, 'format' => 'discount'));
-		$this->_assertSame("$13.95", array('amount' => 13, 'format' => 'discount'));
+		$this->_assertSame("$16.95", array('amount' => 17, 'format' => 'discount'));
+		$this->_assertSame("$12.95", array('amount' => 13, 'format' => 'discount'));
 		$this->_assertSame("$1.50", array('amount' => 1.5, 'format' => 'discount'));
-		$this->_assertSame("$3.10", array('amount' => 3.1, 'format' => 'discount'));
+		$this->_assertSame("$2.95", array('amount' => 3.1, 'format' => 'discount'));
+	}
+
+	public function testRound() {
+		$this->_assertSame("$13.00", array('amount' => 13, 'round' => 0.01));
+		$this->_assertSame("$1.50", array('amount' => 1.5, 'round' => 0.01));
+		$this->_assertSame("$3.10", array('amount' => 3.1, 'round' => 0.01));
+		$this->_assertSame("$3.16", array('amount' => 3.16, 'round' => 0.01));
+
+		$this->_assertSame("$13.00", array('amount' => 13, 'round' => 0.05));
+		$this->_assertSame("$1.50", array('amount' => 1.5, 'round' => 0.05));
+		$this->_assertSame("$3.10", array('amount' => 3.1, 'round' => 0.05));
+		$this->_assertSame("$3.15", array('amount' => 3.16, 'round' => 0.05));
+
+		$this->_assertSame("$13.00", array('amount' => 13, 'round' => 0.10));
+		$this->_assertSame("$1.50", array('amount' => 1.5, 'round' => 0.10));
+		$this->_assertSame("$3.10", array('amount' => 3.1, 'round' => 0.10));
+		$this->_assertSame("$3.20", array('amount' => 3.16, 'round' => 0.10));
 	}
 
 	/**
