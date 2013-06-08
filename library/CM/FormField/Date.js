44735b7ac51d594036f8137bd769ecb4e8eb7290
Tomasz Durka <tomasz@durka.pl>
2013-06-08 19:18:53 +0200

    Add FormField_Date::setFocus()

--- library/CM/FormField/Date.js
+++ library/CM/FormField/Date.js
@@ -3,5 +3,9 @@
  * @extends CM_FormField_Abstract
  */
 var CM_FormField_Date = CM_FormField_Abstract.extend({
-	_class: 'CM_FormField_Date'
+	_class: 'CM_FormField_Date',
+
+	setFocus: function() {
+		this.$('select').first().focus();
+	}
 });
