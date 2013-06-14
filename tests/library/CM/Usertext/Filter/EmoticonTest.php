048ac39637325dcf513ec07d882a9b6ee8ad0172
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2013-06-14 14:38:08 +0200

    Refactored CM_Usertext_Filter_EmoticonTest
    - Always created emoticons on setUp(), moved data to a protected $_emoticonData array
    - Simplified the helper function _getEmoticonImg(), which retrieves the config from $_emoticonData

--- tests/library/CM/Usertext/Filter/EmoticonTest.php
+++ tests/library/CM/Usertext/Filter/EmoticonTest.php
@@ -2,67 +2,72 @@
 
 class CM_Usertext_Filter_EmoticonTest extends CMTest_TestCase {
 
+	public function setUp() {
+		$this->_mockSite = $this->getMockSite(24, null, 'http://www.default.dev');
+		foreach ($this->_emoticonData as $emoticonCode => $emoticonData) {
+			$emoticonData['code'] = $emoticonCode;
+			$this->_emoticonId[$emoticonCode] = CM_Db_Db::insert(TBL_CM_EMOTICON, $emoticonData);
+		}
+	}
+
 	public function tearDown() {
 		CMTest_TH::clearEnv();
 	}
 
 	public function testProcess() {
-		$site = $this->getMockSite(24, null, 'http://www.default.dev');
-
-		$emoticonId = CM_Db_Db::insert(TBL_CM_EMOTICON, array('code' => ':smiley:', 'codeAdditional' => ':),:-)', 'file' => '1.png'));
 		$text = 'foo :) bar :smiley:';
-		$expected = 'foo ' . $this->_getEmoticonImg(':smiley:', '1.png', $emoticonId) .
-				' bar ' . $this->_getEmoticonImg(':smiley:', '1.png', $emoticonId);
+		$expected = 'foo ' . $this->_getEmoticonImg(':smiley:') . ' bar ' . $this->_getEmoticonImg(':smiley:');
 		$filter = new CM_Usertext_Filter_Emoticon();
-		$actual = $filter->transform($text, new CM_Render($site));
+		$actual = $filter->transform($text, new CM_Render($this->_mockSite));
 
 		$this->assertSame($expected, $actual);
 	}
 
 	public function testFixedHeight() {
-		$site = $this->getMockSite(24, null, 'http://www.default.dev');
-
-		$emoticonId = CM_Db_Db::insert(TBL_CM_EMOTICON, array('code' => ':smiley:', 'codeAdditional' => ':),:-)', 'file' => '1.png'));
 		$text = 'foo :) bar :smiley:';
-		$expected = 'foo ' . $this->_getEmoticonImg(':smiley:', '1.png', $emoticonId, 16) .
-				' bar ' . $this->_getEmoticonImg(':smiley:', '1.png', $emoticonId, 16);
+		$expected = 'foo ' . $this->_getEmoticonImg(':smiley:', 16) . ' bar ' . $this->_getEmoticonImg(':smiley:', 16);
 		$filter = new CM_Usertext_Filter_Emoticon(16);
-		$actual = $filter->transform($text, new CM_Render($site));
+		$actual = $filter->transform($text, new CM_Render($this->_mockSite));
 
 		$this->assertSame($expected, $actual);
 	}
 
 	public function testFalseSmileys() {
-		$site = $this->getMockSite(24, null, 'http://www.default.dev');
-
-		$emoticonId = array();
-		foreach (array(
-					 'imp'        => array('code' => ':imp:', 'codeAdditional' => '3),3-)', 'file' => 'imp.png'),
-					 'sunglasses' => array('code' => ':sunglasses:', 'codeAdditional' => 'B-),B),8-),8)', 'file' => 'sunglasses.png'),
-					 'dizzy_face' => array('code' => ':dizzy_face:', 'codeAdditional' => '%-),%),O.o,o.O', 'file' => 'dizzy_face.png'),
-					 'innocent'   => array('code' => ':innocent:', 'codeAdditional' => 'O),o-)', 'file' => 'innocent.png'),
-				 ) as $emoticonName => $emoticonData) {
-			$emoticonId[$emoticonName] = CM_Db_Db::insert(TBL_CM_EMOTICON, $emoticonData);
-		}
-
 		$text = '(2003) (php3) (2008) (win8) (100%) (50 %) (B) (B2B) (O) (CEO) 3) 8) %) B) O)';
-		$expected = '(2003) (php3) (2008) (win8) (100%) (50 %) (B) (B2B) (O) (CEO)' .
-				' ' . $this->_getEmoticonImg(':imp:', 'imp.png', $emoticonId['imp']) .
-				' ' . $this->_getEmoticonImg(':sunglasses:', 'sunglasses.png', $emoticonId['sunglasses']) .
-				' ' . $this->_getEmoticonImg(':dizzy_face:', 'dizzy_face.png', $emoticonId['dizzy_face']) .
-				' ' . $this->_getEmoticonImg(':sunglasses:', 'sunglasses.png', $emoticonId['sunglasses']) .
-				' ' . $this->_getEmoticonImg(':innocent:', 'innocent.png', $emoticonId['innocent']);
+		$expected = '(2003) (php3) (2008) (win8) (100%) (50 %) (B) (B2B) (O) (CEO) ' .
+				$this->_getEmoticonImg(':imp:') . ' ' .
+				$this->_getEmoticonImg(':sunglasses:') . ' ' .
+				$this->_getEmoticonImg(':dizzy_face:') . ' ' .
+				$this->_getEmoticonImg(':sunglasses:') . ' ' .
+				$this->_getEmoticonImg(':innocent:');
 		$filter = new CM_Usertext_Filter_Emoticon();
-		$actual = $filter->transform($text, new CM_Render($site));
+		$actual = $filter->transform($text, new CM_Render($this->_mockSite));
 
 		$this->assertSame($expected, $actual);
 	}
 
-	protected function _getEmoticonImg($emoticonCode, $emoticonFile, $emoticonId, $height = null) {
-		$siteType = 24;
-		$siteUrl = 'http://www.default.dev';
+	protected $_emoticonData = array(
+		':smiley:'     => array('codeAdditional' => ':),:-)', 'file' => '1.png'),
+		':imp:'        => array('codeAdditional' => '3),3-)', 'file' => 'imp.png'),
+		':sunglasses:' => array('codeAdditional' => 'B-),B),8-),8)', 'file' => 'sunglasses.png'),
+		':dizzy_face:' => array('codeAdditional' => '%-),%),O.o,o.O', 'file' => 'dizzy_face.png'),
+		':innocent:'   => array('codeAdditional' => 'O),o-)', 'file' => 'innocent.png'),
+	);
+
+	/** @var  array */
+	protected $_emoticonId;
+
+	/** @var CM_Site_Abstract */
+	protected $_mockSite;
+
+	protected function _getEmoticonImg($emoticonCode, $height = null) {
+		$siteUrl = $this->_mockSite->getUrl();
+		$siteType = $this->_mockSite->getId();
+		$deployVersion = CM_App::getInstance()->getDeployVersion();
+		$emoticonFile = $this->_emoticonData[$emoticonCode]['file'];
+		$emoticonId = $this->_emoticonId[$emoticonCode];
 		$heightAttribute = $height ? ' height="' . $height . '"' : '';
-		return '<img src="' . $siteUrl . '/layout/' . $siteType . '/' . CM_App::getInstance()->getDeployVersion() . '/img/emoticon/' . $emoticonFile .
+		return '<img src="' . $siteUrl . '/layout/' . $siteType . '/' . $deployVersion . '/img/emoticon/' . $emoticonFile .
 		'" class="emoticon emoticon-' . $emoticonId . '" title="' . $emoticonCode . '"' . $heightAttribute . ' />';
 	}
 }
