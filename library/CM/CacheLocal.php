1af072c21095ef849d8130ba0eeacde38bfaf13b
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2013-06-19 15:17:32 +0200

    Coding style
    - Moved static method under non-static one in CM_CacheLocal

--- library/CM/CacheLocal.php
+++ library/CM/CacheLocal.php
@@ -2,11 +2,11 @@
 
 class CM_CacheLocal extends CM_Cache_Apc {
 
-	public static function cleanLanguages() {
-		self::flush();
-	}
-
 	protected function _delete($key) {
 		throw new CM_Exception_NotAllowed('Cannot delete keys on local cache');
+	}
+
+	public static function cleanLanguages() {
+		self::flush();
 	}
 }
