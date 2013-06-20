9c8d8816b6fe3a3453f07672b5e80ccdefe8cf43
Tomasz Durka <tomasz@durka.pl>
2013-06-20 03:33:50 +0200

    Add caching for getModules()
    - add apc fetch/store caching for getModules()

--- library/CM/Bootloader.php
+++ library/CM/Bootloader.php
@@ -183,19 +183,24 @@ class CM_Bootloader {
 	 * @return string[]
 	 */
 	public function getModules() {
-		$packages = $this->_getPackages();
-		$getNamespaces = function ($packageName) use (&$getNamespaces, $packages) {
-			$package = $packages[$packageName];
-			$namespaces = array();
-			foreach ($package['modules'] as $name => $path) {
-				$namespaces[$name] = $package['path'] . $path;
-			}
-			foreach ($package['dependencies'] as $dependencyPackageName) {
-				$namespaces = array_merge($getNamespaces($dependencyPackageName), $namespaces);
-			}
-			return $namespaces;
-		};
-		return $getNamespaces($this->_getName());
+		$cacheKey = DIR_ROOT . '_CM_Modules';
+		if (false === ($modules = apc_fetch($cacheKey))) {
+			$packages = $this->_getPackages();
+			$getNamespaces = function ($packageName) use (&$getNamespaces, $packages) {
+				$package = $packages[$packageName];
+				$namespaces = array();
+				foreach ($package['modules'] as $name => $path) {
+					$namespaces[$name] = $package['path'] . $path;
+				}
+				foreach ($package['dependencies'] as $dependencyPackageName) {
+					$namespaces = array_merge($getNamespaces($dependencyPackageName), $namespaces);
+				}
+				return $namespaces;
+			};
+			$modules = $getNamespaces($this->_getName());
+			apc_store($cacheKey, $modules);
+		}
+		return $modules;
 	}
 
 	/**
