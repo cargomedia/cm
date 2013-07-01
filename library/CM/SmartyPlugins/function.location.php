1ad607c9203571a8336bf41b827bdd8493252bed
Tomasz Durka <tomasz@durka.pl>
2013-07-01 13:03:38 +0200

    Remove link feature in CM location helper (moved to SK)

--- library/CM/SmartyPlugins/function.location.php
+++ library/CM/SmartyPlugins/function.location.php
@@ -1,14 +1,11 @@
 <?php
 
-require_once 'function.distance.php';
-
 function smarty_function_location(array $params, Smarty_Internal_Template $template) {
 	/** @var CM_Render $render */
 	$render = $template->smarty->getTemplateVars('render');
 
 	/** @var CM_Model_Location $location */
 	$location = $params['location'];
-	$showLink = !empty($params['link']);
 	$distanceFrom = isset($params['distanceFrom']) ? $location->getDistance($params['distanceFrom']) : null;
 
 	$parts = array();
@@ -16,26 +13,13 @@ function smarty_function_location(array $params, Smarty_Internal_Template $templ
 	$render = $template->smarty->getTemplateVars('render');
 
 	if ($city = $location->get(CM_Model_Location::LEVEL_CITY)) {
-		$part = $city->getName();
-		if ($showLink) {
-			$part = '<a class="nowrap" href="' . $render->getUrlPage('FB_Page_Users_Search', array('location' => $city)) . '">' . $part . '</a>';
-		}
-		$parts[] = $part;
+		$parts[] = $city->getName();
 	}
 	if ($state = $location->get(CM_Model_Location::LEVEL_STATE)) {
-		$part = $state->getName();
-		if ($showLink) {
-			$part = '<a class="nowrap" href="' . $render->getUrlPage('FB_Page_Users_Search', array('location' => $state)) . '">' . $part . '</a>';
-		}
-		$parts[] = $part;
+		$parts[] = $state->getName();
 	}
 	if ($country = $location->get(CM_Model_Location::LEVEL_COUNTRY)) {
-		$part = $country->getName();
-		if ($showLink) {
-			$part = '<a class="nowrap" href="' . $render->getUrlPage('FB_Page_Users_Search', array('location' => $country)) . '">' . $part .
-					'</a>';
-		}
-		$parts[] = $part;
+		$parts[] = $country->getName();
 	}
 	if (3 == count($parts)) {
 		if ('US' == $location->getAbbreviation(CM_Model_Location::LEVEL_COUNTRY)) {
