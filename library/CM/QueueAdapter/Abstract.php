1a9c1dc8b6be0edf5adde5dfb227e0d4409dcfcc
Alexis Peter <alexis.peter@cargomedia.ch>
2013-07-26 17:47:45 +0200

    implemented CM_QueueAdapter_Redis::pushDelayed() and -popDelayed()

--- library/CM/QueueAdapter/Abstract.php
+++ library/CM/QueueAdapter/Abstract.php
@@ -4,13 +4,27 @@ abstract class CM_QueueAdapter_Abstract {
 
 	/**
 	 * @param string $key
-	 * @param string  $value
+	 * @param string $value
 	 */
 	abstract public function push($key, $value);
+
+	/**
+	 * @param string $key
+	 * @param string $value
+	 * @param int $timestamp
+	 */
+	abstract public function pushDelayed($key, $value, $timestamp);
 
 	/**
 	 * @param string $key
 	 * @return string
 	 */
 	abstract public function pop($key);
+
+	/**
+	 * @param string $key
+	 * @param int $timestampMax
+	 * @return string
+	 */
+	abstract public function popDelayed($key, $timestampMax);
 }
--- library/CM/QueueAdapter/Redis.php
+++ library/CM/QueueAdapter/Redis.php
@@ -6,7 +6,19 @@ class CM_QueueAdapter_Redis extends CM_QueueAdapter_Abstract {
 		CM_Cache_Redis::lPush($key, $value);
 	}
 
+	public function pushDelayed($key, $value, $timestamp) {
+		CM_Cache_Redis::zAdd($key, $timestamp, $value);
+	}
+
 	public function pop($key) {
 		return CM_Cache_Redis::rPop($key);
+	}
+
+	public function popDelayed($key, $timestampMax) {
+		$value = CM_Cache_Redis::zRangeByScore($key, 0, $timestampMax, 1);
+		$value = reset($value);
+		CM_Cache_Redis::zRem($key, $value);
+		return $value;
+
 	}
 }
