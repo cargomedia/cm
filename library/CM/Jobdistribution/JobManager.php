5c03a9c8ce47696865784e3b4c941e6a020e6e9f
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2013-08-07 15:01:48 +0200

    Used CM_Process_Fork to run the worker on a fork in the job manager

--- library/CM/Jobdistribution/JobManager.php
+++ library/CM/Jobdistribution/JobManager.php
@@ -2,55 +2,12 @@
 
 class CM_Jobdistribution_JobManager extends CM_Class_Abstract {
 
-	const RESPAWN_TIMEOUT = 0.2;
-
-	/** @var array */
-	private $_children = array();
-
-	public function __construct() {
-		declare(ticks = 1);
-	}
-
 	public function start() {
-		pcntl_signal(SIGTERM, array($this, '_handleKill'), false);
-		pcntl_signal(SIGINT, array($this, '_handleKill'), false);
-		while (count($this->_children) < $this->_getConfig()->workerCount) {
-			$this->_startWorker();
-		}
-		while (true) {
-			$pid = pcntl_wait($status);
-			if (-1 === $pid) {
-				throw new CM_Exception('Waiting on child processes failed');
-			}
-			unset($this->_children[$pid]);
-			usleep(self::RESPAWN_TIMEOUT * 1000000);
-			$this->_startWorker();
-		}
-	}
+		$process = new CM_Process_Fork(1, true);
+		$process->fork();
 
-	private function _startWorker() {
-		$pid = pcntl_fork();
-		switch ($pid) {
-			case 0: //child
-				$worker = new CM_Jobdistribution_JobWorker();
-				$worker->run();
-				exit;
-			case -1: //failure
-				throw new CM_Exception('Could not fork');
-			default: //parent
-				$this->_children[$pid] = $pid;
-		}
-	}
-
-	/**
-	 * @param int $signal
-	 */
-	public function _handleKill($signal) {
-		foreach ($this->_children as $child) {
-			// To kill worker processes, need to use SIGKILL instead of $signal
-			// Probably because GEARMAN_WORKER_NON_BLOCKING is not enabled in CM_Jobdistribution_JobWorker
-			posix_kill($child, SIGKILL);
-		}
+		$worker = new CM_Jobdistribution_JobWorker();
+		$worker->run();
 		exit;
 	}
 }
