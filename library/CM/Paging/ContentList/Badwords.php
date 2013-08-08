94fb1e94c1575a08c0f486f86cb20162e7f4460a
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-08-08 13:21:55 +0200

    changing methods as discussed

--- library/CM/Paging/ContentList/Badwords.php
+++ library/CM/Paging/ContentList/Badwords.php
@@ -8,14 +8,29 @@ class CM_Paging_ContentList_Badwords extends CM_Paging_ContentList_Abstract {
 		parent::__construct(self::TYPE);
 	}
 
-	/**
-	 * @param string $badword
-	 * @return string
-	 */
-	public function transformToRegexp($badword) {
-		$badword = preg_quote($badword, '#');
-		$badword = str_replace('\*', '[^\s]*', $badword);
+	public function toRegex() {
+		$regexList = array();
+		foreach ($this as $badword) {
+			$badword = preg_quote($badword, '#');
+			$badword = str_replace('\*', '[^\s]*', $badword);
+			$regexList[] = $badword;
+		}
+		$regex = null;
+		if ($regexList) {
+			$regex = '#\b(?:' . implode('|', $regexList) . ')\b#i';
+		}
 
-		return $badword;
+		return $regex;
+	}
+
+	public function toRegexList() {
+		$regexList = array();
+		foreach ($this as $badword) {
+			$badwordRegex = preg_quote($badword, '#');
+			$badwordRegex = str_replace('\*', '[^\s]*', $badwordRegex);
+			$regexList[$badword] = '#' . $badwordRegex . '#i';
+		}
+
+		return $regexList;
 	}
 }
--- library/CM/Usertext/Filter/Badwords.php
+++ library/CM/Usertext/Filter/Badwords.php
@@ -5,21 +5,14 @@ class CM_Usertext_Filter_Badwords implements CM_Usertext_Filter_Interface {
 	public function transform($text, CM_Render $render) {
 		$text = (string) $text;
 		$cacheKey = CM_CacheConst::Usertext_Filter_BadwordRegexp;
-		if (($badwordsRegexp = CM_CacheLocal::get($cacheKey)) === false) {
-			$badwordsRegexpList = array();
-			foreach ($badwordList = new CM_Paging_ContentList_Badwords() as $badword) {
-				$badword = $badwordList->transformToRegexp($badword);
-				$badwordsRegexpList[] = $badword;
-			}
-			$badwordsRegexp = null;
-			if ($badwordsRegexpList) {
-				$badwordsRegexp = '#\b(?:' . implode('|', $badwordsRegexpList) . ')\b#i';
-			}
-			CM_CacheLocal::set($cacheKey, $badwordsRegexp);
+		if (false == ($badwordsRegex = CM_CacheLocal::get($cacheKey))) {
+			$badwordList = new CM_Paging_ContentList_Badwords;
+			$badwordsRegex = $badwordList->toRegex();
+			CM_CacheLocal::set($cacheKey, $badwordsRegex);
 		}
-		if (!$badwordsRegexp) {
+		if (!$badwordsRegex) {
 			return $text;
 		}
-		return preg_replace($badwordsRegexp, '…', $text);
+		return preg_replace($badwordsRegex, '…', $text);
 	}
 }
