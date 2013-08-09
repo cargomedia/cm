7c0ad96970ff39b408c5d815c5a2c225dd7df66c
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2013-08-09 14:15:26 +0200

    Review request
    - Removed unused "exit"

--- library/CM/Jobdistribution/Cli.php
+++ library/CM/Jobdistribution/Cli.php
@@ -8,7 +8,6 @@ class CM_Jobdistribution_Cli extends CM_Cli_Runnable_Abstract {
 	public function startManager() {
 		$worker = new CM_Jobdistribution_JobWorker();
 		$worker->run();
-		exit;
 	}
 
 	public static function getPackageName() {
