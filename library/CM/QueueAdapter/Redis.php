68690ea5eaf2eb184edc7c701d2a20f33cd6a82d
Alexis Peter <alexis.peter@cargomedia.ch>
2013-08-12 12:14:48 +0200

    prefix Queue-keys

--- library/CM/QueueAdapter/Redis.php
+++ library/CM/QueueAdapter/Redis.php
@@ -2,22 +2,27 @@
 
 class CM_QueueAdapter_Redis extends CM_QueueAdapter_Abstract {
 
+
 	public function push($key, $value) {
-		CM_Cache_Redis::lPush($key, $value);
+		CM_Cache_Redis::lPush($this->_getInternalKey($key), $value);
 	}
 
 	public function pushDelayed($key, $value, $timestamp) {
-		CM_Cache_Redis::zAdd($key, $timestamp, $value);
+		CM_Cache_Redis::zAdd($this->_getInternalKey($key), $timestamp, $value);
 	}
 
 	public function pop($key) {
-		return CM_Cache_Redis::rPop($key);
+		return CM_Cache_Redis::rPop($this->_getInternalKey($key));
 	}
 
 	public function popDelayed($key, $timestampMax) {
-		$value = CM_Cache_Redis::zRangeByScore($key, 0, $timestampMax, 1);
+		$value = CM_Cache_Redis::zRangeByScore($this->_getInternalKey($key), 0, $timestampMax, 1);
 		$value = reset($value);
-		CM_Cache_Redis::zRem($key, $value);
+		CM_Cache_Redis::zRem($this->_getInternalKey($key), $value);
 		return $value;
+	}
+
+	private function _getInternalKey($key) {
+		return 'Queue.' . (string) $key;
 	}
 }
