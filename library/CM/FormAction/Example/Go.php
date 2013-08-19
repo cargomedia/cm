5ae4f283b924578c0f336036c33a25eec18fe7f7
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-08-19 17:22:54 +0200

    use new _getRequiredFields()

--- library/CM/FormAction/Example/Go.php
+++ library/CM/FormAction/Example/Go.php
@@ -2,9 +2,8 @@
 
 class CM_FormAction_Example_Go extends CM_FormAction_Abstract {
 
-	public function setup(CM_Form_Abstract $form) {
-		$this->_fieldListRequired = array('text', 'color');
-		parent::setup($form);
+	protected function _getRequiredFields() {
+		return array('text', 'color');
 	}
 
 	protected function _process(CM_Params $params, CM_Response_View_Form $response, CM_Form_Abstract $form) {
