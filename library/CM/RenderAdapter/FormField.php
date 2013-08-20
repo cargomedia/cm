4fb36061a60abc9ba218fcd9533bde6765ab5d0b
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-08-20 17:39:01 +0200

    add fieldname to renderAdapter

--- library/CM/RenderAdapter/FormField.php
+++ library/CM/RenderAdapter/FormField.php
@@ -5,16 +5,17 @@ class CM_RenderAdapter_FormField extends CM_RenderAdapter_Abstract {
 	public function fetch(array $params = array()) {
 		/** @var CM_Form_Abstract $form */
 		$form = $params['form'];
+		$fieldname = $params['fieldname'];
 		/** @var CM_FormField_Abstract $field */
 		$field = $this->_getView();
 
 		$field->setTplParam('field', $field);
-		$field->setTplParam('id', $form->getTagAutoId($field->getName() . '-input'));
-		$field->setTplParam('name', $field->getName());
+		$field->setTplParam('id', $form->getTagAutoId($fieldname . '-input'));
+		$field->setTplParam('name', $fieldname);
 		$field->setTplParam('value', $field->getValue());
 		$field->setTplParam('options', $field->getOptions());
 
-		$html = '<div class="' . implode(' ', $field->getClassHierarchy()) . '" id="' . $form->getAutoId() . '-' . $field->getName() . '">';
+		$html = '<div class="' . implode(' ', $field->getClassHierarchy()) . '" id="' . $form->getAutoId() . '-' . $fieldname . '">';
 		$html .= trim($this->_renderTemplate('default.tpl', $field->getTplParams(), true));
 		if (!$field instanceof CM_FormField_Hidden) {
 			$html .= '<span class="messages"></span>';
