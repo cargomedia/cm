2bf790bcc9907bd66ee534496c285aa3d3a3ffd0
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-08-20 17:38:32 +0200

    move fieldname to the registerField

--- library/CM/Form/Example.php
+++ library/CM/Form/Example.php
@@ -3,18 +3,18 @@
 class CM_Form_Example extends CM_Form_Abstract {
 
 	public function setup() {
-		$this->registerField(new CM_FormField_Text('text'));
-		$this->registerField(new CM_FormField_Integer('int', -10, 20, 2));
-		$this->registerField(new CM_FormField_Distance('locationSlider'));
-		$this->registerField(new CM_FormField_Location('location', null, null, $this->getField('locationSlider')));
-		$this->registerField(new CM_FormField_File('file', 2));
-		$this->registerField(new CM_FormField_FileImage('image', 2));
-		$this->registerField(new CM_FormField_Color('color'));
-		$this->registerField(new CM_FormField_Date('date'));
-		$this->registerField(new CM_FormField_Set('set', array(1 => 'Eins', 2 => 'Zwei'), true));
-		$this->registerField(new CM_FormField_Boolean('boolean'));
-		$this->registerField(new CM_FormField_Set_Select('setSelect1', array(1 => 'Eins', 2 => 'Zwei'), true));
-		$this->registerField(new CM_FormField_Set_Select('setSelect2', array(1 => 'Eins', 2 => 'Zwei'), true));
+		$this->registerField('text', new CM_FormField_Text());
+		$this->registerField('int', new CM_FormField_Integer( -10, 20, 2));
+		$this->registerField('locationSlider', new CM_FormField_Distance());
+		$this->registerField('location', new CM_FormField_Location( null, null, 'locationSlider'));
+		$this->registerField('file', new CM_FormField_File( 2));
+		$this->registerField('image', new CM_FormField_FileImage( 2));
+		$this->registerField('color', new CM_FormField_Color());
+		$this->registerField('date', new CM_FormField_Date());
+		$this->registerField('set', new CM_FormField_Set( array(1 => 'Eins', 2 => 'Zwei'), true));
+		$this->registerField('boolean', new CM_FormField_Boolean());
+		$this->registerField('setSelect1', new CM_FormField_Set_Select( array(1 => 'Eins', 2 => 'Zwei'), true));
+		$this->registerField('setSelect2', new CM_FormField_Set_Select( array(1 => 'Eins', 2 => 'Zwei'), true));
 
 		$this->registerAction(new CM_FormAction_Example_Go($this));
 	}
--- library/CM/Form/ExampleIcon.php
+++ library/CM/Form/ExampleIcon.php
@@ -4,13 +4,13 @@ class CM_Form_ExampleIcon extends CM_Form_Abstract {
 
 	public function setup() {
 
-		$this->registerField(new CM_FormField_Integer('sizeSlider', 6, 120, 0));
-		$this->registerField(new CM_FormField_Color('colorBackground'));
-		$this->registerField(new CM_FormField_Color('color'));
-		$this->registerField(new CM_FormField_Color('shadowColor'));
-		$this->registerField(new CM_FormField_Integer('shadowX', 0, 20, 0));
-		$this->registerField(new CM_FormField_Integer('shadowY', 0, 20, 0));
-		$this->registerField(new CM_FormField_Integer('shadowBlur', 0, 20, 0));
+		$this->registerField('sizeSlider', new CM_FormField_Integer( 6, 120, 0));
+		$this->registerField('colorBackground', new CM_FormField_Color());
+		$this->registerField('color', new CM_FormField_Color());
+		$this->registerField('shadowColor', new CM_FormField_Color());
+		$this->registerField('shadowX', new CM_FormField_Integer( 0, 20, 0));
+		$this->registerField('shadowY', new CM_FormField_Integer( 0, 20, 0));
+		$this->registerField('shadowBlur', new CM_FormField_Integer( 0, 20, 0));
 	}
 
 	public function _renderStart(CM_Params $params) {
