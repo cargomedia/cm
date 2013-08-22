6b553b21bcf0bc5634349587348d9b59c80008aa
Alexis Peter <alexis.peter@cargomedia.ch>
2013-08-22 13:20:58 +0200

    Revert "add some more docu to QueueAdapter Redis"
    This reverts commit 7326f92e95f263c2bec37d6811909692bdbeebf5.

--- library/CM/QueueAdapter/Redis.php
+++ library/CM/QueueAdapter/Redis.php
@@ -5,39 +5,21 @@ class CM_QueueAdapter_Redis extends CM_QueueAdapter_Abstract {
 	/** @var CM_Cache_Redis|null */
 	private $_redis = null;
 
-	/**
-	 * @param string $key
-	 * @param string $value
-	 */
 	public function push($key, $value) {
 		$redis = $this->_getRedisInstance();
 		$redis->lPush($this->_getInternalKey($key), $value);
 	}
 
-	/**
-	 * @param string $key
-	 * @param string $value
-	 * @param int    $timestamp
-	 */
 	public function pushDelayed($key, $value, $timestamp) {
 		$redis = $this->_getRedisInstance();
 		$redis->zAdd($this->_getInternalKey($key), $timestamp, $value);
 	}
 
-	/**
-	 * @param string $key
-	 * @return string|null
-	 */
 	public function pop($key) {
 		$redis = $this->_getRedisInstance();
 		return $redis->rPop($this->_getInternalKey($key));
 	}
 
-	/**
-	 * @param string $key
-	 * @param int    $timestampMax
-	 * @return array
-	 */
 	public function popDelayed($key, $timestampMax) {
 		$redis = $this->_getRedisInstance();
 		$value = $redis->zPopRangeByScore($this->_getInternalKey($key), 0, $timestampMax);
