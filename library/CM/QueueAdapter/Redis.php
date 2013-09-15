d12d171d4f25c26b2259c0d54791af5a383a593c
Tomasz Durka <tomasz@durka.pl>
2013-09-15 16:28:34 +0200

    Refacot RedisQueue

--- library/CM/QueueAdapter/Redis.php
+++ library/CM/QueueAdapter/Redis.php
@@ -3,25 +3,29 @@
 class CM_QueueAdapter_Redis extends CM_QueueAdapter_Abstract {
 
 	/** @var CM_Redis_Client|null */
-	private $_redis = null;
+	private $_redisClient = null;
+
+	public function __construct() {
+		$this->_redisClient = CM_Redis_Client::getInstance();
+	}
 
 	public function push($key, $value) {
-		$redis = $this->_getRedisInstance();
+		$redis = $this->_getRedisClient();
 		$redis->lPush($this->_getInternalKey($key), $value);
 	}
 
 	public function pushDelayed($key, $value, $timestamp) {
-		$redis = $this->_getRedisInstance();
+		$redis = $this->_getRedisClient();
 		$redis->zAdd($this->_getInternalKey($key), $timestamp, $value);
 	}
 
 	public function pop($key) {
-		$redis = $this->_getRedisInstance();
+		$redis = $this->_getRedisClient();
 		return $redis->rPop($this->_getInternalKey($key));
 	}
 
 	public function popDelayed($key, $timestampMax) {
-		$redis = $this->_getRedisInstance();
+		$redis = $this->_getRedisClient();
 		$value = $redis->zPopRangeByScore($this->_getInternalKey($key), 0, $timestampMax);
 		return $value;
 	}
@@ -37,10 +41,7 @@ class CM_QueueAdapter_Redis extends CM_QueueAdapter_Abstract {
 	/**
 	 * @return CM_Redis_Client
 	 */
-	private function _getRedisInstance() {
-		if (null === $this->_redis) {
-			$this->_redis = new CM_Redis_Client();
-		}
-		return $this->_redis;
+	private function _getRedisClient() {
+		return $this->_redisClient;
 	}
 }
