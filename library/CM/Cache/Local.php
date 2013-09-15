93b58cb2eefeacfe5baa75fbf3ced5e493c8770f
Tomasz Durka <tomasz@durka.pl>
2013-09-15 15:19:57 +0200

    Add singletons for Cache_Shared/Local

--- library/CM/Cache/Local.php
+++ library/CM/Cache/Local.php
@@ -2,5 +2,11 @@
 
 class CM_Cache_Local extends CM_Cache_Abstract {
 
-	protected static $_storage;
+	public static function getInstance() {
+		static $instance;
+		if (!$instance) {
+			$instance = new CM_Cache_Local();
+		}
+		return $instance;
+	}
 }
