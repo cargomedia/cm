4cf1fa2d53184f9dca4403b90dea56bdd1a7a84f
Tomasz Durka <tomasz@durka.pl>
2013-09-15 15:18:57 +0200

    Repalce static access with instance

--- library/CM/Cache/Abstract.php
+++ library/CM/Cache/Abstract.php
@@ -3,17 +3,15 @@
 abstract class CM_Cache_Abstract extends CM_Class_Abstract {
 
 	/** @var CM_Cache_Storage_Abstract */
-	protected static $_storage;
+	protected $_storage;
 
-	/**
-	 * @return CM_Cache_Storage_Abstract
-	 */
-	public static function getStorage() {
-		if (!isset(static::$_storage)) {
-			$storageClassName = static::_getConfig()->storage;
-			static::$_storage = new $storageClassName();
-		}
-		return static::$_storage;
+	/** @var CM_Cache_Storage_Runtime */
+	protected $_runtime;
+
+	public function __construct() {
+		$storageClassName = static::_getConfig()->storage;
+		$this->_storage = new $storageClassName();
+		$this->_runtime = CM_Cache_Storage_Runtime::getInstance();
 	}
 
 	/**
@@ -21,25 +19,24 @@ abstract class CM_Cache_Abstract extends CM_Class_Abstract {
 	 * @param mixed    $value
 	 * @param int|null $lifeTime
 	 */
-	public static final function set($key, $value, $lifeTime = null) {
+	public final function set($key, $value, $lifeTime = null) {
 		if (!$lifeTime) {
 			$lifeTime = static::_getConfig()->lifetime;
 		}
-		static::getStorage()->set($key, $value, $lifeTime);
-		static::_getRuntimeStorage()->set($key, $value, $lifeTime);
+		$this->_storage->set($key, $value, $lifeTime);
+		$this->_runtime->set($key, $value, $lifeTime);
 	}
 
 	/**
 	 * @param string $key
 	 * @return mixed|false
 	 */
-	public static final function get($key) {
-		$runtimeStorage = static::_getRuntimeStorage();
-		if (false !== ($value = $runtimeStorage->get($key))) {
+	public final function get($key) {
+		if (false !== ($value = $this->_runtime->get($key))) {
 			return $value;
 		}
-		if (false !== ($value = static::getStorage()->get($key))) {
-			$runtimeStorage->set($key, $value);
+		if (false !== ($value = $this->_storage->get($key))) {
+			$this->_runtime->set($key, $value);
 		}
 		return $value;
 	}
@@ -47,14 +44,14 @@ abstract class CM_Cache_Abstract extends CM_Class_Abstract {
 	/**
 	 * @param string $key
 	 */
-	public static final function delete($key) {
-		static::_getRuntimeStorage()->delete($key);
-		static::getStorage()->delete($key);
+	public final function delete($key) {
+		$this->_runtime->delete($key);
+		$this->_storage->delete($key);
 	}
 
-	public static final function flush() {
-		static::_getRuntimeStorage()->flush();
-		static::getStorage()->flush();
+	public final function flush() {
+		$this->_runtime->flush();
+		$this->_storage->flush();
 	}
 
 	/**
@@ -64,9 +61,9 @@ abstract class CM_Cache_Abstract extends CM_Class_Abstract {
 	 * @param int    $lifeTime
 	 * @return boolean
 	 */
-	public static final function setTagged($tag, $key, $data, $lifeTime = null) {
-		$key = $key . '_tag:' . $tag . '_tagVersion:' . static::_getTagVersion($tag);
-		static::set($key, $data, $lifeTime);
+	public final function setTagged($tag, $key, $data, $lifeTime = null) {
+		$key = $key . '_tag:' . $tag . '_tagVersion:' . $this->_getTagVersion($tag);
+		$this->set($key, $data, $lifeTime);
 	}
 
 	/**
@@ -74,24 +71,24 @@ abstract class CM_Cache_Abstract extends CM_Class_Abstract {
 	 * @param string $key
 	 * @return mixed Result or false
 	 */
-	public static final function getTagged($tag, $key) {
-		$key = $key . '_tag:' . $tag . '_tagVersion:' . static::_getTagVersion($tag);
-		return static::get($key);
+	public final function getTagged($tag, $key) {
+		$key = $key . '_tag:' . $tag . '_tagVersion:' . $this->_getTagVersion($tag);
+		return $this->get($key);
 	}
 
 	/**
 	 * @param string $tag
 	 * @return boolean
 	 */
-	public static final function deleteTag($tag) {
-		static::delete(CM_CacheConst::Tag_Version . '_tag:' . $tag);
+	public final function deleteTag($tag) {
+		$this->delete(CM_CacheConst::Tag_Version . '_tag:' . $tag);
 	}
 
 	/**
 	 * @param mixed $keyPart ...
 	 * @return string
 	 */
-	public static final function key($keyPart) {
+	public final function key($keyPart) {
 		$parts = func_get_args();
 		foreach ($parts as &$part) {
 			if (!is_scalar($part)) {
@@ -105,19 +102,12 @@ abstract class CM_Cache_Abstract extends CM_Class_Abstract {
 	 * @param string $tag
 	 * @return string
 	 */
-	private static final function _getTagVersion($tag) {
+	private final function _getTagVersion($tag) {
 		$cacheKey = CM_CacheConst::Tag_Version . '_tag:' . $tag;
-		if (($tagVersion = static::get($cacheKey)) === false) {
+		if (($tagVersion = $this->get($cacheKey)) === false) {
 			$tagVersion = md5(rand() . uniqid());
-			static::set($cacheKey, $tagVersion);
+			$this->set($cacheKey, $tagVersion);
 		}
 		return $tagVersion;
-	}
-
-	/**
-	 * @return CM_Cache_Storage_Runtime
-	 */
-	private static final function _getRuntimeStorage() {
-		return CM_Cache_Storage_Runtime::getInstance();
 	}
 }
