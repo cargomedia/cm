211905210d359cd441c7f45d80c71d87dbf04824
Reto Kaiser <reto@cargomedia.ch>
2013-09-16 16:13:53 +0200

    Add test for adding and getting log items

---
+++ tests/library/CM/Paging/Log/AbstractTest.php
@@ -0,0 +1,25 @@
+<?php
+
+class CM_Paging_Log_AbstractTest extends CMTest_TestCase {
+
+	public function testAddGet() {
+		$paging = $this->getMockBuilder('CM_Paging_Log_Abstract')->setMethods(array('getType'))
+				->disableOriginalConstructor()->getMockForAbstractClass();
+		$paging->expects($this->any())->method('getType')->will($this->returnValue(14));
+		/** @var CM_Paging_Log_Abstract $paging */
+		$paging->__construct();
+
+		$add = CMTest_TH::getProtectedMethod('CM_Paging_Log_Abstract', '_add');
+		$add->invoke($paging, 'foo');
+		$add->invoke($paging, 'bar', array('meta1' => 12));
+
+		$items = $paging->getItems();
+		$this->assertSame(2, count($items));
+
+		$this->assertSame('bar', $items[0]['msg']);
+		$this->assertSame(array('meta1' => 12), $items[0]['metaInfo']);
+
+		$this->assertSame('foo', $items[1]['msg']);
+		$this->assertSame(null, $items[1]['metaInfo']);
+	}
+}
