d7346464c91a731ee2fd4d7499f0da0c82cb8bff
Reto Kaiser <reto@cargomedia.ch>
2013-09-16 16:21:23 +0200

    Catch unserialize problem on log entries, add test for it

--- tests/library/CM/Paging/Log/AbstractTest.php
+++ tests/library/CM/Paging/Log/AbstractTest.php
@@ -2,6 +2,10 @@
 
 class CM_Paging_Log_AbstractTest extends CMTest_TestCase {
 
+	public function tearDown() {
+		CMTest_TH::clearEnv();
+	}
+
 	public function testAddGet() {
 		$paging = $this->getMockBuilder('CM_Paging_Log_Abstract')->setMethods(array('getType'))
 				->disableOriginalConstructor()->getMockForAbstractClass();
@@ -21,5 +25,23 @@ class CM_Paging_Log_AbstractTest extends CMTest_TestCase {
 
 		$this->assertSame('foo', $items[1]['msg']);
 		$this->assertSame(null, $items[1]['metaInfo']);
+	}
+
+	public function testGetInvalidMetaInfo() {
+		$paging = $this->getMockBuilder('CM_Paging_Log_Abstract')->setMethods(array('getType'))
+				->disableOriginalConstructor()->getMockForAbstractClass();
+		$paging->expects($this->any())->method('getType')->will($this->returnValue(14));
+		/** @var CM_Paging_Log_Abstract $paging */
+		$paging->__construct();
+
+		$add = CMTest_TH::getProtectedMethod('CM_Paging_Log_Abstract', '_add');
+		$stringTooLongForDb = str_repeat('x', 99999);
+		$add->invoke($paging, 'foo', array('meta1' => $stringTooLongForDb));
+
+		$items = $paging->getItems();
+		$this->assertSame(1, count($items));
+
+		$this->assertSame('foo', $items[0]['msg']);
+		$this->assertSame(null, $items[0]['metaInfo']);
 	}
 }
