84b1392b5e32667f30e827554006cc81f877be55
Tomasz Durka <tomasz@durka.pl>
2013-09-18 14:22:47 +0200

    Add constraint name

--- resources/db/update/18.php
+++ resources/db/update/18.php
@@ -12,11 +12,11 @@ if (!$isInnoDb('cm_streamChannel')) {
 if (!$isInnoDb('cm_stream_publish')) {
 	CM_Db_Db::exec("TRUNCATE TABLE  `cm_stream_publish`");
 	CM_Db_Db::exec("ALTER TABLE  `cm_stream_publish` ENGINE = INNODB");
-	CM_Db_Db::exec("ALTER TABLE  `cm_stream_publish` ADD FOREIGN KEY (  `channelId` ) REFERENCES  `skadate`.`cm_streamChannel` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;");
+	CM_Db_Db::exec("ALTER TABLE  `cm_stream_publish` ADD CONSTRAINT `cm_stream_publish-cm_streamChannel` FOREIGN KEY (  `channelId` ) REFERENCES  `skadate`.`cm_streamChannel` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;");
 }
 if (!$isInnoDb('cm_stream_subscribe')) {
 	CM_Db_Db::exec("TRUNCATE TABLE  `cm_stream_subscribe`");
 	CM_Db_Db::exec("ALTER TABLE  `cm_stream_subscribe` ENGINE = INNODB");
-	CM_Db_Db::exec("ALTER TABLE  `cm_stream_subscribe` ADD FOREIGN KEY (  `channelId` ) REFERENCES  `skadate`.`cm_streamChannel` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT ;");
+	CM_Db_Db::exec("ALTER TABLE  `cm_stream_subscribe` ADD CONSTRAINT `cm_stream_subscribe-cm_streamChannel` FOREIGN KEY (  `channelId` ) REFERENCES  `skadate`.`cm_streamChannel` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT ;");
 }
 
