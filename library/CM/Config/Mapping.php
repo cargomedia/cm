34670b5276f2fee4da2af0232db012826cc2dca6
Alexis Peter <alexis.peter@cargomedia.ch>
2013-09-19 19:53:18 +0200

    add real mapping

--- library/CM/Config/Mapping.php
+++ library/CM/Config/Mapping.php
@@ -20,8 +20,20 @@ class CM_Config_Mapping extends CM_Class_Abstract {
 	 */
 	protected function _getMapping() {
 		return array(
-			'foo' => 'CM_Foo',
-			'foobar' => 'CM_FooBar'
+			'Testhelper'=> 'CMTest_TH',
+			'Render' => 'CM_Render',
+			'Mail' => 'CM_Mail',
+			'Tracking' => 'CM_Tracking_Abstract',
+			'Newrelic' => 'CMService_Newrelic',
+			'Redis' => 'CM_Cache_Redis',
+			'SocketRedis' => 'CM_Stream_Adapter_Message_SocketRedis',
+			'Search' => 'CM_Search',
+			'Memcache' => 'CM_Cache_Memcache',
+			'Database' => 'CM_Db_Db',
+			'Wowza' => 'CM_Stream_Video',
+			'JobManager' => 'CM_Jobdistribution_JobManager',
+			'JobWorker' => 'CM_Jobdistribution_JobWorker',
+			'Job' => 'CM_Jobdistribution_Job_Abstract'
 		);
 	}
 
