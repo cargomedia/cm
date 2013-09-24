4155945ae5a9ccbbb033d3f236174d5bd43fd5c7
Tomasz Durka <tomasz@durka.pl>
2013-09-24 14:58:36 +0200

    Add ExceptionHandling formatter

---
+++ library/CM/ExceptionHandling/Formatter/Plain.php
@@ -0,0 +1,22 @@
+<?php
+
+class CM_ExceptionHandling_Formatter_Plain extends CM_ExceptionHandling_Formatter_Abstract {
+
+	public function getHeader(CM_ExceptionHandling_SerializedException $exception) {
+		return $exception->getClass() . ': ' . $exception->getMessage() . ' in ' . $exception->getFile() . ' on line ' . $exception->getLine() . PHP_EOL;
+	}
+
+	public function getTrace(CM_ExceptionHandling_SerializedException $exception) {
+		$traceString = '';
+		$indent = strlen(count($exception->trace)) + 4;
+		foreach ($exception->trace as $number => $entry) {
+			$traceString .= str_pad($number, $indent, ' ', STR_PAD_LEFT) . '. ';
+			$traceString .= $entry['code'] . ' ' . $entry['file'];
+			if (null !== $entry['line']) {
+				$traceString .= ':' . $entry['line'];
+			}
+			$traceString .= PHP_EOL;
+		}
+		return $traceString;
+	}
+}
