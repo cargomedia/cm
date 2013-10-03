a45806aa3683732d0bb7bdd0f8e2ee70329e1ae4
Alexis Peter <alexis.peter@cargomedia.ch>
2013-10-03 15:40:09 +0200

    fmake CM_Config_Generator testable again

--- library/CM/Config/Generator.php
+++ library/CM/Config/Generator.php
@@ -18,7 +18,7 @@ class CM_Config_Generator extends CM_Class_Abstract {
 	public function generateOutput() {
 		$entryList = CM_Params::decode($this->_source->read(), true);
 		$output = '<?php' . PHP_EOL;
-		$mapping = new CM_Config_Mapping();
+		$mapping = $this->_getMapping();
 		foreach ($entryList as $key => $value) {
 			if (is_array($value)) {
 				$keyMapped = $mapping->getConfigKey($key);
@@ -30,5 +30,12 @@ class CM_Config_Generator extends CM_Class_Abstract {
 			}
 		}
 		return $output;
+	}
+
+	/**
+	 * @return CM_Config_Mapping
+	 */
+	protected function _getMapping() {
+		return new CM_Config_Mapping();
 	}
 }
