05a96f114be8b196edbbe0fe34488717cfb0d99d
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2013-10-07 13:35:48 +0200

    Added filtering and ordering functionality to CM_PagingSource_Array

--- library/CM/PagingSource/Array.php
+++ library/CM/PagingSource/Array.php
@@ -8,10 +8,30 @@ class CM_PagingSource_Array extends CM_PagingSource_Abstract {
 	private $_data;
 
 	/**
-	 * @param array $data
+	 * @param array        $data
+	 * @param Closure|null $filter    Callback function returning true for items to keep
+	 * @param Closure|null $sortBy    Callback function returning the value to sort by for each item
+	 * @param int|null     $sortOrder Either SORT_ASC or SORT_DESC
+	 * @param int|null     $sortFlags Sort options, @see array_multisort()
 	 */
-	public function __construct(array $data) {
+	public function __construct(array $data, Closure $filter = null, Closure $sortBy = null, $sortOrder = null, $sortFlags = null) {
 		$this->_data = $data;
+		if (null !== $filter) {
+			$this->_data = array_filter($this->_data, $filter);
+		}
+		if (null !== $sortBy) {
+			$sortArray = array();
+			foreach ($this->_data as $key => $item) {
+				$sortArray[$key] = $sortBy($item);
+			}
+			if (null === $sortOrder) {
+				$sortOrder = SORT_ASC;
+			}
+			if (null === $sortFlags) {
+				$sortFlags = SORT_REGULAR;
+			}
+			array_multisort($sortArray, $sortOrder, $sortFlags, $this->_data);
+		}
 	}
 
 	public function getCount($offset = null, $count = null) {
@@ -25,5 +45,4 @@ class CM_PagingSource_Array extends CM_PagingSource_Abstract {
 	protected function _cacheKeyBase() {
 		throw new CM_Exception_Invalid('`' . __CLASS__ . '` does not support caching.');
 	}
-
 }
--- tests/library/CM/PagingSource/ArrayTest.php
+++ tests/library/CM/PagingSource/ArrayTest.php
@@ -2,6 +2,25 @@
 
 class CM_PagingSource_ArrayTest extends CMTest_TestCase {
 
+	public function testFilter() {
+		$pagingSource = new CM_PagingSource_Array(range(1, 10), function ($item) {
+			return $item % 2 === 0;
+		});
+		$this->assertSame(array(2, 4, 6, 8, 10), $pagingSource->getItems());
+	}
+
+	public function testFilterSort() {
+		$items = range(1, 10);
+		shuffle($items);
+
+		$pagingSource = new CM_PagingSource_Array($items, function ($item) {
+			return $item % 3 === 1;
+		}, function ($item) {
+			return $item;
+		}, SORT_DESC, SORT_STRING);
+		$this->assertSame(array(7, 4, 10, 1), $pagingSource->getItems());
+	}
+
 	public function testGetCount() {
 		$pagingSource = new CM_PagingSource_Array(range(1, 10));
 		$this->assertSame(10, $pagingSource->getCount());
@@ -9,19 +28,42 @@ class CM_PagingSource_ArrayTest extends CMTest_TestCase {
 
 		$pagingSource = new CM_PagingSource_Array(array());
 		$this->assertSame(0, $pagingSource->getCount());
-
 	}
 
 	public function testGetItems() {
 		$pagingSource = new CM_PagingSource_Array(range(1, 10));
-		$this->assertSame(range(1,10), $pagingSource->getItems());
-		$this->assertSame(range(6,10), $pagingSource->getItems(5));
-		$this->assertSame(range(1,5), $pagingSource->getItems(null, 5));
-		$this->assertSame(range(4,8), $pagingSource->getItems(3,5));
+		$this->assertSame(range(1, 10), $pagingSource->getItems());
+		$this->assertSame(range(6, 10), $pagingSource->getItems(5));
+		$this->assertSame(range(1, 5), $pagingSource->getItems(null, 5));
+		$this->assertSame(range(4, 8), $pagingSource->getItems(3, 5));
 
 		$pagingSource = new CM_PagingSource_Array(array());
 		$this->assertSame(array(), $pagingSource->getItems());
 		$this->assertSame(array(), $pagingSource->getItems(2));
 	}
 
+	public function testSort() {
+		$items = range(1, 10);
+		shuffle($items);
+
+		$pagingSource = new CM_PagingSource_Array($items, null, function ($item) {
+			return $item;
+		});
+		$this->assertSame(range(1, 10), $pagingSource->getItems());
+
+		$pagingSource = new CM_PagingSource_Array($items, null, function ($item) {
+			return $item;
+		}, SORT_DESC);
+		$this->assertSame(range(10, 1), $pagingSource->getItems());
+
+		$pagingSource = new CM_PagingSource_Array($items, null, function ($item) {
+			return $item;
+		}, null, SORT_STRING);
+		$this->assertSame(array(1, 10, 2, 3, 4, 5, 6, 7, 8, 9), $pagingSource->getItems());
+
+		$pagingSource = new CM_PagingSource_Array($items, null, function ($item) {
+			return $item;
+		}, SORT_DESC, SORT_STRING);
+		$this->assertSame(array(9, 8, 7, 6, 5, 4, 3, 2, 10, 1), $pagingSource->getItems());
+	}
 }
