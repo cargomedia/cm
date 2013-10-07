fb6852bf3bf7151d40af361bf61a91b3a4dc480b
Tomasz Durka <tomasz@durka.pl>
2013-10-07 17:29:24 +0200

    Adjust tests

--- tests/library/CM/Cache/AbstractTest.php
+++ tests/library/CM/Cache/AbstractTest.php
@@ -7,37 +7,42 @@ class CM_Cache_AbstractTest extends CMTest_TestCase {
 	}
 
 	public function testGetSet() {
-		$storageAdapter = $this->getMockBuilder('CM_Cache_Storage_Abstract')
+		$cacheStorage = $this->getMockBuilder('CM_Cache_Storage_Abstract')
 				->setMethods(array('_set', '_get', '_getKeyArmored'))->getMockForAbstractClass();
-		$storageAdapter->expects($this->any())->method('_getKeyArmored')->will($this->returnCallback(function ($key) {
+		$cacheStorage->expects($this->any())->method('_getKeyArmored')->will($this->returnCallback(function ($key) {
 			return 'armor-' . $key;
 		}));
-		$storageAdapter->expects($this->exactly(2))->method('_get')->with('armor-foo')->will($this->onConsecutiveCalls(false, 'cached-bar'));
-		$storageAdapter->expects($this->once())->method('_set')->with('armor-foo', 'bar', 100);
+		$cacheStorage->expects($this->exactly(2))->method('_get')->with('armor-foo')->will($this->onConsecutiveCalls(false, 'cached-bar'));
+		$cacheStorage->expects($this->once())->method('_set')->with('armor-foo', 'bar', 100);
 
-		$cache = $this->getMockBuilder('CM_Cache_Abstract')->setMethods(array('_getStorage'))->disableOriginalConstructor()->getMockForAbstractClass();
-		$cache->expects($this->any())->method('_getStorage')->will($this->returnValue($storageAdapter));
+		$cacheRuntime = new CM_Cache_Storage_Runtime();
+
+		$cache = $this->getMockBuilder('CM_Cache_Abstract')->setMethods(array('_getStorage', '_getRuntime'))->disableOriginalConstructor()->getMockForAbstractClass();
+		$cache->expects($this->any())->method('_getStorage')->will($this->returnValue($cacheStorage));
+		$cache->expects($this->any())->method('_getRuntime')->will($this->returnValue($cacheRuntime));
 		/** @var CM_Cache_Abstract $cache */
 		$this->assertFalse($cache->get('foo'));
 		$cache->set('foo', 'bar', 100);
 		$this->assertSame('bar', $cache->get('foo'));
-		CM_Cache_Storage_Runtime::getInstance()->flush();
+		$cacheRuntime->flush();
 		$this->assertSame('cached-bar', $cache->get('foo'));
 	}
 
 	public function testRuntime() {
-		$storageAdapter = $this->getMockBuilder('CM_Cache_Storage_Abstract')->setMethods(array('_set', '_get'))->getMockForAbstractClass();
-		$storageAdapter->expects($this->any())->method('_get')->will($this->returnValue('bar'));
+		$cacheStorage = $this->getMockBuilder('CM_Cache_Storage_Abstract')->setMethods(array('_set', '_get'))->getMockForAbstractClass();
+		$cacheStorage->expects($this->any())->method('_get')->will($this->returnValue('bar'));
 
-		$cache = $this->getMockBuilder('CM_Cache_Abstract')->setMethods(array('_getStorage'))->disableOriginalConstructor()->getMockForAbstractClass();
-		$cache->expects($this->any())->method('_getStorage')->will($this->returnValue($storageAdapter));
+		$cacheRuntime = new CM_Cache_Storage_Runtime();
+
+		$cache = $this->getMockBuilder('CM_Cache_Abstract')->setMethods(array('_getStorage', '_getRuntime'))->disableOriginalConstructor()->getMockForAbstractClass();
+		$cache->expects($this->any())->method('_getStorage')->will($this->returnValue($cacheStorage));
+		$cache->expects($this->any())->method('_getRuntime')->will($this->returnValue($cacheRuntime));
 		/** @var CM_Cache_Abstract $cache */
 
-		$runtimeCache = CM_Cache_Storage_Runtime::getInstance();
-		$this->assertFalse($runtimeCache->get('foo'));
+		$this->assertFalse($cacheRuntime->get('foo'));
 		$cache->get('foo');
-		$this->assertSame('bar', $runtimeCache->get('foo'));
+		$this->assertSame('bar', $cacheRuntime->get('foo'));
 		$cache->set('foo', 'zoo', 100);
-		$this->assertSame('zoo', $runtimeCache->get('foo'));
+		$this->assertSame('zoo', $cacheRuntime->get('foo'));
 	}
 }
