c86a4427cd91999aa136a506f6f37e65ad8c27ee
Tomasz Durka <tomasz@durka.pl>
2013-10-07 16:47:41 +0200

    Docu

--- library/CM/Cache/Local.php
+++ library/CM/Cache/Local.php
@@ -2,6 +2,9 @@
 
 class CM_Cache_Local extends CM_Cache_Abstract {
 
+	/**
+	 * @return CM_Cache_Local
+	 */
 	public static function getInstance() {
 		static $instance;
 		if (!$instance) {
--- library/CM/Cache/Shared.php
+++ library/CM/Cache/Shared.php
@@ -2,6 +2,9 @@
 
 class CM_Cache_Shared extends CM_Cache_Abstract {
 
+	/**
+	 * @return CM_Cache_Shared
+	 */
 	public static function getInstance() {
 		static $instance;
 		if (!$instance) {
