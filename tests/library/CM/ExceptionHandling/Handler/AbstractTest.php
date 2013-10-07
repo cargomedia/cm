dd578d11c68e71e102e375e0b4ffe2fcb5308f08
Tomasz Durka <tomasz@durka.pl>
2013-10-07 16:30:58 +0200

    Add print test

--- tests/library/CM/ExceptionHandling/Handler/AbstractTest.php
+++ tests/library/CM/ExceptionHandling/Handler/AbstractTest.php
@@ -34,6 +34,41 @@ class CM_ExceptionHandling_Handler_AbstractTest extends CMTest_TestCase {
 		$this->assertContains('### Original Exception: ', $logContents);
 	}
 
+	public function testPrintException() {
+		$errorException = new CM_Exception();
+		$nativeException = new Exception();
+		$fatalException = new CM_Exception(null, null, null, CM_Exception::FATAL);
+
+		$exceptionHandler = $this->getMockBuilder('CM_ExceptionHandling_Handler_Abstract')
+				->setMethods(array('_logException', '_printException'))->getMockForAbstractClass();
+		$exceptionHandler->expects($this->at(1))->method('_printException')->with($errorException);
+		$exceptionHandler->expects($this->at(3))->method('_printException')->with($nativeException);
+		$exceptionHandler->expects($this->at(5))->method('_printException')->with($fatalException);
+		/** @var CM_ExceptionHandling_Handler_Abstract $exceptionHandler */
+
+		$exceptionHandler->handleException($errorException);
+		$exceptionHandler->handleException($nativeException);
+		$exceptionHandler->handleException($fatalException);
+	}
+
+	public function testPrintExceptionPrintSeverity() {
+		$errorException = new CM_Exception();
+		$nativeException = new Exception();
+		$fatalException = new CM_Exception(null, null, null, CM_Exception::FATAL);
+
+		$exceptionHandler = $this->getMockBuilder('CM_ExceptionHandling_Handler_Abstract')
+				->setMethods(array('_logException', '_printException'))->getMockForAbstractClass();
+		$exceptionHandler->expects($this->any())->method('_logException');
+		$exceptionHandler->expects($this->at(2))->method('_printException')->with($nativeException);
+		$exceptionHandler->expects($this->at(4))->method('_printException')->with($fatalException);
+		/** @var CM_ExceptionHandling_Handler_Abstract $exceptionHandler */
+
+		$exceptionHandler->setPrintSeverityMin(CM_Exception::FATAL);
+		$exceptionHandler->handleException($errorException);
+		$exceptionHandler->handleException($nativeException);
+		$exceptionHandler->handleException($fatalException);
+	}
+
 	/**
 	 * @expectedException ErrorException
 	 * @expectedExceptionMessage E_USER_ERROR: Raw error message
