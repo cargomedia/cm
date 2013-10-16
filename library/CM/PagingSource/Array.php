cee84dded72bfec1dc6aadd934448c8b8fd5e903
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2013-10-16 08:08:23 +0200

    Used lazy loading for paging source items

--- library/CM/PagingSource/Array.php
+++ library/CM/PagingSource/Array.php
@@ -2,10 +2,17 @@
 
 class CM_PagingSource_Array extends CM_PagingSource_Abstract {
 
-	/**
-	 * @var array
-	 */
+	/** @var Closure|null */
+	private $_closureFilter, $_closureSortBy;
+
+	/** @var array */
 	private $_data;
+
+	/** @var array|null */
+	private $_dataRaw;
+
+	/** @var int|null */
+	private $_sortFlags, $_sortOrder;
 
 	/** @var CM_PagingSource_Abstract|null */
 	private $_source;
@@ -21,27 +28,22 @@ class CM_PagingSource_Array extends CM_PagingSource_Abstract {
 	public function __construct($data, Closure $filter = null, Closure $sortBy = null, $sortOrder = null, $sortFlags = null) {
 		if ($data instanceof CM_PagingSource_Abstract) {
 			$this->_source = $data;
-			$data = $this->_source->getItems();
-		}
-		if (!is_array($data)) {
+		} elseif (is_array($data)) {
+			$this->_dataRaw = $data;
+		} else {
 			throw new CM_Exception_Invalid('Paging data should be either an array or a paging source.');
 		}
-		$this->_data = $data;
-		if (null !== $filter) {
-			$this->_data = array_filter($this->_data, $filter);
-		}
+		$this->_closureFilter = $filter;
+		$this->_closureSortBy = $sortBy;
 		if (null !== $sortBy) {
-			$sortArray = array();
-			foreach ($this->_data as $key => $item) {
-				$sortArray[$key] = $sortBy($item);
-			}
 			if (null === $sortOrder) {
 				$sortOrder = SORT_ASC;
 			}
 			if (null === $sortFlags) {
 				$sortFlags = SORT_REGULAR;
 			}
-			array_multisort($sortArray, $sortOrder, $sortFlags, $this->_data);
+			$this->_sortOrder = (int) $sortOrder;
+			$this->_sortFlags = (int) $sortFlags;
 		}
 	}
 
@@ -54,14 +56,36 @@ class CM_PagingSource_Array extends CM_PagingSource_Abstract {
 	}
 
 	public function getCount($offset = null, $count = null) {
-		return count($this->_data);
+		return count($this->getItems());
 	}
 
 	public function getItems($offset = null, $count = null) {
-		return array_slice($this->_data, $offset, $count);
+		return array_slice($this->_getData(), $offset, $count);
 	}
 
 	protected function _cacheKeyBase() {
 		throw new CM_Exception_Invalid('`' . __CLASS__ . '` does not support caching.');
+	}
+
+	protected function _getData() {
+		if (isset($this->_data)) {
+			return $this->_data;
+		}
+		if (!isset($this->_dataRaw) && $this->_source) {
+			$this->_dataRaw = $this->_source->getItems();
+		}
+		$this->_data = $this->_dataRaw;
+		if (null !== $this->_closureFilter) {
+			$this->_data = array_filter($this->_data, $this->_closureFilter);
+		}
+		if (null !== $this->_closureSortBy) {
+			$sortArray = array();
+			foreach ($this->_data as $key => $item) {
+				$sortArray[$key] = call_user_func($this->_closureSortBy, $item);
+			}
+			array_multisort($sortArray, $this->_sortOrder, $this->_sortFlags, $this->_data);
+		}
+		unset($this->_dataRaw);
+		return $this->_data;
 	}
 }
