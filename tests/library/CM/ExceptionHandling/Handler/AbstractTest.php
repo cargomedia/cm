de5db3382c47c48cd29030c9612d5a66f61c9f4b
Tomasz Durka <tomasz@durka.pl>
2013-10-27 03:40:46 +0100

    Error_log adjustments

--- tests/library/CM/ExceptionHandling/Handler/AbstractTest.php
+++ tests/library/CM/ExceptionHandling/Handler/AbstractTest.php
@@ -15,20 +15,21 @@ class CM_ExceptionHandling_Handler_AbstractTest extends CMTest_TestCase {
 	}
 
 	public function testLogExceptionFileLog() {
+		$errorLog = DIR_DATA_LOG . 'error.log';
+
 		$log = $this->getMockBuilder('CM_Paging_Log_Error')->setMethods(array('add'))->disableOriginalConstructor()->getMock();
 		$log->expects($this->any())->method('add')->will($this->throwException(new Exception('foo')));
 
 		$exception = $this->getMockBuilder('CM_Exception')->setMethods(array('getLog'))->disableOriginalConstructor()->getMock();
 		$exception->expects($this->any())->method('getLog')->will($this->returnValue($log));
 
-		CM_Util::mkDir(DIR_DATA_LOG);
-		file_put_contents(DIR_DATA_LOG . 'error.log', '');
+		$this->assertFileNotExists($errorLog);
 
 		$method = CMTest_TH::getProtectedMethod('CM_ExceptionHandling_Handler_Abstract', '_logException');
 		$exceptionHandler = $this->getMockBuilder('CM_ExceptionHandling_Handler_Abstract')->getMockForAbstractClass();
 		$method->invoke($exceptionHandler, $exception);
 
-		$logContents = file_get_contents(DIR_DATA_LOG . 'error.log');
+		$logContents = file_get_contents($errorLog);
 		$this->assertNotEmpty($logContents);
 		$this->assertContains('### Cannot log error: ', $logContents);
 		$this->assertContains('### Original Exception: ', $logContents);
