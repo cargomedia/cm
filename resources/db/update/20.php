5ae55a3cd05bca5b4c73f42498c7db7f2d01e11e
Marian Sollmann <marian.sollmann@cargomedia.ch>
2013-11-19 16:10:51 +0100

    skadate is not needed

--- resources/db/update/20.php
+++ resources/db/update/20.php
@@ -8,5 +8,5 @@ $isInnoDb = function ($tableName) {
 if (!$isInnoDb('cm_streamChannel_video')) {
 	CM_Db_Db::exec("DELETE v FROM `cm_streamChannel_video` AS v LEFT JOIN `cm_streamChannel` AS s using(`id`) where `s`.`id` IS NULL");
 	CM_Db_Db::exec("ALTER TABLE `cm_streamChannel_video` ENGINE = INNODB");
-	CM_Db_Db::exec("ALTER TABLE `cm_streamChannel_video` ADD CONSTRAINT `cm_streamChannel_video-cm_streamChannel` FOREIGN KEY (`id`) REFERENCES `skadate`.`cm_streamChannel` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;");
+	CM_Db_Db::exec("ALTER TABLE `cm_streamChannel_video` ADD CONSTRAINT `cm_streamChannel_video-cm_streamChannel` FOREIGN KEY (`id`) REFERENCES `cm_streamChannel` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;");
 }
