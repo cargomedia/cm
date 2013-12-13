c3cd4a83feccae5351ea1f0b407471636c31aed1
Tomasz Durka <tomasz@durka.pl>
2013-12-13 03:33:07 +0100

    Rename run -> start

--- library/CM/Maintenance/Cli.php
+++ library/CM/Maintenance/Cli.php
@@ -79,7 +79,7 @@ class CM_Maintenance_Cli extends CM_Cli_Runnable_Abstract {
 	 */
 	protected function _registerClockworkCallbacks(DateInterval $interval, $callbacks) {
 		foreach ($callbacks as $name => $callback) {
-			$transactionName = 'cm.php ' . static::getPackageName() . ' run: ' . $name;
+			$transactionName = 'cm.php ' . static::getPackageName() . ' start: ' . $name;
 			$this->_clockworkManager->registerCallback($interval, function () use ($transactionName, $callback) {
 				CMService_Newrelic::getInstance()->startTransaction($transactionName);
 				try {
