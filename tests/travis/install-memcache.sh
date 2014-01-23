87c7f9f5b078e0fe384d11325e0cee5e1316f6e3
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2014-01-23 17:01:37 +0100

    Enabling Memcache on Travis

--- tests/travis/install-memcache.sh
+++ tests/travis/install-memcache.sh
@@ -7,4 +7,4 @@ MEMCACHE=2.2.6
 wget http://pecl.php.net/get/memcache-$MEMCACHE.tgz
 tar zxvf memcache-$MEMCACHE.tgz
 cd "memcache-${MEMCACHE}"
-phpize && ./configure && make install && echo "Installed ext/memcache-${MEMCACHE}"
+phpize && ./configure --enable-memcache && make install && echo "Installed ext/memcache-${MEMCACHE}"
