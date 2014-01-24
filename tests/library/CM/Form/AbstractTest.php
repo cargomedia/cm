f0a51057717fb8195c1e36ad0544d152be4f908a
Marian Sollmann <marian.sollmann@cargomedia.ch>
2014-01-24 13:34:30 +0100

    fixed test

--- tests/library/CM/Form/AbstractTest.php
+++ tests/library/CM/Form/AbstractTest.php
@@ -53,6 +53,6 @@ class CM_FormAction_MockForm_TestExampleAction extends CM_FormAction_Abstract {
 	}
 
 	protected function _process(CM_Params $params, CM_Response_View_Form $response, CM_Form_Abstract $form) {
-		CM_FormTest::$formActionProcessCount++;
+		CM_Form_AbstractTest::$formActionProcessCount++;
 	}
 }
