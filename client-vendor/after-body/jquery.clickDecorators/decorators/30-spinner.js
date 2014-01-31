c7ac96524e07fdd6d34d18d8f2ea19e2abb1537d
Reto Kaiser <reto@cargomedia.ch>
2014-01-31 18:43:00 +0100

    Remove previous spinner before adding spinner

--- client-vendor/after-body/jquery.clickDecorators/decorators/30-spinner.js
+++ client-vendor/after-body/jquery.clickDecorators/decorators/30-spinner.js
@@ -7,6 +7,7 @@
 		after: function(event, returnValue) {
 			if (returnValue && _.isFunction(returnValue.promise)) {
 				var $inputTarget = $(event.currentTarget).closest('button');
+				$inputTarget.find('.spinner').remove();
 				var $spinner = $('<div class="spinner" />').appendTo($inputTarget);
 				returnValue.always(function() {
 					$spinner.remove();
