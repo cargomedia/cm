2a8abcee385938448b77280d6cc90544ebb8be81
Reto Kaiser <reto@cargomedia.ch>
2014-01-31 18:39:44 +0100

    Remove console.log

--- client-vendor/after-body/jquery.clickDecorators/decorators/30-spinner.js
+++ client-vendor/after-body/jquery.clickDecorators/decorators/30-spinner.js
@@ -5,7 +5,6 @@
 
 	$.clickDecorators.spinner = {
 		after: function(event, returnValue) {
-			console.log('returnValue', returnValue);
 			if (returnValue && _.isFunction(returnValue.promise)) {
 				var $inputTarget = $(event.currentTarget).closest('button');
 				var $spinner = $('<div class="spinner" />').appendTo($inputTarget);
