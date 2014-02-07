234448444328689ec00efad8648328f66b9d141e
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2014-02-07 17:54:07 +0100

    disable button while spinning

--- client-vendor/after-body/jquery.clickDecorators/decorators/spinner.js
+++ client-vendor/after-body/jquery.clickDecorators/decorators/spinner.js
@@ -7,10 +7,10 @@
 		after: function(event, returnValue) {
 			if (returnValue && _.isFunction(returnValue.promise)) {
 				var $inputTarget = $(event.currentTarget).closest('button');
-				$inputTarget.addClass('hasSpinner').find('.spinner').remove();
+				$inputTarget.addClass('hasSpinner').attr('disabled', true).find('.spinner').remove();
 				var $spinner = $('<div class="spinner" />').appendTo($inputTarget);
 				returnValue.always(function() {
-					$inputTarget.removeClass('hasSpinner');
+					$inputTarget.attr('disabled', false).removeClass('hasSpinner');
 					$spinner.remove();
 				});
 			}
