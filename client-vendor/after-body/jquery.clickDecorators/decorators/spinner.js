d46ca8e8287209d971d0569ef27b120c8305361a
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2014-02-07 15:12:27 +0100

    spinner.js: toggle additional .hasSpinner class

--- client-vendor/after-body/jquery.clickDecorators/decorators/spinner.js
+++ client-vendor/after-body/jquery.clickDecorators/decorators/spinner.js
@@ -7,9 +7,10 @@
 		after: function(event, returnValue) {
 			if (returnValue && _.isFunction(returnValue.promise)) {
 				var $inputTarget = $(event.currentTarget).closest('button');
-				$inputTarget.find('.spinner').remove();
+				$inputTarget.addClass('hasSpinner').find('.spinner').remove();
 				var $spinner = $('<div class="spinner" />').appendTo($inputTarget);
 				returnValue.always(function() {
+					$inputTarget.removeClass('hasSpinner');
 					$spinner.remove();
 				});
 			}
