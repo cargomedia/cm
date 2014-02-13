0209f552324c1376fd865e2521552443f53c00fb
Marian Sollmann <marian.sollmann@cargomedia.ch>
2014-02-13 16:55:45 +0100

    fixed library/CM/FormField/GeoPoint.js

--- library/CM/FormField/GeoPoint.js
+++ library/CM/FormField/GeoPoint.js
@@ -6,6 +6,6 @@ var CM_FormField_GeoPoint = CM_FormField_Abstract.extend({
 	_class: 'CM_FormField_GeoPoint',
 
 	isEmpty: function(value) {
-		return _.isEmpty(value['latitude']) || _.isEmpty(value['longitude']);
+		return _.isEmpty(value.latitude) || _.isEmpty(value.longitude);
 	}
 });
