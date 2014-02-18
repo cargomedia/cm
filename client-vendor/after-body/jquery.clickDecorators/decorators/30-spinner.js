51b8db6d2331b4738b1bf0aeb0bb4cf799da0e18
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2014-02-18 10:19:17 +0100

    make spinner globally available (not button exclusive)

--- client-vendor/after-body/jquery.clickDecorators/decorators/30-spinner.js
+++ client-vendor/after-body/jquery.clickDecorators/decorators/30-spinner.js
@@ -6,7 +6,7 @@
 	$.clickDecorators.spinner = {
 		after: function(event, returnValue) {
 			if (returnValue && _.isFunction(returnValue.promise)) {
-				var $inputTarget = $(event.currentTarget).closest('button');
+				var $inputTarget = $(event.currentTarget).closest('[data-click-spinner]');
 				$inputTarget.addClass('hasSpinner').prop('disabled', true).find('.spinner').remove();
 				var $spinner = $('<div class="spinner" />').appendTo($inputTarget);
 				returnValue.always(function() {
