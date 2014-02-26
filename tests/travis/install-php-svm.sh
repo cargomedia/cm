a4a9f58655f9a0dd2338651ac303118adcc33462
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2014-02-26 14:30:01 +0100

    Trying to fix libsvm install script

--- tests/travis/install-php-svm.sh
+++ tests/travis/install-php-svm.sh
@@ -3,13 +3,16 @@
 LIBSVM_VERSION="3.17"
 PHPSVM_VERSION="0.1.9"
 
-curl http://www.csie.ntu.edu.tw/~cjlin/libsvm/libsvm-${LIBSVM_VERSION}.tar.gz | tar xz
-cd libsvm-${LIBSVM_VERSION}
-sudo make lib
-sudo cp libsvm.so.2 /usr/lib
-sudo ln -sf /usr/lib/libsvm.so.2 /usr/lib/libsvm.so
-ldconfig
-ldconfig --print | grep libsvm
+wget -c http://www.csie.ntu.edu.tw/~cjlin/libsvm/libsvm-$LIBSVM_VERSION.tar.gz
+tar xvfz libsvm-$LIBSVM_VERSION.tar.gz
+cd libsvm-$LIBSVM_VERSION
+make lib all
+sudo install -D -m755 svm-train /usr/bin/svm-train
+sudo install -D -m755 svm-predict /usr/bin/svm-predict
+sudo install -D -m755 svm-scale /usr/bin/svm-scale
+sudo install -D -m644 libsvm.so.2 /usr/lib/libsvm.so.2
+sudo ldconfig
+cd ..
 
 curl -L https://github.com/ianbarber/php-svm/archive/${PHPSVM_VERSION}.tar.gz | tar xz
 cd php-svm-${PHPSVM_VERSION}
