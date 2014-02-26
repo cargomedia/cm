3e3817a9a0206421abb8c674cb33243ecedb5a56
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2014-02-26 14:20:12 +0100

    Make lib as su

--- tests/travis/install-php-svm.sh
+++ tests/travis/install-php-svm.sh
@@ -5,7 +5,7 @@ PHPSVM_VERSION="0.1.9"
 
 curl http://www.csie.ntu.edu.tw/~cjlin/libsvm/libsvm-${LIBSVM_VERSION}.tar.gz | tar xz
 cd libsvm-${LIBSVM_VERSION}
-make lib
+sudo make lib
 sudo cp libsvm.so.2 /usr/lib
 sudo ln -sf /usr/lib/libsvm.so.2 /usr/lib/libsvm.so
 ldconfig
