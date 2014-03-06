64ceb7622e923c1e01854e363b01ef77e1c6da31
Reto Kaiser <reto@cargomedia.ch>
2014-03-06 22:21:19 +0100

    Revert "Remove adprovider-app"
    This reverts commit af4318f69f70c115d33162f168458ea4c21e62b6.

---
+++ tests/library/CM/AdproviderAdapter/AbstractTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class CM_AdproviderAdapter_AbstractTest extends CMTest_TestCase {
+
+  public function testFactory() {
+    CM_Config::get()->CM_AdproviderAdapter_Abstract->class = 'CM_AdproviderAdapter_Openx';
+
+    $this->assertInstanceOf('CM_AdproviderAdapter_Openx', CM_AdproviderAdapter_Abstract::factory());
+
+    CMTest_TH::clearConfig();
+  }
+}
