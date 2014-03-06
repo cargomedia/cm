3698505cfdcaa1c225105e28d7874f5b160b72ab
Reto Kaiser <reto@cargomedia.ch>
2014-03-06 21:53:47 +0100

    remove other tests

--- tests/library/CM/Config/NodeTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class CM_Config_NodeTest extends CMTest_TestCase {
-
-  public function testNodeClass() {
-    $reflection = new ReflectionClass('CM_Config_Node');
-    $this->assertEmpty($reflection->getProperties());
-  }
-}
