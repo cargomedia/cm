9d3d5b7f479d002211deca733a3c6559f52e5a27
Reto Kaiser <reto@cargomedia.ch>
2014-03-06 21:46:46 +0100

    Revert "remove amazon api"
    This reverts commit 37ee65306b34e67a52f464fdb425bfd58a6b7409.

---
+++ library/CMService/Amazon/Abstract.php
@@ -0,0 +1,17 @@
+<?php
+
+abstract class CMService_Amazon_Abstract extends CM_Class_Abstract {
+
+  public function __construct() {
+    $accessKey = self::_getConfig()->accessKey;
+    if (!$accessKey) {
+      throw new CM_Exception_Invalid('Amazon S3 `accessKey` not set');
+    }
+    $secretKey = self::_getConfig()->secretKey;
+    if (!$secretKey) {
+      throw new CM_Exception_Invalid('Amazon S3 `secretKey` not set');
+    }
+    CFCredentials::set(array('development' => array('key'                   => $accessKey, 'secret' => $secretKey, 'default_cache_config' => '',
+                                                    'certificate_authority' => false), '@default' => 'development'));
+  }
+}
