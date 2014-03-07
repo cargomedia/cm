49155e45344e5b723ee1b212423df63293fcf588
Christophe Schwyzer <christophe.schwyzer@cargomedia.ch>
2014-03-07 15:41:21 +0100

    formatting

--- library/CM/SmartyPlugins/function.button.php
+++ library/CM/SmartyPlugins/function.button.php
@@ -1,82 +1,82 @@
 <?php
 
 function smarty_function_button(array $params, Smarty_Internal_Template $template) {
-  /** @var CM_Render $render */
-  $render = $template->smarty->getTemplateVars('render');
-  /** @var CM_Form_Abstract $form */
-  $form = $render->getStackLast('forms');
-  if (empty($params['action'])) {
-    trigger_error('Param `action` missing.');
-  }
-  $action = $form->getAction($params['action']);
-  $title = isset($params['title']) ? (string) $params['title'] : null;
-  $theme = isset($params['theme']) ? (string) $params['theme'] : 'default';
-
-  $class = 'button ' . 'button-' . $theme;
-  if (isset($params['class'])) {
-    $class .= ' ' . trim($params['class']);
-  }
-
-  $data = array();
-  if (isset($params['data'])) {
-    $data = $params['data'];
-    unset($params['data']);
-  }
-
-  if (isset($params['event'])) {
-    $data['event'] = (string) $params['event'];
-    unset($params['event']);
-  }
-
-  $icon = null;
-  $iconConfirm = null;
-  if (isset($params['icon'])) {
-    $icon = $params['icon'];
-
-    if (isset($params['iconConfirm'])) {
-      $iconConfirm = $params['iconConfirm'];
+    /** @var CM_Render $render */
+    $render = $template->smarty->getTemplateVars('render');
+    /** @var CM_Form_Abstract $form */
+    $form = $render->getStackLast('forms');
+    if (empty($params['action'])) {
+        trigger_error('Param `action` missing.');
     }
-  }
+    $action = $form->getAction($params['action']);
+    $title = isset($params['title']) ? (string) $params['title'] : null;
+    $theme = isset($params['theme']) ? (string) $params['theme'] : 'default';
 
-  $label = '';
-  if (isset($params['label'])) {
-    $label = $params['label'];
-  }
-
-  if ($label) {
-    $class .= ' hasLabel';
-  }
-  if ($icon) {
-    $class .= ' hasIcon';
-  }
-  if ($title) {
-    $class .= ' showTooltip';
-  }
-
-  $id = $form->getAutoId() . '-' . $action->getName() . '-button';
-
-  $html = '';
-  $html .= '<button class="' . $class . '" id="' . $id . '" type="submit" value="' . $label . '" data-click-spinner="true"';
-  if ($title) {
-    $html .= ' title="' . $title . '"';
-  }
-  if (!empty($data)) {
-    foreach ($data as $name => $value) {
-      $html .= ' data-' . $name . '="' . CM_Util::htmlspecialchars($value) . '"';
+    $class = 'button ' . 'button-' . $theme;
+    if (isset($params['class'])) {
+        $class .= ' ' . trim($params['class']);
     }
-  }
-  $html .= '>';
-  if ($icon) {
-    if ($iconConfirm) {
-      $html .= '<span class="icon icon-' . $icon . ' confirmClick-state-inactive"></span>'
-        . '<span class="icon icon-' . $iconConfirm . ' confirmClick-state-active"></span>';
-    } else {
-      $html .= '<span class="icon icon-' . $icon . '"></span>';
+
+    $data = array();
+    if (isset($params['data'])) {
+        $data = $params['data'];
+        unset($params['data']);
     }
-  }
-  if ($label) {
-    $html .= '<span class="label">' . CM_Util::htmlspecialchars($label) . '</span>';
-  }
-  $html .= '</button>';
-  return $html;
+
+    if (isset($params['event'])) {
+        $data['event'] = (string) $params['event'];
+        unset($params['event']);
+    }
+
+    $icon = null;
+    $iconConfirm = null;
+    if (isset($params['icon'])) {
+        $icon = $params['icon'];
+
+        if (isset($params['iconConfirm'])) {
+            $iconConfirm = $params['iconConfirm'];
+        }
+    }
+
+    $label = '';
+    if (isset($params['label'])) {
+        $label = $params['label'];
+    }
+
+    if ($label) {
+        $class .= ' hasLabel';
+    }
+    if ($icon) {
+        $class .= ' hasIcon';
+    }
+    if ($title) {
+        $class .= ' showTooltip';
+    }
+
+    $id = $form->getAutoId() . '-' . $action->getName() . '-button';
+
+    $html = '';
+    $html .= '<button class="' . $class . '" id="' . $id . '" type="submit" value="' . $label . '" data-click-spinner="true"';
+    if ($title) {
+        $html .= ' title="' . $title . '"';
+    }
+    if (!empty($data)) {
+        foreach ($data as $name => $value) {
+            $html .= ' data-' . $name . '="' . CM_Util::htmlspecialchars($value) . '"';
+        }
+    }
+    $html .= '>';
+    if ($icon) {
+        if ($iconConfirm) {
+            $html .= '<span class="icon icon-' . $icon . ' confirmClick-state-inactive"></span>'
+                . '<span class="icon icon-' . $iconConfirm . ' confirmClick-state-active"></span>';
+        } else {
+            $html .= '<span class="icon icon-' . $icon . '"></span>';
+        }
+    }
+    if ($label) {
+        $html .= '<span class="label">' . CM_Util::htmlspecialchars($label) . '</span>';
+    }
+    $html .= '</button>';
+    return $html;
 }
--- library/CM/SmartyPlugins/function.button_link.php
+++ library/CM/SmartyPlugins/function.button_link.php
@@ -2,106 +2,106 @@
 require_once 'function.linkUrl.php';
 
 function smarty_function_button_link(array $params, Smarty_Internal_Template $template) {
-  $label = '';
-  if (isset($params['label'])) {
-    $label = CM_Util::htmlspecialchars($params['label']);
-    unset($params['label']);
-  }
-
-  $attrs = '';
-  $icon = null;
-  $iconConfirm = null;
-  if (isset($params['icon'])) {
-    $icon = $params['icon'];
-
-    if (isset($params['iconConfirm'])) {
-      $iconConfirm = $params['iconConfirm'];
-    }
-  }
-  unset($params['icon']);
-  unset($params['iconConfirm']);
-
-  $iconPosition = 'left';
-  if (!empty($params['iconPosition']) && $params['iconPosition'] == 'right') {
-    $iconPosition = 'right';
-  }
-  unset($params['iconPosition']);
-
-  $title = null;
-  if (isset($params['title'])) {
-    $title = (string) $params['title'];
-    $attrs .= ' title="' . CM_Util::htmlspecialchars($title) . '"';
-  }
-  unset($params['title']);
-
-  if (isset($params['id'])) {
-    $attrs .= ' id="' . $params['id'] . '"';
-  }
-  unset($params['id']);
-
-  $theme = isset($params['theme']) ? (string) $params['theme'] : 'default';
-  $class = 'button ' . 'button-' . $theme . ' ';
-  if (isset($params['class'])) {
-    $class .= $params['class'];
-  }
-  unset($params['theme']);
-  unset($params['class']);
-
-  if ($label) {
-    $class .= ' hasLabel';
-  }
-
-  $iconMarkup = '';
-  if ($icon) {
-    if ($iconConfirm) {
-      $iconMarkup = '<span class="icon icon-' . $icon . ' confirmClick-state-inactive"></span>'
-        . '<span class="icon icon-' . $iconConfirm . ' confirmClick-state-active"></span>';
-    } else {
-      $iconMarkup = '<span class="icon icon-' . $icon . '"></span>';
+    $label = '';
+    if (isset($params['label'])) {
+        $label = CM_Util::htmlspecialchars($params['label']);
+        unset($params['label']);
     }
 
-    if ($iconPosition == 'right') {
-      $class .= ' hasIconRight';
-    } else {
-      $class .= ' hasIcon';
+    $attrs = '';
+    $icon = null;
+    $iconConfirm = null;
+    if (isset($params['icon'])) {
+        $icon = $params['icon'];
+
+        if (isset($params['iconConfirm'])) {
+            $iconConfirm = $params['iconConfirm'];
+        }
     }
-  }
+    unset($params['icon']);
+    unset($params['iconConfirm']);
 
-  if ($title) {
-    $class .= ' showTooltip';
-  }
-
-  $onclick = false;
-  if (isset($params['onclick'])) {
-    $onclick = $params['onclick'];
-    unset($params['onclick']);
-  }
-  if (isset($params['page'])) {
-    $onclick .= ' cm.router.route(\'' . smarty_function_linkUrl($params, $template) . '\');';
-  }
-
-  if ($onclick) {
-    $attrs .= ' onclick="' . $onclick . '"';
-  }
-
-  if (isset($params['data'])) {
-    foreach ($params['data'] as $name => $value) {
-      $attrs .= ' data-' . $name . '="' . CM_Util::htmlspecialchars($value) . '"';
+    $iconPosition = 'left';
+    if (!empty($params['iconPosition']) && $params['iconPosition'] == 'right') {
+        $iconPosition = 'right';
     }
-  }
+    unset($params['iconPosition']);
 
-  $html = '';
-  $html .= '<button class="' . $class . '" type="button" value="' . $label . '" ' . $attrs . '>';
-  if ($icon && $iconPosition == 'left') {
-    $html .= $iconMarkup;
-  }
-  if ($label) {
-    $html .= '<span class="label">' . CM_Util::htmlspecialchars($label) . '</span>';
-  }
+    $title = null;
+    if (isset($params['title'])) {
+        $title = (string) $params['title'];
+        $attrs .= ' title="' . CM_Util::htmlspecialchars($title) . '"';
+    }
+    unset($params['title']);
 
-  if ($icon && $iconPosition == 'right') {
-    $html .= $iconMarkup;
-  }
-  $html .= '</button>';
-  return $html;
+    if (isset($params['id'])) {
+        $attrs .= ' id="' . $params['id'] . '"';
+    }
+    unset($params['id']);
+
+    $theme = isset($params['theme']) ? (string) $params['theme'] : 'default';
+    $class = 'button ' . 'button-' . $theme . ' ';
+    if (isset($params['class'])) {
+        $class .= $params['class'];
+    }
+    unset($params['theme']);
+    unset($params['class']);
+
+    if ($label) {
+        $class .= ' hasLabel';
+    }
+
+    $iconMarkup = '';
+    if ($icon) {
+        if ($iconConfirm) {
+            $iconMarkup = '<span class="icon icon-' . $icon . ' confirmClick-state-inactive"></span>'
+                . '<span class="icon icon-' . $iconConfirm . ' confirmClick-state-active"></span>';
+        } else {
+            $iconMarkup = '<span class="icon icon-' . $icon . '"></span>';
+        }
+
+        if ($iconPosition == 'right') {
+            $class .= ' hasIconRight';
+        } else {
+            $class .= ' hasIcon';
+        }
+    }
+
+    if ($title) {
+        $class .= ' showTooltip';
+    }
+
+    $onclick = false;
+    if (isset($params['onclick'])) {
+        $onclick = $params['onclick'];
+        unset($params['onclick']);
+    }
+    if (isset($params['page'])) {
+        $onclick .= ' cm.router.route(\'' . smarty_function_linkUrl($params, $template) . '\');';
+    }
+
+    if ($onclick) {
+        $attrs .= ' onclick="' . $onclick . '"';
+    }
+
+    if (isset($params['data'])) {
+        foreach ($params['data'] as $name => $value) {
+            $attrs .= ' data-' . $name . '="' . CM_Util::htmlspecialchars($value) . '"';
+        }
+    }
+
+    $html = '';
+    $html .= '<button class="' . $class . '" type="button" value="' . $label . '" ' . $attrs . '>';
+    if ($icon && $iconPosition == 'left') {
+        $html .= $iconMarkup;
+    }
+    if ($label) {
+        $html .= '<span class="label">' . CM_Util::htmlspecialchars($label) . '</span>';
+    }
+
+    if ($icon && $iconPosition == 'right') {
+        $html .= $iconMarkup;
+    }
+    $html .= '</button>';
+    return $html;
 }
