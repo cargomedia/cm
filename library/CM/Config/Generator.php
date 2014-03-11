bab3e1846d143df8f0cf4c7d8f054d060e88c04a
Marian Sollmann <marian.sollmann@cargomedia.ch>
2014-03-11 10:38:43 +0100

    remove logic of CM_Config_Mapping

--- library/CM/Config/Generator.php
+++ library/CM/Config/Generator.php
@@ -11,27 +11,6 @@ class CM_Config_Generator extends CM_Class_Abstract {
     private $_namespaceTypes = array();
 
     /**
-     * @param CM_File $source
-     * @return string
-     */
-    public function generateMappedOutput(CM_File $source) {
-        $entryList = CM_Params::decode($source->read(), true);
-        $output = '<?php' . PHP_EOL;
-        $mapping = $this->_getMapping();
-        foreach ($entryList as $key => $value) {
-            if (is_array($value)) {
-                $keyMapped = $mapping->getConfigKey($key);
-                foreach ($value as $subKey => $subValue) {
-                    $output .= '$config->' . $keyMapped . '->' . $subKey . ' = ' . var_export($subValue, true) . ';' . PHP_EOL;
-                }
-            } else {
-                $output .= '$config->' . $key . ' = ' . var_export($value, true) . ';' . PHP_EOL;
-            }
-        }
-        return $output;
-    }
-
-    /**
      * @throws CM_Exception_Invalid
      * @return array[]
      */
@@ -180,12 +159,5 @@ class CM_Config_Generator extends CM_Class_Abstract {
             }
         }
         return $actionVerbs;
-    }
-
-    /**
-     * @return CM_Config_Mapping
-     */
-    protected function _getMapping() {
-        return new CM_Config_Mapping();
     }
 }
