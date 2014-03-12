bd125e6150ae56997dabd6bb741e4985ec9c4aa9
tomaszdurka <tomasz@durka.pl>
2014-03-12 19:05:57 +0100

    Add test for setDefault()

--- tests/library/CM/ModelAsset/User/PreferencesTest.php
+++ tests/library/CM/ModelAsset/User/PreferencesTest.php
@@ -2,17 +2,39 @@
 
 class CM_ModelAsset_User_PreferencesTest extends CMTest_TestCase {
 
-    public static function setUpBeforeClass() {
-        CM_ModelAsset_User_Preferences::setDefault('test', 'foo', false, true);
-        CM_ModelAsset_User_Preferences::setDefault('test', 'bar', true, true);
-    }
-
-    public static function tearDownAfterClass() {
-        parent::tearDownAfterClass();
+    public function tearDown() {
         CM_Db_Db::truncate('cm_user_preferenceDefault');
     }
 
+    public function testSetDefault() {
+        $this->assertSame(array(), CM_ModelAsset_User_Preferences::getDefaults());
+
+        CM_ModelAsset_User_Preferences::setDefault('foo', 'bar', true, false);
+        CM_Cache_Local::getInstance()->flush();
+        $preferences = CM_ModelAsset_User_Preferences::getDefaults();
+        $this->assertCount(1, $preferences);
+        $this->assertTrue($preferences['foo']['bar']['value']);
+        $this->assertFalse($preferences['foo']['bar']['configurable']);
+        $id = $preferences['foo']['bar']['id'];
+
+        CM_ModelAsset_User_Preferences::setDefault('foo', 'bar', false, false);
+        CM_Cache_Local::getInstance()->flush();
+        $preferences = CM_ModelAsset_User_Preferences::getDefaults();
+        $this->assertCount(1, $preferences);
+        $this->assertSame($id, $preferences['foo']['bar']['id']);
+        $this->assertFalse($preferences['foo']['bar']['value']);
+
+        CM_ModelAsset_User_Preferences::setDefault('bar', 'foo', false, false);
+        CM_Cache_Local::getInstance()->flush();
+        $preferences = CM_ModelAsset_User_Preferences::getDefaults();
+        $this->assertCount(2, $preferences);
+
+    }
+
     public function testGetSet() {
+        CM_ModelAsset_User_Preferences::setDefault('test', 'foo', false, true);
+        CM_ModelAsset_User_Preferences::setDefault('test', 'bar', true, true);
+
         $preferences = CMTest_TH::createUser()->getPreferences();
         $defaults = $preferences->getDefaults();
         $sections = array_keys($defaults);
@@ -29,6 +51,8 @@ class CM_ModelAsset_User_PreferencesTest extends CMTest_TestCase {
     }
 
     public function testReset() {
+        CM_ModelAsset_User_Preferences::setDefault('test', 'foo', false, true);
+        CM_ModelAsset_User_Preferences::setDefault('test', 'bar', true, true);
         $preferences = CMTest_TH::createUser()->getPreferences();
         $defaults = $preferences->getDefaults();
         $sections = array_keys($defaults);
@@ -44,6 +68,8 @@ class CM_ModelAsset_User_PreferencesTest extends CMTest_TestCase {
     }
 
     public function testInvalidatedModel() {
+        CM_ModelAsset_User_Preferences::setDefault('test', 'foo', false, true);
+        CM_ModelAsset_User_Preferences::setDefault('test', 'bar', true, true);
         $user = CMTest_TH::createUser();
         $user->_change();
         $user->getPreferences()->getAll();
