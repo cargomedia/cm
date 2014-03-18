b618d1a27371cec8e772922e1c8a0617b994a1a5
tomaszdurka <tomasz@durka.pl>
2014-03-18 14:22:04 +0100

    Add i18n setup-translations command

--- library/CM/Internationalization/Cli.php
+++ library/CM/Internationalization/Cli.php
@@ -12,6 +12,11 @@ class CM_Internationalization_Cli extends CM_Cli_Runnable_Abstract {
         $this->_getOutput()->writeln($name . ' (' . $abbreviation . ') language created');
     }
 
+    public function setupTranslations() {
+        CM_App::getInstance()->setupTranslations();
+        $this->_getOutput()->writeln('Translations imported');
+    }
+
     public static function getPackageName() {
         return 'i18n';
     }
