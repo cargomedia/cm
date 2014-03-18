97a23f5493c82d252facc2310a9ae2292913b0c0
Alexis Peter <alexis.peter@cargomedia.ch>
2014-03-18 11:57:23 +0100

    split _process() method into _process() and _loadPage(), fix redirection to external url

--- library/CM/Response/JsonP.php
+++ library/CM/Response/JsonP.php
@@ -10,6 +10,13 @@ class CM_Response_JsonP extends CM_Response_Abstract {
     }
 
     protected function _process() {
+        $data = $this->_loadPage();
+        $content =
+            '<script type="text/javascript">parent.cm.findView(\'CM_Layout_Abstract\').injectPage(' . CM_Params::encode($data, true) . ');</script>';
+        $this->_setContent($content);
+    }
+
+    protected function _loadPage() {
         $request = new CM_Request_Get($this->getRequest()->getPath(), $this->getRequest()->getHeaders(), $this->getRequest()->getServer(), $this->getRequest()->getViewer());
 
         $count = 0;
@@ -48,12 +55,9 @@ class CM_Response_JsonP extends CM_Response_Abstract {
         $menuList = array_merge($this->getSite()->getMenus(), $responsePage->getRender()->getMenuList());
         $menuEntryHashList = $this->_getMenuEntryHashList($menuList, $page);
 
-        $data = array('autoId'            => $page->getAutoId(), 'html' => $html, 'js' => $js, 'title' => $title, 'url' => $url,
-                      'layoutClass'       => $layoutClass,
-                      'menuEntryHashList' => $menuEntryHashList);
-
-        $content = '<script type="text/javascript">parent.cm.findView(\'CM_Layout_Abstract\').injectPage(' . CM_Params::encode($data, true) . ');</script>';
-        $this->_setContent($content);
+        return array('autoId'            => $page->getAutoId(), 'html' => $html, 'js' => $js, 'title' => $title, 'url' => $url,
+                     'layoutClass'       => $layoutClass,
+                     'menuEntryHashList' => $menuEntryHashList);
     }
 
     /**
@@ -75,7 +79,6 @@ class CM_Response_JsonP extends CM_Response_Abstract {
         }
         return $menuEntryHashList;
     }
-
 
     public static function match(CM_Request_Abstract $request) {
         return $request->getPathPart(0) === 'jsonp';
