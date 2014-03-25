a0d253493e3e75d9de87c862fa8b11a3068b65bf
Reto Kaiser <reto@cargomedia.ch>
2014-03-25 16:02:07 +0100

    Adjust assert(Not)Row docu

--- tests/helpers/CMTest/library/CMTest/TestCase.php
+++ tests/helpers/CMTest/library/CMTest/TestCase.php
@@ -466,19 +466,22 @@ abstract class CMTest_TestCase extends PHPUnit_Framework_TestCase {
     }
 
     /**
-     * @param string $table
-     * @param array  $where WHERE conditions: ('attr' => 'value', 'attr2' => 'value')
-     * @param int    $rowCount
+     * @param string            $table
+     * @param array|string|null $where WHERE conditions: ('attr' => 'value', 'attr2' => 'value')
+     * @param int|null          $rowCount
      */
-    public static function assertRow($table, $where = null, $rowCount = 1) {
+    public static function assertRow($table, $where = null, $rowCount = null) {
+        if (null === $rowCount) {
+            $rowCount = 1;
+        }
         $result = CM_Db_Db::select($table, '*', $where);
         $rowCountActual = count($result->fetchAll());
         self::assertEquals($rowCount, $rowCountActual);
     }
 
     /**
-     * @param string     $table
-     * @param array|null $where
+     * @param string            $table
+     * @param array|string|null $where
      */
     public static function assertNotRow($table, $where = null) {
         self::assertRow($table, $where, 0);
