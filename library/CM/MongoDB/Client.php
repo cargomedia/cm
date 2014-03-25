2e5b7c10082b6220a73b1a989971070ed8fd5bef
Jan Puziewicz <jan.puziewicz@cargomedia.ch>
2014-03-25 17:07:09 +0100

    base mongodb client

---
+++ library/CM/MongoDB/Client.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * Class CM_Mongodb_Client
+ */
+class CM_Mongodb_Client extends CM_Class_Abstract {
+
+    /** @var \MongoClient */
+    private $_mongodb = null;
+
+    /** @var MongoDB */
+    private $_db = null;
+
+    public function __construct() {
+        $this->_mongodb = new MongoClient();
+        $this->useDatabase('fuckbook');
+    }
+
+    /**
+     * @return string
+     */
+    public function getNewId() {
+        $mongoId = new MongoId();
+        return (string) $mongoId;
+    }
+
+    /**
+     * @param $databaseName
+     */
+    public function useDatabase($databaseName) {
+        if ($this->_db) {
+            unset($this->_db);
+        }
+        $this->_db = $this->_mongodb->{$databaseName};
+    }
+
+    /**
+     * @param $collection
+     * @return MongoCollection
+     */
+    public function getCollection($collection) {
+        return $this->_db->{$collection};
+    }
+
+    /**
+     * @param $collection
+     * @param $object
+     * @return array|bool
+     */
+    public function insert($collection, $object) {
+        return $this->getCollection($collection)->insert($object);
+    }
+
+    /**
+     * @param $collection
+     * @param $query
+     * @return array
+     */
+    public function findOne($collection, $query) {
+        return $this->getCollection($collection)->findOne($query);
+    }
+
+    /**
+     * @param $collection
+     * @param $query
+     * @return MongoCursor
+     */
+    public function find($collection, $query) {
+        return $this->getCollection($collection)->find($query);
+    }
+
+    /**
+     * @param     $collection
+     * @param     $query
+     * @param int $limit
+     * @param int $skip
+     * @return int
+     */
+    public function count($collection, $query, $limit = 0, $skip = 0) {
+        return $this->getCollection($collection)->count($query, $limit, $skip);
+    }
+
+    /**
+     * @param $collection
+     * @return array
+     */
+    public function drop($collection) {
+        return $this->getCollection($collection)->drop();
+    }
+
+    /**
+     * @return CM_Mongodb_Client
+     */
+    public static function getInstance() {
+        static $instance;
+        if (!$instance) {
+            $instance = new self();
+        }
+        return $instance;
+    }
+}
