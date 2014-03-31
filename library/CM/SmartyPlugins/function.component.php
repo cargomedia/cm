b367dc1acfeab3c055be01862bb278c57498900c
tomaszdurka <tomasz@durka.pl>
2014-03-31 14:41:27 +0200

    Allow to pass params as whole to function.component

--- library/CM/SmartyPlugins/function.component.php
+++ library/CM/SmartyPlugins/function.component.php
@@ -8,14 +8,14 @@ function smarty_function_component(array $params, Smarty_Internal_Template $temp
     /** @var CM_Render $render */
     $render = $template->smarty->getTemplateVars('render');
     unset($params['name']);
-    if ($name instanceof CM_Component_Abstract) {
-        $componentParams = CM_Params::factory($params['params']);
-        $component = $name;
-    } else {
-        $componentParams = CM_Params::factory($params);
-        $component = CM_Component_Abstract::factory($name, $render, $componentParams, $render->getViewer());
+
+    $componentParams = $params;
+    unset($componentParams['params']);
+    if (isset($params['params'])) {
+        $componentParams = array_merge($componentParams, $params['params']);
     }
+    $component = CM_Component_Abstract::factory($name, $render, $componentParams, $render->getViewer());
 
     $renderAdapter = new CM_RenderAdapter_Component($render, $component);
-    return $renderAdapter->fetch($componentParams);
+    return $renderAdapter->fetch(CM_Params::factory($componentParams));
 }
