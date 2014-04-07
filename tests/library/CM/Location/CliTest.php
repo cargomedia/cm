6184d4763f97cb453f019c683acfaa9cad7afd62
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2014-04-07 15:18:42 +0200

    Added import test with several entries per level

--- tests/library/CM/Location/CliTest.php
+++ tests/library/CM/Location/CliTest.php
@@ -199,6 +199,70 @@ class CM_Location_CliTest extends CMTest_TestCase {
         );
     }
 
+    public function testImport() {
+        $this->_import(
+            array(
+                array('France', 'FR'),
+                array('United States', 'US'),
+            ),
+            array(
+                array('FR', 'A7', 'Haute-Normandie'),
+                array('US', 'CA', 'California'),
+                array('US', 'HI', 'Hawaii'),
+            ),
+            array(
+                array('75', 'FR', '', '', '', '48.86', '2.35'),
+                array('50221', 'FR', 'A7', 'Le Havre', '', '49.5', '0.1333'),
+                array('50221', 'FR', 'A7', 'Le Havre', '76620', '49.4938', '0.1077'),
+                array('223', 'US', '', '', '', '38', '-97'),
+                array('11101', 'US', 'CA', 'San Francisco', '', '37.7749', '-122.4194'),
+                array('608', 'US', 'CA', 'San Francisco', '94124', '37.7312', '-122.3826'),
+                array('757', 'US', 'CA', 'San Francisco', '94105', '37.7898', '-122.3942'),
+                array('11532', 'US', 'CA', 'Los Angeles', '', '34.0522', '-118.2437'),
+            ),
+            array(
+                array('33555968', '33556223', '75'),
+                array('87097600', '87097855', '50221'),
+                array('266578176', '266578431', '223'),
+                array('266586368', '266586623', '223'),
+                array('68444672', '68444735', '11101'),
+                array('68444800', '68444927', '11101'),
+            )
+        );
+        $this->_verify(
+            array(
+                array('id' => 1, 'abbreviation' => 'FR', 'name' => 'France'),
+                array('id' => 2, 'abbreviation' => 'US', 'name' => 'United States'),
+            ),
+            array(
+                array('id' => 1, 'countryId' => 1, 'name' => 'Haute-Normandie', '_maxmind' => 'FRA7', 'abbreviation' => null),
+                array('id' => 2, 'countryId' => 2, 'name' => 'California', '_maxmind' => 'USCA', 'abbreviation' => 'CA'),
+                array('id' => 3, 'countryId' => 2, 'name' => 'Hawaii', '_maxmind' => 'USHI', 'abbreviation' => 'HI'),
+            ),
+            array(
+                array('id' => 1, 'stateId' => 1, 'countryId' => 1, 'name' => 'Le Havre', 'lat' => 49.5, 'lon' => 0.1333, '_maxmind' => 50221),
+                array('id' => 2, 'stateId' => 2, 'countryId' => 2, 'name' => 'Los Angeles', 'lat' => 34.0522, 'lon' => -118.244, '_maxmind' => 11532),
+                array('id'       => 3, 'stateId' => 2, 'countryId' => 2, 'name' => 'San Francisco', 'lat' => 37.7749, 'lon' => -122.419,
+                      '_maxmind' => 11101),
+            ),
+            array(
+                array('id' => 1, 'name' => '76620', 'cityId' => 1, 'lat' => 49.4938, 'lon' => 0.1077),
+                array('id' => 2, 'name' => '94105', 'cityId' => 3, 'lat' => 37.7898, 'lon' => -122.394),
+                array('id' => 3, 'name' => '94124', 'cityId' => 3, 'lat' => 37.7312, 'lon' => -122.383),
+            ),
+            array(
+                array('countryId' => 1, 'ipStart' => 33555968, 'ipEnd' => 33556223),
+                array('countryId' => 2, 'ipStart' => 266578176, 'ipEnd' => 266578431),
+                array('countryId' => 2, 'ipStart' => 266586368, 'ipEnd' => 266586623),
+            ),
+            array(
+                array('cityId' => 1, 'ipStart' => 87097600, 'ipEnd' => 87097855),
+                array('cityId' => 3, 'ipStart' => 68444672, 'ipEnd' => 68444735),
+                array('cityId' => 3, 'ipStart' => 68444800, 'ipEnd' => 68444927),
+            )
+        );
+    }
+
     protected function _import($countryDataMock, $regionDataMock, $locationDataMock, $ipDataMock) {
         $cmLocationCli = $this->getMock('CM_Location_Cli', array('_getCountryData', '_getRegionData', '_getLocationData', '_getIpData'));
         $cmLocationCli->expects($this->any())->method('_getCountryData')->will($this->returnValue($countryDataMock));
