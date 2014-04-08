27dec96dec0bd499d035f6cd72f0de3539282cd1
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2014-04-08 13:57:49 +0200

    Added test for city in unknown region

--- tests/library/CMService/MaxMindTest.php
+++ tests/library/CMService/MaxMindTest.php
@@ -231,6 +231,33 @@ class CMService_MaxMindTest extends CMTest_TestCase {
         );
     }
 
+    public function testCity_unknownRegion() {
+        $this->_import(
+            array(
+                array('France', 'FR'),
+            ),
+            array(),
+            array(
+                array('75', 'FR', '', '', '', '48.86', '2.35'),
+                array('436884', 'FR', 'A7', '', '', '49.4333', '1.0833'),
+                array('50221', 'FR', 'A7', 'Le Havre', '', '49.5', '0.1333'),
+            ),
+            array()
+        );
+        $this->_verify(
+            array(
+                array('id' => 1, 'abbreviation' => 'FR', 'name' => 'France'),
+            ),
+            array(),
+            array(
+                array('id' => 1, 'stateId' => null, 'countryId' => 1, 'name' => 'Le Havre', 'lat' => 49.5, 'lon' => 0.1333, '_maxmind' => 50221),
+            ),
+            array(),
+            array(),
+            array()
+        );
+    }
+
     public function testZipCode() {
         $this->_import(
             array(
