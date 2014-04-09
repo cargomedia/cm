17f1e31de16aecc7251b8d47fadf7acb7e92f382
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2014-04-09 09:42:37 +0200

    Added test for cities with set/unset region

--- tests/library/CMService/MaxMindTest.php
+++ tests/library/CMService/MaxMindTest.php
@@ -1274,6 +1274,116 @@ class CMService_MaxMindTest extends CMTest_TestCase {
         );
     }
 
+    public function testUpdateCityRegion_set() {
+        $this->_import(
+            array(
+                array('United States', 'US'),
+            ),
+            array(
+                array('US', 'CA', 'California'),
+            ),
+            array(
+                array('223', 'US', '', '', '', '38', '-97'),
+                array('2221', 'US', 'CA', '', '', '34.0522', '-118.243'),
+                array('11101', 'US', '', 'San Francisco', '', '37.7749', '-122.4194'),
+            ),
+            array(
+                array('68444672', '68444735', '11101'),
+                array('68444800', '68444927', '11101'),
+            )
+        );
+        $this->_import(
+            array(
+                array('United States', 'US'),
+            ),
+            array(
+                array('US', 'CA', 'California'),
+            ),
+            array(
+                array('223', 'US', '', '', '', '38', '-97'),
+                array('2221', 'US', 'CA', '', '', '34.0522', '-118.243'),
+                array('11101', 'US', 'CA', 'San Francisco', '', '37.7749', '-122.4194'),
+            ),
+            array(
+                array('68444672', '68444735', '11101'),
+                array('68444800', '68444927', '11101'),
+            )
+        );
+        $this->_verify(
+            array(
+                array('id' => 1, 'abbreviation' => 'US', 'name' => 'United States'),
+            ),
+            array(
+                array('id' => 1, 'countryId' => 1, 'name' => 'California', '_maxmind' => 'USCA', 'abbreviation' => 'CA'),
+            ),
+            array(
+                array('id'       => 1, 'stateId' => 1, 'countryId' => 1, 'name' => 'San Francisco', 'lat' => 37.7749, 'lon' => -122.419,
+                      '_maxmind' => 11101),
+            ),
+            array(),
+            array(),
+            array(
+                array('cityId' => 1, 'ipStart' => 68444800, 'ipEnd' => 68444927),
+                array('cityId' => 1, 'ipStart' => 68444672, 'ipEnd' => 68444735),
+            )
+        );
+    }
+
+    public function testUpdateCityRegion_unset() {
+        $this->_import(
+            array(
+                array('United States', 'US'),
+            ),
+            array(
+                array('US', 'CA', 'California'),
+            ),
+            array(
+                array('223', 'US', '', '', '', '38', '-97'),
+                array('2221', 'US', 'CA', '', '', '34.0522', '-118.243'),
+                array('11101', 'US', 'CA', 'San Francisco', '', '37.7749', '-122.4194'),
+            ),
+            array(
+                array('68444672', '68444735', '11101'),
+                array('68444800', '68444927', '11101'),
+            )
+        );
+        $this->_import(
+            array(
+                array('United States', 'US'),
+            ),
+            array(
+                array('US', 'CA', 'California'),
+            ),
+            array(
+                array('223', 'US', '', '', '', '38', '-97'),
+                array('2221', 'US', 'CA', '', '', '34.0522', '-118.243'),
+                array('11101', 'US', '', 'San Francisco', '', '37.7749', '-122.4194'),
+            ),
+            array(
+                array('68444672', '68444735', '11101'),
+                array('68444800', '68444927', '11101'),
+            )
+        );
+        $this->_verify(
+            array(
+                array('id' => 1, 'abbreviation' => 'US', 'name' => 'United States'),
+            ),
+            array(
+                array('id' => 1, 'countryId' => 1, 'name' => 'California', '_maxmind' => 'USCA', 'abbreviation' => 'CA'),
+            ),
+            array(
+                array('id'       => 1, 'stateId' => null, 'countryId' => 1, 'name' => 'San Francisco', 'lat' => 37.7749, 'lon' => -122.419,
+                      '_maxmind' => 11101),
+            ),
+            array(),
+            array(),
+            array(
+                array('cityId' => 1, 'ipStart' => 68444800, 'ipEnd' => 68444927),
+                array('cityId' => 1, 'ipStart' => 68444672, 'ipEnd' => 68444735),
+            )
+        );
+    }
+
     public function testAddZipCode() {
         $this->_import(
             array(
