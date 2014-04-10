e1276c593e71fff98d98819673af816303cc82e2
Reto Kaiser <reto@cargomedia.ch>
2014-04-10 15:23:09 +0200

    Allow to show tab programmatically in jquery.tabs

--- client-vendor/after-body/jquery.tabs/jquery.tabs.js
+++ client-vendor/after-body/jquery.tabs/jquery.tabs.js
@@ -2,32 +2,69 @@
  * Author: CM
  */
 (function($) {
-  $.fn.tabs = function() {
+
+  /**
+   * @param {jQuery} $buttonsContainer
+   * @constructor
+   */
+  var Tabs = function($buttonsContainer) {
+    this.$buttonsContainer = $buttonsContainer;
+    this.$contentContainer = this.$buttonsContainer.next('.tabs-content');
+    if (!this.$contentContainer.length) {
+      throw 'No tabs contents found';
+    }
+
+    var self = this;
+    this.$buttonsContainer.on('click', 'a', function(event) {
+      self.showTab($(this).closest('.tabs > *'));
+    });
+
+    var $tabs = this.$buttonsContainer.find('> *');
+    var $activeTab = $tabs.filter('.active');
+    if (!$activeTab.length) {
+      $activeTab = $tabs.first();
+    }
+    this.showTab($activeTab);
+  };
+
+  Tabs.prototype = {
+    $buttonsContainer: null,
+    $contentContainer: null,
+
+    showTabByName: function(tab) {
+      var $tab = this.$buttonsContainer.children('[data-tab="' + tab + '"]:first');
+      if (!$tab.length) {
+        throw 'No tab with name `' + tab + '` found';
+      }
+      this.showTab($tab);
+    },
+
+    showTab: function($tab) {
+      var index = $tab.index();
+      $tab.addClass('active').siblings().removeClass('active');
+      var $tabContent = this.$contentContainer.find('> *').eq(index);
+      $tabContent.addClass('active').show().find(':focusable:first').focus();
+      $tabContent.siblings().removeClass('active').hide();
+    }
+  };
+
+  /**
+   * @param {String} [tab]
+   * @return {jQuery}
+   */
+  $.fn.tabs = function(tab) {
     return this.each(function() {
-      var $buttonsContainer = $(this);
-      var $contentContainer = $buttonsContainer.next('.tabs-content');
-      if (!$contentContainer.length) {
-        return;
+      var $self = $(this);
+      var tabs = $self.data('tabs');
+
+      if (!tabs) {
+        tabs = new Tabs($self);
+        $self.data('tabs', tabs);
       }
 
-      function showTab($tab) {
-        var index = $tab.index();
-        $tab.addClass('active').siblings().removeClass('active');
-        var $tabContent = $contentContainer.find('> *').eq(index);
-        $tabContent.addClass('active').show().find(':focusable:first').focus();
-        $tabContent.siblings().removeClass('active').hide();
+      if (tab) {
+        tabs.showTabByName(tab);
       }
-
-      $buttonsContainer.on('click', 'a', function(event) {
-        showTab($(this).closest('.tabs > *'));
-      });
-
-      var $tabs = $buttonsContainer.find('> *');
-      var $activeTab = $tabs.filter('.active');
-      if (!$activeTab.length) {
-        $activeTab = $tabs.first();
-      }
-      showTab($activeTab);
     });
   };
 })(jQuery);
