38a46daeffed038d101ccfcec14136958dd789cb
Reto Kaiser <reto@cargomedia.ch>
2014-04-10 19:51:34 +0200

    Set debug=true for testing example cmp

--- tests/library/CM/Page/ExampleTest.php
+++ tests/library/CM/Page/ExampleTest.php
@@ -2,16 +2,39 @@
 
 class CM_Page_ExampleTest extends CMTest_TestCase {
 
-    public function testAccessible() {
-        $page = $this->_createPage('CM_Page_Example');
+    public function testNotAccessible() {
+        $debugBackup = CM_Bootloader::getInstance()->isDebug();
+        CM_Bootloader::getInstance()->setDebug(false);
 
+        $page = $this->_createPage('CM_Page_Example');
         $this->assertPageViewable($page);
+
+        CM_Bootloader::getInstance()->setDebug($debugBackup);
+    }
+
+    public function testAccessible() {
+        $debugBackup = CM_Bootloader::getInstance()->isDebug();
+
+        CM_Bootloader::getInstance()->setDebug(true);
+        $page = $this->_createPage('CM_Page_Example');
+        $this->assertPageViewable($page);
+        $this->_renderPage($page);
+
+        CM_Bootloader::getInstance()->setDebug(false);
+        $page = $this->_createPage('CM_Page_Example');
+        $this->assertPageNotRenderable($page);
+
+        CM_Bootloader::getInstance()->setDebug($debugBackup);
     }
 
     public function testTidy() {
+        $debugBackup = CM_Bootloader::getInstance()->isDebug();
+
+        CM_Bootloader::getInstance()->setDebug(true);
         $page = $this->_createPage('CM_Page_Example');
         $html = $this->_renderPage($page);
-
         $this->assertTidy($html, false);
+
+        CM_Bootloader::getInstance()->setDebug($debugBackup);
     }
 }
