e20b325e45a24bfb2efd7625f2dd141d1640ce50
Reto Kaiser <reto@cargomedia.ch>
2014-04-10 18:02:19 +0200

    Use "_changeState" for example tabs

--- library/CM/Page/Example.js
+++ library/CM/Page/Example.js
@@ -5,5 +5,13 @@
 var CM_Page_Example = CM_Page_Abstract.extend({
 
   /** @type String */
-  _class: 'CM_Page_Example'
+  _class: 'CM_Page_Example',
+
+  _stateParams: ['tab'],
+
+  _changeState: function(state) {
+    if (state['tab']) {
+      this.findChild('CM_Component_Example').showTab(state['tab']);
+    }
+  }
 });
