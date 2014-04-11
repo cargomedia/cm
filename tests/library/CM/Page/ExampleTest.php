d815902474fb7c5ea064582c2d44eb66a6216523
Reto Kaiser <reto@cargomedia.ch>
2014-04-11 18:18:34 +0200

    Tidy up example page test

--- tests/library/CM/Page/ExampleTest.php
+++ tests/library/CM/Page/ExampleTest.php
@@ -2,22 +2,11 @@
 
 class CM_Page_ExampleTest extends CMTest_TestCase {
 
-    public function testNotAccessible() {
-        $debugBackup = CM_Bootloader::getInstance()->isDebug();
-        CM_Bootloader::getInstance()->setDebug(false);
-
-        $page = $this->_createPage('CM_Page_Example');
-        $this->assertPageViewable($page);
-
-        CM_Bootloader::getInstance()->setDebug($debugBackup);
-    }
-
     public function testAccessible() {
         $debugBackup = CM_Bootloader::getInstance()->isDebug();
 
         CM_Bootloader::getInstance()->setDebug(true);
         $page = $this->_createPage('CM_Page_Example');
-        $this->assertPageViewable($page);
         $this->_renderPage($page);
 
         CM_Bootloader::getInstance()->setDebug(false);
