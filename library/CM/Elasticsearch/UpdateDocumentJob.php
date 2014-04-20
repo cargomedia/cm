43dd6ce328aea0b307a080f47bdb9b82cfd13864
Reto Kaiser <reto@cargomedia.ch>
2014-04-20 16:09:23 +0200

    Pass id-array to index::update()

--- library/CM/Elasticsearch/UpdateDocumentJob.php
+++ library/CM/Elasticsearch/UpdateDocumentJob.php
@@ -9,7 +9,7 @@ class CM_Elasticsearch_UpdateDocumentJob extends CM_Jobdistribution_Job_Abstract
         /** @var CM_Elastica_Type_Abstract $index */
         $index = new $indexClassName();
 
-        $index->update($id);
+        $index->update(array($id));
         $index->getIndex()->refresh();
     }
 }
