9b5ae5d1bc75a8e9e88ac8d48186adcc43384e6c
Reto Kaiser <reto@cargomedia.ch>
2014-04-30 18:55:36 +0200

    Revert "Delete cache-clockwork"
    This reverts commit dcdad8b65a188710d575ea6bcc84031f3ab1d197.

---
+++ tests/library/CM/Cache/SharedTest.php
@@ -0,0 +1,36 @@
+<?php
+
+class CM_Cache_SharedTest extends CMTest_TestCase {
+
+    public function testKeys() {
+        $cache = CM_Cache_Shared::getInstance();
+        $cache->set('key1', 'data1');
+        $cache->set('key2', 'data2');
+        $this->assertEquals('data1', $cache->get('key1'));
+        $this->assertEquals('data2', $cache->get('key2'));
+
+        $this->assertFalse($cache->get('keyNonexistent'));
+
+        $cache->delete('key1');
+        $this->assertFalse($cache->get('key1'));
+        $this->assertEquals('data2', $cache->get('key2'));
+    }
+
+    public function testTagged() {
+        $cache = CM_Cache_Shared::getInstance();
+        $cache->setTagged('tag1', 'key1', 'data1');
+        $cache->setTagged('tag1', 'key2', 'data2');
+        $cache->setTagged('tag2', 'key3', 'data3');
+        $this->assertEquals('data1', $cache->getTagged('tag1', 'key1'));
+        $this->assertEquals('data2', $cache->getTagged('tag1', 'key2'));
+        $this->assertEquals('data3', $cache->getTagged('tag2', 'key3'));
+
+        $this->assertFalse($cache->getTagged('tag1', 'keyNonexistent'));
+        $this->assertFalse($cache->getTagged('tagNonexistent', 'key1'));
+
+        $cache->deleteTag('tag1');
+        $this->assertFalse($cache->getTagged('tag1', 'key1'));
+        $this->assertFalse($cache->getTagged('tag1', 'key2'));
+        $this->assertEquals('data3', $cache->getTagged('tag2', 'key3'));
+    }
+}
---
+++ tests/library/CM/Cache/Storage/AbstractTest.php
@@ -0,0 +1,45 @@
+<?php
+
+class CM_Cache_Storage_AbstractTest extends CMTest_TestCase {
+
+    public function tearDown() {
+        CM_Cache_Storage_Runtime::getInstance()->flush();
+    }
+
+    public function testGetSet() {
+        $cacheStorage = $this->getMockBuilder('CM_Cache_Storage_Abstract')->setMethods(array('_set', '_get',
+            '_getRuntime'))->getMockForAbstractClass();
+        $localStorage = array();
+        $cacheStorage->expects($this->any())->method('_set')->will(new PHPUnit_Framework_MockObject_Stub_ReturnCallback(function ($key, $value) use (&$localStorage) {
+            $localStorage[$key] = $value;
+        }));
+        $cacheStorage->expects($this->exactly(2))->method('_get')->will(new PHPUnit_Framework_MockObject_Stub_ReturnCallback(function ($key) use (&$localStorage) {
+            if (!array_key_exists($key, $localStorage)) {
+                return false;
+            }
+            return $localStorage[$key];
+        }));
+        $cacheRuntime = new CM_Cache_Storage_Runtime();
+        $cacheStorage->expects($this->any())->method('_getRuntime')->will($this->returnValue($cacheRuntime));
+
+        /** @var CM_Cache_Abstract $cacheStorage */
+        $this->assertFalse($cacheStorage->get('foo'));
+        $cacheStorage->set('foo', 'bar');
+        $this->assertSame('bar', $cacheStorage->get('foo'));
+        $cacheRuntime->flush();
+        $this->assertSame('bar', $cacheStorage->get('foo'));
+    }
+
+    public function testGetMulti() {
+        $cacheRuntime = new CM_Cache_Storage_Runtime();
+        $cacheStorage = $this->getMockBuilder('CM_Cache_Storage_Memcache')->setMethods(array('_getRuntime'))->getMock();
+        $cacheStorage->expects($this->any())->method('_getRuntime')->will($this->returnValue($cacheRuntime));
+
+        $this->assertSame(false, $cacheRuntime->get('foo'));
+        $this->assertSame(false, $cacheRuntime->get('missed'));
+        $cacheStorage->set('foo', 'bar');
+        $this->assertSame(array('foo' => 'bar'), $cacheStorage->getMulti(array('foo', 'missed')));
+        $this->assertSame('bar', $cacheRuntime->get('foo'));
+        $this->assertSame(false, $cacheRuntime->get('missed'));
+    }
+}
