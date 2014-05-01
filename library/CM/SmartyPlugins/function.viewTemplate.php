3e2ad777cdd9e359c9ad2e394f924a7310247a8c
tomaszdurka <tomasz@durka.pl>
2014-05-02 00:34:03 +0200

    Adjust stack form reference

--- library/CM/SmartyPlugins/function.viewTemplate.php
+++ library/CM/SmartyPlugins/function.viewTemplate.php
@@ -3,10 +3,9 @@
 function smarty_function_viewTemplate(array $params, Smarty_Internal_Template $template) {
     /** @var CM_Render $render */
     $render = $template->smarty->getTemplateVars('render');
-    /** @var CM_View_Abstract $view */
-    $view = $render->getStackLast('views');
+    $viewResponse = $render->getStackLast('views');
 
     $tplName = (string) $params['file'];
     unset($params['file']);
-    return $render->fetchViewTemplate($view, $tplName, $params);
+    return $render->fetchViewTemplate($viewResponse->getView(), $tplName, $params);
 }
