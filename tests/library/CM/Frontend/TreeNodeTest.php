3cfe75c50e53e2e0a90b290ed0b5ce16786d6fea
tomaszdurka <tomasz@durka.pl>
2014-05-02 01:07:07 +0200

    Extend TreeNode tests

--- tests/library/CM/Frontend/TreeNodeTest.php
+++ tests/library/CM/Frontend/TreeNodeTest.php
@@ -7,7 +7,7 @@ class CM_Frontend_TreeNodeTest extends CMTest_TestCase {
         /** @var CM_Layout_Abstract $layout */
         $page = $this->getMockBuilder('CM_Page_Abstract')->getMockForAbstractClass();
         /** @var CM_Page_Abstract $page */
-        $component = $this->getMockBuilder('CM_View_Abstract')->getMockForAbstractClass();
+        $component = $this->getMockBuilder('CM_Component_Abstract')->getMockForAbstractClass();
         /** @var CM_Component_Abstract $component */
 
         $node1 = new CM_Frontend_TreeNode(new CM_ViewResponse($layout));
@@ -21,6 +21,10 @@ class CM_Frontend_TreeNodeTest extends CMTest_TestCase {
         $node4 = new CM_Frontend_TreeNode(new CM_ViewResponse($component));
         $node4->setParent($node3);
 
+        $this->assertSame($node4, $node4->getClosest('CM_View_Abstract'));
+        $this->assertSame($node4, $node4->getClosest('CM_Component_Abstract'));
         $this->assertSame($node2, $node4->getClosest('CM_Page_Abstract'));
+        $this->assertSame($node1, $node4->getClosest('CM_Layout_Abstract'));
+        $this->assertSame(null, $node4->getClosest('CM_Form_Abstract'));
     }
 }
