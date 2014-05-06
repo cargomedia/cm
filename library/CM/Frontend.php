3bdcaf688bac62313f4a9079f2e471282ec488c8
tomaszdurka <tomasz@durka.pl>
2014-05-06 11:29:09 +0200

    Simplify frontend access

--- library/CM/Frontend.php
+++ library/CM/Frontend.php
@@ -148,7 +148,7 @@ class CM_Frontend {
         $code .= 'el:$("#' . $viewResponse->getAutoId() . '").get(0),';
         $code .= 'params:' . CM_Params::encode($view->getParams()->getAllOriginal(), true);
 
-        $parentNode = $this->_render->getFrontend()->getTreeCurrent()->getParent();
+        $parentNode = $this->getTreeCurrent()->getParent();
         if ($parentNode) {
             $code .= ',parent:cm.views["' . $parentNode->getValue()->getAutoId() . '"]';
         }
