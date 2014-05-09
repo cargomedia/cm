e58e13740452c5657dc10d22e0682a5b7d1a9eb1
Reto Kaiser <reto@cargomedia.ch>
2014-05-09 12:38:39 +0200

    Add wowza thumbnail import cli command

--- library/CM/Stream/Cli.php
+++ library/CM/Stream/Cli.php
@@ -9,6 +9,25 @@ class CM_Stream_Cli extends CM_Cli_Runnable_Abstract {
         CM_Stream_Message::getInstance()->startSynchronization();
     }
 
+    /**
+     * @param string  $streamName
+     * @param CM_File $thumbnailSource
+     * @throws CM_Exception_Invalid
+     */
+    public function wowzaImportThumbnail($streamName, CM_File $thumbnailSource) {
+        $streamChannel = CM_Model_StreamChannel_Video::findByKey($streamName);
+        if (!$streamChannel) {
+            throw new CM_Exception_Invalid('Cannot find streamChannel with key `' . $streamName . '`.');
+        }
+        $thumbnailCount = $streamChannel->getThumbnailCount();
+        $thumbnailDest = $streamChannel->getThumbnail($thumbnailCount + 1);
+        if (0 == $thumbnailCount) {
+            $thumbnailDest->ensureParentDirectory();
+        }
+        $thumbnailSource->copyToFile($thumbnailDest);
+        $streamChannel->setThumbnailCount($thumbnailCount + 1);
+    }
+
     public static function getPackageName() {
         return 'stream';
     }
