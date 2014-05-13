8e2adfca2c928ef143b46d91bb7097e479d7882d
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2014-05-13 19:44:13 +0200

    add BooleanTest

--- tests/library/CM/FormField/BooleanTest.php
+++ tests/library/CM/FormField/BooleanTest.php
@@ -8,6 +8,20 @@ class CM_FormField_BooleanTest extends CMTest_TestCase {
     }
 
     public function testRender() {
-        // @todo add some tests
+        $form = $this->getMockForm();
+        $field = new CM_FormField_Boolean();
+        $doc = $this->_renderFormField($form, $field, 'foo');
+
+        $this->assertFalse($doc->exists('.switch'));
+        $this->assertTrue($doc->exists('input[name="foo"][value="0"]'));
+        $this->assertTrue($doc->exists('input[name="foo"][value="1"]'));
+    }
+
+    public function testRenderSwitch() {
+        $form = $this->getMockForm();
+        $field = new CM_FormField_Boolean();
+        $doc = $this->_renderFormField($form, $field, 'foo', array('template' => 'switch'));
+
+        $this->assertTrue($doc->exists('.switch'));
     }
 }
