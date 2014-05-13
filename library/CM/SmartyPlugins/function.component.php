57f83363637882e62e506a057bcc9d9e41cbd953
tomaszdurka <tomasz@durka.pl>
2014-05-13 13:16:28 +0200

    Remove possibility to pass component as instance as name param

--- library/CM/SmartyPlugins/function.component.php
+++ library/CM/SmartyPlugins/function.component.php
@@ -9,15 +9,7 @@ function smarty_function_component(array $params, Smarty_Internal_Template $temp
     $render = $template->smarty->getTemplateVars('render');
     unset($params['name']);
 
-    if ($name instanceof CM_Component_Abstract) {
-        if (count($params)) {
-            throw new CM_Exception_Invalid('Cannot set any params when passing component directly');
-        }
-        $component = $name;
-    } else {
-        $component = CM_Component_Abstract::factory($name, $params);
-    }
-
+    $component = CM_Component_Abstract::factory($name, $params);
     if ($component instanceof CM_Page_Abstract) {
         $renderAdapter = new CM_RenderAdapter_Page($render, $component);
     } else {
