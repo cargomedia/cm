23c2a92ed7e0d691fd6756b8a9a826a6aeff373f
Reto Kaiser <reto@cargomedia.ch>
2014-05-19 21:30:57 +0200

    Implement getters for version response

--- library/CMService/AwsS3Versioning/Response/Version.php
+++ library/CMService/AwsS3Versioning/Response/Version.php
@@ -2,12 +2,42 @@
 
 class CMService_AwsS3Versioning_Response_Version {
 
+    /** @var string */
+    private $_id;
+
+    /** @var string */
+    private $_key;
+
+    /** @var DateTime */
+    private $_lastModified;
+
     /**
      * @param array $data
      */
     public function __construct(array $data) {
-        $key = $data['Key'];
-        $id = $data['VersionId'];
-        $lastModified = $data['LastModified'];
+        $this->_key = (string) $data['Key'];
+        $this->_id = (string) $data['VersionId'];
+        $this->_lastModified = new DateTime($data['LastModified']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getId() {
+        return $this->_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey() {
+        return $this->_key;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getLastModified() {
+        return $this->_lastModified;
     }
 }
