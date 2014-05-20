fac8b4c1f69c3735f09da6255d79e8338707779a
Reto Kaiser <reto@cargomedia.ch>
2014-05-20 21:32:21 +0200

    Add docu for manager-aware trait

--- library/CM/Service/ManagerAwareTrait.php
+++ library/CM/Service/ManagerAwareTrait.php
@@ -5,10 +5,17 @@ trait CM_Service_ManagerAwareTrait {
     /** @var CM_Service_Manager */
     private $_serviceManager;
 
+    /**
+     * @param CM_Service_Manager $serviceManager
+     */
     public function setServiceManager(CM_Service_Manager $serviceManager) {
         $this->_serviceManager = $serviceManager;
     }
 
+    /**
+     * @return CM_Service_Manager
+     * @throws CM_Exception_Invalid
+     */
     public function getServiceManager() {
         if (null === $this->_serviceManager) {
             throw new CM_Exception_Invalid('Service manager not set');
