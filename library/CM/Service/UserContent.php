1bb79be3e2ebc619e5288269bb9cc0b3f3ccf26e
Reto Kaiser <reto@cargomedia.ch>
2014-05-21 15:18:33 +0200

    Validate usercontent service config

--- library/CM/Service/UserContent.php
+++ library/CM/Service/UserContent.php
@@ -3,13 +3,15 @@
 class CM_Service_UserContent extends CM_Service_ManagerAware {
 
     /** @var array */
-    private $_config;
+    private $_configList;
 
     /**
-     * @param array $config
+     * @param array $configList
      */
-    public function __construct(array $config) {
-        $this->_config = $config;
+    public function __construct(array $configList) {
+        foreach ($configList as $namespace => $config) {
+            $this->_addNamespaceConfig($namespace, $config);
+        }
     }
 
     /**
@@ -26,7 +28,7 @@ class CM_Service_UserContent extends CM_Service_ManagerAware {
      * @return string
      */
     public function getUrl($namespace) {
-        return (string) $this->_getNamespaceConfig($namespace)['url'];
+        return $this->_getNamespaceConfig($namespace)['url'];
     }
 
     /**
@@ -34,7 +36,7 @@ class CM_Service_UserContent extends CM_Service_ManagerAware {
      */
     public function getFilesystemList() {
         $filesystemList = array();
-        foreach ($this->_config as $namespace => $config) {
+        foreach ($this->_configList as $namespace => $config) {
             $filesystemList[$namespace] = $this->getServiceManager()->get($config['filesystem'], 'CM_File_Filesystem');
         }
         return $filesystemList;
@@ -45,8 +47,8 @@ class CM_Service_UserContent extends CM_Service_ManagerAware {
      */
     public function getUrlList() {
         $urlList = array();
-        foreach ($this->_config as $namespace => $config) {
-            $urlList[$namespace] = (string) $config['url'];
+        foreach ($this->_configList as $namespace => $config) {
+            $urlList[$namespace] = $config['url'];
         }
         return $urlList;
     }
@@ -56,9 +58,22 @@ class CM_Service_UserContent extends CM_Service_ManagerAware {
      * @return array
      */
     private function _getNamespaceConfig($namespace) {
-        if (isset($this->_config[$namespace])) {
-            return $this->_config[$namespace];
+        if (isset($this->_configList[$namespace])) {
+            return $this->_configList[$namespace];
         }
-        return $this->_config['default'];
+        return $this->_configList['default'];
+    }
+
+    /**
+     * @param string $namespace
+     * @param array  $config
+     */
+    private function _addNamespaceConfig($namespace, array $config) {
+        $namespace = (string) $namespace;
+        $config = array(
+            'url'        => (string) $config['url'],
+            'filesystem' => (string) $config['filesystem'],
+        );
+        $this->_configList[$namespace] = $config;
     }
 }
