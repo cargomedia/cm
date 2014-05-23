a0799cb11d3cab2d3ed5f25c06b5df155a05b365
Jan Puziewicz <jan.puziewicz@cargomedia.ch>
2014-05-23 19:06:26 +0200

    iterating over CM_Cache_Storage_Abstract for cache clear

--- library/CM/Cache/Cli.php
+++ library/CM/Cache/Cli.php
@@ -3,16 +3,16 @@
 class CM_Cache_Cli extends CM_Cli_Runnable_Abstract {
 
     public function clear() {
-        echo 'Clearing cache...' . PHP_EOL;
-        $cache = new CM_Cache_Storage_Memcache();
-        $cache->flush();
+        $this->_getOutput()->writeln('Clearing cache...');
 
-        $cache = new CM_Cache_Storage_Apc();
-        $cache->flush();
+        $classes = CM_Util::getClassChildren('CM_Cache_Storage_Abstract', false);
+        foreach ($classes as $className) {
+            $this->_getOutput()->writeln('  ' . $className);
+            $cache = new $className;
+            $cache->flush();
+        }
 
-        $cache = new CM_Cache_Storage_File();
-        $cache->flush();
-        echo 'Cache cleared.' . PHP_EOL;
+        $this->_getOutput()->writeln('Cache cleared.');
     }
 
     public static function getPackageName() {
