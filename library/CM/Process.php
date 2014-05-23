1a4bf298ddd7f1833bf40890e03c0fe8fd2f631c
Reto Kaiser <reto@cargomedia.ch>
2014-05-23 20:53:14 +0200

    Add IPC stream to send back result+exception form forked workload

--- library/CM/Process.php
+++ library/CM/Process.php
@@ -27,17 +27,26 @@ class CM_Process {
      * @throws CM_Exception
      */
     public function fork(Closure $workload) {
+        $sockets = stream_socket_pair(STREAM_PF_UNIX, STREAM_SOCK_STREAM, STREAM_IPPROTO_IP);
+        if (false === $sockets) {
+            throw new CM_Exception('Cannot open stream socket pair');
+        }
+
         $pid = pcntl_fork();
         if ($pid === -1) {
             throw new CM_Exception('Could not spawn child process');
         }
         if ($pid) {
             // parent
-            $this->_forkHandlerList[$pid] = new CM_Process_ForkHandler($workload);
+            fclose($sockets[0]);
+            $this->_forkHandlerList[$pid] = new CM_Process_ForkHandler($workload, $sockets[1]);
         } else {
             // child
+            fclose($sockets[1]);
             $this->_reset();
-            $workload();
+            $forkHandler = new CM_Process_ForkHandler($workload, $sockets[0]);
+            $forkHandler->runAndSendWorkload();
+            $forkHandler->closeIpcStream();
             exit;
         }
     }
@@ -68,11 +77,13 @@ class CM_Process {
     /**
      * @param bool|null    $keepAlive
      * @param Closure|null $terminationCallback
+     * @return CM_Process_WorkloadResult[]
      * @throws CM_Exception
      */
     public function waitForChildren($keepAlive = null, Closure $terminationCallback = null) {
         $keepAlive = (bool) $keepAlive;
         $this->_terminationCallback = $terminationCallback;
+        $workloadResultList = array();
         do {
             $pid = pcntl_wait($status);
             pcntl_signal_dispatch();
@@ -80,6 +91,8 @@ class CM_Process {
                 throw new CM_Exception('Waiting on child processes failed');
             }
             $forkHandler = $this->_forkHandlerList[$pid];
+            $workloadResultList[] = $forkHandler->receiveWorkloadResult();
+            $forkHandler->closeIpcStream();
             unset($this->_forkHandlerList[$pid]);
             if ($keepAlive) {
                 $warning = new CM_Exception('Respawning dead child `' . $pid . '`.', null, array('severity' => CM_Exception::WARN));
@@ -89,6 +102,7 @@ class CM_Process {
             }
         } while (!empty($this->_forkHandlerList));
         $this->executeTerminationCallback();
+        return $workloadResultList;
     }
 
     /**
--- library/CM/Process/ForkHandler.php
+++ library/CM/Process/ForkHandler.php
@@ -5,11 +5,16 @@ class CM_Process_ForkHandler {
     /** @var Closure */
     private $_workload;
 
+    /** @var resource */
+    private $_ipcStream;
+
     /**
-     * @param Closure $workload
+     * @param Closure  $workload
+     * @param resource $ipcStream
      */
-    public function __construct(Closure $workload) {
+    public function __construct(Closure $workload, $ipcStream) {
         $this->_workload = $workload;
+        $this->_ipcStream = $ipcStream;
     }
 
     /**
@@ -17,5 +22,54 @@ class CM_Process_ForkHandler {
      */
     public function getWorkload() {
         return $this->_workload;
+    }
+
+    /**
+     * @return resource
+     */
+    public function getIpcStream() {
+        return $this->_ipcStream;
+    }
+
+    public function closeIpcStream() {
+        fclose($this->_ipcStream);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function runAndSendWorkload() {
+        $workload = $this->_workload;
+        $return = null;
+        $exception = null;
+        try {
+            $return = $workload();
+        } catch (Exception $e) {
+            $exception = $e;
+        }
+
+        $result = new CM_Process_WorkloadResult($return, $exception);
+        fwrite($this->_ipcStream, serialize($result));
+
+        if (null !== $exception) {
+            throw $exception;
+        }
+    }
+
+    /**
+     * @return CM_Process_WorkloadResult
+     * @throws CM_Exception
+     */
+    public function receiveWorkloadResult() {
+        $ipcData = fgets($this->_ipcStream);
+        if (false === $ipcData) {
+            throw new CM_Exception('Received no data from IPC stream.');
+        }
+        /** @var CM_Process_WorkloadResult $workloadResult */
+        $workloadResult = unserialize($ipcData);
+        if (!$workloadResult instanceof CM_Process_WorkloadResult) {
+            throw new CM_Exception('Received unexpected data over IPC stream.');
+        }
+        return $workloadResult;
     }
 }
