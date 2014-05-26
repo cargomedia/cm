e9ccd18bdff6bf2013ba6bb38387436f967df718
Reto Kaiser <reto@cargomedia.ch>
2014-05-26 19:28:59 +0200

    Add test for runMultiple with failures

--- tests/library/CM/Jobdistribution/Job/AbstractTest.php
+++ tests/library/CM/Jobdistribution/Job/AbstractTest.php
@@ -66,13 +66,8 @@ class CM_Jobdistribution_Job_AbstractTest extends CMTest_TestCase {
         $gearmanClient->expects($this->exactly(1))->method('setFailCallback');
         /** @var GearmanClient $gearmanClient */
 
-        $job = $this->getMockBuilder('CM_Jobdistribution_Job_Abstract')
-            ->setMethods(array('_getGearmanClient', '_execute', '_getJobName'))->getMockForAbstractClass();
+        $job = $this->getMockBuilder('CM_Jobdistribution_Job_Abstract')->setMethods(array('_getGearmanClient'))->getMockForAbstractClass();
         $job->expects($this->any())->method('_getGearmanClient')->will($this->returnValue($gearmanClient));
-        $job->expects($this->any())->method('_getJobName')->will($this->returnValue('myJob'));
-        $job->expects($this->any())->method('_execute')->will($this->returnCallback(function (CM_Params $params) {
-            return array_flip($params->getAllOriginal());
-        }));
         /** @var CM_Jobdistribution_Job_Abstract $job */
 
         $result = $job->runMultiple(array(
@@ -84,6 +79,41 @@ class CM_Jobdistribution_Job_AbstractTest extends CMTest_TestCase {
             array('bar1' => 'foo1'),
             array('bar2' => 'foo2'),
         ), $result);
+    }
+
+    /**
+     * @expectedException CM_Exception
+     * @expectedExceptionMessage Job `myJob` failed (1/2 results)
+     */
+    public function testRunMultipleWithFailures() {
+        if (!extension_loaded('gearman')) {
+            $this->markTestSkipped('Gearman Pecl Extension not installed.');
+        }
+        CM_Config::get()->CM_Jobdistribution_Job_Abstract->gearmanEnabled = true;
+
+        $gearmanClient = $this->getMock('GearmanClient', array('addTask', 'runTasks', 'setCompleteCallback', 'setFailCallback'));
+        $gearmanClient->expects($this->exactly(2))->method('addTask')->will($this->returnValue(true));
+        $gearmanClient->expects($this->exactly(1))->method('runTasks')->will($this->returnValue(true));
+        $gearmanClient->expects($this->exactly(1))->method('setCompleteCallback')->will($this->returnCallback(function ($completeCallback) {
+            $task1 = $this->getMockBuilder('GearmanTask')->setMethods(array('data'))->getMock();
+            $task1->expects($this->once())->method('data')->will($this->returnValue(array('bar1' => 'foo1')));
+            $completeCallback($task1);
+        }));
+        $gearmanClient->expects($this->exactly(1))->method('setFailCallback')->will($this->returnCallback(function ($failCallback) {
+            $failCallback(new GearmanTask());
+        }));
+        /** @var GearmanClient $gearmanClient */
+
+        $job = $this->getMockBuilder('CM_Jobdistribution_Job_Abstract')
+            ->setMethods(array('_getGearmanClient', '_getJobName'))->getMockForAbstractClass();
+        $job->expects($this->any())->method('_getGearmanClient')->will($this->returnValue($gearmanClient));
+        $job->expects($this->any())->method('_getJobName')->will($this->returnValue('myJob'));
+        /** @var CM_Jobdistribution_Job_Abstract $job */
+
+        $job->runMultiple(array(
+            array('foo1' => 'bar1'),
+            array('foo2' => 'bar2'),
+        ));
     }
 
     public function testRunGearmanDisabled() {
