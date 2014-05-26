fae18c1f39c47220fe8806983d88b31f24253739
Jan Puziewicz <jan.puziewicz@cargomedia.ch>
2014-05-26 18:42:32 +0200

    Changed usercontent image tag format

--- library/CM/Usertext/Filter/Markdown/UserContent.php
+++ library/CM/Usertext/Filter/Markdown/UserContent.php
@@ -16,9 +16,9 @@ class CM_Usertext_Filter_Markdown_UserContent implements CM_Usertext_Filter_Inte
 
     public function transform($text, CM_Render $render) {
         $text = (string) $text;
-        $text = preg_replace_callback('#!\[usercontent\]\(([^\]]+)\)#m', function ($matches) {
-            $relativeUrl = trim($matches[1]);
-            $userFile = new CM_File_UserContent('usercontent', $relativeUrl, null, $this->getServiceManager());
+        $text = preg_replace_callback('#!\[formatter\]\(([^\]]+)\)#m', function ($matches) {
+            $filename = trim($matches[1]);
+            $userFile = new CM_File_UserContent('formatter', $filename, null, $this->getServiceManager());
             return '<img src="' . $userFile->getUrl() . '" alt="image"/>';
         }, $text);
 
