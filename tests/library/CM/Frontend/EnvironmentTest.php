cc812a137161d8d0f3bc9fb7f6e13c625595f602
tomaszdurka <tomasz@durka.pl>
2014-05-27 15:45:41 +0200

    Add tests for CM_Frontend_Environment

---
+++ tests/library/CM/Frontend/EnvironmentTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class CM_Frontend_EnvironmentTest extends CMTest_TestCase {
+
+    public function testGetters() {
+        $site = CM_Site_Abstract::factory();
+        $user = CM_Model_User::createStatic();
+        $language = CM_Model_Language::create('English', 'en', true);
+        $timezone = new DateTimeZone('US/West');
+        $debug = true;
+        $environment = new CM_Frontend_Environment($site, $user, $language, $timezone, $debug);
+
+        $this->assertSame($site, $environment->getSite());
+        $this->assertSame($user, $environment->getViewer(true));
+        $this->assertSame($language, $environment->getLanguage());
+        $this->assertSame($language->getAbbreviation(), $environment->getLocale());
+        $this->assertSame($timezone, $environment->getTimeZone());
+        $this->assertSame($debug, $environment->isDebug());
+    }
+
+    public function testHasViewer() {
+        $environment = new CM_Frontend_Environment();
+        $this->assertFalse($environment->hasViewer());
+        $environment = new CM_Frontend_Environment(null, CM_Model_User::createStatic());
+        $this->assertTrue($environment->hasViewer());
+    }
+}
