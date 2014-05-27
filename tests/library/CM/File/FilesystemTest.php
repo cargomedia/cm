0b91ef432da382f610f42d584dfa639793a55ea1
Reto Kaiser <reto@cargomedia.ch>
2014-05-27 09:23:22 +0200

    Simplify filesystem test a bit

--- tests/library/CM/File/FilesystemTest.php
+++ tests/library/CM/File/FilesystemTest.php
@@ -2,23 +2,14 @@
 
 class CM_File_FilesystemTest extends CMTest_TestCase {
 
-    /** @var CM_File_Filesystem */
-    private $_filesystem;
-
-    /** @var string */
-    private $_path;
-
-    protected function setUp() {
-        $adapter = new CM_File_Filesystem_Adapter_Local();
-        $this->_filesystem = new CM_File_Filesystem($adapter);
-        $this->_path = CM_Bootloader::getInstance()->getDirTmp();
-    }
-
     protected function tearDown() {
         CMTest_TH::clearEnv();
     }
 
     public function testDeleteByPrefix() {
+        $filesystem = new CM_File_Filesystem(new CM_File_Filesystem_Adapter_Local());
+        $dirTmp = CM_Bootloader::getInstance()->getDirTmp();
+
         $pathList = array(
             'foo/foobar/bar',
             'foo/bar2',
@@ -27,7 +18,7 @@ class CM_File_FilesystemTest extends CMTest_TestCase {
         /** @var CM_File[] $fileList */
         $fileList = array();
         foreach ($pathList as $path) {
-            $file = new CM_File($this->_path . $path, $this->_filesystem);
+            $file = new CM_File($dirTmp . $path, $filesystem);
             $file->ensureParentDirectory();
             $file->write('hello');
             $fileList[] = $file;
@@ -38,13 +29,12 @@ class CM_File_FilesystemTest extends CMTest_TestCase {
             $this->assertTrue($file->getExists());
         }
 
-        $this->_filesystem->deleteByPrefix($this->_path);
+        $filesystem->deleteByPrefix($dirTmp);
 
         foreach ($fileList as $file) {
             $this->assertFalse($file->getExists());
         }
-        $dirBase = new CM_File($this->_path);
-        $this->assertTrue($dirBase->getExists());
+        $this->assertTrue((new CM_File($dirTmp))->getExists());
     }
 
     public function testNormalizePath() {
