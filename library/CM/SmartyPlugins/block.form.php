d68abeab8fb97d5a1fe9330d6b065064386b6651
tomaszdurka <tomasz@durka.pl>
2014-06-04 16:11:01 +0200

    Do not use factory within block.form (it's done inside anyway)

--- library/CM/SmartyPlugins/block.form.php
+++ library/CM/SmartyPlugins/block.form.php
@@ -5,7 +5,7 @@ function smarty_block_form($params, $content, Smarty_Internal_Template $template
     $render = $template->smarty->getTemplateVars('render');
     $frontend = $render->getGlobalResponse();
     if ($open) {
-        $form = CM_Form_Abstract::factory($params['name'], CM_Params::factory($params));
+        $form = CM_Form_Abstract::factory($params['name'], $params);
         $form->prepare($render->getEnvironment());
 
         $viewResponse = new CM_Frontend_ViewResponse($form);
