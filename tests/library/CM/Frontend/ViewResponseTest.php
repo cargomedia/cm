0132810fb273160c3f024ca3cbc630fd447ac27b
tomaszdurka <tomasz@durka.pl>
2014-06-10 15:19:34 +0200

    Add test for getCssClasses

---
+++ tests/library/CM/Frontend/ViewResponseTest.php
@@ -0,0 +1,30 @@
+<?php
+
+class CM_Frontend_ViewResponseTest extends CMTest_TestCase {
+
+    public function testSetGetTemplate() {
+        /** @var CM_View_Abstract $view */
+        $view = $this->getMock('CM_View_Abstract');
+        $viewResponse = new CM_Frontend_ViewResponse($view);
+
+        $this->assertSame('default', $viewResponse->getTemplateName());
+        $viewResponse->setTemplateName('foo');
+        $this->assertSame('foo', $viewResponse->getTemplateName());
+    }
+
+    public function testGetCssClasses() {
+        $view = $this->getMock('CM_View_Abstract', ['getClassHierarchy']);
+        $classNames = [
+            'foo',
+            'bar',
+        ];
+        $view->expects($this->any())->method('getClassHierarchy')->will($this->returnValue($classNames));
+        /** @var CM_View_Abstract $view */
+        $viewResponse = new CM_Frontend_ViewResponse($view);
+        $this->assertSame($classNames, $viewResponse->getCssClasses());
+
+        $viewResponse->setTemplateName('zoo');
+        $classNames[] = 'zoo';
+        $this->assertSame($classNames, $viewResponse->getCssClasses());
+    }
+}
