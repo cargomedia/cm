97e979e6ef3bb79013a7d27f82e26328cafaa47a
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2014-06-10 14:38:34 +0200

    Removed the implementation of CM_Usertext_Filter_Abstract::transform()
    - Added by error

--- library/CM/Usertext/Filter/Abstract.php
+++ library/CM/Usertext/Filter/Abstract.php
@@ -1,14 +1,10 @@
 <?php
 
-class CM_Usertext_Filter_Abstract implements CM_Usertext_Filter_Interface {
+abstract class CM_Usertext_Filter_Abstract implements CM_Usertext_Filter_Interface {
 
     public function getCacheKey() {
         return array('filter' => get_called_class());
     }
 
-    public function transform($text, CM_Frontend_Render $render) {
-        $text = (string) $text;
-        $text = htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
-        return $text;
-    }
+    abstract public function transform($text, CM_Frontend_Render $render);
 }
