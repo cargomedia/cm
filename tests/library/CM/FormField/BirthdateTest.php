67c062116dde3f131c64268540ff6c880cea7ee8
Jan Puziewicz <jan.puziewicz@cargomedia.ch>
2014-06-12 15:44:17 +0200

    Refactored CM_FormField_BirthdateTest

--- tests/library/CM/FormField/BirthdateTest.php
+++ tests/library/CM/FormField/BirthdateTest.php
@@ -2,17 +2,22 @@
 
 class CM_FormField_BirthdateTest extends CMTest_TestCase {
 
+    public function testValidate() {
+        $formField = new CM_FormField_Birthdate(['name' => 'foo', 'minAge' => 18, 'maxAge' => 30]);
+        $environment = new CM_Frontend_Environment();
+        $parsedValue = $formField->parseUserInput(array('year' => 1984, 'month' => 1, 'day' => 1));
+        $formField->validate($environment, $parsedValue);
+        $this->assertEquals(new DateTime('1984-01-01'), $parsedValue);
+    }
+
     /**
      * @expectedException CM_Exception_FormFieldValidation
      */
-    public function testValidate() {
+    public function testValidateInvalidYear()
+    {
         $formField = new CM_FormField_Birthdate(['name' => 'foo', 'minAge' => 18, 'maxAge' => 30]);
-        $request = CM_Request_Abstract::factory('get', '/foo');
-        $response = CM_Response_Abstract::factory($request);
         $environment = new CM_Frontend_Environment();
-        $value = $formField->validate($environment, array('year' => 1995, 'month' => 1, 'day' => 1));
-        $this->assertEquals(new DateTime('1995-01-01'), $value);
-
-        $formField->validate($environment, array('year' => 2005, 'month' => 1, 'day' => 1));
+        $parsedValue = $formField->parseUserInput(array('year' => 2000, 'month' => 1, 'day' => 1));
+        $formField->validate($environment, $parsedValue);
     }
 }
