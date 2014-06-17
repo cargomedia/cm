506372f719e8659d00e3f587128434abd9f74d01
Jan Puziewicz <jan.puziewicz@cargomedia.ch>
2014-06-17 10:34:46 +0200

    removed unnecessary validate()

--- library/CM/FormField/Float.php
+++ library/CM/FormField/Float.php
@@ -7,7 +7,6 @@ class CM_FormField_Float extends CM_FormField_Text {
     }
 
     public function validate(CM_Frontend_Environment $environment, $userInput) {
-        parent::validate($environment, $userInput);
         if (!is_numeric($userInput)) {
             throw new CM_Exception_FormFieldValidation('Not numeric');
         }
