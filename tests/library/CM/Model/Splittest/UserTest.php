7b61f1e99c782a8b1920e244ddb3d9ecad3338fe
tomaszdurka <tomasz@durka.pl>
2014-06-18 18:57:16 +0200

    clearEnv for splittest tests

--- tests/library/CM/Model/Splittest/UserTest.php
+++ tests/library/CM/Model/Splittest/UserTest.php
@@ -3,7 +3,7 @@
 class CM_Model_Splittest_UserTest extends CMTest_TestCase {
 
     public function tearDown() {
-        CMTest_TH::clearDb();
+        CMTest_TH::clearEnv();
     }
 
     public function testIsVariationFixture() {
