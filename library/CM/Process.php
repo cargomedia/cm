444af9c31b1fedac3ddb0ce840a8517e5bda1a62
Alexis Peter <alexis.peter@cargomedia.ch>
2014-06-18 17:00:52 +0200

    introduced CM_Process::listenForChildren() which doesn't halt execution

--- library/CM/Process.php
+++ library/CM/Process.php
@@ -58,37 +58,21 @@ class CM_Process {
     }
 
     /**
+     * @return CM_Process_WorkloadResult[]
+     * @throws CM_Exception
+     */
+    public function listenForChildren() {
+        return $this->_wait(null, null, true);
+    }
+
+    /**
      * @param bool|null    $keepAlive
      * @param Closure|null $terminationCallback
      * @return CM_Process_WorkloadResult[]
      * @throws CM_Exception
      */
     public function waitForChildren($keepAlive = null, Closure $terminationCallback = null) {
-        $keepAlive = (bool) $keepAlive;
-        $this->_terminationCallback = $terminationCallback;
-        $workloadResultList = array();
-        do {
-            $pid = pcntl_wait($status);
-            pcntl_signal_dispatch();
-            if (-1 === $pid) {
-                throw new CM_Exception('Waiting on child processes failed');
-            }
-            $forkHandlerSequence = $this->_getForkHandlerSequenceByPid($pid);
-            $forkHandler = $this->_forkHandlerList[$forkHandlerSequence];
-            $workloadResultList[$forkHandlerSequence] = $forkHandler->receiveWorkloadResult();
-            $forkHandler->closeIpcStream();
-            unset($this->_forkHandlerList[$forkHandlerSequence]);
-            if ($keepAlive) {
-                $warning = new CM_Exception('Respawning dead child `' . $pid . '`.', null, array('severity' => CM_Exception::WARN));
-                CM_Bootloader::getInstance()->getExceptionHandler()->handleException($warning);
-                usleep(self::RESPAWN_TIMEOUT * 1000000);
-                $this->_fork($forkHandler->getWorkload(), $forkHandlerSequence);
-            }
-        } while (!empty($this->_forkHandlerList));
-        $this->executeTerminationCallback();
-
-        ksort($workloadResultList);
-        return $workloadResultList;
+        return $this->_wait($keepAlive, $terminationCallback);
     }
 
     /**
@@ -157,6 +141,44 @@ class CM_Process {
         $this->_forkHandlerList = array();
         pcntl_signal(SIGTERM, SIG_DFL);
         pcntl_signal(SIGINT, SIG_DFL);
+    }
+
+    /**
+     * @param bool|null    $keepAlive
+     * @param Closure|null $terminationCallback
+     * @param boolean      $nohang
+     * @throws CM_Exception
+     * @return CM_Process_WorkloadResult[]
+     */
+    private function _wait($keepAlive = null, Closure $terminationCallback = null, $nohang = null) {
+        $keepAlive = (bool) $keepAlive;
+        $this->_terminationCallback = $terminationCallback;
+        $workloadResultList = array();
+        $waitOption = $nohang ? WNOHANG : 0;
+        do {
+            $pid = pcntl_wait($status, $waitOption);
+            var_dump($pid);
+            pcntl_signal_dispatch();
+            if (-1 === $pid) {
+                throw new CM_Exception('Waiting on child processes failed');
+            } elseif ($pid > 0) {
+                $forkHandlerSequence = $this->_getForkHandlerSequenceByPid($pid);
+                $forkHandler = $this->_forkHandlerList[$forkHandlerSequence];
+                $workloadResultList[$forkHandlerSequence] = $forkHandler->receiveWorkloadResult();
+                $forkHandler->closeIpcStream();
+                unset($this->_forkHandlerList[$forkHandlerSequence]);
+                if ($keepAlive) {
+                    $warning = new CM_Exception('Respawning dead child `' . $pid . '`.', null, array('severity' => CM_Exception::WARN));
+                    CM_Bootloader::getInstance()->getExceptionHandler()->handleException($warning);
+                    usleep(self::RESPAWN_TIMEOUT * 1000000);
+                    $this->_fork($forkHandler->getWorkload(), $forkHandlerSequence);
+                }
+            }
+        } while ((!$nohang && !empty($this->_forkHandlerList)) || ($nohang && $pid > 0));
+        $this->executeTerminationCallback();
+
+        ksort($workloadResultList);
+        return $workloadResultList;
     }
 
     /**
