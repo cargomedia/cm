86cfe91539d8a5bb9772ff0cc13276740ed884ae
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2014-06-24 14:10:59 +0200

    Renamed variable for consistency

--- library/CMService/KissMetrics/TrackEventJob.php
+++ library/CMService/KissMetrics/TrackEventJob.php
@@ -6,9 +6,9 @@ class CMService_KissMetrics_TrackEventJob extends CM_Jobdistribution_Job_Abstrac
         $userId = $params->getInt('userId');
         $eventName = $params->getString('eventName');
         $propertyList = $params->getArray('propertyList');
-        /** @var CMService_KissMetrics_Client $kissMetricsClient */
-        $kissMetricsClient = CM_Service_Manager::getInstance()->get('tracking-kissmetrics');
-        $kissMetricsClient->setUserId($userId);
-        $kissMetricsClient->trackEvent($eventName, $propertyList);
+        /** @var CMService_KissMetrics_Client $kissMetrics */
+        $kissMetrics = CM_Service_Manager::getInstance()->get('tracking-kissmetrics');
+        $kissMetrics->setUserId($userId);
+        $kissMetrics->trackEvent($eventName, $propertyList);
     }
 }
