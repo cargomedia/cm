9cf9f980d1646ad98718f19dd9906e63ae5519cc
Alexis Peter <alexis.peter@cargomedia.ch>
2014-07-08 16:38:07 +0200

    add compatibility with old, nonserialised meta info entries

--- library/CM/Paging/Log/Abstract.php
+++ library/CM/Paging/Log/Abstract.php
@@ -122,6 +122,9 @@ abstract class CM_Paging_Log_Abstract extends CM_Paging_Abstract implements CM_T
      */
     protected function _unserialize($value) {
         $result = @unserialize($value);
+        if (false === $result) {
+            return $value;
+        }
         if (is_array($result)) {
             foreach ($result as &$element) {
                 $element = $this->_unserialize($element);
