d956c64a64b43d8ddfbc41ef2b6edfb86562e919
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2014-07-11 11:53:03 +0200

    Used PHP_QUERY_RFC3986

--- library/CMService/KissMetrics/Transport/Socket.php
+++ library/CMService/KissMetrics/Transport/Socket.php
@@ -7,12 +7,7 @@ class CMService_KissMetrics_Transport_Socket implements \KISSmetrics\Transport\T
 
     public function submitData(array $dataList) {
         foreach ($dataList as $data) {
-            $query = http_build_query($data[1], '', '&');
-            $query = str_replace(
-                array('+', '%7E'),
-                array('%20', '~'),
-                $query
-            );
+            $query = http_build_query($data[1], '', '&', PHP_QUERY_RFC3986);
             $url = '/' . $data[0] . '?' . $query;
             $request = "GET $url HTTP/1.1\r\nHost: trk.kissmetrics.com\r\nConnection: Keep-Alive\r\n\r\n";
             $this->_sendRequest($request);
@@ -25,11 +20,12 @@ class CMService_KissMetrics_Transport_Socket implements \KISSmetrics\Transport\T
      */
     protected function _getSocket() {
         if (!self::$_socket) {
-            if (!self::$_socket = @fsockopen('trk.kissmetrics.com', 80, $errno, $errstr, 30)) {
-                throw new \KISSmetrics\Transport\TransportException('Cannot connect to the KISSmetrics server: ' . $errstr);
+            self::$_socket = @fsockopen('trk.kissmetrics.com', 80, $errno, $errstr, 30);
+            if (!self::$_socket) {
+                throw new \KISSmetrics\Transport\TransportException('Could not connect to the KISSmetrics server: ' . $errstr);
             }
             if (!@stream_set_blocking(self::$_socket, 0)) {
-                throw new \KISSmetrics\Transport\TransportException('Cannot switch the connection to the KISSmetrics server to non-blocking mode');
+                throw new \KISSmetrics\Transport\TransportException('Could not switch the connection to the KISSmetrics server to non-blocking mode');
             }
         }
         return self::$_socket;
@@ -49,7 +45,7 @@ class CMService_KissMetrics_Transport_Socket implements \KISSmetrics\Transport\T
                 self::$_socket = null;
             } else {
                 throw new \KISSmetrics\Transport\TransportException('Could not send the request (retried ' . $try . 'x): ' .
-                    preg_replace('/\s+/', ' ', $request));
+                    preg_replace('/\s+/', ' ', trim($request)));
             }
         }
     }
