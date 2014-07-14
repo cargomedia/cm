c0a905c57fa264d26c66d350a7d4b401dd6ee95a
tomaszdurka <tomasz@durka.pl>
2014-07-15 00:11:09 +0200

    Do not use form due to Mocka changes (mocka solution incoming)

--- tests/library/CM/SmartyPlugins/function.viewTemplateTest.php
+++ tests/library/CM/SmartyPlugins/function.viewTemplateTest.php
@@ -3,31 +3,31 @@
 class smarty_function_viewTemplateTest extends CMTest_TestCase {
 
     public function testRender() {
+        /** @var CM_Page_Abstract $page */
+        $page = $this->mockObject('CM_Page_Abstract');
+        $pageViewResponse = new CM_Frontend_ViewResponse($page);
+
         /** @var CM_Component_Abstract $component */
         $component = $this->mockObject('CM_Component_Abstract');
         $componentViewResponse = new CM_Frontend_ViewResponse($component);
 
-        /** @var CM_Form_Abstract $form */
-        $form = $this->mockObject('CM_Form_Abstract');
-        $formViewResponse = new CM_Frontend_ViewResponse($form);
-
         $render = $this->mockObject('CM_Frontend_Render');
         $method = $render->mockMethod('fetchViewTemplate')
-            ->at(0, function ($view, $templateName, $data) use ($form) {
-                $this->assertSame($form, $view);
+            ->at(0, function ($view, $templateName, $data) use ($component) {
+                $this->assertSame($component, $view);
                 $this->assertSame('jar', $templateName);
                 $this->assertArrayContains(['bar' => 'bar', 'foo' => 'foo'], $data);
             })
-            ->at(1, function ($view, $templateName, $data) use ($component) {
-                $this->assertSame($component, $view);
+            ->at(1, function ($view, $templateName, $data) use ($page) {
+                $this->assertSame($page, $view);
                 $this->assertSame('foo', $templateName);
             });
         /** @var CM_Frontend_Render $render */
+        $render->getGlobalResponse()->treeExpand($pageViewResponse);
         $render->getGlobalResponse()->treeExpand($componentViewResponse);
-        $render->getGlobalResponse()->treeExpand($formViewResponse);
 
         $render->parseTemplateContent('{viewTemplate name="jar" bar="bar"}', ['foo' => 'foo']);
-        $render->parseTemplateContent('{viewTemplate view="CM_Component_Abstract" name="foo"}');
+        $render->parseTemplateContent('{viewTemplate view="CM_Page_Abstract" name="foo"}');
 
         $this->assertSame(2, $method->getCallCount());
     }
