50fff87d08298205a4da41770948b671098556cb
Jan Puziewicz <jan.puziewicz@cargomedia.ch>
2014-07-15 16:44:38 +0200

    Added test for too many elements in suggest one FF

--- tests/library/CM/FormField/SuggestOneTest.php
+++ tests/library/CM/FormField/SuggestOneTest.php
@@ -8,10 +8,16 @@ class CM_FormField_SuggestOneTest extends CMTest_TestCase {
         $parsedInput = $field->parseUserInput('foo');
         $this->assertSame('foo', $parsedInput);
 
-        $parsedInput = $field->parseUserInput('foo,bar');
-        $this->assertSame('foo', $parsedInput);
-
         $parsedInput = $field->parseUserInput(null);
         $this->assertSame('', $parsedInput);
+    }
+
+    /**
+     * @expectedException CM_Exception_FormFieldValidation
+     */
+    public function testTooMany() {
+        $field = $this->getMockForAbstractClass('CM_FormField_SuggestOne');
+        $parsedInput = $field->parseUserInput('foo,bar');
+        $this->assertSame('foo', $parsedInput);
     }
 }
