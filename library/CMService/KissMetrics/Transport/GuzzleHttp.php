6c4b2e3cb58854cac4ff1269e48db25c2461c266
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2014-07-15 14:38:00 +0200

    Review requests
    - Used Guzzle 4.1 (4.1.2 upwards)
    - Relied on Guzzle throwing on errors (and don't throw on 2xx codes)

--- library/CMService/KissMetrics/Transport/GuzzleHttp.php
+++ library/CMService/KissMetrics/Transport/GuzzleHttp.php
@@ -11,11 +11,7 @@ class CMService_KissMetrics_Transport_GuzzleHttp implements \KISSmetrics\Transpo
         }
         foreach ($dataList as $data) {
             $url = '/' . $data[0] . '?' . http_build_query($data[1], '', '&', PHP_QUERY_RFC3986);
-            $response = self::$_client->get($url);
-            /** @var \GuzzleHttp\Message\Response $response */
-            if (200 !== $response->getStatusCode()) {
-                throw new \KISSmetrics\Transport\TransportException($response->getReasonPhrase());
-            }
+            self::$_client->get($url);
         }
     }
 }
