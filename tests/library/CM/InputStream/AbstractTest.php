af17ecc4b6a40d4a36f1492fcb374f33d97151dc
tomaszdurka <tomasz@durka.pl>
2014-07-24 23:38:39 +0200

    Adjust tests to use mocka

--- tests/library/CM/InputStream/AbstractTest.php
+++ tests/library/CM/InputStream/AbstractTest.php
@@ -3,16 +3,20 @@
 class CM_InputStream_AbstractTest extends CMTest_TestCase {
 
     public function testRead() {
-        $input = $this->getMockBuilder('CM_InputStream_Abstract')->setMethods(array('_read'))->getMockForAbstractClass();
-        $input->expects($this->exactly(3))->method('_read')->will($this->onConsecutiveCalls('foo', 'foo', 'bar'));
-        $input->expects($this->at(0))->method('_read')->with('Hint ');
-        $input->expects($this->at(1))->method('_read')->with(null);
-        $input->expects($this->at(2))->method('_read')->with(null);
-
+        $input = $this->mockClass('CM_InputStream_Abstract')->newInstance();
+        $readMethod = $input->mockMethod('_read')
+            ->at(0, function ($hint) {
+                $this->assertSame('Hint ', $hint);
+                return 'foo';
+            })
+            ->at(1, function ($hint) {
+                $this->assertNull($hint);
+                return '';
+            });
         /** @var $input CM_InputStream_Abstract */
         $this->assertSame('foo', $input->read('Hint'));
-        $this->assertSame('foo', $input->read());
         $this->assertSame('bar', $input->read(null, 'bar'));
+        $this->assertSame(2, $readMethod->getCallCount());
     }
 
     public function testConfirm() {
