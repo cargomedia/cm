075eedda9c6874b86a0f2e14efe92b72d06f0295
tomaszdurka <tomasz@durka.pl>
2014-08-06 17:14:07 +0200

    Test that CM_Paging_Translation_Language->remove, delete specific language value only (not all langs)

--- tests/library/CM/Paging/Translation/LanguageTest.php
+++ tests/library/CM/Paging/Translation/LanguageTest.php
@@ -3,12 +3,16 @@
 class CM_Paging_Translation_LanguageTest extends CMTest_TestCase {
 
     public function testRemove() {
-        $language = CM_Model_Language::create('Foo', 'foo', true);
-        $paging = $language->getTranslations();
-        $paging->set('phrase', 'abc');
-        $this->assertSame('abc', $paging->get('phrase', null, true));
-        $paging->remove('phrase');
-        $paging->_change();
-        $this->assertNull($paging->get('phrase', null, true));
+        $languagePagingFoo = CM_Model_Language::create('Foo', 'foo', true)->getTranslations();
+        $languagePagingBar = CM_Model_Language::create('Bar', 'bar', true)->getTranslations();
+
+        $languagePagingFoo->set('phrase', 'foo');
+        $languagePagingBar->set('phrase', 'bar');
+        $this->assertSame('foo', $languagePagingFoo->get('phrase', null, true));
+        $this->assertSame('bar', $languagePagingBar->get('phrase', null, true));
+
+        $languagePagingFoo->remove('phrase');
+        $this->assertNull($languagePagingFoo->get('phrase', null, true));
+        $this->assertSame('bar', $languagePagingBar->get('phrase', null, true));
     }
 }
