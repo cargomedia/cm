d0e9490cc18c21e4e5427a947fae6c499f500b19
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2014-08-11 17:46:21 +0200

    Revert "add smarty block.box plugin"
    This reverts commit eea5451ed68c15293e8ae03349310e4df1b711ac.

--- library/CM/SmartyPlugins/block.box_footer.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function smarty_block_box_footer($params, $content, Smarty_Internal_Template $template, $open) {
-    if ($open) {
-    } else {
-        $template->assign('box-footer', $content);
-    }
-}
--- library/CM/SmartyPlugins/block.box_header.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function smarty_block_box_header($params, $content, Smarty_Internal_Template $template, $open) {
-    if ($open) {
-    } else {
-        $template->assign('box-header', $content);
-    }
-}
