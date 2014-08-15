54ddff8b413c25b2b0e2ea8c2eccd5545444767e
Marian Sollmann <ms@sollmann-interactive.com>
2014-08-15 16:55:45 +0200

    use * for auto detect and remove namespace param

--- library/CM/SmartyPlugins/function.component.php
+++ library/CM/SmartyPlugins/function.component.php
@@ -10,22 +10,14 @@ function smarty_function_component(array $params, Smarty_Internal_Template $temp
     }
     $classname = null;
     $name = $params['name'];
-    $namespace = !empty($params['namespace']) ? $params['namespace'] : null;
-    $availableNamespaceList = $render->getSite()->getModules();
+    unset($params['name']);
 
     if (class_exists($name)) {
         $classname = $name;
     } else {
-        if ($namespace) {
-            if (!in_array($namespace, $availableNamespaceList)) {
-                throw new CM_Exception_Invalid('Given namespace is not available.', array(
-                    'namespace'           => $namespace,
-                    'availableNamespaces' => $availableNamespaceList,
-                ));
-            }
-            $classname = $namespace . '_' . $name;
-        } else {
-            foreach ($availableNamespaceList as $availableNamespace) {
+        if (0 === strpos($name, '*')) {
+            $name = str_replace('*_', '', $name);
+            foreach ($render->getSite()->getModules() as $availableNamespace) {
                 $classString = $availableNamespace . '_' . $name;
                 if (class_exists($classString)) {
                     $classname = $classString;
@@ -37,9 +29,6 @@ function smarty_function_component(array $params, Smarty_Internal_Template $temp
             }
         }
     }
-
-    unset($params['name']);
-    unset($params['namespace']);
 
     $component = CM_Component_Abstract::factory($classname, $params);
     $renderAdapter = CM_RenderAdapter_Component::factory($render, $component);
--- tests/library/CM/SmartyPlugins/function.componentTest.php
+++ tests/library/CM/SmartyPlugins/function.componentTest.php
@@ -21,13 +21,8 @@ class smarty_function_componentTest extends CMTest_TestCase {
         $this->assertTrue($html->has('.CM_Component_Notfound'));
     }
 
-    public function testRenderWithNamespace() {
-        $html = new CM_Dom_NodeList(smarty_function_component(array('name' => 'Component_Notfound', 'namespace' => 'CM'), $this->_template));
-        $this->assertTrue($html->has('.CM_Component_Notfound'));
-    }
-
     public function testRenderWithoutNamespace() {
-        $html = new CM_Dom_NodeList(smarty_function_component(array('name' => 'Component_Notfound'), $this->_template));
+        $html = new CM_Dom_NodeList(smarty_function_component(array('name' => '*_Component_Notfound'), $this->_template));
         $this->assertTrue($html->has('.CM_Component_Notfound'));
     }
 
@@ -36,14 +31,6 @@ class smarty_function_componentTest extends CMTest_TestCase {
      * @expectedExceptionMessage The class was not found in any namespace.
      */
     public function testRenderNotExists() {
-        smarty_function_component(array('name' => 'BAR_Component_Notfound'), $this->_template);
-    }
-
-    /**
-     * @expectedException CM_Exception_Invalid
-     * @expectedExceptionMessage Given namespace is not available.
-     */
-    public function testRenderNamespaceNotAvailable() {
-        smarty_function_component(array('name' => 'Component_Notfound', 'namespace' => 'FOO'), $this->_template);
+        smarty_function_component(array('name' => '*_Component_IsNotExisting'), $this->_template);
     }
 }
