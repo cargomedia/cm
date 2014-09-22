e7b2582d56af5f35caceb1a80129b6629770c38f
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2014-09-22 17:49:09 +0200

    Allow to render layout resources as smarty templates

--- library/CM/Response/Resource/Layout.php
+++ library/CM/Response/Resource/Layout.php
@@ -3,16 +3,23 @@
 class CM_Response_Resource_Layout extends CM_Response_Resource_Abstract {
 
     protected function _process() {
-        $file = null;
-        if ($path = $this->getRender()->getLayoutPath('resource/' . $this->getRequest()->getPath(), null, true, false)) {
-            $file = new CM_File($path);
-        }
-        if (!$file) {
-            throw new CM_Exception_Nonexistent('Invalid filename: `' . $this->getRequest()->getPath() . '`', null, array('severity' => CM_Exception::WARN));
+        $content = null;
+        $mimeType = null;
+
+        if ($pathRaw = $this->getRender()->getLayoutPath('resource/' . $this->getRequest()->getPath(), null, true, false)) {
+            $file = new CM_File($pathRaw);
+            $content = $file->read();
+            $mimeType = $file->getMimeType();
+        } elseif ($pathTpl = $this->getRender()->getLayoutPath('resource/' . $this->getRequest()->getPath() . '.tpl', null, true, false)) {
+            $content = $this->getRender()->fetchTemplate($pathTpl);
+            $mimeType = CM_File::getMimeTypeByContent($content);
+        } else {
+            throw new CM_Exception_Nonexistent('Invalid filename: `' . $this->getRequest()->getPath() . '`', null,
+                array('severity' => CM_Exception::WARN));
         }
         $this->enableCache();
-        $this->setHeader('Content-Type', $file->getMimeType());
-        $this->_setContent($file->read());
+        $this->setHeader('Content-Type', $mimeType);
+        $this->_setContent($content);
     }
 
     public static function match(CM_Request_Abstract $request) {
