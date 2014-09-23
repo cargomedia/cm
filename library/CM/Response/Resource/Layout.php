78af139484716e3d4dc0576220f0b6df9ee6f4b8
Alexis Peter <alexis.peter@cargomedia.ch>
2014-09-23 18:33:38 +0200

    add some tests; adjust exceptions

--- library/CM/Response/Resource/Layout.php
+++ library/CM/Response/Resource/Layout.php
@@ -9,7 +9,7 @@ class CM_Response_Resource_Layout extends CM_Response_Resource_Abstract {
         if ($pathRaw = $this->getRender()->getLayoutPath('resource/' . $this->getRequest()->getPath(), null, true, false)) {
             $file = new CM_File($pathRaw);
             if (in_array($file->getExtension(), $this->_getFiletypesForbidden())) {
-                throw new CM_Exception_Nonexistent('Forbidden file-type', ['path' => $this->getRequest()->getPath()], ['severity' => CM_Exception::WARN]);
+                throw new CM_Exception_Nonexistent('Forbidden filetype', ['path' => $this->getRequest()->getPath()], ['severity' => CM_Exception::WARN]);
             }
             $content = $file->read();
             $mimeType = $file->getMimeType();
@@ -17,8 +17,7 @@ class CM_Response_Resource_Layout extends CM_Response_Resource_Abstract {
             $content = $this->getRender()->fetchTemplate($pathTpl);
             $mimeType = CM_File::getMimeTypeByContent($content);
         } else {
-            throw new CM_Exception_Nonexistent('Invalid filename: `' . $this->getRequest()->getPath() . '`', null,
-                array('severity' => CM_Exception::WARN));
+            throw new CM_Exception_Nonexistent('Invalid filename', ['path' => $this->getRequest()->getPath()], ['severity' => CM_Exception::WARN]);
         }
         $this->setHeader('Content-Type', $mimeType);
         $this->_setContent($content);
--- tests/library/CM/Response/Resource/LayoutTest.php
+++ tests/library/CM/Response/Resource/LayoutTest.php
@@ -2,13 +2,42 @@
 
 class CM_Response_Resource_LayoutTest extends CMTest_TestCase {
 
+    public function testProcess() {
+        $filePath = 'img/logo.png';
+        $response = $this->getResponseResourceLayout($filePath);
+        $this->assertContains('Content-Type: image/png', $response->getHeaders());
+    }
+
     public function testFiletypeForbidden() {
         $filePath = 'browserconfig.xml.smarty';
         try {
             $this->getResponseResourceLayout($filePath);
         } catch (CM_Exception_Nonexistent $ex) {
-            $this->assertSame('Forbidden file-type', $ex->getMessage());
+            $this->assertSame('Forbidden filetype', $ex->getMessage());
             $this->assertSame(['path' => '/browserconfig.xml.smarty'], $ex->getMetaInfo(true));
+        }
+    }
+
+    public function testRendering() {
+        $filePath = 'browserconfig.xml';
+        /** @var CM_Response_Resource_Layout $response */
+        $response = $this->getResponseResourceLayout($filePath);
+        $this->assertContains('Content-Type: application/xml', $response->getHeaders());
+        try {
+            $response->getRender()->getLayoutFile('resource/' . $filePath);
+            $this->fail('File to be rendered actually exists');
+        } catch (CM_Exception_Invalid $ex) {
+            $this->assertContains('Cannot find `resource/browserconfig.xml`', $ex->getMessage());
+        }
+    }
+
+    public function testNonexistentFile() {
+        $filePath = 'nonExistent.css';
+        try {
+            $this->getResponseResourceLayout($filePath);
+        } catch (CM_Exception_Nonexistent $ex) {
+            $this->assertSame('Invalid filename', $ex->getMessage());
+            $this->assertSame(['path' => '/nonExistent.css'], $ex->getMetaInfo(true));
         }
     }
 
