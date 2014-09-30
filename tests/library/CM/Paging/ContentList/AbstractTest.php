b150a57374eca66c1b9209c68ae4974c87aa786e
tomaszdurka <tomasz@durka.pl>
2014-09-30 14:26:48 +0200

    Add CM_Paging_ContentList_AbstractTest

---
+++ tests/library/CM/Paging/ContentList/AbstractTest.php
@@ -0,0 +1,41 @@
+<?php
+
+class CM_Paging_ContentList_AbstractTest extends CMTest_TestCase {
+
+    public function testAdd() {
+        $paging = $this->_getPaging();
+        $paging->add('foo');
+        $paging->addMultiple(['bar', 'zoo']);
+        $this->assertSame(['bar', 'foo', 'zoo'], $paging->getItems());
+        $paging->add('bar');
+        $this->assertSame(['bar', 'foo', 'zoo'], $paging->getItems());
+    }
+
+    public function testRemove() {
+        $paging = $this->_getPaging();
+        $paging->addMultiple(['foo', 'bar']);
+
+        $this->assertSame(['bar', 'foo'], $paging->getItems());
+        $paging->remove('foo');
+        $this->assertSame(['bar'], $paging->getItems());
+
+        $paging->remove('foo');
+        $this->assertSame(['bar'], $paging->getItems());
+    }
+
+    public function testRemoveAll() {
+        $paging = $this->_getPaging();
+        $paging->addMultiple(['foo', 'bar', 'zoo']);
+        $this->assertSame(3, $paging->getCount());
+        $paging->removeAll();
+        $this->assertSame(0, $paging->getCount());
+    }
+
+    /**
+     * @return \Mocka\AbstractClassTrait|CM_Paging_ContentList_Abstract
+     */
+    private function _getPaging() {
+        $type = rand(0, 100);
+        return $this->mockObject('CM_Paging_ContentList_Abstract', [$type]);
+    }
+}
