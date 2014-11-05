1c2144cf4a4b69100da7a94a003913d5fa1f133c
tomaszdurka <tomasz@durka.pl>
2014-11-05 17:25:08 +0100

    Add CM_MongoDb_SetupScript
    - load to create collections
    - unload to drop database
    - isLoaded to check if collections has been loaded

---
+++ library/CM/MongoDb/SetupScript.php
@@ -0,0 +1,27 @@
+<?php
+
+class CM_MongoDb_SetupScript extends CM_Provision_Script_Abstract {
+
+    public function load(CM_Service_Manager $manager, CM_OutputStream_Interface $output) {
+        $mongoClient = $manager->getMongoDb();
+
+        foreach (CM_Util::getResourceFiles('mongo/collections.json') as $dump) {
+            $collectionInfo = CM_Params::jsonDecode($dump->read());
+            foreach ($collectionInfo as $collection => $indexes) {
+                $mongoClient->createCollection($collection);
+                foreach ($indexes as $indexInfo) {
+                    $mongoClient->createIndex($collection, $indexInfo['key'], $indexInfo['options']);
+                }
+            }
+        }
+    }
+
+    public function unload(CM_Service_Manager $manager, CM_OutputStream_Interface $output) {
+        $manager->getMongoDb()->dropDatabase();
+    }
+
+    public function isLoaded(CM_Service_Manager $manager) {
+        $hasCollections = $manager->getMongoDb()->listCollectionNames() > 0;
+        return $hasCollections;
+    }
+}
