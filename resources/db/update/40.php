7a0e1a9a53f24ac296cb5d04b854f0bfa58930b2
tomaszdurka <tomasz@durka.pl>
2014-11-12 18:06:29 +0100

    Remove script

--- resources/db/update/40.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$setupScriptClassList = CM_Util::getClassChildren('CM_Provision_Script_Abstract');
-foreach ($setupScriptClassList as $setupScriptClass) {
-    if (in_array('CM_Provision_Script_IsLoadedOptionTrait', class_uses($setupScriptClass))) {
-        $optionName = 'SetupScript.' . $setupScriptClass;
-        CM_Option::getInstance()->set($optionName, true);
-    }
-}
