c1c53a8f9c028c3e5ffacc4bb73aa50993db8fc5
Alexis Peter <alexis.peter@cargomedia.ch>
2014-11-14 14:56:08 +0100

    delete duplicate languageKeys, keeping the one with the lowest id

---
+++ resources/db/update/40.php
@@ -0,0 +1,8 @@
+<?php
+
+$duplicates = CM_Db_Db::exec("SELECT * FROM (SELECT name, count(*) AS count FROM cm_model_languagekey GROUP BY BINARY name) AS t1 WHERE count > 1 ORDER BY count DESC;");
+while ($row = $duplicates->fetch()) {
+    $name = $row['name'];
+    CM_Db_Db::exec("DELETE cm_model_languagekey, cm_languageValue FROM cm_model_languagekey JOIN cm_languageValue ON (id = languageKeyId)
+                    WHERE name = ? AND id > (SELECT minId FROM (SELECT min(id) AS minId FROM cm_model_languagekey WHERE name = ?) as t1)", [$name, $name]);
+}
