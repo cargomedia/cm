49eecebbb90578eeee08dd2c9ad2b8078e06271b
Alexis Peter <alexis.peter@cargomedia.ch>
2014-11-14 14:24:35 +0100

    include languagekeys with trailing slashes in language-translation paging

--- tests/library/CM/Paging/Translation/LanguageTest.php
+++ tests/library/CM/Paging/Translation/LanguageTest.php
@@ -15,4 +15,13 @@ class CM_Paging_Translation_LanguageTest extends CMTest_TestCase {
         $this->assertNull($languagePagingFoo->get('phrase', null, true));
         $this->assertSame('bar', $languagePagingBar->get('phrase', null, true));
     }
+
+    public function testTrailingWhitespaceInLanguageKeyName() {
+        CM_Db_Db::insert('cm_model_languagekey', ['name'], [['foo '],['foo']]);
+
+        $language = CM_Model_Language::create('Foo', 'foo', true);
+        $language->getTranslations()->getAssociativeArray();
+        $this->assertEquals(['foo ', 'foo'], array_keys($language->getTranslations()->getAssociativeArray()));
+        $this->assertCount(2, $language->getTranslations());
+    }
 }
