cbdc8e93d2755eed66b41f3612ee5936b7e717d5
tomaszdurka <tomasz@durka.pl>
2014-11-28 12:49:03 +0100

    Do not create search indices on test clearEnv

--- resources/config/test.php
+++ resources/config/test.php
@@ -5,7 +5,6 @@ return function (CM_Config_Node $config) {
     $config->CM_App->setupScriptClasses[] = 'CM_File_Filesystem_SetupScript';
     $config->CM_App->setupScriptClasses[] = 'CM_Db_SetupScript';
     $config->CM_App->setupScriptClasses[] = 'CM_MongoDb_SetupScript';
-    $config->CM_App->setupScriptClasses[] = 'CM_Elasticsearch_SetupScript';
     $config->CM_App->setupScriptClasses[] = 'CM_App_SetupScript_Core';
 
     $config->CM_Mail->send = false;
