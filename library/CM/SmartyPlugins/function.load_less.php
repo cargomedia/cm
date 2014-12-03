7a20eb8f7d51cb478fd3afe373bc9e23699378ea
Marian Sollmann <ms@sollmann-interactive.com>
2014-12-03 09:57:43 +0100

    renamed load_less to load_lessFile

--- library/CM/SmartyPlugins/function.load_less.php
+++ library/CM/SmartyPlugins/function.load_less.php
@@ -1,6 +1,6 @@
 <?php
 
-function smarty_function_load_less(array $params, Smarty_Internal_Template $template) {
+function smarty_function_load_lessFile(array $params, Smarty_Internal_Template $template) {
     /** @var CM_Frontend_Render $render */
     $render = $template->smarty->getTemplateVars('render');
 
