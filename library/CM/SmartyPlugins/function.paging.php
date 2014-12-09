0f0ef63e848957cdf7f315f9f272d8fb94fac265
Reto Kaiser <reto@cargomedia.ch>
2014-12-09 13:03:48 +0100

    Change pagination styles

--- library/CM/SmartyPlugins/function.paging.php
+++ library/CM/SmartyPlugins/function.paging.php
@@ -17,14 +17,22 @@ function smarty_function_paging(array $params, Smarty_Internal_Template $templat
     $urlParams = !empty($params['urlParams']) ? (array) $params['urlParams'] : array();
 
     $ajax = empty($urlPage);
-    $size = 5;
+    $size = 1;
 
     if ($paging->getPageCount() <= 1) {
         return '';
     }
 
+    $class = 'function-paging';
+    if (1 === $paging->getPage()) {
+        $class .= ' paging-isFirst';
+    }
+    if ($paging->getPageCount() === $paging->getPage()) {
+        $class .= ' paging-isLast';
+    }
+
     $html = '';
-    $html .= '<div class="paging">';
+    $html .= '<div class="' . $class . '">';
 
     $boundDistMin = min($paging->getPage() - 1, $paging->getPageCount() - $paging->getPage());
     $sizeMax = $size - min($boundDistMin, floor($size / 2)) - 1;
@@ -33,22 +41,29 @@ function smarty_function_paging(array $params, Smarty_Internal_Template $templat
 
     if ($paging->getPage() > 1) {
         if ($pageMin > 1) {
-            $html .= _smarty_function_paging_link($render, $urlPage, $urlParams, $component, 1, '1', $ajax, 'pagingFirst');
+            $hrefFirst = _smarty_function_paging_href($render, $urlPage, $urlParams, $component, 1, $ajax);
+            $html .= _smarty_function_paging_link($hrefFirst, $render->getTranslation('First'), 'arrow-first', 'before', 'pagingFirst');
         }
-        $html .= _smarty_function_paging_link($render, $urlPage, $urlParams, $component,
-            $paging->getPage() - 1, '', $ajax, 'pagingPrev icon-arrow-left');
+        $hrefPrevious = _smarty_function_paging_href($render, $urlPage, $urlParams, $component, $paging->getPage() - 1, $ajax);
+        $html .= _smarty_function_paging_link($hrefPrevious, $render->getTranslation('Previous'), 'nav-left', 'before', 'pagingPrevious');
     }
 
     for ($p = $pageMin; $p <= $pageMax; $p++) {
-        $classActive = ($p == $paging->getPage()) ? 'active' : '';
-        $html .= _smarty_function_paging_link($render, $urlPage, $urlParams, $component, $p, $p, $ajax, 'pagingNumber ' . $classActive);
+        $labelNumber = $render->getTranslation('Page {$number}', ['number' => $p]);
+        if ($p == $paging->getPage()) {
+            $html .= '<span class="pagingCurrent">' . $labelNumber . '</span>';
+        } else {
+            $hrefNumber = _smarty_function_paging_href($render, $urlPage, $urlParams, $component, $p, $ajax);
+            $html .= _smarty_function_paging_link($hrefNumber, $labelNumber, null, null, 'pagingNumber');
+        }
     }
 
     if ($paging->getPage() < $paging->getPageCount()) {
-        $html .= _smarty_function_paging_link($render, $urlPage, $urlParams, $component,
-            $paging->getPage() + 1, $render->getTranslation('next'), $ajax, 'pagingNext');
+        $hrefNext = _smarty_function_paging_href($render, $urlPage, $urlParams, $component, $paging->getPage() + 1, $ajax);
+        $html .= _smarty_function_paging_link($hrefNext, $render->getTranslation('Next'), 'nav-right', 'after', 'pagingNext');
         if ($pageMax < $paging->getPageCount()) {
-            $html .= _smarty_function_paging_link($render, $urlPage, $urlParams, $component, $paging->getPageCount(), $paging->getPageCount(), $ajax, 'pagingLast');
+            $hrefLast = _smarty_function_paging_href($render, $urlPage, $urlParams, $component, $paging->getPageCount(), $ajax);
+            $html .= _smarty_function_paging_link($hrefLast, $render->getTranslation('Last'), 'arrow-last', 'after', 'pagingLast');
         }
     }
 
@@ -63,26 +78,53 @@ function smarty_function_paging(array $params, Smarty_Internal_Template $templat
  * @param array                    $urlParams
  * @param CM_Frontend_ViewResponse $component
  * @param int                      $page
- * @param string                   $text
  * @param bool                     $ajax
- * @param string|null              $class
  * @return string
  */
-function _smarty_function_paging_link(CM_Frontend_Render $render, $urlPage, array $urlParams, CM_Frontend_ViewResponse $component, $page, $text, $ajax, $class = null) {
+function _smarty_function_paging_href(CM_Frontend_Render $render, $urlPage, array $urlParams, CM_Frontend_ViewResponse $component, $page, $ajax) {
     if ($ajax) {
-        $href = 'javascript:;';
         $onClick = 'cm.views["' . $component->getAutoId() . '"].reload(' . json_encode(array('page' => $page)) . ')';
+        return 'javascript:' . CM_Util::htmlspecialchars($onClick) . ';';
     } else {
-        $href = $render->getUrlPage($urlPage, array_merge($urlParams, array('page' => $page)));
-        $onClick = null;
+        return $render->getUrlPage($urlPage, array_merge($urlParams, array('page' => $page)));
     }
-    $html = '<a href="' . $href . '"';
-    if ($onClick) {
-        $html .= ' onclick="' . htmlentities($onClick) . ';return false;"';
+}
+
+/**
+ * @param string      $href
+ * @param string      $label
+ * @param string|null $icon
+ * @param string|null $iconPosition
+ * @param string|null $class
+ * @return string
+ */
+function _smarty_function_paging_link($href, $label, $icon = null, $iconPosition = null, $class = null) {
+    if (!in_array($iconPosition, ['before', 'after'])) {
+        $iconPosition = 'before';
     }
-    if ($class) {
-        $html .= ' class="' . $class . '"';
+
+    $class = (string) $class;
+    $class .= ' hasLabel';
+
+    $iconHtml = null;
+    if (null !== $icon) {
+        if ('after' === $iconPosition) {
+            $class .= ' hasIconRight';
+        } else {
+            $class .= ' hasIcon';
+        }
+        $iconHtml .= '<span class="icon icon-' . $icon . '"></span>';
     }
-    $html .= '>' . $text . '</a>';
+
+    $html = '<a href="' . CM_Util::htmlspecialchars($href) . '" class="' . $class . '">';
+    if (null !== $iconHtml && 'before' === $iconPosition) {
+        $html .= $iconHtml;
+    }
+    $html .= '<span class="label">' . $label . '</span>';
+    if (null !== $iconHtml && 'after' === $iconPosition) {
+        $html .= $iconHtml;
+    }
+    $html .= '</a>';
+
     return $html;
 }
