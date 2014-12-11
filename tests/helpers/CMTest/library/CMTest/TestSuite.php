ce0dbbad4f681a8d4b6d7b3515ffb4defc98083e
Alexis Peter <alexis.peter@cargomedia.ch>
2014-12-11 16:27:31 +0100

    generate internal config in-memory when running tests

--- tests/helpers/CMTest/library/CMTest/TestSuite.php
+++ tests/helpers/CMTest/library/CMTest/TestSuite.php
@@ -10,6 +10,16 @@ class CMTest_TestSuite {
     }
 
     public function bootstrap() {
+        $this->generateInternalConfig();
         CMTest_TH::init();
+    }
+
+    public function generateInternalConfig() {
+        $generator = new CM_Config_Generator();
+        $internalConfig = $generator->getConfigClassTypes()->extendWithConfig($generator->getConfigActionVerbs());
+        $originalConfig = CM_Config::get();
+        $mergedConfig = $internalConfig->extendWithConfig($originalConfig);
+        $config = $mergedConfig->export();
+        CM_Config::set($config);
     }
 }
