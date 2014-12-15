9a5b5a5b2b00a6df43065c9f006f47456016a344
tomaszdurka <tomasz@durka.pl>
2014-12-15 10:59:37 +0100

    Adjust CM_Request -> CM_Http_Request

--- tests/library/CM/Response/Resource/Css/LibraryTest.php
+++ tests/library/CM/Response/Resource/Css/LibraryTest.php
@@ -4,7 +4,7 @@ class CM_Response_Resource_Css_LibraryTest extends CMTest_TestCase {
 
     public function testProcess() {
         $render = new CM_Frontend_Render(new CM_Frontend_Environment());
-        $request = new CM_Request_Get($render->getUrlResource('library-css', 'all.css'));
+        $request = new CM_Http_Request_Get($render->getUrlResource('library-css', 'all.css'));
         $response = new CM_Response_Resource_Css_Library($request);
         $response->process();
         $this->assertContains('body{', $response->getContent());
--- tests/library/CM/Response/Resource/Javascript/VendorTest.php
+++ tests/library/CM/Response/Resource/Javascript/VendorTest.php
@@ -4,7 +4,7 @@ class CM_Response_Resource_Javascript_VendorTest extends CMTest_TestCase {
 
     public function testProcessBeforeBody() {
         $render = new CM_Frontend_Render(new CM_Frontend_Environment());
-        $request = new CM_Request_Get($render->getUrlResource('vendor-js', 'before-body.js'));
+        $request = new CM_Http_Request_Get($render->getUrlResource('vendor-js', 'before-body.js'));
         $response = new CM_Response_Resource_Javascript_Vendor($request);
         $response->process();
         $this->assertContains('function()', $response->getContent());
@@ -13,7 +13,7 @@ class CM_Response_Resource_Javascript_VendorTest extends CMTest_TestCase {
 
     public function testProcessAfterBody() {
         $render = new CM_Frontend_Render(new CM_Frontend_Environment());
-        $request = new CM_Request_Get($render->getUrlResource('vendor-js', 'after-body.js'));
+        $request = new CM_Http_Request_Get($render->getUrlResource('vendor-js', 'after-body.js'));
         $response = new CM_Response_Resource_Javascript_Vendor($request);
         $response->process();
         $this->assertContains('function()', $response->getContent());
