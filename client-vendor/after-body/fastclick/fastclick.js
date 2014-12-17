f4fdf1de3b5b908384fa204850c6fdfb57823de8
vogdb <vozhdb@gmail.com>
2014-12-17 19:47:26 +0300

    Revert "re-add fastclick fix"
    This reverts commit 913872065689ba103038bcf7eb509fbae18e1392.

--- client-vendor/after-body/fastclick/fastclick.js
+++ client-vendor/after-body/fastclick/fastclick.js
@@ -1,4 +1,3 @@
-// patched with: https://github.com/ftlabs/fastclick/pull/103/files
 /**
  * @preserve FastClick: polyfill to remove click delays on browsers with touch UIs.
  *
@@ -281,7 +280,7 @@ FastClick.prototype.needsFocus = function(target) {
  */
 FastClick.prototype.sendClick = function(targetElement, event) {
 	'use strict';
-	var clickEvent, mouseupEvent, touch;
+	var clickEvent, touch;
 
 	// On some Android devices activeElement needs to be blurred otherwise the synthetic click will have no effect (#24)
 	if (document.activeElement && document.activeElement !== targetElement) {
@@ -289,11 +288,6 @@ FastClick.prototype.sendClick = function(targetElement, event) {
 	}
 
 	touch = event.changedTouches[0];
-
-	mouseupEvent = document.createEvent('MouseEvents');
-	mouseupEvent.initMouseEvent('mouseup', true, true, window, 1, touch.screenX, touch.screenY, touch.clientX, touch.clientY, false, false, false, false, 0, null);
-	mouseupEvent.forwardedTouchEvent = true;
-	targetElement.dispatchEvent(mouseupEvent);
 
 	// Synthesise a click event, with an extra attribute so it can be tracked
 	clickEvent = document.createEvent('MouseEvents');
