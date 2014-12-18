126942953438aa5e657123c1a28f59effb1f38b7
vogdb <vozhdb@gmail.com>
2014-12-18 16:20:19 +0300

    Remove Modernizr.svg.

--- client-vendor/before-body/01-modernizr/modernizr.js
+++ client-vendor/before-body/01-modernizr/modernizr.js
@@ -678,31 +678,6 @@
   }(this, document));
 
   
-/*!
-{
-  "name": "SVG",
-  "property": "svg",
-  "caniuse": "svg",
-  "tags": ["svg"],
-  "authors": ["Erik Dahlstrom"],
-  "polyfills": [
-    "svgweb",
-    "raphael",
-    "amplesdk",
-    "canvg",
-    "svg-boilerplate",
-    "sie",
-    "dojogfx",
-    "fabricjs"
-  ]
-}
-!*/
-/* DOC
-Detects support for SVG in `<embed>` or `<object>` elements.
-*/
-
-  Modernizr.addTest('svg', !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect);
-
 
   var createElement = function() {
     if (typeof document.createElement !== 'function') {
