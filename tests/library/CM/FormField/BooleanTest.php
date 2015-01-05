e7c585e021b17fbd516929a99ec9df8de51ce794
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2015-01-05 19:09:01 +0100

    fix FormField/BooleanTest

--- tests/library/CM/FormField/BooleanTest.php
+++ tests/library/CM/FormField/BooleanTest.php
@@ -6,8 +6,7 @@ class CM_FormField_BooleanTest extends CMTest_TestCase {
         $field = new CM_FormField_Boolean(['name' => 'checkbox']);
         $doc = $this->_renderFormField($field);
 
-        $this->assertCount(0, $doc->find('.switch'));
-        $this->assertCount(1, $doc->find('.checkbox'));
+        $this->assertCount(0, $doc->find('.checkbox-switch'));
         $this->assertCount(2, $doc->find('input[name="checkbox"]'));
         $this->assertSame('0', $doc->find('input[name="checkbox"][type="hidden"]')->getAttribute('value'));
         $this->assertSame('1', $doc->find('input[name="checkbox"][type="checkbox"]')->getAttribute('value'));
@@ -17,9 +16,8 @@ class CM_FormField_BooleanTest extends CMTest_TestCase {
         $field = new CM_FormField_Boolean(['name' => 'switch']);
         $doc = $this->_renderFormField($field, array('display' => CM_FormField_Boolean::DISPLAY_SWITCH));
 
-        $this->assertCount(1, $doc->find('.switch'));
+        $this->assertCount(1, $doc->find('.checkbox-switch'));
         $this->assertCount(1, $doc->find('.handle'));
-        $this->assertCount(0, $doc->find('.checkbox'));
         $this->assertCount(2, $doc->find('input[name="switch"]'));
         $this->assertSame('0', $doc->find('input[name="switch"][type="hidden"]')->getAttribute('value'));
         $this->assertSame('1', $doc->find('input[name="switch"][type="checkbox"]')->getAttribute('value'));
