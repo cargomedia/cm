aee1029129ed3a7767db8ca7840c8d18a5aba69a
Alexis Peter <alexis.peter@cargomedia.ch>
2015-01-06 11:56:37 +0100

    remove pid from workload result

--- library/CM/Process/WorkloadResult.php
+++ library/CM/Process/WorkloadResult.php
@@ -8,32 +8,16 @@ class CM_Process_WorkloadResult {
     /** @var CM_ExceptionHandling_SerializableException|null */
     private $_exception;
 
-    /** @var int|null */
-    private $_pid;
-
     /**
-     * @param int            $pid
      * @param mixed          $result
      * @param Exception|null $exception
      */
-    public function __construct($result, Exception $exception = null, $pid = null) {
+    public function __construct($result, Exception $exception = null) {
         if (null !== $exception) {
             $exception = new CM_ExceptionHandling_SerializableException($exception);
         }
         $this->_result = $result;
         $this->_exception = $exception;
-        $this->_pid = (null !== $pid) ? (int) $pid : null;
-    }
-
-    /**
-     * @return int
-     * @throws CM_Exception_Invalid
-     */
-    public function getPid() {
-        if (null === $this->_pid) {
-            throw new CM_Exception_Invalid('Pid is undefined');
-        }
-        return $this->_pid;
     }
 
     /**
