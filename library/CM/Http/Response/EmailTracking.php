653f3cbac1622d764f97daecf7e48b2d0b74c79c
Reto Kaiser <reto@cargomedia.ch>
2015-01-09 16:00:22 +0100

    Mark warnings: "nonexistent" and "invalidparam" exceptions in email-tracking response

--- library/CM/Http/Response/EmailTracking.php
+++ library/CM/Http/Response/EmailTracking.php
@@ -11,8 +11,14 @@ class CM_Http_Response_EmailTracking extends CM_Http_Response_Abstract {
             $action = new CM_Action_Email(CM_Action_Abstract::VIEW, $user, $mailType);
             $action->prepare();
             $action->notify($user, $mailType);
-        } catch (CM_Exception_Nonexistent $e) {
-            // will be ignored
+        } catch (CM_Exception $e) {
+            if (in_array(get_class($e), [
+                'CM_Exception_Nonexistent',
+                'CM_Exception_InvalidParam',
+            ])) {
+                $e->setSeverity(CM_Exception::WARN);
+            }
+            throw $e;
         }
 
         $this->setHeader('Content-Type', 'image/gif');
