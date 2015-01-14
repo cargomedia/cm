c0a2f3d9bb23836ed2dd49719d6289b3d6690470
Alexis Peter <alexis.peter@cargomedia.ch>
2015-01-14 16:30:58 +0100

    - adjust emoticon pagings

--- library/CM/Paging/Emoticon/All.php
+++ library/CM/Paging/Emoticon/All.php
@@ -3,8 +3,8 @@
 class CM_Paging_Emoticon_All extends CM_Paging_Emoticon_Abstract {
 
     public function __construct() {
-        $source = new CM_PagingSource_Sql('id, code, codeAdditional, file', 'cm_emoticon', null, '`id`');
-        $source->enableCacheLocal();
+        $emoticons = CM_Emoticon::getEmoticonData();
+        $source = new CM_PagingSource_Array(\Functional\pluck($emoticons, 'name'));
         parent::__construct($source);
     }
 }
