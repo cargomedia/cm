4ba29b1b4330b9545e3cfef52b4cb1c6eabd7d63
Alexis Peter <alexis.peter@cargomedia.ch>
2015-01-15 15:48:33 +0100

    remove CM_Css_Cli::emoticonRefresh()

--- library/CM/Css/Cli.php
+++ library/CM/Css/Cli.php
@@ -45,49 +45,6 @@ class CM_Css_Cli extends CM_Cli_Runnable_Abstract {
         $this->_getStreamOutput()->writeln('Created web-font and stylesheet.');
     }
 
-    public function emoticonRefresh() {
-        $emoticonList = array();
-
-        foreach (CM_Bootloader::getInstance()->getModules() as $namespace) {
-            $emoticonPath = CM_Util::getModulePath($namespace) . 'layout/default/resource/img/emoticon/';
-            $paths = glob($emoticonPath . '*');
-            foreach ($paths as $path) {
-                $file = new CM_File($path);
-                $name = strtolower($file->getFileNameWithoutExtension());
-                $emoticonList[$name] = array('name' => $name, 'fileName' => $file->getFileName());
-            }
-        }
-
-        $insertList = array();
-        foreach ($emoticonList as $emoticon) {
-            $insertList[] = array(':' . $emoticon['name'] . ':', $emoticon['fileName']);
-        }
-
-        CM_Db_Db::insertIgnore('cm_emoticon', array('code', 'file'), $insertList);
-        $this->_getStreamOutput()->writeln('Updated ' . count($insertList) . ' emoticons.');
-
-        $this->_checkEmoticonValidity();
-    }
-
-    private function _checkEmoticonValidity() {
-        $paging = new CM_Paging_Emoticon_All();
-        $codes = array();
-        foreach ($paging as $emoticon) {
-            if (false !== array_search('', $emoticon['codes'])) {
-                $this->_getStreamError()->writeln('WARNING: Empty emoticon with ID `' . $emoticon['id'] . '`.');
-                return;
-            }
-            $codes = array_merge($codes, $emoticon['codes']);
-        }
-        for ($i = 0; $i < count($codes); $i++) {
-            for ($j = $i + 1; $j < count($codes); $j++) {
-                if (false !== strpos($codes[$i], $codes[$j]) || false !== strpos($codes[$j], $codes[$i])) {
-                    $this->_getStreamError()->writeln('WARNING: Emoticon intersection: `' . $codes[$i] . '` <-> `' . $codes[$j] . '`.');
-                }
-            }
-        }
-    }
-
     public static function getPackageName() {
         return 'css';
     }
