df731285491d203493d0f0cfbfe435dbf80e45d3
Marian Sollmann <ms@sollmann-interactive.com>
2015-01-29 12:50:45 +0100

    formatting

--- library/CM/Paging/Log/NotFound.php
+++ library/CM/Paging/Log/NotFound.php
@@ -7,7 +7,7 @@ class CM_Paging_Log_NotFound extends CM_Paging_Log_Abstract {
      * @param array|null $metaInfo
      */
     public function add($msg, array $metaInfo = null) {
-       $metaInfo = array_merge((array) $metaInfo, $this->_getDefaultMetaInfo());
+        $metaInfo = array_merge((array) $metaInfo, $this->_getDefaultMetaInfo());
         $this->_add($msg, $metaInfo);
     }
 }
