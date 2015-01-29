b8f82b674dc647badd9b20b12508411bd694e64e
Reto Kaiser <reto@cargomedia.ch>
2015-01-29 10:01:03 +0100

    Add basic client for GA Measurement Protocol

---
+++ library/CMService/GoogleAnalytics/MeasurementProtocol/Client.php
@@ -0,0 +1,52 @@
+<?php
+
+class CMService_GoogleAnalytics_MeasurementProtocol_Client {
+
+    /** @var \GuzzleHttp\Client */
+    protected static $_client;
+
+    /** @var string */
+    protected $_propertyId;
+
+    /**
+     * @param string $propertyId
+     */
+    public function __construct($propertyId) {
+        $this->_propertyId = (string) $propertyId;
+    }
+
+    /**
+     * @return string
+     */
+    protected function _getPropertyId() {
+        return $this->_propertyId;
+    }
+
+    /**
+     * @param string $hitType
+     * @param array  $parameterList
+     */
+    public function sendHit($hitType, array $parameterList) {
+        $parameterList['v'] = 1;
+        $parameterList['tid'] = $this->_getPropertyId();
+        $parameterList['t'] = (string) $hitType;
+        $this->_sendRequest($parameterList);
+    }
+
+    /**
+     * @param array $parameterList
+     */
+    protected function _sendRequest(array $parameterList) {
+        $this->_getGuzzleClient()->post('/collect', ['body' => $parameterList]);
+    }
+
+    /**
+     * @return \GuzzleHttp\Client
+     */
+    protected function _getGuzzleClient() {
+        if (!self::$_client) {
+            self::$_client = new \GuzzleHttp\Client(['base_url' => 'http://www.google-analytics.com']);
+        }
+        return self::$_client;
+    }
+}
