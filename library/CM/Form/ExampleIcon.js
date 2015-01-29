bc57529a6cd1ef4f2174844054c985ae0fb735f0
vogdb <vozhdb@gmail.com>
2015-01-29 13:56:20 +0300

    Fix CM_Form_ExampleIcon initialization.

--- library/CM/Form/ExampleIcon.js
+++ library/CM/Form/ExampleIcon.js
@@ -17,7 +17,10 @@ var CM_Form_ExampleIcon = CM_Form_Abstract.extend({
     this.on('change', function() {
       this._updateCss();
     }, this);
-    this._updateCss();
+    var self = this;
+    setTimeout(function() {
+      self._updateCss();
+    }, 0);
   },
 
   /**
@@ -35,10 +38,14 @@ var CM_Form_ExampleIcon = CM_Form_Abstract.extend({
   },
 
   _updateCss: function() {
-    this.$('.iconBox').css('background-color', this.getField('colorBackground').getValue());
-    this.$('.iconBox .icon').css('text-shadow', this._getShadowValue());
-    this.$('.iconBox .icon').css('color', this.getField('color').getValue());
-    this.$('.iconBox .icon').css('font-size', this.getField('sizeSlider').getValue() + "px");
-    this.$('.iconCss').html('background-color: ' + this.$(".iconBox").css('background-color') + ';<br />' + this.$(".iconBox .icon").first().attr('style').replace(/; /g, ";<br />"));
+    var appliedStyles = {};
+    appliedStyles['background-color'] = this.$('.iconBox').css('background-color', this.getField('colorBackground').getValue()).css('background-color');
+    appliedStyles['text-shadow'] = this.$('.iconBox .icon').css('text-shadow', this._getShadowValue()).css('text-shadow');
+    appliedStyles['color'] = this.$('.iconBox .icon').css('color', this.getField('color').getValue()).css('color');
+    appliedStyles['font-size'] = this.$('.iconBox .icon').css('font-size', this.getField('sizeSlider').getValue() + "px").css('font-size');
+
+    this.$('.iconCss').html(_.reduce(appliedStyles, function(memo, value, key) {
+      return memo + key + ': ' + value + ';<br />';
+    }, ''));
   }
 });
