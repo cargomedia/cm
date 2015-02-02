3af725c6f562a79b6db4d95b870070e8805cd2bc
Alexis Peter <alexis.peter@cargomedia.ch>
2015-02-02 13:46:22 +0100

    keep daemon processes alive - do not track them in newrelic

--- library/CM/Stream/Cli.php
+++ library/CM/Stream/Cli.php
@@ -4,6 +4,7 @@ class CM_Stream_Cli extends CM_Cli_Runnable_Abstract {
 
     /**
      * @synchronized
+     * @keepalive
      */
     public function startMessageSynchronization() {
         CM_Stream_Message::getInstance()->startSynchronization();
