9e2c111e1a13d1bf1d95469a8b723c32b5c7123e
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2015-02-10 10:52:04 +0100

    Add {lessVariable} function

---
+++ library/CM/SmartyPlugins/function.lessVariable.php
@@ -0,0 +1,18 @@
+<?php
+
+function smarty_function_lessVariable(array $params, Smarty_Internal_Template $template) {
+    /** @var CM_Frontend_Render $render */
+    $render = $template->smarty->getTemplateVars('render');
+    $variableName = (string) $params['name'];
+
+    $assetCss = new CM_Asset_Css($render);
+    $assetCss->addVariables();
+    $assetCss->add('foo: @' . $variableName . '');
+
+    $css = $assetCss->get(true);
+
+    if (!preg_match('/^foo:(.+);$/', $css, $matches)) {
+        throw new CM_Exception_Invalid('Cannot detect variable `' . $variableName . '` from CSS `' . $css . '`.');
+    }
+    return $matches[1];
+}
