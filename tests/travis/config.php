5c40eaf43f84d822a9e6cd5764bcc7c33f88a0ec
tomaszdurka <tomasz@durka.pl>
2015-02-21 13:11:39 +0100

    Remove unused Redis_Client config

--- tests/travis/config.php
+++ tests/travis/config.php
@@ -1,7 +1,6 @@
 <?php
 
 return function (CM_Config_Node $config) {
-    $config->CM_Redis_Client->server = array('host' => 'localhost', 'port' => 6379);
     $config->CM_Memcache_Client->servers = array(
         array('host' => 'localhost', 'port' => 11211),
     );
