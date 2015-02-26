6a58f8734dd9ed42cf51410ee74c3dc7841bc77b
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2015-02-26 15:43:05 +0100

    Adjusted tests

--- tests/library/CM/SmartyPlugins/prefilter.translateTest.php
+++ tests/library/CM/SmartyPlugins/prefilter.translateTest.php
@@ -18,32 +18,27 @@ class smarty_prefilter_translateTest extends CMTest_TestCase {
         $this->assertSame($expected, smarty_prefilter_translate($source, $this->_template));
     }
 
-    public function testStatic() {
-        $this->_assertSame("{translateStatic key='foo'}", "{translate 'foo'}");
-        $this->_assertSame('{translateStatic key="foo bar"}', '{translate "foo bar"}');
-        $this->_assertSame("{translateStatic key=foo}", "{translate foo}");
-    }
+    public function testPrefilter() {
+        $this->_assertSame("{translate key='foo'}", "{translate 'foo'}");
+        $this->_assertSame('{translate key="foo bar"}', '{translate "foo bar"}');
+        $this->_assertSame("{translate key=foo}", "{translate foo}");
 
-    public function testVariable() {
-        $this->_assertSame("{translateVariable key='foo' a=\$a}", "{translate 'foo' a=\$a}");
-        $this->_assertSame("{translateVariable key=foo a=\$a}", "{translate foo a=\$a}");
+        $this->_assertSame("{translate key='foo' a=\$a}", "{translate 'foo' a=\$a}");
+        $this->_assertSame("{translate key=foo a=\$a}", "{translate foo a=\$a}");
 
-        $this->_assertSame('{translateVariable key=$foo}', '{translate $foo}');
-        $this->_assertSame('{translateVariable key="$foo"}', '{translate "$foo"}');
-        $this->_assertSame('{translateVariable key="hello $foo there"}', '{translate "hello $foo there"}');
-        $this->_assertSame('{translateVariable key="hello {$foo} there"}', '{translate "hello {$foo} there"}');
-        $this->_assertSame('{translateVariable key={anything_here}}', '{translate {anything_here}}');
+        $this->_assertSame('{translate key=$foo}', '{translate $foo}');
+        $this->_assertSame('{translate key="$foo"}', '{translate "$foo"}');
+        $this->_assertSame('{translate key="hello $foo there"}', '{translate "hello $foo there"}');
+        $this->_assertSame('{translate key="hello {$foo} there"}', '{translate "hello {$foo} there"}');
+        $this->_assertSame('{translate key={anything_here}}', '{translate {anything_here}}');
 
-        $this->_assertSame('{translateVariable key="hello {foo} there"}', '{translate "hello {foo} there"}');
-        $this->_assertSame("{translateVariable key='hello {\$a} there' a=\$a}", "{translate 'hello {\$a} there' a=\$a}");
-        $this->_assertSame("{translateVariable key='hello {\$a} there' a=\$a->foo()}", "{translate 'hello {\$a} there' a=\$a->foo()}");
+        $this->_assertSame('{translate key="hello {foo} there"}', '{translate "hello {foo} there"}');
+        $this->_assertSame("{translate key='hello {\$a} there' a=\$a}", "{translate 'hello {\$a} there' a=\$a}");
+        $this->_assertSame("{translate key='hello {\$a} there' a=\$a->foo()}", "{translate 'hello {\$a} there' a=\$a->foo()}");
 
-        $this->_assertSame('{translateVariable key="bar\"$foo"}', '{translate "bar\"$foo"}');
-        $this->_assertSame('{translateVariable key="bar\$foo"}', '{translate "bar\$foo"}');
-    }
+        $this->_assertSame('{translate key="bar\"$foo"}', '{translate "bar\"$foo"}');
+        $this->_assertSame('{translate key="bar\$foo"}', '{translate "bar\$foo"}');
 
-    public function testVariableWithCurlybracketInKey() {
-        $this->markTestSkipped('Not implemented');
-        $this->_assertSame('{translateVariable "foo}" a=$a}', '{translate "foo}" a=$a}');
+        $this->_assertSame('{translate key="foo}" a=$a}', '{translate "foo}" a=$a}');
     }
 }
