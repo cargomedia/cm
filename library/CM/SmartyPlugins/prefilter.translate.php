77d6cee4433dca4eda10c4e5608cadc1efa671c2
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2015-02-26 11:41:23 +0100

    Translated all phrases at run-time
    - Removed smarty_compiler_translateStatic()
    - Always used smarty_function_translateVariable(), renamed to smarty_function_translate()

--- library/CM/SmartyPlugins/prefilter.translate.php
+++ library/CM/SmartyPlugins/prefilter.translate.php
@@ -1,7 +1,6 @@
 <?php
 
 function smarty_prefilter_translate($source, Smarty_Internal_Template $template) {
-    $source = preg_replace('#{translate\s+([^}${=]+})#', '{translateStatic key=$1', $source);
-    $source = preg_replace('#{translate\s+#', '{translateVariable key=', $source);
+    $source = preg_replace('#{translate\s+#', '{translate key=', $source);
     return $source;
 }
