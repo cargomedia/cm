5d66d64e019d908b05338f0f3e017dfa30220f43
vogdb <vozhdb@gmail.com>
2015-03-09 12:46:05 +0300

    Add toggle method to jquery.toggleNext.

--- client-vendor/after-body/jquery.toggleNext/jquery.toggleNext.js
+++ client-vendor/after-body/jquery.toggleNext/jquery.toggleNext.js
@@ -2,27 +2,39 @@
  * Author: CM
  */
 (function($) {
-  $.fn.toggleNext = function() {
+  $.fn.toggleNext = function(methodName, value) {
     return this.each(function() {
       var $toggler = $(this);
       var $content = $toggler.next('.toggleNext-content');
+      var $icon = $toggler.find('.icon-arrow-right');
 
-      if (!$content.length || $toggler.data('toggleNext')) {
+      if (!$content.length || ($toggler.data('toggleNext') && !methodName)) {
         return;
       }
+      if ('toggle' === methodName && $toggler.data('toggleNext')) {
+        return toggle(value);
+      }
 
-      var icon = $('<span />').addClass('icon-arrow-right');
-      $toggler.prepend(icon);
+      $icon = $('<span />').addClass('icon-arrow-right');
+      $toggler.prepend($icon);
 
       if ($toggler.hasClass('active')) {
-        icon.addClass('active');
+        $icon.addClass('active');
         $content.show();
       }
 
-      $toggler.on('click.toggleNext', function() {
-        var state = !$toggler.hasClass('active');
+      $toggler.on('click.toggleNext', toggle);
+      $toggler.data('toggleNext', true);
+
+      function toggle(state) {
+        var currentState = $toggler.hasClass('active');
+        if ('undefined' === typeof state) {
+          state = !currentState;
+        } else if (state === currentState) {
+          return;
+        }
         $toggler.toggleClass('active', state);
-        icon.toggleClass('active', state);
+        $icon.toggleClass('active', state);
         $content.slideToggle(100, function() {
           var eventData = {
             toggler: $toggler,
@@ -35,8 +47,7 @@
             $toggler.trigger('toggleNext-close', eventData);
           }
         });
-      });
-      $toggler.data('toggleNext', true);
+      }
     });
 
   };
