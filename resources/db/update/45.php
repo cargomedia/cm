97278192ad8f5c440d28d5c10d6679fe521b28d5
Marian Sollmann <ms@sollmann-interactive.com>
2015-03-11 11:04:44 +0100

    add migration to update script to set expiration stamps

--- resources/db/update/45.php
+++ resources/db/update/45.php
@@ -5,3 +5,13 @@ if (!CM_Db_Db::existsColumn('cm_ipBlocked', 'expirationStamp')) {
                     ADD `expirationStamp` int(10) unsigned NOT NULL,
                     ADD KEY `expirationStamp` (`expirationStamp`);");
 }
+
+$config = CM_Config::get();
+$IpsWithoutExpirationStamp = CM_Db_Db::select('cm_ipBlocked', '*', ['expirationStamp' => 0])->fetchAll();
+
+foreach ($IpsWithoutExpirationStamp as $row) {
+    if ($row['expirationStamp'] == 0) {
+        $expirationStamp = $row['createStamp'] + $config->CM_Paging_Ip_Blocked->maxAge;
+        CM_Db_Db::update('cm_ipBlocked', ['expirationStamp' => $expirationStamp], ['ip' => $row['ip']]);
+    }
+}
