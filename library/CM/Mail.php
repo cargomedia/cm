0a658f9b3818d2de7ed81f0960f3f5b4382dda4a
ppp0 <philipp.gysin@cargomedia.ch>
2015-03-23 18:04:24 +0100

    Move getting MDA from config to protected function

--- library/CM/Mail.php
+++ library/CM/Mail.php
@@ -355,6 +355,18 @@ class CM_Mail extends CM_View_Abstract implements CM_Typed {
         return $renderAdapter->fetch();
     }
 
+    /**
+     * @return string|null
+     */
+    protected function _getMailDeliveryAgent() {
+        return $this->_getConfig()->mailDeliveryAgent;
+    }
+
+    /**
+     * @throws CM_Exception_Invalid
+     * @throws CM_Exception_NotAllowed
+     * @throws phpmailerException
+     */
     protected function _send($subject, $text, $html = null) {
         if (!self::_getConfig()->send) {
             $this->_log($subject, $text);
@@ -374,7 +386,7 @@ class CM_Mail extends CM_View_Abstract implements CM_Typed {
             foreach ($this->_bcc as $bcc) {
                 $mail->AddBCC($bcc['address'], $bcc['name']);
             }
-            if ($mailDeliveryAgent = $this->_getConfig()->mailDeliveryAgent) {
+            if ($mailDeliveryAgent = $this->_getMailDeliveryAgent()) {
                 $mail->AddCustomHeader('X-MDA: ' . $mailDeliveryAgent);
             }
             $mail->SetFrom($this->_sender['address'], $this->_sender['name']);
