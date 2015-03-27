b3f7a712ab8321dab628a843bc9144661484e34e
vogdb <vozhdb@gmail.com>
2015-03-27 10:17:46 +0300

    Streamline docu.

--- library/CM/Usertext/MarkdownImgLazy.php
+++ library/CM/Usertext/MarkdownImgLazy.php
@@ -2,19 +2,23 @@
 
 class CM_Usertext_MarkdownImgLazy extends CM_Usertext_Markdown {
 
-    function _doImages_reference_callback($matches) {
+    protected function _doImages_reference_callback($matches) {
         $key = parent::_doImages_reference_callback($matches);
-        $this->html_hashes[$key] = $this->addLazyAttrs($this->html_hashes[$key]);
+        $this->html_hashes[$key] = $this->_addLazyAttrs($this->html_hashes[$key]);
         return $key;
     }
 
-    function _doImages_inline_callback($matches) {
+    protected function _doImages_inline_callback($matches) {
         $key = parent::_doImages_inline_callback($matches);
-        $this->html_hashes[$key] = $this->addLazyAttrs($this->html_hashes[$key]);
+        $this->html_hashes[$key] = $this->_addLazyAttrs($this->html_hashes[$key]);
         return $key;
     }
 
-    function addLazyAttrs($tagText) {
+    /**
+     * @param string $tagText
+     * @return string
+     */
+    private function _addLazyAttrs($tagText) {
         $tagText = str_replace('>', ' class="lazy">', $tagText);
         return str_replace('src=', 'data-original=', $tagText);
     }
