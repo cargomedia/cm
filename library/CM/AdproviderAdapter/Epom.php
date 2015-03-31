192e65566d9c4043b02b79478b1b52935496c734
vogdb <vozhdb@gmail.com>
2015-03-31 12:45:29 +0300

    Change epom keys from CamelCase to underscore.

--- library/CM/AdproviderAdapter/Epom.php
+++ library/CM/AdproviderAdapter/Epom.php
@@ -4,7 +4,7 @@ class CM_AdproviderAdapter_Epom extends CM_AdproviderAdapter_Abstract {
 
     public function getHtml($zoneData, array $variables) {
         $zoneId = CM_Util::htmlspecialchars($zoneData['zoneId']);
-        $variables = $this->_variablesToCamelCase($variables);
+        $variables = $this->_variableKeysToUnderscore($variables);
         $variables['key'] = '2ea5b261f06ca771033a5fa9e22493f1';
 
         $html = '<div id="epom-' . $zoneId . '-' . '2ea5b261f06ca771033a5fa9e22493f1-300x100' . '" class="epom-ad" data-zone-id="' . $zoneId . '" data-variables="' .
@@ -12,18 +12,12 @@ class CM_AdproviderAdapter_Epom extends CM_AdproviderAdapter_Abstract {
         return $html;
     }
 
-    private function _variablesToCamelCase($variables) {
+    private function _variableKeysToUnderscore($variables) {
         foreach ($variables as $key => $value) {
             unset ($variables[$key]);
-            $camelCaseKey = $this->_dashesToCamelCase($key);
-            $variables[$camelCaseKey] = $value;
+            $underscoreKey = str_replace('-', '_', $key);
+            $variables[$underscoreKey] = $value;
         }
         return $variables;
-    }
-
-    private function _dashesToCamelCase($string) {
-        $str = str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
-        $str[0] = strtolower($str[0]);
-        return $str;
     }
 }
