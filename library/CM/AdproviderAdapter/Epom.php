dc2721f8d3d6e6b78664e9d1b438fa56b4b9c9cf
vogdb <vozhdb@gmail.com>
2015-03-31 12:46:21 +0300

    Clean obsolete epom key usage.

--- library/CM/AdproviderAdapter/Epom.php
+++ library/CM/AdproviderAdapter/Epom.php
@@ -7,7 +7,7 @@ class CM_AdproviderAdapter_Epom extends CM_AdproviderAdapter_Abstract {
         $variables = $this->_variableKeysToUnderscore($variables);
         $variables['key'] = '2ea5b261f06ca771033a5fa9e22493f1';
 
-        $html = '<div id="epom-' . $zoneId . '-' . '2ea5b261f06ca771033a5fa9e22493f1-300x100' . '" class="epom-ad" data-zone-id="' . $zoneId . '" data-variables="' .
+        $html = '<div id="epom-' . $zoneId . '" class="epom-ad" data-zone-id="' . $zoneId . '" data-variables="' .
             CM_Util::htmlspecialchars(json_encode($variables, JSON_FORCE_OBJECT)) . '"></div>';
         return $html;
     }
