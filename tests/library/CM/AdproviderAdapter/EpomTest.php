0440aa0d4eadeb489da778844c04648b40fd773d
vogdb <vozhdb@gmail.com>
2015-04-01 14:49:00 +0300

    Fix EpomTest.

--- tests/library/CM/AdproviderAdapter/EpomTest.php
+++ tests/library/CM/AdproviderAdapter/EpomTest.php
@@ -3,14 +3,12 @@
 class CM_AdproviderAdapter_EpomTest extends CMTest_TestCase {
 
     public function testGetHtml() {
-        CM_Config::get()->CM_Adprovider->enabled = true;
-        CM_Config::get()->CM_AdproviderAdapter_Epom = array('accessKey' => 'accessKey');
         $epom = new CM_AdproviderAdapter_Epom();
 
-        $html = $epom->getHtml(array('zoneId' => 'zone1'), array('foo' => 'bar'));
+        $html = $epom->getHtml(array('zoneId' => 'zone1', 'accessKey' => 'accessKey'), array('foo' => 'bar'));
         $this->assertContains('<div id="epom-zone1" class="epom-ad" data-zone-id="zone1" data-variables="{&quot;foo&quot;:&quot;bar&quot;,&quot;key&quot;:&quot;accessKey&quot;}"', $html);
 
-        $html = $epom->getHtml(array('zoneId' => 'zone1'), array());
+        $html = $epom->getHtml(array('zoneId' => 'zone1', 'accessKey' => 'accessKey'), array());
         $this->assertContains('<div id="epom-zone1" class="epom-ad" data-zone-id="zone1" data-variables="{&quot;key&quot;:&quot;accessKey&quot;}"', $html);
     }
 }
