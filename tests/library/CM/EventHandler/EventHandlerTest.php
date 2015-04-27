a4cdeda14e3adb9d38b3856fa2a8438a9d8cf580
tomaszdurka <tomasz@durka.pl>
2015-04-27 15:23:53 +0200

    Distribute tests into proper test cases

--- tests/library/CM/EventHandler/EventHandlerTest.php
+++ tests/library/CM/EventHandler/EventHandlerTest.php
@@ -5,69 +5,6 @@ class CM_EventHandler_EventHandlerTest extends CMTest_TestCase {
     public static $_foo;
     public static $_counter;
 
-    public function testBind() {
-        $counter = 0;
-        $eventHandler = new CM_EventHandler_EventHandler();
-        $eventHandler->bind('foo', function ($value) use (&$counter) {
-            $counter += $value;
-        });
-        $eventHandler->trigger('bar');
-        $this->assertSame(0, $counter);
-        $eventHandler->trigger('foo', 1);
-        $this->assertSame(1, $counter);
-        $eventHandler->bind('foo', function () use (&$counter) {
-            $counter--;
-        });
-        $eventHandler->trigger('foo', 2);
-        $this->assertSame(2, $counter);
-    }
-
-    /**
-     * @expectedException ErrorException
-     * @expectedExceptionMessage Missing argument 1
-     */
-    public function testBindMissingArguments() {
-        $eventHandler = new CM_EventHandler_EventHandler();
-        $eventHandler->bind('foo', function ($requiredArgument) {
-            // Do nothing
-        });
-        $eventHandler->trigger('foo');
-    }
-
-    public function testUnbind() {
-        $counter = 0;
-        $eventHandler = new CM_EventHandler_EventHandler();
-        $callback = function () use (&$counter) {
-            $counter++;
-        };
-        $eventHandler->bind('foo', $callback);
-        $eventHandler->trigger('foo');
-        $this->assertSame(1, $counter);
-
-        $eventHandler->unbind('foo', function () {
-        });
-        $eventHandler->trigger('foo');
-        $this->assertSame(2, $counter);
-
-        $eventHandler->unbind('foo', $callback);
-        $eventHandler->trigger('foo');
-        $this->assertSame(2, $counter);
-    }
-
-    public function testUnbindAll() {
-        $counter = 0;
-        $eventHandler = new CM_EventHandler_EventHandler();
-        $eventHandler->bind('foo', function () use (&$counter) {
-            $counter++;
-        });
-        $this->assertSame(0, $counter);
-        $eventHandler->trigger('foo');
-        $this->assertSame(1, $counter);
-        $eventHandler->unbind('foo');
-        $eventHandler->trigger('foo');
-        $this->assertSame(1, $counter);
-    }
-
     public function testBindJob() {
         $eventHandler = new CM_EventHandler_EventHandler();
         self::$_foo = '';
