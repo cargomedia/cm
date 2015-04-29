8552aa72b250b5dd06d2ae9419b2249325e535ff
ppp0 <philipp.gysin@cargomedia.ch>
2015-04-29 13:34:56 +0200

    Adjust db update script and general structure file

--- resources/db/update/47.php
+++ resources/db/update/47.php
@@ -2,5 +2,5 @@
 
 if (!CM_Db_Db::existsColumn('cm_mail', 'customHeaders')) {
     CM_Db_Db::exec('
-        ALTER TABLE `cm_mail` ADD COLUMN `customHeaders` text DEFAULT NULL AFTER `bcc`;');
+        ALTER TABLE `cm_mail` ADD COLUMN `customHeaders` text AFTER `bcc`;');
 }
