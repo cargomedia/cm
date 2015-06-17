ba2255de482177d7e797c0f68fde102a55c7c005
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2015-06-17 14:36:47 +0200

    Refactor click effect

--- client-vendor/after-body/jquery.clickDecorators/decorators/10-feedback.js
+++ client-vendor/after-body/jquery.clickDecorators/decorators/10-feedback.js
@@ -6,19 +6,29 @@
 
   $.clickDecorators.feedback = {
     isApplicable: function($element) {
-      return $element.is('.button:not([data-click-confirmed])');
+      return $element.is('.button');
     },
 
     before: function(event) {
       var $this = $(this);
-      $this.append('<span class="click-feedback" />');
+      var buttonOffset = $this.offset();
+      var feedbackSize = 2 * Math.sqrt(Math.pow($this.outerWidth(), 2) + Math.pow($this.outerHeight(), 2));
 
-      $this.find('.click-feedback').transition({
-        scale: 1,
-        opacity: 1,
-        'border-radius': 0
-      }, '200ms', 'snap', function() {
-        $(this).remove('.click-feedback');
+      var posX = event.pageX;
+      var posY = event.pageY;
+
+      var $feedback = $('<div class="click-feedback" />');
+      $feedback.css({
+        width: feedbackSize,
+        height: feedbackSize,
+        left: posX - buttonOffset.left - (feedbackSize / 2),
+        top: posY - buttonOffset.top - (feedbackSize / 2)
+      });
+      $this.append($feedback);
+      $feedback.transition({
+        scale: 1
+      }, '500ms', 'ease', function() {
+        $feedback.remove();
       });
     }
   };
--- library/CM/Component/Example.js
+++ library/CM/Component/Example.js
@@ -27,7 +27,7 @@ var CM_Component_Example = CM_Component_Abstract.extend({
     'click .throwError_CM_Exception_AuthRequired_public': 'error_CM_Exception_AuthRequired_public',
     'click .throwError_CM_Exception_AuthRequired': 'error_CM_Exception_AuthRequired',
     'click .callAjaxPing': 'ping',
-    'click button': 'clickFeedback',
+    'click button:not(.confirmAction)': 'clickFeedback',
     'click .confirmAction': 'confirmAction',
     'click .toggleWindow': function(e) {
       var $opener = $(e.currentTarget);
@@ -135,11 +135,11 @@ var CM_Component_Example = CM_Component_Abstract.extend({
   },
 
   confirmAction: function() {
+    console.log('Action Confirmed!');
     cm.window.hint('Action Confirmed!');
   },
 
   clickFeedback: function() {
-    cm.window.hint('Feedback!');
   },
 
   toggleWindow: function($opener) {
