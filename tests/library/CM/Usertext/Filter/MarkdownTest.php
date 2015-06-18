5d427282d0d12f20a18aaaba4f4a26300cc6c2db
vogdb <vozhdb@gmail.com>
2015-06-18 10:46:13 +0200

    Update Markdown tests to the latest changes.

--- tests/library/CM/Usertext/Filter/MarkdownTest.php
+++ tests/library/CM/Usertext/Filter/MarkdownTest.php
@@ -4,7 +4,7 @@ class CM_Usertext_Filter_MarkdownTest extends CMTest_TestCase {
 
     public function testProcess() {
         $text = "#Headline#\n * element1\n * element1\n\nparagraph\n\nfoo_bar_foo\nfoo _bar_ foo\nLink: [google.com](http://www.google.com)\ntest2\n![Img text](/path/to/img.jpg)";
-        $expected = "<h1>Headline</h1>\n<ul>\n<li>element1</li>\n<li>element1</li>\n</ul>\n<p>paragraph</p>\n<p>foo_bar_foo</p>\n<p>foo <em>bar</em> foo</p>\n<p>Link: <a href=\"http://www.google.com\">google.com</a></p>\n<p>test2</p>\n<p><img src=\"/path/to/img.jpg\" alt=\"Img text\" /></p>\n";
+        $expected = "<h1>Headline</h1>\n<ul>\n<li>element1</li>\n<li>element1</li>\n</ul>\n<p>paragraph</p>\n<p>foo_bar_foo</p>\n<p>foo <em>bar</em> foo</p>\n<p>Link: <a href=\"http://www.google.com\">google.com</a></p>\n<p>test2</p>\n<p><div class=\"usertext-image\"><img src=\"/path/to/img.jpg\" alt=\"Img text\"/></div></p>\n";
         $filter = new CM_Usertext_Filter_Markdown();
         $actual = $filter->transform($text, new CM_Frontend_Render());
 
