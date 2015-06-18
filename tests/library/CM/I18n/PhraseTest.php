0352311859c2291610bbbff51ef22dccb8fc0eed
Vladimir Finko <vladimir.finko@cargomedia.ch>
2015-06-18 11:39:09 +0200

    fix formation

--- tests/library/CM/I18n/PhraseTest.php
+++ tests/library/CM/I18n/PhraseTest.php
@@ -5,7 +5,6 @@ class CM_I18n_PhraseTest extends CMTest_TestCase {
     public function testTranslate() {
         $phrase = '{$hello} world {$time}';
         $variables = ['hello' => 'hi', 'time' => 'now'];
-
         $i18n_phrase = new CM_I18n_Phrase($phrase, $variables);
         $this->assertInstanceOf('CM_I18n_Phrase', $i18n_phrase);
 
@@ -13,7 +12,7 @@ class CM_I18n_PhraseTest extends CMTest_TestCase {
         $render = $this->mockClass('CM_Frontend_Render')->newInstance();
         $getTranslationMethod = $render->mockMethod('getTranslation')->set(function ($key, $params) use ($phrase, $variables) {
             $this->assertSame($phrase, $key);
-            $this->assertSame($variables,$params);
+            $this->assertSame($variables, $params);
         });
 
         $i18n_phrase->translate($render);
