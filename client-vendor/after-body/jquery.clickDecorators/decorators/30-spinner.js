9daf356f162b225c3316f41f450a9f0dd8b92c6f
vogdb <vozhdb@gmail.com>
2015-06-22 13:04:11 +0300

    Fix spinner decorator.

--- client-vendor/after-body/jquery.clickDecorators/decorators/30-spinner.js
+++ client-vendor/after-body/jquery.clickDecorators/decorators/30-spinner.js
@@ -5,11 +5,11 @@
 
   $.clickDecorators.spinner = {
     after: function(event, returnValue) {
-      if (returnValue && _.isFunction(returnValue.promise) && 'pending' === returnValue.state()) {
+      if (returnValue && returnValue instanceof Promise && returnValue.isPending()) {
         var $inputTarget = $(event.currentTarget).closest('[data-click-spinner]');
         $inputTarget.addClass('hasSpinner').prop('disabled', true).find('.spinner').remove();
         var $spinner = $('<div class="spinner" />').appendTo($inputTarget);
-        returnValue.always(function() {
+        returnValue.finally(function() {
           $inputTarget.prop('disabled', false).removeClass('hasSpinner');
           $spinner.remove();
         });
