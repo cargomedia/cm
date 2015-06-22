63386e33493e16c26cf64cf05deb674409aa932b
vogdb <vozhdb@gmail.com>
2015-06-22 21:22:54 +0300

    Refactor confirmed click decorator.

--- client-vendor/after-body/jquery.clickDecorators/decorators/20-confirmed.js
+++ client-vendor/after-body/jquery.clickDecorators/decorators/20-confirmed.js
@@ -3,6 +3,46 @@
  */
 (function($) {
 
+  var ConfirmationMonitor = function() {
+  };
+
+  ConfirmationMonitor.prototype.isActivated = function($elem) {
+    return $elem.hasClass('clickConfirmed-active');
+  };
+
+  ConfirmationMonitor.prototype.activate = function($elem) {
+    $elem.addClass('confirmClick');
+    $elem.addClass('clickConfirmed-active');
+
+    var self = this;
+    $elem.data('confirmClick-data', {
+      timeout: setTimeout(function() {
+        self.deactivate($elem);
+      }, 4000),
+      documentClickHandler: function(e) {
+        if (!$elem.length || e.target !== $elem[0] && !$.contains($elem[0], e.target)) {
+          self.deactivate($elem);
+        }
+      }
+    });
+
+    setTimeout(function() {
+      $(document).on('click.clickConfirmed', $elem.data('confirmClick-data').documentClickHandler);
+    }, 0);
+  };
+
+  ConfirmationMonitor.prototype.deactivate = function($elem) {
+    $elem.removeClass('confirmClick');
+    $elem.removeClass('clickConfirmed-active');
+
+    var data = $elem.data('confirmClick-data');
+    clearTimeout(data.timeout);
+    $(document).off('click.clickConfirmed', data.documentClickHandler);
+    $elem.removeData('confirmClick-data');
+  };
+
+  var monitor = new ConfirmationMonitor();
+
   $.clickDecorators.confirmed = {
     isApplicable: function($element) {
       return $element.data('click-confirmed');
@@ -11,41 +51,18 @@
     before: function(event) {
       var $this = $(this);
 
-      var activateButton = function() {
-        $this.addClass('confirmClick');
-        $this.addClass('clickConfirmed-active');
-
-        var deactivateButton = function() {
-          $this.removeClass('confirmClick');
-          $this.removeClass('clickConfirmed-active');
-          $this.removeData('clickConfirmed.deactivate');
-          clearTimeout(deactivateTimeout);
-          $(document).off('click.clickConfirmed', documentClickHandler);
-        };
-
-        $this.data('clickConfirmed.deactivate', deactivateButton);
-
-        var deactivateTimeout = setTimeout(function() {
-          deactivateButton();
-        }, 4000);
-
-        var documentClickHandler = function(e) {
-          if (!$this.length || e.target !== $this[0] && !$.contains($this[0], e.target)) {
-            deactivateButton();
-          }
-        };
-
-        setTimeout(function() {
-          $(document).on('click.clickConfirmed', documentClickHandler);
-        }, 0);
-      };
-
-      if ($this.hasClass('confirmClick')) {
-        $this.data('clickConfirmed.deactivate')();
-      } else {
-        activateButton();
+      if (!monitor.isActivated($this)) {
+        monitor.activate($this);
         event.preventDefault();
         event.stopImmediatePropagation();
+      }
+    },
+
+    after: function(event) {
+      var $this = $(this);
+
+      if (monitor.isActivated($this)) {
+        monitor.deactivate($this);
       }
     }
   };
