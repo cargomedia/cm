3450ee2a89f283bf99cfa7a6b78a20383a6d3f47
vogdb <vozhdb@gmail.com>
2015-06-24 14:30:02 +0300

    Revert "Merge pull request #1811 from vogdb/issue-1811"
    This reverts commit 40a42d9d19f16ab6acefd6142755c9c640154331, reversing
    changes made to fa0aa4242d47e9f1ce358cce9fc972ba28371c29.

---
+++ client-vendor/after-body/jquery.clickFeedback/jquery.clickFeedback.js
@@ -0,0 +1,35 @@
+/*
+ * Author: CM
+ * Dependencies: jquery.transit.js
+ */
+(function($) {
+
+  document.addEventListener('click', function(event) {
+    var $elem = $(event.target).closest('.clickFeedback');
+
+    if ($elem.length) {
+      var buttonOffset = $elem.offset();
+      var feedbackSize = 2 * Math.sqrt(Math.pow($elem.outerWidth(), 2) + Math.pow($elem.outerHeight(), 2));
+
+      var posX = event.pageX;
+      var posY = event.pageY;
+
+      var $feedback = $('<div class="clickFeedback-ripple" />');
+      $feedback.css({
+        width: feedbackSize,
+        height: feedbackSize,
+        left: posX - buttonOffset.left - (feedbackSize / 2),
+        top: posY - buttonOffset.top - (feedbackSize / 2)
+      });
+      $elem.append($feedback);
+      $feedback.transition({
+        scale: 1
+      }, '200ms', 'in').transition({
+        opacity: 0
+      }, '200ms', 'out', function() {
+        $feedback.remove();
+      });
+    }
+  });
+
+})(jQuery);
