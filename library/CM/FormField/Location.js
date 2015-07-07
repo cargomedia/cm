597f3891f731e29683c7c43a83e200689d2ce12b
vogdb <vozhdb@gmail.com>
2015-07-07 19:28:43 +0300

    Translation of 'Unable to detect location'.

--- library/CM/FormField/Location.js
+++ library/CM/FormField/Location.js
@@ -62,7 +62,7 @@ var CM_FormField_Location = CM_FormField_SuggestOne.extend({
         }
       })
       .catch(function(error) {
-        cm.window.hint('Unable to detect location');
+        cm.window.hint(cm.language.get('Unable to detect location'));
       })
       .finally(function() {
         self.$('.detect-location').removeClass('waiting');
