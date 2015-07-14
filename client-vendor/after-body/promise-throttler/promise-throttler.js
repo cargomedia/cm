295f48137be8ea1f173c6470fb2edc2a1143a3f6
vogdb <vozhdb@gmail.com>
2015-07-14 20:29:52 +0300

    Revert "Add an option to ignore later calls in promiseThrottler."
    This reverts commit 436b738fad78136bba65438ff092b98a422c59cb.

--- client-vendor/after-body/promise-throttler/promise-throttler.js
+++ client-vendor/after-body/promise-throttler/promise-throttler.js
@@ -12,48 +12,32 @@
 
   /**
    * @param {PromiseThrottled} fn
-   * @param {Object|null} [options]
+   * @param {Object|null} options
    * @param {Boolean} options.cancel Whether to cancel the previous promise if it is still running.
    * @returns {PromiseThrottled}
    */
   function throttleFunction(fn, options) {
     options = _.defaults(options || {}, {cancel: false});
     var promise;
-    var laterPromiseToCancel;
 
     return function() {
-      console.log('promise is ', promise);
       if (promise && promise.isPending()) {
         if (options.cancel && promise.isCancellable()) {
-          console.log('promise cancelled');
           promise.cancel();
           promise = null;
         }
       }
 
       if (!promise || !promise.isPending()) {
-        promise = fn.apply(null, arguments);
-        console.log('promise created ', promise);
-        if (!promise instanceof Promise) {
-          console.log('promise resolved');
-          promise = Promise.resolve();
+        var result = fn.apply(null, arguments);
+        if (!result instanceof Promise) {
+          result = Promise.resolve(result);
         }
-      } else if (!options.cancel) {
-        if (!laterPromiseToCancel) {
-          console.log('create redundant promise');
-          laterPromiseToCancel = promise.finally(function() {
-            console.log('cancel redundant promise');
-            //redundantPromise.cancel();
-            laterPromiseToCancel = null;
-            promise = null;
-            throw new Promise.CancellationError();
-          });
-        }
-        console.log('return redundant promise');
-        return laterPromiseToCancel;
       }
-      console.log('return promise');
-      return promise;
+      result.then(function() {
+        promise = null;
+      });
+      return result;
     };
   }
 
@@ -79,35 +63,5 @@
 
   // temporary backward compatibility
   global.promiseThrottler = throttleFunction;
-
-
-  /**
-   * TODO to discuss.
-   * It still has the problems.
-   * If you make `test` cancellable then the console window in web inspector will tremble for real like it is under the fever.
-   * If you make the difference between second .then timeout 500 and the Promise timeout 1500 then there is a chance that both .then will be executed cause
-   * cancellation of the second .then will happen later than .then itself.
-   */
-  var test = throttleFunction(function() {
-    return new Promise(function(resolve, reject) {
-      setTimeout(function() {
-        resolve(5);
-      }, 1500);
-    }).cancellable();
-  }/*, {cancel: true}*/);
-
-  test().then(function(result) {
-    console.log('first test is finished ' + result);
-  });
-
-  setTimeout(function() {
-    test().then(function(result) {
-      console.log('second test is finished ' + result);
-    });
-  }, 500);
-
-  test().then(function(result) {
-    console.log('third test is finished ' + result);
-  });
 
 })(window);
