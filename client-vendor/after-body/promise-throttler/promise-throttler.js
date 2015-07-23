3296a988b263f8427cde04284a60df3b56bca2aa
vogdb <vozhdb@gmail.com>
2015-07-23 10:35:11 +0300

    Add 'cancelTrailing' option to promiseThrottler.

--- client-vendor/after-body/promise-throttler/promise-throttler.js
+++ client-vendor/after-body/promise-throttler/promise-throttler.js
@@ -14,11 +14,17 @@
    * @param {PromiseThrottled} fn
    * @param {Object|null} [options]
    * @param {Boolean} options.cancelLeading Whether to cancel the previous promise if it is still running.
+   * @param {Boolean} options.cancelTrailing Whether to cancel the next promises if the current is not yet finished.
    * @param {String} options.key A custom key to store the resulted PromiseThrottled.
    * @returns {PromiseThrottled}
    */
   function promiseThrottler(fn, options) {
-    options = _.defaults(options || {}, {cancelLeading: false});
+    options = _.defaults(
+      options || {}, {
+        cancelLeading: false,
+        cancelTrailing: false
+      }
+    );
     if (options.key) {
       return namespaceThrottler(options.key, fn, options);
     } else {
@@ -36,6 +42,9 @@
       if (options.cancelLeading && promise && promise.isPending() && promise.isCancellable()) {
         promise.cancel();
         promise = null;
+      }
+      if (options.cancelTrailing && promise && promise.isPending()) {
+        return Promise.reject(new Promise.CancellationError);
       }
       if (!promise || !promise.isPending()) {
         promise = fn.apply(null, arguments);
