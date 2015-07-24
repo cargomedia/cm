2dcf0cef38f7f8a1eb39c1b502c9aefa767b0a81
Vladimir Finko <vladimir.finko@cargomedia.ch>
2015-07-24 17:14:30 +0200

    change CM_Exception constructor signature

--- library/CM/Exception.php
+++ library/CM/Exception.php
@@ -16,18 +16,19 @@ class CM_Exception extends Exception {
     private $_messagePublic;
 
     /**
-     * @param string|null         $message
-     * @param int|null            $severity
-     * @param array|null          $metaInfo
-     * @param CM_I18n_Phrase|null $messagePublic
+     * @param string|null $message
+     * @param int|null    $severity
+     * @param array|null  $metaInfo
+     * @param array|null  $options
      */
-    public function __construct($message = null, $severity = null, array $metaInfo = null, CM_I18n_Phrase $messagePublic = null) {
-        $this->_metaInfo = null !== $metaInfo ? $metaInfo : array();
+    public function __construct($message = null, $severity = null, array $metaInfo = null, array $options = null) {
+        $this->_validateOptions($options);
 
+        $this->_metaInfo = null !== $metaInfo ? $metaInfo : array();
         if (null !== $severity) {
             $this->setSeverity((int) $severity);
         }
-        $this->_messagePublic = $messagePublic;
+        $this->_messagePublic = $options['messagePublic'];
         parent::__construct($message);
     }
 
@@ -89,6 +90,32 @@ class CM_Exception extends Exception {
             default:
                 return new CM_Paging_Log_Fatal();
                 break;
+        }
+    }
+
+    /**
+     * @param array|null $options
+     * @throws CM_Exception_InvalidParam
+     */
+    private function _validateOptions(array $options = null) {
+        if (is_null($options)) {
+            return;
+        }
+
+        $validKeysToTypes = [
+            'messagePublic' => 'CM_I18n_Phrase',
+        ];
+
+        if (count($validKeysToTypes) < count($options)) {
+            throw new CM_Exception_InvalidParam('$options parameter contains invalid key(s)');
+        }
+
+        foreach ($options as $optionKey => $optionValue) {
+            if (!array_key_exists($optionKey, $validKeysToTypes)) {
+                throw new CM_Exception_InvalidParam('Invalid key for $options: `' . $optionKey . '`');
+            } elseif ($validKeysToTypes[$optionKey] !== null && !($options[$optionKey] instanceof $validKeysToTypes[$optionKey])) {
+                throw new CM_Exception_InvalidParam('Invalid type for key `' . $optionKey . '`');
+            }
         }
     }
 }
