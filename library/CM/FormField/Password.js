f54ab67001b2685d367a3c2df570fb1b9534d289
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2015-07-28 16:10:06 +0200

    Change to input type text on focus (only if empty)

--- library/CM/FormField/Password.js
+++ library/CM/FormField/Password.js
@@ -3,20 +3,30 @@
  * @extends CM_FormField_Text
  */
 var CM_FormField_Password = CM_FormField_Text.extend({
-  _class: 'CM_FormField_Password',
+    _class: 'CM_FormField_Password',
 
-  events: {
-    'click .togglePasswordMask': 'togglePasswordMask'
-  },
 
-  togglePasswordMask: function() {
-    var $input = this.$('input');
-    var $buttonText = this.$('.mode-text');
-    var $buttonPassword = this.$('.mode-password');
+    events: {
+        'focus input[type=password]': function (e) {
+            this.togglePasswordMask(e);
+        },
+        'click .togglePasswordMask': function (e) {
+            this.togglePasswordMask(e);
+        }
+    },
 
-    var showText = ($input.attr('type') === 'password');
-    $input.attr('type', showText ? 'text' : 'password');
-    $buttonText.toggle(showText);
-    $buttonPassword.toggle(!showText);
-  }
+    togglePasswordMask: function (e) {
+        var $input = this.$('input');
+        var $buttonText = this.$('.mode-text');
+        var $buttonPassword = this.$('.mode-password');
+        var showText = ($input.attr('type') === 'password');
+
+        if (e.type === 'focusin' && $input.val().length !== 0) {
+            return;
+        }
+
+        $input.attr('type', showText ? 'text' : 'password');
+        $buttonText.toggle(showText);
+        $buttonPassword.toggle(!showText);
+    }
 });
