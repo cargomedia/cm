5e575219d1a532d1310fa2502ba722b9d8119a95
vogdb <vozhdb@gmail.com>
2015-07-31 12:28:10 +0300

    Streamline formatting.

--- library/CM/FormField/Password.js
+++ library/CM/FormField/Password.js
@@ -3,52 +3,52 @@
  * @extends CM_FormField_Text
  */
 var CM_FormField_Password = CM_FormField_Text.extend({
-    _class: 'CM_FormField_Password',
+  _class: 'CM_FormField_Password',
 
-    _visibilityDesired: true,
+  _visibilityDesired: true,
 
-    events: {
-        'click .togglePasswordVisibility': function () {
-            this.togglePasswordVisibility();
-        }
-    },
-
-    ready: function () {
-        this.enableTriggerChangeOnInput();
-        this.on('change', function () {
-            this._applyDesiredPasswordVisibility();
-        });
-        var self = this;
-        this.getForm().on('submit', function() {
-            self.togglePasswordVisibility(false);
-        });
-    },
-
-    /**
-     * @param {Boolean} [state]
-     */
-    togglePasswordVisibility: function (state) {
-        if ('undefined' === typeof state) {
-            state = !this._visibilityDesired;
-        }
-        this._visibilityDesired = state;
-        this.$('.mode-text').toggle(state);
-        this.$('.mode-password').toggle(!state);
-        this._applyDesiredPasswordVisibility();
-    },
-
-    _applyDesiredPasswordVisibility: function () {
-        if (0 === this.getValue().length) {
-            this._togglePasswordVisibilityInput(false);
-        } else {
-            this._togglePasswordVisibilityInput(this._visibilityDesired);
-        }
-    },
-
-    /**
-     * @param {Boolean} state
-     */
-    _togglePasswordVisibilityInput: function (state) {
-        this.getInput().attr('type', state ? 'text' : 'password');
+  events: {
+    'click .togglePasswordVisibility': function() {
+      this.togglePasswordVisibility();
     }
+  },
+
+  ready: function() {
+    this.enableTriggerChangeOnInput();
+    this.on('change', function() {
+      this._applyDesiredPasswordVisibility();
+    });
+    var self = this;
+    this.getForm().on('submit', function() {
+      self.togglePasswordVisibility(false);
+    });
+  },
+
+  /**
+   * @param {Boolean} [state]
+   */
+  togglePasswordVisibility: function(state) {
+    if ('undefined' === typeof state) {
+      state = !this._visibilityDesired;
+    }
+    this._visibilityDesired = state;
+    this.$('.mode-text').toggle(state);
+    this.$('.mode-password').toggle(!state);
+    this._applyDesiredPasswordVisibility();
+  },
+
+  _applyDesiredPasswordVisibility: function() {
+    if (0 === this.getValue().length) {
+      this._togglePasswordVisibilityInput(false);
+    } else {
+      this._togglePasswordVisibilityInput(this._visibilityDesired);
+    }
+  },
+
+  /**
+   * @param {Boolean} state
+   */
+  _togglePasswordVisibilityInput: function(state) {
+    this.getInput().attr('type', state ? 'text' : 'password');
+  }
 });
