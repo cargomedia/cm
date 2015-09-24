c8031428bca29d8d44ab997f8bd29a6a68a52b55
vogdb <vozhdb@gmail.com>
2015-09-24 22:04:25 +0300

    Replace textarea with contenteditable.

--- library/CM/FormField/Textarea.js
+++ library/CM/FormField/Textarea.js
@@ -3,5 +3,28 @@
  * @extends CM_FormField_Text
  */
 var CM_FormField_Textarea = CM_FormField_Text.extend({
-  _class: 'CM_FormField_Textarea'
+  _class: 'CM_FormField_Textarea',
+
+  ready: function() {
+    this._initPlaceholder();
+    this._initPlaintextonly();
+  },
+
+  _initPlaceholder: function() {
+    this.$("[contenteditable]").focusout(function() {
+      var $this = $(this);
+      if (!$this.text().trim().length) {
+        $this.empty();
+      }
+    });
+  },
+
+  _initPlaintextonly: function() {
+    if (Modernizr['contenteditable-plaintext']) {
+      this.$("[contenteditable]").attr('contenteditable', 'plaintext-only')
+    } else {
+
+    }
+  }
+
 });
