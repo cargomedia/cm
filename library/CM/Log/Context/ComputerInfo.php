65c2ec4f47e8ca2b8e25751159a998e5389015c3
Michel Meyer <michel.meyer@cargomedia.ch>
2015-10-01 11:52:54 +0200

    Instantiate ComputerInfo with the fqdn and the php version

--- library/CM/Log/Context/ComputerInfo.php
+++ library/CM/Log/Context/ComputerInfo.php
@@ -2,9 +2,28 @@
 
 class CM_Log_Context_ComputerInfo {
 
-    /** @var string|null */
+    /** @var string */
     private $_fqdn;
 
-    /** @var string|null */
+    /** @var string */
     private $_phpVersion;
+
+    public function __construct() {
+        $this->_fqdn = php_uname('a');
+        $this->_phpVersion = phpversion();
+    }
+
+    /**
+     * @return string
+     */
+    public function getFullQualifiedDomainName() {
+        return $this->_fqdn;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhpVersion() {
+        return $this->_phpVersion;
+    }
 }
