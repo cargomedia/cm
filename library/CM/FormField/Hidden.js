d7df9fa7c43abba796c506586151fd3e808298f4
tomaszdurka <tomasz@durka.pl>
2015-10-02 17:03:14 +0200

    Adjust CM_FormField_Hidden

--- library/CM/FormField/Hidden.js
+++ library/CM/FormField/Hidden.js
@@ -3,19 +3,5 @@
  * @extends CM_FormField_Abstract
  */
 var CM_FormField_Hidden = CM_FormField_Abstract.extend({
-  _class: 'CM_FormField_Hidden',
-
-  /**
-   * @return String
-   */
-  getValue: function() {
-    return this.$('input:not([disabled])').val();
-  },
-
-  /**
-   * @param {String} value
-   */
-  setValue: function(value) {
-    this.$('input').val(value);
-  }
+  _class: 'CM_FormField_Hidden'
 });
