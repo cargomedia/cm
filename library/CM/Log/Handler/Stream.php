0fb9a75300f8263f22fdd0b64b6bcd48ddecaa33
Michel Meyer <michel.meyer@cargomedia.ch>
2015-10-02 17:53:32 +0200

    Implement context and exception formatting in StreamHandler

--- library/CM/Log/Handler/Stream.php
+++ library/CM/Log/Handler/Stream.php
@@ -19,7 +19,7 @@ class CM_Log_Handler_Stream extends CM_Log_Handler_Abstract {
      */
     public function __construct(CM_OutputStream_Interface $stream, $level = null, $format = null, $dateFormat = null) {
         $format = null === $format ? '[{datetime} - {levelname}] {message}' : (string) $format;
-        $dateFormat = null === $dateFormat ? 'd-m-Y H:i:s' : (string) $dateFormat;
+        $dateFormat = null === $dateFormat ? 'c' : (string) $dateFormat;
 
         $this->_format = $format;
         $this->_dateFormat = $dateFormat;
@@ -41,14 +41,89 @@ class CM_Log_Handler_Stream extends CM_Log_Handler_Abstract {
      * @return string
      */
     protected function _formatRecord(CM_Log_Record $record) {
-        $data = [
+        $message = $this->_format($this->_format, [
             'datetime'  => $record->getCreatedAt()->format($this->_dateFormat),
             'levelname' => CM_Log_Logger::getLevelName($record->getLevel()),
             'message'   => $record->getMessage(),
-        ];
-        return preg_replace_callback('/\{([a-z]+)\}/i', function ($matches) use ($data) {
-            return isset($data[$matches[1]]) ? $data[$matches[1]] : '';
-        }, $this->_format);
+        ]);
+
+        $context = $record->getContext();
+        $computerInfo = $context->getComputerInfo();
+        $user = $context->getUser();
+        $httpRequest = $context->getHttpRequest();
+        $extra = $context->getExtra();
+
+        $data = [];
+
+        if (null !== $user) {
+            $data['user'] = $this->_format('id: {id}, email: {email}', [
+                'id'    => $user->getId(),
+                'email' => $user->getEmail(),
+            ]);
+        }
+        if (null != $httpRequest) {
+            $server = $httpRequest->getServer();
+            $httpRequestText = '{type} {path} {proto}, host: {host}, ip: {ip}, referer: {referer}, user-agent: {agent}';
+            $data['httpRequest'] = $this->_format($httpRequestText, [
+                'type'    => $server['REQUEST_METHOD'],
+                'path'    => $httpRequest->getPath(),
+                'proto'   => $server['SERVER_PROTOCOL'],
+                'host'    => $httpRequest->getHost(),
+                'ip'      => $httpRequest->getIp(),
+                'referer' => $httpRequest->getHeader('referer'),
+                'agent'   => $httpRequest->getHeader('user-agent'),
+            ]);
+        }
+        if (!empty($extra)) {
+            $extraText = [];
+            foreach ($extra as $key => $value) {
+                $extraText[] = sprintf('%s: %s', $key, $value);
+            }
+            $data['extra'] = implode(', ', $extraText);
+        }
+        if ($record instanceof CM_Log_Record_Exception) {
+            $exception = $record->getException();
+
+            $traceData = [];
+            $traceCount = 0;
+            foreach ($exception->getTrace() as $trace) {
+                $traceData[] = sprintf('     %02d. %s %s:%s', $traceCount++, $trace['code'], $trace['file'], $trace['line']);
+            }
+            $traceText = implode(PHP_EOL, $traceData);
+
+            $data['exception'] = PHP_EOL . $this->_formatArrayToLines('   - %s: %s', [
+                    'message'    => $exception->getMessage(),
+                    'type'       => $exception->getClass(),
+                    'stacktrace' => PHP_EOL . $traceText,
+                ]);
+        }
+
+        return $message . PHP_EOL . $this->_formatArrayToLines(' - %s: %s', $data);
     }
 
+    /**
+     * @param string $text
+     * @param array  $data
+     * @return string
+     */
+    protected function _format($text, array $data) {
+        $text = (string) $text;
+        return preg_replace_callback('/\{([a-z]+)\}/i', function ($matches) use ($data) {
+            return isset($data[$matches[1]]) ? $data[$matches[1]] : '';
+        }, $text);
+    }
+
+    /**
+     * @param string $format
+     * @param array  $data
+     * @return string
+     */
+    protected function _formatArrayToLines($format, array $data) {
+        $format = (string) $format;
+        $dataText = [];
+        foreach ($data as $key => $value) {
+            $dataText[] = sprintf($format, $key, $value);
+        }
+        return implode(PHP_EOL, $dataText);
+    }
 }
