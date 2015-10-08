1f5992ea7a5d76f3e04dab12f59a86226848c116
tomaszdurka <tomasz@durka.pl>
2015-10-08 10:57:13 +0200

    Add HtmlConverter class

---
+++ library/CM/HtmlConverter.php
@@ -0,0 +1,13 @@
+<?php
+
+class CM_HtmlConverter {
+
+    /**
+     * @param string $html
+     * @return string
+     */
+    public function convertHtmlToPlainText($html) {
+        return $html;
+    }
+
+}
---
+++ tests/library/CM/HtmlConverterTest.php
@@ -0,0 +1,26 @@
+<?php
+
+class CM_HtmlConverterTest extends CMTest_TestCase {
+
+    public function testConvertHtmlToPlainText() {
+        $suites = [
+            'Chrome, Firefox, Safari, iOS, Android' => [
+                'foo<br />&nbsp; bar<br /><br />' => "foo\nbar\n\n",
+            ],
+            'Internet Explorer 11' => [
+                '<p>foo<br />&nbsp; bar</p><p><br /></p>' => "foo\nbar\n\n",
+            ],
+            'Internet Explorer Edge' => [
+                '<div>foo<br />&nbsp; bar</div><div></div>' => "foo\nbar\n\n",
+            ],
+        ];
+
+        $converter = new CM_HtmlConverter();
+        foreach ($suites as $suiteName => $cases) {
+            foreach ($cases as $html => $expectedPlainText) {
+                $message = "Conversion failed for `{$suiteName}` for `{$html}`";
+                $this->assertSame($expectedPlainText, $converter->convertHtmlToPlainText($html), $message);
+            }
+        }
+    }
+}
