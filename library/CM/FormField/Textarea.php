939a3d95d4e5b93d64ee7ef27a18fd3d4da000be
tomaszdurka <tomasz@durka.pl>
2015-10-09 12:42:26 +0200

    Add test for non-xhtml br tag

--- library/CM/FormField/Textarea.php
+++ library/CM/FormField/Textarea.php
@@ -2,4 +2,9 @@
 
 class CM_FormField_Textarea extends CM_FormField_Text {
 
+    public function validate(CM_Frontend_Environment $environment, $userInput) {
+        $converter = new CM_HtmlConverter();
+        $userInput = $converter->convertHtmlToPlainText($userInput);
+        return parent::validate($environment, $userInput);
+    }
 }
