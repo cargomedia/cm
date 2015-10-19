8105a0330ccb3e8c69645d9ee5c457dca8288f80
tomaszdurka <tomasz@durka.pl>
2015-10-19 13:55:05 +0200

    Simplify code

--- library/CM/Wowza/Factory.php
+++ library/CM/Wowza/Factory.php
@@ -9,14 +9,14 @@ class CM_Wowza_Factory {
     public function createService(array $servers) {
         $configuration = new CM_Wowza_Configuration();
         foreach ($servers as $serverId => $serverConfig) {
-            $publicHost = $serverConfig['publicHost'];
-            $publicIp = $serverConfig['publicIp'];
-            $privateIp = $serverConfig['privateIp'];
-            $wowzaPort = $serverConfig['wowzaHost'];
-            $httpPort = $serverConfig['httpPort'];
-
-            $server = new CM_MediaStreams_Server($serverId, $publicHost, $publicIp, $privateIp, $wowzaPort, $httpPort);
-            $configuration->add($server);
+            $configuration->add(new CM_MediaStreams_Server(
+                $serverId,
+                $serverConfig['publicHost'],
+                $serverConfig['publicIp'],
+                $serverConfig['privateIp'],
+                $serverConfig['wowzaHost'],
+                $serverConfig['httpPort']
+            ));
         }
         return new CM_Wowza_Service($configuration);
     }
