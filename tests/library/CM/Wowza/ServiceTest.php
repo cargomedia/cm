25a5fa123a13324726d78c33d5544287730d547f
tomaszdurka <tomasz@durka.pl>
2015-10-20 13:09:44 +0200

    Fix CM_Wowza_ServiceTest

--- tests/library/CM/Wowza/ServiceTest.php
+++ tests/library/CM/Wowza/ServiceTest.php
@@ -2,9 +2,6 @@
 
 class CM_Wowza_ServiceTest extends CMTest_TestCase {
 
-    public function testSynchronize() {
-    }
-
     public function testStopStream() {
         $streamChannel = $this->mockClass('CM_Model_StreamChannel_Abstract')->newInstanceWithoutConstructor();
         $streamChannel->mockMethod('getServerId')->set(5);
@@ -12,25 +9,26 @@ class CM_Wowza_ServiceTest extends CMTest_TestCase {
 
         $stream = $this->mockObject('CM_Model_Stream_Abstract');
         $stream->mockMethod('getStreamChannel')->set($streamChannel);
-        $stream->mockMethod('getClientKey')->set('foo');
+        $stream->mockMethod('getKey')->set('foo');
         /** @var CM_Model_Stream_Abstract $stream */
 
-        $server = $this->mockClass('CM_MediaStreams_Server')->newInstanceWithoutConstructor();
-        $configuration = $this->mockObject('configuration');
-        $getServerMethod = $configuration->mockMethod('getServer')->set(function ($serverId) {
+        $server = $this->mockClass('CM_Wowza_Server')->newInstanceWithoutConstructor();
+        $configuration = $this->mockObject('CM_Wowza_Configuration');
+        $configuration->mockMethod('getServer')->set(function ($serverId) use ($server) {
             $this->assertSame(5, $serverId);
+            return $server;
         });
         /** @var CM_Wowza_Configuration $configuration */
 
-        $httpClient = $this->mockObject('CM_Wowza_HttpClient');
-        $stopClientMethod = $httpClient->mockMethod('stopClient')->set(function ($clientKey) {
+        $httpClient = $this->mockClass('CM_Wowza_HttpApiClient')->newInstanceWithoutConstructor();
+        $stopClientMethod = $httpClient->mockMethod('stopClient')->set(function ($passedServer, $clientKey) use ($server) {
             $this->assertSame('foo', $clientKey);
+            $this->assertSame($server, $passedServer);
         });
         /** @var CM_Wowza_HttpApiClient $httpClient */
 
         $wowza = new CM_Wowza_Service($configuration, $httpClient);
         $this->callProtectedMethod($wowza, '_stopStream', [$stream]);
-        $this->assertSame(1, $getServerMethod);
-        $this->assertSame(1, $stopClientMethod);
+        $this->assertSame(1, $stopClientMethod->getCallCount());
     }
 }
