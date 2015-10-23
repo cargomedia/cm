5affaba83e06a1e89f5f8fa3321d2aa9222199d8
Michel Meyer <michel.meyer@cargomedia.ch>
2015-10-23 17:33:08 +0200

    Change the method to pass the env variable

--- library/CM/Asset/Javascript/Abstract.php
+++ library/CM/Asset/Javascript/Abstract.php
@@ -51,7 +51,7 @@ class CM_Asset_Javascript_Abstract extends CM_Asset_Abstract {
             if ($debug) {
                 $args[] = '--debug';
             }
-            return CM_Util::exec('browserify', $args, null, null, ['NODE_PATH' => $rootPath]);
+            return CM_Util::exec('NODE_PATH=' . $rootPath . ' browserify', $args, null, null);
         });
     }
 }
