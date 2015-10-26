38e0d7410306245ac040197b19cd3b328a6b43d4
Michel Meyer <michel.meyer@cargomedia.ch>
2015-10-26 12:01:13 +0100

    Add quote to inline env var used in browserify cmd

--- library/CM/Asset/Javascript/Abstract.php
+++ library/CM/Asset/Javascript/Abstract.php
@@ -55,7 +55,7 @@ class CM_Asset_Javascript_Abstract extends CM_Asset_Abstract {
             if ($debug) {
                 $args[] = '--debug';
             }
-            return CM_Util::exec('NODE_PATH=' . $rootPath . ' browserify', $args, null, null);
+            return CM_Util::exec('NODE_PATH="' . $rootPath . '" browserify', $args, null, null);
         });
     }
 }
