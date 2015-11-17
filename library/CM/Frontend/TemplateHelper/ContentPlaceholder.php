298ab2eb3e6c2e30141f0799b8d5238c98bb5c83
Michel Meyer <michel.meyer@cargomedia.ch>
2015-11-17 12:14:48 +0100

    Revert "Add color param to contentPlaceholder"
    This reverts commit 35d6690300c73621ad72273f3d9843905d37b299.

--- library/CM/Frontend/TemplateHelper/ContentPlaceholder.php
+++ library/CM/Frontend/TemplateHelper/ContentPlaceholder.php
@@ -3,25 +3,21 @@
 class CM_Frontend_TemplateHelper_ContentPlaceholder {
 
     /**
-     * @param string     $content
-     * @param int|null   $width
-     * @param int|null   $height
-     * @param bool|null  $stretch
-     * @param array|null $color [R,G,B,A]
+     * @param string    $content
+     * @param int|null  $width
+     * @param int|null  $height
+     * @param bool|null $stretch
      * @return string
      */
-    public static function create($content, $width = null, $height = null, $stretch = null, array $color = null) {
+    public static function create($content, $width = null, $height = null, $stretch = null) {
         if (null === $width) {
             $width = 1;
         }
         if (null === $height) {
             $height = 1;
         }
-        if (null === $color) {
-            $color = [255, 255, 255, 0];
-        }
         $stretch = (bool) $stretch;
-        $imageData = self::_createBlankImage($width, $height, $color);
+        $imageData = self::_createBlankImage($width, $height);
         $imageSrc = 'data:image/png;base64,' . base64_encode($imageData);
 
         $output = '<div class="contentPlaceholder' . ($stretch ? ' stretch' : '') . '">';
@@ -33,18 +29,16 @@ class CM_Frontend_TemplateHelper_ContentPlaceholder {
     }
 
     /**
-     * @param int   $width
-     * @param int   $height
-     * @param array $color [R,G,B,A]
+     * @param int $width
+     * @param int $height
      * @return string
      */
-    private static function _createBlankImage($width, $height, array $color) {
+    private static function _createBlankImage($width, $height) {
         $cache = CM_Cache_Local::getInstance();
-
-        return $cache->get($cache->key(__CLASS__, $width, $height, $color), function () use ($width, $height, $color) {
+        return $cache->get($cache->key(__CLASS__, $width, $height), function () use ($width, $height) {
             $image = imagecreate($width, $height);
             ob_start();
-            imagecolorallocatealpha($image, $color[0], $color[1], $color[2], $color[3]);
+            imagecolorallocate($image, 255, 255, 255);
             imagepng($image);
             $imageData = ob_get_contents();
             ob_end_clean();
