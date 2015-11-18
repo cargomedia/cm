a4e89ea06f57efea83b11a3af4825d759969ac10
vogdb <vozhdb@gmail.com>
2015-11-18 15:53:50 +0300

    Add Modernizr deviceorientation-motion test

--- client-vendor/before-body/01-modernizr/modernizr.js
+++ client-vendor/before-body/01-modernizr/modernizr.js
@@ -1,6 +1,6 @@
 /*!
  * modernizr v3.2.0
- * Build http://modernizr.com/download?-classlist-csstransforms3d-fileinput-fullscreen-objectfit-peerconnection-requestanimationframe-touchevents-webgl-addtest-printshiv-dontmin
+ * Build http://modernizr.com/download?-classlist-csstransforms3d-devicemotion_deviceorientation-fileinput-fullscreen-objectfit-peerconnection-requestanimationframe-touchevents-webgl-addtest-printshiv-dontmin
  *
  * Copyright (c)
  *  Faruk Ates
@@ -1638,6 +1638,37 @@
 !*/
 
   Modernizr.addTest('objectfit', !!prefixed('objectFit'), {aliases: ['object-fit']});
+
+/*!
+{
+  "name": "Orientation and Motion Events",
+  "property": ["devicemotion", "deviceorientation"],
+  "caniuse": "deviceorientation",
+  "notes": [{
+    "name": "W3C Editor's Draft",
+    "href": "http://dev.w3.org/geo/api/spec-source-orientation.html"
+  },{
+    "name": "Implementation by iOS Safari (Orientation)",
+    "href": "http://goo.gl/fhce3"
+  },{
+    "name": "Implementation by iOS Safari (Motion)",
+    "href": "http://goo.gl/rLKz8"
+  }],
+  "authors": ["Shi Chuan"],
+  "tags": ["event"],
+  "builderAliases": ["event_deviceorientation_motion"]
+}
+!*/
+/* DOC
+Part of Device Access aspect of HTML5, same category as geolocation.
+
+`devicemotion` tests for Device Motion Event support, returns boolean value true/false.
+
+`deviceorientation` tests for Device Orientation Event support, returns boolean value true/false
+*/
+
+  Modernizr.addTest('devicemotion', 'DeviceMotionEvent' in window);
+  Modernizr.addTest('deviceorientation', 'DeviceOrientationEvent' in window);
 
 /*!
 {
