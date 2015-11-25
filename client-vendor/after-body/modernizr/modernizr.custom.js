8d895d600bc1a3067851c3c2cb396914ae8ac962
vogdb <vozhdb@gmail.com>
2015-11-25 20:02:06 +0300

    Revert. Add custom Modernizr getusermedia test

--- client-vendor/after-body/modernizr/modernizr.custom.js
+++ client-vendor/after-body/modernizr/modernizr.custom.js
@@ -10,9 +10,4 @@
   Modernizr.addTest('webvr', function() {
     return ('getVRDevices' in navigator) || ('mozGetVRDevices' in navigator);
   });
-  Modernizr.addTest('getusermedia', function() {
-    var inMediaDevices = (navigator.mediaDevices && !!Modernizr.prefixed('getUserMedia', navigator.mediaDevices));
-    var inNavigator = !!Modernizr.prefixed('getUserMedia', navigator);
-    return inMediaDevices || inNavigator;
-  });
 })();
