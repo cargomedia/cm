1bc98c3c2b53d9309eee4d05b6b9e17645b37c5b
vogdb <vozhdb@gmail.com>
2015-11-25 14:30:53 +0300

    Add custom Modernizr getusermedia test

--- client-vendor/after-body/modernizr/modernizr.custom.js
+++ client-vendor/after-body/modernizr/modernizr.custom.js
@@ -10,4 +10,9 @@
   Modernizr.addTest('webvr', function() {
     return ('getVRDevices' in navigator) || ('mozGetVRDevices' in navigator);
   });
+  Modernizr.addTest('getusermedia', function() {
+    var inMediaDevices = (navigator.mediaDevices && !!Modernizr.prefixed('getUserMedia', navigator.mediaDevices));
+    var inNavigator = !!Modernizr.prefixed('getUserMedia', navigator);
+    return inMediaDevices || inNavigator;
+  });
 })();
