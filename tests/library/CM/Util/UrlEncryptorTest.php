2e2ae906cc6ffc70b10f6a83086715da3b0b9f55
Alexis Peter <alexis.peter@cargomedia.ch>
2015-12-02 17:29:37 +0100

    add base64 check

--- tests/library/CM/Util/UrlEncryptorTest.php
+++ tests/library/CM/Util/UrlEncryptorTest.php
@@ -8,6 +8,7 @@ class CM_Util_UrlEncryptorTest extends CMTest_TestCase {
 
         $encryptor = new CM_Util_UrlEncryptor();
         $encrypted = $encryptor->encrypt($plain, $encryptionKey);
+        $this->assertNotSame(false, base64_decode($encrypted, true), 'Encrypted data is not valid base64 string');
         $this->assertNotEquals($plain, $encryptor->decrypt($encrypted, $encryptionKey . '_'));
         $this->assertSame($plain, $encryptor->decrypt($encrypted, $encryptionKey));
     }
