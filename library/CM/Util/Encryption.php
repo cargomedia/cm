d567e59cd48edf15c343ffe5a18e328cd6177df9
Vladimir Finko <vladimir.finko@cargomedia.ch>
2015-12-03 13:17:39 +0100

    add key validation, renamed classes

---
+++ library/CM/Util/Encryption.php
@@ -0,0 +1,54 @@
+<?php
+
+class CM_Util_Encryption {
+
+    /**
+     * @param string $data
+     * @param string $secretKey
+     * @return string
+     */
+    public function encryptUrl($data, $secretKey) {
+        return base64_encode($this->encrypt($data, $secretKey));
+    }
+
+    /**
+     * @param string $data
+     * @param string $secretKey
+     * @return string
+     */
+    public function decryptUrl($data, $secretKey) {
+        return $this->decrypt(base64_decode($data), $secretKey);
+    }
+
+    /**
+     * @param string $data
+     * @param string $secretKey
+     * @return string
+     */
+    public function encrypt($data, $secretKey) {
+        $this->_validateKey($secretKey);
+        return mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $secretKey, $data, MCRYPT_MODE_ECB);
+    }
+
+    /**
+     * @param $data
+     * @param $secretKey
+     * @return string
+     */
+    public function decrypt($data, $secretKey) {
+        $this->_validateKey($secretKey);
+        return rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $secretKey, $data, MCRYPT_MODE_ECB), "\0");
+    }
+
+    /**
+     * @param string $secretKey
+     * @throws CM_Exception_Invalid
+     */
+    private function _validateKey($secretKey) {
+        $keySize = strlen($secretKey);
+        $validKeySizes = mcrypt_module_get_supported_key_sizes(MCRYPT_RIJNDAEL_128);
+        if (!in_array($keySize, $validKeySizes, true)) {
+            throw new CM_Exception_Invalid('Using wrong key for encryption/decryption');
+        }
+    }
+}
---
+++ tests/library/CM/Util/EncryptionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+class CM_Util_EncryptionTest extends CMTest_TestCase {
+
+    public function testInvalidKeyFailing() {
+        $encryption = new CM_Util_Encryption();
+
+        $exception = $this->catchException(function () use ($encryption) {
+            $encryption->encrypt('foo', 'BadKey');
+        });
+        $this->assertInstanceOf('CM_Exception_Invalid', $exception);
+        $this->assertSame('Using wrong key for encryption/decryption', $exception->getMessage());
+
+        $exception = $this->catchException(function () use ($encryption) {
+            $encryption->decrypt('bar', 'BadKey');
+        });
+        $this->assertInstanceOf('CM_Exception_Invalid', $exception);
+        $this->assertSame('Using wrong key for encryption/decryption', $exception->getMessage());
+    }
+
+    public function testEncryptDecryptUrl() {
+        $encryptionKey = '!@#12345AbCdE901';
+        $plain = 'highlySecretData';
+        $encryption = new CM_Util_Encryption();
+        $encrypted = $encryption->encryptUrl($plain, $encryptionKey);
+        $this->assertNotSame(false, base64_decode($encrypted, true), 'Encrypted data is not valid base64 string');
+        $this->assertNotEquals($plain, $encryption->decryptUrl($encrypted, str_replace('1', '2', $encryptionKey)));
+        $this->assertSame($plain, $encryption->decryptUrl($encrypted, $encryptionKey));
+    }
+}
