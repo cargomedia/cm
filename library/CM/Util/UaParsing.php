9403e41348386b3f5476763cb90913edf5420fa0
Vladimir Finko <vladimir.finko@cargomedia.ch>
2015-12-10 10:42:28 +0100

    cache drafts

--- library/CM/Util/UaParsing.php
+++ library/CM/Util/UaParsing.php
@@ -2,6 +2,8 @@
 
 class CM_Util_UaParsing extends Jenssegers\Agent\Agent {
 
+    const CACHE_PREFIX = 'CM_Util_UaParsing:';
+
     protected $_CMRules = [
         'Samsung' => 'SM-G925F',
     ];
@@ -22,7 +24,42 @@ class CM_Util_UaParsing extends Jenssegers\Agent\Agent {
         $this->setDetectionType(self::DETECTION_TYPE_EXTENDED);
         $_rules = $this->getRules();
 
-        return $this->match($_rules[$brand]) && (bool) preg_match(sprintf('#%s#is', $_rules[$brand]), $modelToken);
+        return $this->matchUAAgainstKey($brand) && (bool) preg_match(sprintf('#%s#is', $_rules[$brand]), $modelToken);
     }
+
+    public function checkHttpHeadersForMobile() {
+        $cacheKey = self::CACHE_PREFIX . md5(serialize($this->httpHeaders)) . ':checkHttpHeadersForMobile:';
+        $cache = CM_Cache_Shared::getInstance();
+        if (($isMatch = $cache->get($cacheKey)) === false) {
+            $isMatch = parent::checkHttpHeadersForMobile();
+            $cache->set($cacheKey, (int) $isMatch);
+        }
+        return (bool) $isMatch;
+    }
+
+    protected function matchUAAgainstKey($key) {
+        $cacheKey = self::CACHE_PREFIX . md5($this->userAgent) . ':matchUAAgainstKey:' . $key;
+        $cache = CM_Cache_Shared::getInstance();
+        if (($isMatch = $cache->get($cacheKey)) === false) {
+            $isMatch = parent::matchUAAgainstKey($key);
+            $cache->set($cacheKey, (int) $isMatch);
+        }
+        return (bool) $isMatch;
+    }
+
+    protected function matchDetectionRulesAgainstUA($userAgent = null) {
+        $cacheKey = self::CACHE_PREFIX . md5($this->userAgent) . ':matchDetectionRulesAgainstUA';
+        $cache = CM_Cache_Shared::getInstance();
+        if (($isMatch = $cache->get($cacheKey)) === false) {
+            $isMatch = parent::matchDetectionRulesAgainstUA();
+            $cache->set($cacheKey, (int) $isMatch);
+        }
+        return (bool) $isMatch;
+    }
+
+    //TODO cache
+    //findDetectionRulesAgainstUA
+    //isTablet
+    //isRobot
 
 }
