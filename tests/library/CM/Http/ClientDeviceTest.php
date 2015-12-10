5a4e544ca45bffda6278e1b5bfcf1ceace1d137d
Vladimir Finko <vladimir.finko@cargomedia.ch>
2015-12-10 13:03:18 +0100

    add tests

---
+++ tests/library/CM/Http/ClientDeviceTest.php
@@ -0,0 +1,42 @@
+<?php
+
+class CM_Http_ClientDeviceTest extends CMTest_TestCase {
+
+    public function testIsMobile() {
+
+        $nonMobileHeaders = [
+            'content-type' => '',
+            'content-length' => '',
+            'host' => 'dev.cm',
+            'connection' => 'keep-alive',
+            'cache-control' => 'max-age=0',
+            'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'upgrade-insecure-requests' => '1',
+            'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36',
+            'accept-encoding' => 'gzip, deflate, sdch',
+            'accept-language' => 'en-US,en;q=0.8,de-CH;q=0.6,de;q=0.4,fr;q=0.2,it;q=0.2,pt;q=0.2,es;q=0.2',
+            'cookie' => '__utma=12212262.646672065.1416384804.1441359798.1442238760.18; __utmc=12212262; __utmz=12212262.1437935861.16.3.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided);',
+        ];
+
+        $clientDeviceDetector = new CM_Http_ClientDevice(CM_Http_Request_Abstract::factory('get', '/foo', $nonMobileHeaders));
+
+        $this->assertInstanceOf('CM_Http_ClientDevice', $clientDeviceDetector);
+        $this->assertFalse($clientDeviceDetector->isMobile());
+
+        $mobileHeaders = [
+            'content-type' => '',
+            'content-length' => '',
+            'host' => 'fuckbook.dev.cargomedia.ch',
+            'connection' => 'keep-alive',
+            'cache-control' => 'max-age=0',
+            'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'upgrade-insecure-requests' => '1',
+            'user-agent' => 'Mozilla/5.0 (Linux; Android 5.0.2; SAMSUNG SM-G925F Build/LRX22G) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/3.0 Chrome/38.0.2125.102 Mobile Safari/537.36',
+            'accept-encoding' => 'gzip, deflate, sdch',
+            'accept-language' => 'en-US,en;q=0.8,de-CH;q=0.6,de;q=0.4,fr;q=0.2,it;q=0.2,pt;q=0.2,es;q=0.2',
+        ];
+
+        $clientDeviceDetector = new CM_Http_ClientDevice(CM_Http_Request_Abstract::factory('post', '/bar', $mobileHeaders));
+        $this->assertTrue($clientDeviceDetector->isMobile());
+    }
+}
