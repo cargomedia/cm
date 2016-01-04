b1b6b4f74590d4d1fa6b1005175c3bd3c4a8d62f
Alexis Peter <alexis.peter@cargomedia.ch>
2016-01-04 16:31:18 +0100

    add paging to fetch all streamchannel archives with a certain key

---
+++ tests/library/CM/Paging/StreamChannelArchiveMedia/KeyTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class CM_Paging_StreamChannelArchive_KeyTest extends CMTest_TestCase {
+
+    public function testPaging() {
+        $key = 'foo';
+        $streamChannel1 = CMTest_TH::createStreamChannel();
+        $streamChannel1->_set('key', $key);
+        $streamChannel2 = CMTest_TH::createStreamChannel();
+        $streamChannel2->_set('key', $key);
+
+        $this->assertEquals([], new CM_Paging_StreamChannelArchiveMedia_Key($key));
+        $streamChannelArchive1 = CMTest_TH::createStreamChannelVideoArchive($streamChannel1);
+        CMTest_TH::timeForward(1);
+        $streamChannelArchive2 = CMTest_TH::createStreamChannelVideoArchive($streamChannel2);
+        $this->assertEquals([$streamChannelArchive2, $streamChannelArchive1], new CM_Paging_StreamChannelArchiveMedia_Key($key));
+    }
+}
