911d5d88cdf1f1b36a9c43dbe7024b334f4e77bf
Alexis Peter <alexis.peter@cargomedia.ch>
2016-01-26 16:03:51 +0100

    Remove publish-dependency from archive

--- library/CM/Model/StreamChannelArchive/Media.php
+++ library/CM/Model/StreamChannelArchive/Media.php
@@ -146,11 +146,6 @@ class CM_Model_StreamChannelArchive_Media extends CM_Model_StreamChannelArchive_
         $streamChannel = $data['streamChannel'];
         $createStamp = $streamChannel->getCreateStamp();
         $userId = null;
-        if ($streamChannel->hasStreamPublish()) {
-            $streamPublish = $streamChannel->getStreamPublish();
-            $createStamp = $streamPublish->getStart();
-            $userId = $streamPublish->getUserId();
-        }
         $file = isset($data['file']) ? $data['file'] : null;
         $end = time();
         $duration = $end - $createStamp;
