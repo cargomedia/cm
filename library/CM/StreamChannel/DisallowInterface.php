ce437df2221d47cad4cb8d7705250ad5463b8aa8
Alexis Peter <alexis.peter@cargomedia.ch>
2016-01-26 13:44:50 +0100

    user should be nullable?

--- library/CM/StreamChannel/DisallowInterface.php
+++ library/CM/StreamChannel/DisallowInterface.php
@@ -7,14 +7,14 @@ interface CM_StreamChannel_DisallowInterface {
      * @param int           $allowedUntil
      * @return int
      */
-    function canPublish(CM_Model_User $user, $allowedUntil);
+    function canPublish(CM_Model_User $user = null, $allowedUntil);
 
-    /**
+    /**user
      * @param CM_Model_User $user
      * @param int           $allowedUntil
      * @return int
      */
-    function canSubscribe(CM_Model_User $user, $allowedUntil);
+    function canSubscribe(CM_Model_User $user = null, $allowedUntil);
 
     /**
      * @return boolean
