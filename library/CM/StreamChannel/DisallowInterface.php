1aad2769c824058c4bda7994b9edeced6930261d
Alexis Peter <alexis.peter@cargomedia.ch>
2016-01-27 11:13:01 +0100

    fix docu

--- library/CM/StreamChannel/DisallowInterface.php
+++ library/CM/StreamChannel/DisallowInterface.php
@@ -3,15 +3,15 @@
 interface CM_StreamChannel_DisallowInterface {
 
     /**
-     * @param CM_Model_User $user
-     * @param int           $allowedUntil
+     * @param CM_Model_User|null $user
+     * @param int                $allowedUntil
      * @return int
      */
     function canPublish(CM_Model_User $user = null, $allowedUntil);
 
-    /**user
-     * @param CM_Model_User $user
-     * @param int           $allowedUntil
+    /**
+     * @param CM_Model_User|null $user
+     * @param int                $allowedUntil
      * @return int
      */
     function canSubscribe(CM_Model_User $user = null, $allowedUntil);
