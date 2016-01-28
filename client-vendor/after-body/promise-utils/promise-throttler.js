4760e14dc33c4b7c8cf941b13423d33fd0e823fe
vogdb <vozhdb@gmail.com>
2016-01-28 20:05:20 +0300

    Remove cancelTrailing functionality from promiseThrottler

--- client-vendor/after-body/promise-utils/promise-throttler.js
+++ client-vendor/after-body/promise-utils/promise-throttler.js
@@ -14,7 +14,6 @@
    * @param {PromiseThrottled} fn
    * @param {Object|null} [options]
    * @param {Boolean} options.cancelLeading Whether to cancel the previous promise if it is still running.
-   * @param {Boolean} options.cancelTrailing Whether to cancel the next promises if the current is not yet finished.
    * @param {Boolean} options.queue Whether to queue the next promises.
    * @param {String} options.key A custom key to store the resulted PromiseThrottled.
    * @returns {PromiseThrottled}
@@ -23,12 +22,11 @@
     options = _.defaults(
       options || {}, {
         cancelLeading: false,
-        cancelTrailing: false,
         queue: false
       }
     );
-    if ((+options.cancelLeading) + (+options.cancelTrailing) + (+options.queue) > 1) {
-      throw new Error('PromiseThrottler options "cancelLeading", "cancelTrailing", "queue" exclude each other.');
+    if ((+options.cancelLeading) + (+options.queue) > 1) {
+      throw new Error('PromiseThrottler options "cancelLeading", "queue" exclude each other.');
     }
     if (options.key) {
       return namespaceThrottler(options.key, fn, options);
@@ -47,9 +45,6 @@
       if (options.cancelLeading && promise && promise.isPending() && promise.isCancellable()) {
         promise.cancel();
         promise = null;
-      }
-      if (options.cancelTrailing && promise && promise.isPending()) {
-        return Promise.reject(new Promise.CancellationError);
       }
       if (options.queue && promise && promise.isPending()) {
         var args = arguments;
