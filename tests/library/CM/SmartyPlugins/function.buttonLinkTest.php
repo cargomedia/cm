3b5114f1f7ad814bbf576b8f94a7079f8a3d4d72
Reto Kaiser <reto@cargomedia.ch>
2016-01-29 14:32:17 +0100

    Use <a> tag for button with page target
    Also removing unused "onclick" parameter

--- tests/library/CM/SmartyPlugins/function.buttonLinkTest.php
+++ tests/library/CM/SmartyPlugins/function.buttonLinkTest.php
@@ -4,16 +4,17 @@ require_once CM_Util::getModulePath('CM') . 'library/CM/SmartyPlugins/function.b
 
 class smarty_function_button_linkTest extends CMTest_TestCase {
 
-    /**
-     * @var Smarty_Internal_Template
-     */
+    /** @var CM_Frontend_Render */
+    private $_render;
+
+    /** @var Smarty_Internal_Template */
     private $_template;
 
     public function setUp() {
         $smarty = new Smarty();
-        $render = new CM_Frontend_Render();
+        $this->_render = new CM_Frontend_Render();
         $this->_template = $smarty->createTemplate('string:');
-        $this->_template->assignGlobal('render', $render);
+        $this->_template->assignGlobal('render', $this->_render);
     }
 
     public function testRender() {
@@ -28,6 +29,21 @@ class smarty_function_button_linkTest extends CMTest_TestCase {
         $this->_assertContains('<span class="label">Some text <br /> with html tags</span>', array_merge($params, array('isHtmlLabel' => true)));
         $this->_assertContains('value="Some text &lt;br /&gt; with html tags"', array_merge($params, array('isHtmlLabel' => false)));
         $this->_assertContains('<span class="label">Some text &lt;br /&gt; with html tags</span>', array_merge($params, array('isHtmlLabel' => false)));
+    }
+
+    public function testRenderHref() {
+        $this->assertRegExp(
+            '#<a.*href="http://example.com".*>.*Click me.*</a>#',
+            smarty_function_button_link(['label' => 'Click me', 'href' => 'http://example.com'], $this->_template)
+        );
+    }
+
+    public function testRenderPage() {
+        $urlPage = $this->_render->getUrlPage('CM_Page_Example', ['foo' => '12']);
+        $this->assertRegExp(
+            '#<a.*href="' . preg_quote($urlPage) . '".*>.*Click me.*</a>#',
+            smarty_function_button_link(['label' => 'Click me', 'page' => 'CM_Page_Example', 'foo' => '12'], $this->_template)
+        );
     }
 
     /**
