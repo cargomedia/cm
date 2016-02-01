17c5ef268a830c49bd6d57679ea2dcb935c3ddcb
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-02-01 20:41:39 +0100

    set backupGlobals in config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,8 @@
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-// phpunit fail to serialize the Bootloader if it's has some unserializable object, so let's wrap it in a Closure to remove it from $_GLOBALS
-call_user_func(function(){
-    $bootloader = new CM_Bootloader_Testing(dirname(__DIR__) . '/');
-    $bootloader->load();
-});
+$bootloader = new CM_Bootloader_Testing(dirname(__DIR__) . '/');
+$bootloader->load();
 
 $suite = new CMTest_TestSuite();
 $suite->setDirTestData(__DIR__ . '/data/');
