a82f69b9abbf659dda14883696ce8d0a247973af
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-02-04 16:01:24 +0100

    rename bubble param

--- library/CM/Log/Handler/Factory.php
+++ library/CM/Log/Handler/Factory.php
@@ -10,15 +10,15 @@ class CM_Log_Handler_Factory implements CM_Service_ManagerAwareInterface {
      * @param string|null $formatMessage
      * @param string|null $formatDate
      * @param int|null    $level
-     * @param bool|null   $bubble
+     * @param bool|null   $stopPropagation
      * @return CM_Log_Handler_Stream
      */
-    public function createStreamHandler($streamClass, array $streamArguments = null, $formatMessage = null, $formatDate = null, $level = null, $bubble = null) {
+    public function createStreamHandler($streamClass, array $streamArguments = null, $formatMessage = null, $formatDate = null, $level = null, $stopPropagation = null) {
         $streamClass = new ReflectionClass($streamClass);
         $streamArguments = null !== $streamArguments ? (array) $streamArguments : [];
         /** @var CM_OutputStream_Interface $stream */
         $stream = $streamClass->newInstanceArgs($streamArguments);
-        return $this->_createOutputStreamHandler($stream, $formatMessage, $formatDate, $level, $bubble);
+        return $this->_createOutputStreamHandler($stream, $formatMessage, $formatDate, $level, $stopPropagation);
     }
 
     /**
@@ -26,15 +26,15 @@ class CM_Log_Handler_Factory implements CM_Service_ManagerAwareInterface {
      * @param string|null $formatMessage
      * @param string|null $formatDate
      * @param int|null    $level
-     * @param bool|null   $bubble
+     * @param bool|null   $stopPropagation
      * @return CM_Log_Handler_Stream
      */
-    public function createFileHandler($path, $formatMessage = null, $formatDate = null, $level = null, $bubble = null) {
+    public function createFileHandler($path, $formatMessage = null, $formatDate = null, $level = null, $stopPropagation = null) {
         $path = (string) $path;
         $filesystem = $this->getServiceManager()->getFilesystems()->getData();
         $file = new CM_File($path, $filesystem);
         $stream = new CM_OutputStream_File($file);
-        return $this->_createOutputStreamHandler($stream, $formatMessage, $formatDate, $level, $bubble);
+        return $this->_createOutputStreamHandler($stream, $formatMessage, $formatDate, $level, $stopPropagation);
     }
 
     /**
@@ -42,16 +42,16 @@ class CM_Log_Handler_Factory implements CM_Service_ManagerAwareInterface {
      * @param string|null               $formatMessage
      * @param string|null               $formatDate
      * @param int|null                  $level
-     * @param bool|null                 $bubble
+     * @param bool|null                 $stopPropagation
      * @return CM_Log_Handler_Stream
      */
-    protected function _createOutputStreamHandler(CM_OutputStream_Interface $stream, $formatMessage = null, $formatDate = null, $level = null, $bubble = null) {
+    protected function _createOutputStreamHandler(CM_OutputStream_Interface $stream, $formatMessage = null, $formatDate = null, $level = null, $stopPropagation = null) {
         $level = null !== $level ? (int) $level : $level;
-        $bubble = null !== $bubble ? (bool) $bubble : $bubble;
-
+        $stopPropagation = null !== $stopPropagation ? (bool) $stopPropagation : null;
         $formatMessage = null !== $formatMessage ? (string) $formatMessage : $formatMessage;
         $formatDate = null !== $formatDate ? (string) $formatDate : $formatDate;
         $formatter = new CM_Log_Formatter_Text($formatMessage, $formatDate);
-        return new CM_Log_Handler_Stream($stream, $formatter, $level, $bubble);
+
+        return new CM_Log_Handler_Stream($stream, $formatter, $level, $stopPropagation);
     }
 }
