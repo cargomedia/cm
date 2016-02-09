7eee1f3632907704d804ce1a21702ae82019ee4b
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-02-09 16:54:04 +0100

    remove handler CLI

--- library/CM/Log/Handler/Factory.php
+++ library/CM/Log/Handler/Factory.php
@@ -17,7 +17,7 @@ class CM_Log_Handler_Factory implements CM_Service_ManagerAwareInterface {
 
         $stream = new CM_OutputStream_Stream_StandardError();
         $formatter = new CM_Log_Formatter_Text($formatMessage, $formatDate);
-        return $this->_createStreamHandler($stream, $formatter, $level, $stopPropagation, true);
+        return $this->_createStreamHandler($stream, $formatter, $level, $stopPropagation);
     }
 
     /**
@@ -37,7 +37,7 @@ class CM_Log_Handler_Factory implements CM_Service_ManagerAwareInterface {
         $file = new CM_File($path, $filesystem);
         $stream = new CM_OutputStream_File($file);
         $formatter = new CM_Log_Formatter_Text($formatMessage, $formatDate);
-        return $this->_createStreamHandler($stream, $formatter, $level, $stopPropagation, false);
+        return $this->_createStreamHandler($stream, $formatter, $level, $stopPropagation);
     }
 
     /**
@@ -45,18 +45,12 @@ class CM_Log_Handler_Factory implements CM_Service_ManagerAwareInterface {
      * @param CM_Log_Formatter_Abstract $formatter
      * @param int|null                  $level
      * @param bool|null                 $stopPropagation
-     * @param bool|null                 $isCli
      * @return CM_Log_Handler_Stream
      */
-    protected function _createStreamHandler(CM_OutputStream_Interface $stream, CM_Log_Formatter_Abstract $formatter, $level = null, $stopPropagation = null, $isCli = null) {
+    protected function _createStreamHandler(CM_OutputStream_Interface $stream, CM_Log_Formatter_Abstract $formatter, $level = null, $stopPropagation = null) {
         $level = null !== $level ? (int) $level : $level;
         $stopPropagation = null !== $stopPropagation ? (bool) $stopPropagation : null;
-        $isCli = (bool) $isCli;
 
-        if ($isCli) {
-            return new CM_Log_Handler_Stream_Cli($stream, $formatter, $level, $stopPropagation);
-        } else {
-            return new CM_Log_Handler_Stream($stream, $formatter, $level, $stopPropagation);
-        }
+        return new CM_Log_Handler_Stream($stream, $formatter, $level, $stopPropagation);
     }
 }
