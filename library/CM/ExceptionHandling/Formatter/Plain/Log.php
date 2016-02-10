9e287477c8d986492bc29f06be35f34e43a1d41c
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-02-10 19:01:21 +0100

    delete plain log formatter

--- library/CM/ExceptionHandling/Formatter/Plain/Log.php
+++
@@ -1,8 +0,0 @@
-<?php
-//TODO remove
-class CM_ExceptionHandling_Formatter_Plain_Log extends CM_ExceptionHandling_Formatter_Plain {
-
-    public function getMetaInfo(CM_ExceptionHandling_SerializableException $exception) {
-        return '';
-    }
-}
