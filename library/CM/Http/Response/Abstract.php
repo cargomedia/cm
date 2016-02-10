38a3a8fcc27a24aec62674d2fe4ac33a8635f82b
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-02-10 19:00:31 +0100

    log response errors

--- library/CM/Http/Response/Abstract.php
+++ library/CM/Http/Response/Abstract.php
@@ -252,8 +252,8 @@ abstract class CM_Http_Response_Abstract extends CM_Class_Abstract implements CM
     }
 
     /**
-     * @param callable $regularCode
-     * @param callable $errorCode
+     * @param Closure $regularCode
+     * @param Closure $errorCode
      * @return mixed
      * @throws CM_Exception
      */
@@ -268,13 +268,10 @@ abstract class CM_Http_Response_Abstract extends CM_Class_Abstract implements CM
                 return is_a($ex, $exceptionClass);
             });
             $catchException = null !== $errorOptions;
-            if ($catchException) {
-                if (isset($errorOptions['log'])) {
-                    $formatter = new CM_ExceptionHandling_Formatter_Plain_Log();
-                    /** @var CM_Paging_Log_Abstract $log */
-                    $log = new $errorOptions['log']();
-                    $log->add($formatter->formatException($ex), $ex->getMetaInfo());
-                }
+            if ($catchException && isset($errorOptions['log'])) {
+                /** @type CM_Paging_Log_Abstract $logClass */
+                $logClass = $errorOptions['log'];
+                $this->getServiceManager()->getLogger()->addException($ex, null, $logClass::getTypeStatic());
             }
             if (!$catchException && ($catchPublicExceptions && $ex->isPublic())) {
                 $errorOptions = [];
