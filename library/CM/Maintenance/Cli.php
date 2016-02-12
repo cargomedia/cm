81431e44feb5decf7fc49b14245625a472ee073f
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-02-12 12:18:01 +0100

    adjust cli task to use CM_Paging_Log

--- library/CM/Maintenance/Cli.php
+++ library/CM/Maintenance/Cli.php
@@ -99,10 +99,9 @@ class CM_Maintenance_Cli extends CM_Cli_Runnable_Abstract {
             'CM_Action_Abstract::deleteTransgressionsOlder' => function () {
                 CM_Action_Abstract::deleteTransgressionsOlder(3 * 31 * 86400);
             },
-            'CM_Paging_Log_Abstract::cleanup'               => function () {
-                foreach (CM_Paging_Log_Abstract::getClassChildren() as $logClass) {
-                    /** @var CM_Paging_Log_Abstract $log */
-                    $log = new $logClass();
+            'CM_Paging_Log::cleanup'                        => function () {
+                foreach (CM_Log_Logger::getLevels() as $logLevel) {
+                    $log = new CM_Paging_Log($logLevel);
                     $log->cleanUp();
                 }
             },
