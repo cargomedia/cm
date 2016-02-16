a0ec473edbf5587ad7b732f8d85b091a631fac34
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-02-16 11:49:17 +0100

    Fix HandlerStream test - adjust default log level for native exceptions

--- tests/library/CM/Log/Handler/StreamTest.php
+++ tests/library/CM/Log/Handler/StreamTest.php
@@ -43,9 +43,8 @@ class CM_Log_Handler_StreamTest extends CMTest_TestCase {
         $mockStreamInterface = $this->mockInterface('CM_OutputStream_Interface')->newInstanceWithoutConstructor();
         /** @var Mocka\FunctionMock $mockWritelnMethod */
         $mockWritelnMethod = $mockStreamInterface->mockMethod('writeln');
-
         $mockWritelnMethod->set(function ($message) {
-            $this->assertRegExp('#^\[[0-9T\:\-\+]+ - none - php none - ERROR\] Exception: foo\n - exception:.*$#s', $message);
+            $this->assertRegExp('#^\[[0-9T\:\-\+]+ - none - php none - CRITICAL\] Exception: foo\n - exception:.*$#s', $message);
         });
 
         $record = new CM_Log_Record_Exception(new Exception('foo'), new CM_Log_Context());
