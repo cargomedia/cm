c009b03468ba970fef4241c5b0a4a75746e40c4d
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-03-25 14:13:11 +0100

    proper cleanup

--- library/CM/Maintenance/Cli.php
+++ library/CM/Maintenance/Cli.php
@@ -100,10 +100,13 @@ class CM_Maintenance_Cli extends CM_Cli_Runnable_Abstract {
                 CM_Action_Abstract::deleteTransgressionsOlder(3 * 31 * 86400);
             },
             'CM_Paging_Log::cleanup'                        => function () {
-                foreach (CM_Log_Logger::getLevels() as $logLevel) {
-                    $log = new CM_Paging_Log($logLevel);
+                $allLevelsList = CM_Log_Logger::getLevels();
+                foreach (CM_Paging_Log::getClassChildren() as $pagingLogClass) {
+                    /** @type CM_Paging_Log $log */
+                    $log = new $pagingLogClass($allLevelsList);
                     $log->cleanUp();
                 }
+                (new CM_Paging_Log($allLevelsList))->cleanUp(); //deletes all untyped records
             },
         ));
         if ($this->getServiceManager()->has('maxmind')) {
