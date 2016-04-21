8133782ada34153992ffa27a79f3373f875487b5
Alexis Peter <alexis.peter@cargomedia.ch>
2016-04-21 17:02:19 +0200

    simplified query-construction

--- library/CM/Paging/Translation/Language/All.php
+++ library/CM/Paging/Translation/Language/All.php
@@ -12,15 +12,15 @@ class CM_Paging_Translation_Language_All extends CM_Paging_Translation_Language_
     public function __construct(CM_Model_Language $language, $javascriptOnly = null) {
         $this->_language = $language;
         $this->_javascriptOnly = (boolean) $javascriptOnly;
-        $where = array();
+        $where = null;
         if ($javascriptOnly) {
-            $where[] = 'k.javascript = 1';
+            $where = 'k.javascript = 1';
         }
         $orderBy = 'k.name ASC';
         $join = 'LEFT JOIN `cm_languageValue` AS v ON k.id = v.languageKeyId AND v.languageId = ' . $this->_language->getId() . ' ';
         $groupBy = 'BINARY k.name';
         $source = new CM_PagingSource_Sql_Deferred('k.name AS `key`, v.value, k.variables',
-            'cm_model_languagekey` as `k', implode(' AND ', $where), $orderBy, $join, $groupBy);
+            'cm_model_languagekey` as `k', $where, $orderBy, $join, $groupBy);
         $source->enableCache(null, CM_Cache_Persistent::getInstance());
         parent::__construct($source);
     }
