8c759420946bf06745ef4e63d2a0aa141b99917c
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-04-21 12:46:48 +0200

    fix types docu

--- library/CM/ExceptionHandling/SerializableException.php
+++ library/CM/ExceptionHandling/SerializableException.php
@@ -17,7 +17,7 @@ class CM_ExceptionHandling_SerializableException {
     /** @var array|null */
     public $trace;
 
-    /** @var array */
+    /** @var string */
     public $traceString;
 
     /** @var array */
@@ -73,7 +73,7 @@ class CM_ExceptionHandling_SerializableException {
     }
 
     /**
-     * @return array
+     * @return string
      */
     public function getTraceAsString() {
         return $this->traceString;
