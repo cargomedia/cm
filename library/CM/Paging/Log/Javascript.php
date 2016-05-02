65cb78693fbe9ea91ad8ae29bbbfe827a7939388
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-05-02 13:56:58 +0200

    Add javascript errors type

---
+++ library/CM/Paging/Log/Javascript.php
@@ -0,0 +1,8 @@
+<?php
+
+class CM_Paging_Log_Javascript extends CM_Paging_Log {
+
+    public function __construct(array $filterLevelList, $aggregate = false, $ageMax = null) {
+        parent::__construct($filterLevelList, self::getTypeStatic(), $aggregate, $ageMax);
+    }
+}
