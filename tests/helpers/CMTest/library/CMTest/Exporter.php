c4af78ac127adfdecbf00b6078bb3ff20fac2abf
Alexis Peter <alexis.peter@cargomedia.ch>
2016-05-13 13:51:38 +0200

    use CM_Debug_VariableInspector for instances of CM_Debug_DebugInfoInterface

--- tests/helpers/CMTest/library/CMTest/Exporter.php
+++ tests/helpers/CMTest/library/CMTest/Exporter.php
@@ -3,15 +3,14 @@
 class CMTest_Exporter extends \SebastianBergmann\Exporter\Exporter {
 
     public function shortenedExport($value) {
-        if ($value instanceof CM_Model_Abstract) {
-            $idString = $value->hasId() ? $value->getId() : 'No Id'; 
-            return get_class($value) . "({$idString})";
+        if ($value instanceof CM_Debug_DebugInfoInterface) {
+            return (new CM_Debug_VariableInspector())->getDebugInfo($value);
         }
         return parent::shortenedExport($value);
     }
 
     public function recursiveExport(&$value, $indentation, $processed = null) {
-        if ($value instanceof CM_Model_Abstract) {
+        if ($value instanceof CM_Debug_DebugInfoInterface) {
             return $this->shortenedExport($value);
         }
         return parent::recursiveExport($value, $indentation, $processed);
