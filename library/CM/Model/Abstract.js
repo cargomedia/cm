53c296ce54109452d0ba2535012a4db7f4d493cb
vogdb <vozhdb@gmail.com>
2016-05-17 13:46:48 +0300

    CM_Model_Abstract.toJSON

--- library/CM/Model/Abstract.js
+++ library/CM/Model/Abstract.js
@@ -2,4 +2,8 @@
  * @class CM_Model_Abstract
  * @extends Backbone.Model
  */
-var CM_Model_Abstract = Backbone.Model.extend({});
+var CM_Model_Abstract = Backbone.Model.extend({
+  toJSON: function() {
+    return {id: this.get('id'), type: this.get('_type'), _type: this.get('_type')};
+  }
+});
