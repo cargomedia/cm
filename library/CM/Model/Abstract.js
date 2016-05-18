db8db0e5dfd10a4f487531bbbc2f80ab11d15bee
vogdb <vozhdb@gmail.com>
2016-05-18 17:24:06 +0300

    CM_Model_Abstract require only '_id' and '_type'

--- library/CM/Model/Abstract.js
+++ library/CM/Model/Abstract.js
@@ -7,10 +7,14 @@ var CM_Model_Abstract = Backbone.Model.extend({
   idAttribute: '_compoundId',
 
   initialize: function() {
-    this.set('_compoundId', this.get('id') + '-' + this.get('_type'));
+    this.set('_compoundId', this.get('_id').id + '-' + this.get('_type'));
   },
 
   toJSON: function() {
-    return {id: this.get('id'), _type: this.get('_type')};
+    var json = {_id: this.get('_id'), _type: this.get('_type')};
+    if (this.has('id')) {
+      json['id'] = this.get('id');
+    }
+    return json;
   }
 });
