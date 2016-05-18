923e7ef878b58a9b0d2aa7321d87308e21d9ff7e
vogdb <vozhdb@gmail.com>
2016-05-18 14:52:12 +0300

    compoundId to CM_Model_Abstract

--- library/CM/Model/Abstract.js
+++ library/CM/Model/Abstract.js
@@ -3,7 +3,14 @@
  * @extends Backbone.Model
  */
 var CM_Model_Abstract = Backbone.Model.extend({
+
+  idAttribute: '_compoundId',
+
+  initialize: function() {
+    this.set('_compoundId', this.get('id') + '-' + this.get('_type'));
+  },
+
   toJSON: function() {
-    return {id: this.get('id'), type: this.get('_type'), _type: this.get('_type')};
+    return {id: this.get('id'), _type: this.get('_type')};
   }
 });
