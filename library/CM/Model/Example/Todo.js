f22d9fa609f31f82d1abcf0822837704604f8ddf
Michel Meyer <michel.meyer@cargomedia.ch>
2016-06-08 14:04:58 +0200

    Add JS model for CM_Model_Example_Todo

---
+++ library/CM/Model/Example/Todo.js
@@ -0,0 +1,9 @@
+/**
+ * @class CM_Model_Example_Todo
+ * @extends CM_Model_Abstract
+ */
+var CM_Model_Example_Todo = CM_Model_Abstract.extend({
+
+  /** @type {String} */
+  _class: 'CM_Model_Example_Todo'
+});
