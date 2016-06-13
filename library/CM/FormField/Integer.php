53ab2f0791b4192d05bb9320031f9cab321fd9ff
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2016-06-13 14:55:06 +0200

    Make CM_FormField_Integer a simple text field

--- library/CM/FormField/Integer.php
+++ library/CM/FormField/Integer.php
@@ -1,26 +1,12 @@
 <?php
 
-class CM_FormField_Integer extends CM_FormField_Abstract {
-
-    public function prepare(CM_Params $renderParams, CM_Frontend_Environment $environment, CM_Frontend_ViewResponse $viewResponse) {
-        $viewResponse->set('class', $renderParams->has('class') ? $renderParams->getString('class') : null);
-    }
+class CM_FormField_Integer extends CM_FormField_Text {
 
     public function validate(CM_Frontend_Environment $environment, $userInput) {
         if (!is_numeric($userInput)) {
             throw new CM_Exception_FormFieldValidation(new CM_I18n_Phrase('Invalid number'));
         }
-        $value = (int) $userInput;
-        if ($value < $this->_options['min'] || $value > $this->_options['max']) {
-            throw new CM_Exception_FormFieldValidation(new CM_I18n_Phrase('Value not in range.'));
-        }
-        return $value;
-    }
-
-    protected function _initialize() {
-        $this->_options['min'] = $this->_params->getInt('min', 0);
-        $this->_options['max'] = $this->_params->getInt('max', 100);
-        $this->_options['step'] = $this->_params->getInt('step', 1);
-        parent::_initialize();
+   
+        return (int) $userInput;
     }
 }
