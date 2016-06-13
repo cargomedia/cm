309b307ea322a9a00bf49346752caf4b50a3a1bb
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2016-06-13 14:56:11 +0200

    Adjust usages

--- library/CM/Form/ExampleIcon.php
+++ library/CM/Form/ExampleIcon.php
@@ -3,13 +3,13 @@
 class CM_Form_ExampleIcon extends CM_Form_Abstract {
 
     protected function _initialize() {
-        $this->registerField(new CM_FormField_Integer(['name' => 'sizeSlider', 'min' => 6, 'max' => 120]));
+        $this->registerField(new CM_FormField_Distance(['name' => 'sizeSlider', 'min' => 6, 'max' => 120]));
         $this->registerField(new CM_FormField_Color(['name' => 'colorBackground']));
         $this->registerField(new CM_FormField_Color(['name' => 'color']));
         $this->registerField(new CM_FormField_Color(['name' => 'shadowColor']));
-        $this->registerField(new CM_FormField_Integer(['name' => 'shadowX', 'min' => 0, 'max' => 20]));
-        $this->registerField(new CM_FormField_Integer(['name' => 'shadowY', 'min' => 0, 'max' => 20]));
-        $this->registerField(new CM_FormField_Integer(['name' => 'shadowBlur', 'min' => 0, 'max' => 20]));
+        $this->registerField(new CM_FormField_Distance(['name' => 'shadowX', 'min' => 0, 'max' => 20]));
+        $this->registerField(new CM_FormField_Distance(['name' => 'shadowY', 'min' => 0, 'max' => 20]));
+        $this->registerField(new CM_FormField_Distance(['name' => 'shadowBlur', 'min' => 0, 'max' => 20]));
     }
 
     public function prepare(CM_Frontend_Environment $environment, CM_Frontend_ViewResponse $viewResponse) {
