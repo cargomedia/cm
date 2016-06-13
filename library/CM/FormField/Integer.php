ee7a4681a0ddcb1f77fa09e572e7fc8b982eb129
christopheschwyzer <christophe.schwyzer@cargomedia.ch>
2016-06-13 17:06:36 +0200

    Add Min/Max to CM_FormField_Integer

--- library/CM/FormField/Integer.php
+++ library/CM/FormField/Integer.php
@@ -6,7 +6,17 @@ class CM_FormField_Integer extends CM_FormField_Text {
         if (!is_numeric($userInput)) {
             throw new CM_Exception_FormFieldValidation(new CM_I18n_Phrase('Invalid number'));
         }
-   
-        return (int) $userInput;
+        $value = (int) $userInput;
+        if ($value < $this->_options['min'] || $value > $this->_options['max']) {
+            throw new CM_Exception_FormFieldValidation(new CM_I18n_Phrase('Value not in range.'));
+        }
+
+        return $value;
+    }
+
+    protected function _initialize() {
+        $this->_options['min'] = $this->_params->getInt('min', 0);
+        $this->_options['max'] = $this->_params->getInt('max', 100);
+        parent::_initialize();
     }
 }
