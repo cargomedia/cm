1c639f59b1e8d4adf3b60f27fcc03b78071b3700
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-06-14 18:00:15 +0200

    index for cm_log.message

---
+++ resources/db/update/68.php
@@ -0,0 +1,7 @@
+<?php
+
+$mongo = CM_Service_Manager::getInstance()->getMongoDb();
+
+if (!$mongo->hasIndex('cm_log', ['message' => 1])) {
+    $mongo->createIndex('cm_log', ['message' => 1], ['background' => true]);
+}
