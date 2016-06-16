50cd3eefec6b1eb73c9a2107234622f1223d265c
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-06-16 15:35:54 +0200

    insert return statement to migration

--- resources/db/update/68.php
+++ resources/db/update/68.php
@@ -1,5 +1,8 @@
 <?php
 
+echo 'Please run CM script 68 manually' . PHP_EOL;
+return;
+
 $mongo = CM_Service_Manager::getInstance()->getMongoDb();
 
 if (!$mongo->hasIndex('cm_log', ['message' => 1])) {
