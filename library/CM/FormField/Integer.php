aad884a090cba56f1c8f4cfa45308b847d088142
Tomasz Durka <tomasz@durka.pl>
2016-06-16 11:44:05 +0200

    Add CM_FormField_IntegerTest

--- library/CM/FormField/Integer.php
+++ library/CM/FormField/Integer.php
@@ -3,8 +3,9 @@
 class CM_FormField_Integer extends CM_FormField_Float {
     
     public function validate(CM_Frontend_Environment $environment, $userInput) {
-        $value = parent::validate($environment, $userInput);
-        if ((int) $value != $value) {
+        $userInput = parent::validate($environment, $userInput);
+        $value = (int) $userInput;
+        if ($value != $userInput) {
             throw new CM_Exception_FormFieldValidation(new CM_I18n_Phrase('Invalid integer'));
         }
         return $value;
