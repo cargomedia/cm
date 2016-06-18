5386f3ed32a2d651811e5db1f3755b333549399a
Reto Kaiser <reto@cargomedia.ch>
2016-06-18 15:04:07 +0200

    Add UrlParser::getPath

--- tests/library/CM/Http/UrlParserTest.php
+++ tests/library/CM/Http/UrlParserTest.php
@@ -16,4 +16,14 @@ class CM_Http_UrlParserTest extends CMTest_TestCase {
         $urlParser->getHost();
     }
 
+    public function testGetPath() {
+        $urlParser = new CM_Http_UrlParser('http://www.example.com/foo?bar=12');
+        $this->assertSame('/foo', $urlParser->getPath());
+    }
+
+    public function testGetPathEmpty() {
+        $urlParser = new CM_Http_UrlParser('http://www.example.com');
+        $this->assertSame('/', $urlParser->getPath());
+    }
+
 }
