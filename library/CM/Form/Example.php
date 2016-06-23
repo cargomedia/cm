037fc4126c3a6885ab560f0f444c6fdd58b96134
Reto Kaiser <reto@cargomedia.ch>
2016-06-23 16:12:01 +0200

    Add captcha to example form

--- library/CM/Form/Example.php
+++ library/CM/Form/Example.php
@@ -28,6 +28,7 @@ class CM_Form_Example extends CM_Form_Abstract {
         $this->registerField(new CM_FormField_Set_Select(['name' => 'setSelect2', 'values' => [1 => 'Eins', 2 => 'Zwei'], 'labelsInValues' => true]));
         $this->registerField(new CM_FormField_Set_Select(['name' => 'setSelect3', 'values' => [1 => 'Foo', 2 => 'Bar'], 'labelsInValues' => true]));
         $this->registerField(new CM_FormField_TreeSelect(['name' => 'treeselect', 'tree' => CM_Model_LanguageKey::getTree()]));
+        $this->registerField(new CM_FormField_Captcha(['name' => 'captcha']));
 
         $this->registerAction(new CM_FormAction_Example_Submit($this));
     }
