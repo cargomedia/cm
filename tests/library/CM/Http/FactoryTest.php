b5b063ec5bc99d7c087d7196f49d16505fe44ffa
Reto Kaiser <reto@cargomedia.ch>
2016-06-25 17:11:52 +0200

    Refactor response factory test

--- tests/library/CM/Http/FactoryTest.php
+++ tests/library/CM/Http/FactoryTest.php
@@ -11,21 +11,34 @@ class CM_Http_ResponseFactoryTest extends CMTest_TestCase {
         $responses['/captcha'] = 'CM_Http_Response_Captcha';
         $responses['/emailtracking'] = 'CM_Http_Response_EmailTracking';
         $responses['/rpc'] = 'CM_Http_Response_RPC';
-        $responses['/upload'] = 'CM_Http_Response_Upload';
-        $responses['/library-css'] = 'CM_Http_Response_Resource_Css_Library';
-        $responses['/vendor-css'] = 'CM_Http_Response_Resource_Css_Vendor';
-        $responses['/library-js'] = 'CM_Http_Response_Resource_Javascript_Library';
-        $responses['/vendor-js'] = 'CM_Http_Response_Resource_Javascript_Vendor';
-        $responses['/layout'] = 'CM_Http_Response_Resource_Layout';
         $responses['/ajax'] = 'CM_Http_Response_View_Ajax';
         $responses['/form'] = 'CM_Http_Response_View_Form';
         $responses['/homepage'] = 'CM_Http_Response_Page';
 
         $responseClassList = array_values($responses);
+        $site = $this->getMockSite();
         $factory = new CM_Http_ResponseFactory($this->getServiceManager(), $responseClassList);
 
         foreach ($responses as $path => $expectedResponse) {
-            $request = new CM_Http_Request_Post($path . '/null/timestamp', null, null, '[]');
+            $request = new CM_Http_Request_Get($path, ['host' => $site->getHost()]);
+            $this->assertInstanceOf($expectedResponse, $factory->getResponse($request));
+        }
+    }
+
+    public function testGetResponseResouce() {
+        $responses = array();
+        $responses['/library-css'] = 'CM_Http_Response_Resource_Css_Library';
+        $responses['/vendor-css'] = 'CM_Http_Response_Resource_Css_Vendor';
+        $responses['/library-js'] = 'CM_Http_Response_Resource_Javascript_Library';
+        $responses['/vendor-js'] = 'CM_Http_Response_Resource_Javascript_Vendor';
+        $responses['/layout'] = 'CM_Http_Response_Resource_Layout';
+
+        $responseClassList = array_values($responses);
+        $site = $this->getMockSite();
+        $factory = new CM_Http_ResponseFactory($this->getServiceManager(), $responseClassList);
+
+        foreach ($responses as $path => $expectedResponse) {
+            $request = new CM_Http_Request_Get($path . '/' . $site->getId() . '/timestamp', ['host' => $site->getHost()]);
             $this->assertInstanceOf($expectedResponse, $factory->getResponse($request));
         }
     }
