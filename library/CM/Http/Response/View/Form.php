324bec77349093cfca3fda81fd88fb6a4d82df2a
Alexis Peter <alexis.peter@cargomedia.ch>
2016-07-06 16:43:39 +0200

    pop the `form`-pathpart from the url in FormResponse::createFromRequest()

--- library/CM/Http/Response/View/Form.php
+++ library/CM/Http/Response/View/Form.php
@@ -79,6 +79,7 @@ class CM_Http_Response_View_Form extends CM_Http_Response_View_Abstract {
     public static function createFromRequest(CM_Http_Request_Abstract $request, CM_Site_Abstract $site, CM_Service_Manager $serviceManager) {
         if ($request->getPathPart(0) === 'form') {
             $request = clone $request;
+            $request->popPathPart(0);
             $request->popPathLanguage();
             return new self($request, $site, $serviceManager);
         }
