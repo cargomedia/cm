f7ae6d21955364500dcc5b5a907bdb6024c6c496
Michel Meyer <michel.meyer@cargomedia.ch>
2016-07-11 15:56:49 +0200

    Introduce CM_Paging_List client side collection

---
+++ library/CM/Paging/List.js
@@ -0,0 +1,111 @@
+/**
+ * @class CM_Paging_List
+ * @extends Backbone.Collection
+ * @mixes CM_Frontend_SynchronizableTrait
+ */
+var CM_Paging_List = Backbone.Collection.extend({
+
+  _class: 'CM_Paging_List',
+
+  constructor: function() {
+    // TODO: move me out of the constructor when CM will use CommonJS module... ;(
+    CM_Frontend_SynchronizableTrait.applyImplementation(CM_Paging_List.prototype);
+    return Backbone.Collection.prototype.constructor.apply(this, arguments);
+  },
+
+  /**
+   * @param {CM_Paging_List} list
+   * @returns {{removed: Array, added: Object, updated: Object}|null}
+   */
+  sync: function(list) {
+    if (!(list instanceof CM_Paging_List)) {
+      throw Error('Failed to update the collection, incompatible parameter.');
+    }
+    var result = {
+      removed: [],
+      added: [],
+      updated: {}
+    };
+    var resultCleanup = function(result) {
+      _.each(result, function(val, key) {
+        if (_.isEmpty(val)) {
+          delete result[key];
+        }
+      });
+      return _.isEmpty(result) ? null : result;
+    };
+
+    list
+      .chain()
+      .filter(function(item) {
+        return !!this.get(item);
+      }, this)
+      .each(function(itemToMerge) {
+        var localItem = this.get(itemToMerge);
+        if (localItem.isSynchronizable(itemToMerge) && !localItem.equals(itemToMerge)) {
+          result.updated[localItem.id] = localItem.sync(itemToMerge);
+        }
+      }, this);
+
+    list
+      .chain()
+      .filter(function(item) {
+        return !this.get(item);
+      }, this)
+      .each(function(itemToAdd) {
+        this.add(itemToAdd);
+        result.added.push(itemToAdd);
+      }, this);
+
+    this
+      .chain()
+      .filter(function(item) {
+        return !list.get(item);
+      }, this)
+      .each(function(itemToRemove) {
+        this.remove(itemToRemove);
+        result.removed.push(itemToRemove);
+      }, this);
+
+    if (result = resultCleanup(result)) {
+      this.trigger('list:sync', this, result);
+    }
+    return result;
+  },
+
+  equals: function(list) {
+    if (!list || !(list instanceof CM_Paging_List) || this.size() !== list.size()) {
+      return false;
+    }
+
+    return list.any(function(item) {
+      var localItem = this.get(item);
+      if (localItem) {
+        if (this.isSynchronizable(localItem) && this.isSynchronizable(item)) {
+          return localItem.equals(item);
+        } else {
+          return _.isEqual(localItem, item);
+        }
+      }
+      return false;
+    }, this);
+  },
+
+  toJSON: function() {
+    return Backbone.Collection.prototype.toJSON.apply(this, arguments);
+  },
+
+  /**
+   * @returns {String}
+   */
+  getClass: function() {
+    return this._class;
+  },
+
+  destruct: function() {
+  },
+
+  fetch: function() {
+    throw new Error('Not implemented.');
+  }
+});
---
+++ tests/library/CM/Paging/ListTest.js
@@ -0,0 +1,216 @@
+define(["CM/Frontend/JsonSerializable", "CM/Paging/List"], function() {
+
+  QUnit.module('CM/Paging/List:flat', {
+    beforeEach: function() {
+      var list = new CM_Paging_List([
+        new CM_Frontend_JsonSerializable({id: 1, foo: 1}),
+        new CM_Frontend_JsonSerializable({id: 2, foo: 2}),
+        new CM_Frontend_JsonSerializable({id: 3, foo: 3})
+      ]);
+      var clone = new CM_Paging_List([
+        new CM_Frontend_JsonSerializable({id: 1, foo: 1}),
+        new CM_Frontend_JsonSerializable({id: 2, foo: 2}),
+        new CM_Frontend_JsonSerializable({id: 3, foo: 3})
+      ]);
+
+      var synced = [];
+      list.on('list:sync', function(collection, syncAttributes) {
+        synced.push(syncAttributes);
+      });
+
+      this.list = list;
+      this.clone = clone;
+      this.synced = synced;
+    },
+
+    afterEach: function() {
+      this.list.off();
+      this.list = null;
+      this.clone = null;
+      this.synced = null;
+    }
+  });
+
+  QUnit.test("equals", function(assert) {
+    var list = this.list;
+    var clone = this.clone;
+    assert.notOk(list.equals());
+    assert.notOk(list.equals([]));
+    assert.notOk(list.equals([{id: 1, foo: 1}, {id: 2, foo: 2}, {id: 3, foo: 3}]));
+    assert.ok(list.equals(clone));
+  });
+
+  QUnit.test("toJSON", function(assert) {
+    var list = this.list;
+    assert.deepEqual(list.toJSON(), [{id: 1, foo: 1}, {id: 2, foo: 2}, {id: 3, foo: 3}]);
+  });
+
+  QUnit.test("sync: invalid", function(assert) {
+    var list = this.list;
+    assert.throws(function() {
+      list.sync();
+    }, /Failed to update the collection, incompatible parameter/);
+    assert.throws(function() {
+      list.sync([{id: 1, foo: 1}]);
+    }, /Failed to update the collection, incompatible parameter/);
+    assert.throws(function() {
+      list.sync(new CM_Frontend_JsonSerializable());
+    }, /Failed to update the collection, incompatible parameter/);
+  });
+
+  QUnit.test("sync: equals", function(assert) {
+    var list = this.list;
+    var clone = this.clone;
+    var synced = this.synced;
+
+    var result = list.sync(clone);
+    assert.equal(list.size(), 3);
+    assert.equal(result, null);
+    assert.deepEqual(synced, []);
+  });
+
+  QUnit.test("sync: add", function(assert) {
+    var list = this.list;
+    var clone = this.clone;
+    var synced = this.synced;
+
+    clone.add(new CM_Frontend_JsonSerializable({id: 4, foo: 4}));
+    var result = list.sync(clone);
+    assert.equal(list.size(), 4);
+    assert.deepEqual(result.added[0].toJSON(), {id: 4, foo: 4});
+    assert.deepEqual(synced[0].added[0].toJSON(), {id: 4, foo: 4});
+  });
+
+  QUnit.test("sync: change", function(assert) {
+    var list = this.list;
+    var clone = this.clone;
+    var synced = this.synced;
+
+    clone.at(1).set('foo', 9);
+    var result = list.sync(clone);
+    assert.equal(list.size(), 3);
+    assert.deepEqual(result, {updated: {2: {updated: {foo: 9}}}});
+    assert.deepEqual(synced[0], {updated: {2: {updated: {foo: 9}}}});
+  });
+
+  QUnit.test("sync: remove", function(assert) {
+    var list = this.list;
+    var clone = this.clone;
+    var synced = this.synced;
+
+    clone.remove(2);
+    var result = list.sync(clone);
+    assert.equal(list.size(), 2);
+    assert.deepEqual(result.removed[0].toJSON(), {id: 2, foo: 2});
+    assert.deepEqual(synced[0].removed[0].toJSON(), {id: 2, foo: 2});
+  });
+
+
+  QUnit.module('CM/Paging/List:nested', {
+    beforeEach: function() {
+      var list = new CM_Paging_List([
+        new CM_Frontend_JsonSerializable({
+          id: 1,
+          foo: 1,
+          '1.1': new CM_Paging_List([
+            new CM_Frontend_JsonSerializable({id: 'a', bar: 1})
+          ])
+        })
+      ]);
+      var clone = new CM_Paging_List([
+        new CM_Frontend_JsonSerializable({
+          id: 1,
+          foo: 1,
+          '1.1': new CM_Paging_List([
+            new CM_Frontend_JsonSerializable({id: 'a', bar: 1})
+          ])
+        })
+      ]);
+
+      var synced = [];
+      list.on('list:sync', function(collection, syncAttributes) {
+        synced.push(syncAttributes);
+      });
+
+      this.list = list;
+      this.clone = clone;
+      this.synced = synced;
+    },
+
+    afterEach: function() {
+      this.list.off();
+      this.list = null;
+      this.clone = null;
+      this.synced = null;
+    }
+  });
+
+  QUnit.test("equals", function(assert) {
+    var list = this.list;
+    var clone = this.clone;
+
+    assert.ok(list.equals(clone));
+    clone.get(1).get('1.1').get('a').set('bar', 2);
+    assert.notOk(list.equals(clone));
+  });
+
+  QUnit.test("toJSON", function(assert) {
+    var list = this.list;
+    assert.deepEqual(list.toJSON(), [{
+      id: 1,
+      foo: 1,
+      '1.1': [{id: 'a', bar: 1}]
+    }]);
+  });
+
+  QUnit.test("sync: equals", function(assert) {
+    var list = this.list;
+    var clone = this.clone;
+    var synced = this.synced;
+
+    var result = list.sync(clone);
+    assert.equal(list.size(), 1);
+    assert.equal(list.get(1).get('1.1').size(), 1);
+    assert.equal(result, null);
+    assert.deepEqual(synced, []);
+  });
+
+  QUnit.test("sync: add to child", function(assert) {
+    var list = this.list;
+    var clone = this.clone;
+    var synced = this.synced;
+
+    clone.get(1).get('1.1').add(new CM_Frontend_JsonSerializable({id: 'b', bar: 2}));
+    var result = list.sync(clone);
+    assert.equal(list.size(), 1);
+    assert.equal(list.get(1).get('1.1').size(), 2);
+    assert.deepEqual(result.updated[1].updated['1.1'].added[0].toJSON(), {id: 'b', bar: 2});
+    assert.deepEqual(synced[0].updated[1].updated['1.1'].added[0].toJSON(), {id: 'b', bar: 2});
+  });
+
+  QUnit.test("sync: change child", function(assert) {
+    var list = this.list;
+    var clone = this.clone;
+    var synced = this.synced;
+
+    clone.get(1).get('1.1').get('a').set('bar', 9);
+    var result = list.sync(clone);
+    assert.equal(list.size(), 1);
+    assert.equal(list.get(1).get('1.1').size(), 1);
+    assert.deepEqual(result.updated[1].updated['1.1'].updated['a'], {updated: {bar: 9}});
+    assert.deepEqual(synced[0].updated[1].updated['1.1'].updated['a'], {updated: {bar: 9}});
+  });
+
+  QUnit.test("sync: remove", function(assert) {
+    var list = this.list;
+    var clone = this.clone;
+    var synced = this.synced;
+
+    clone.get(1).get('1.1').remove('a');
+    var result = list.sync(clone);
+    assert.equal(list.size(), 1);
+    assert.equal(list.get(1).get('1.1').size(), 0);
+    assert.deepEqual(result.updated[1].updated['1.1'].removed[0].toJSON(), {id: 'a', bar: 1});
+    assert.deepEqual(synced[0].updated[1].updated['1.1'].removed[0].toJSON(), {id: 'a', bar: 1});
+  });
+});
