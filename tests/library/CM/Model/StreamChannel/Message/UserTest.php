9f4fb3165a116d68e3f9846ca6908735949e086f
Michel Meyer <michel.meyer@cargomedia.ch>
2016-07-20 16:48:25 +0200

    Add a test for CM_Model_StreamChannel_Message_User::onSubscribe

--- tests/library/CM/Model/StreamChannel/Message/UserTest.php
+++ tests/library/CM/Model/StreamChannel/Message/UserTest.php
@@ -28,4 +28,33 @@ class CM_Model_StreamChannel_Message_UserTest extends CMTest_TestCase {
         CMTest_TH::reinstantiateModel($user);
         $this->assertSame(false, $user->getOnline());
     }
+
+    public function testOnSubscribe() {
+        $user = CMTest_TH::createUser();
+        /** @var CM_Model_StreamChannel_Message_User $channel */
+        $channel = CM_Model_StreamChannel_Message_User::createStatic([
+            'key'         => CM_Model_StreamChannel_Message_User::getKeyByUser($user),
+            'adapterType' => CM_MessageStream_Adapter_SocketRedis::getTypeStatic(),
+        ]);
+        $subscribe = CMTest_TH::createStreamSubscribe($user, $channel);
+        $delayedQueue = new CM_Jobdistribution_DelayedQueue($this->getServiceManager());
+
+        $subscribe->delete();
+        $channel->delete();
+        $user->setOnline(true);
+
+        CMTest_TH::timeForward(CM_Model_User::OFFLINE_DELAY);
+        $delayedQueue->queueOutstanding();
+        CMTest_TH::reinstantiateModel($user);
+        $this->assertSame(false, $user->getOnline());
+
+        /** @var CM_Model_StreamChannel_Message_User $channel */
+        $channel = CM_Model_StreamChannel_Message_User::createStatic([
+            'key'         => CM_Model_StreamChannel_Message_User::getKeyByUser($user),
+            'adapterType' => CM_MessageStream_Adapter_SocketRedis::getTypeStatic(),
+        ]);
+        $subscribe = CMTest_TH::createStreamSubscribe($user, $channel);
+        CMTest_TH::reinstantiateModel($user);
+        $this->assertSame(true, $user->getOnline());
+    }
 }
