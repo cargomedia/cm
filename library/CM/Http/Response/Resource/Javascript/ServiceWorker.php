9481dc921497a0542219f58c3a1506055c237f8f
Michel Meyer <michel.meyer@cargomedia.ch>
2016-08-03 18:05:17 +0200

    Use cm-bundler for service worker

--- library/CM/Http/Response/Resource/Javascript/ServiceWorker.php
+++ library/CM/Http/Response/Resource/Javascript/ServiceWorker.php
@@ -5,7 +5,7 @@ class CM_Http_Response_Resource_Javascript_ServiceWorker extends CM_Http_Respons
     protected function _process() {
         $debug = $this->getEnvironment()->isDebug();
 
-        $this->_setAsset(new CM_Asset_Javascript_ServiceWorker($this->getSite(), $debug));
+        $this->_setAsset(new CM_Asset_Javascript_Bundle_ServiceWorker($this->getSite(), $debug));
     }
 
     public static function createFromRequest(CM_Http_Request_Abstract $request, CM_Site_Abstract $site, CM_Service_Manager $serviceManager) {
