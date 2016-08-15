a89d67ed204bde3181320a8a517510b7510bbe9b
Reto Kaiser <reto@cargomedia.ch>
2016-08-15 13:41:06 +0200

    Make sure language cache is invalidated when a new javascript version is set

--- library/CM/Paging/Translation/Language/All.php
+++ library/CM/Paging/Translation/Language/All.php
@@ -15,12 +15,15 @@ class CM_Paging_Translation_Language_All extends CM_Paging_Translation_Language_
         $where = null;
         if ($javascriptOnly) {
             $where = 'k.javascript = 1';
+
+            // Include the javascript version in the cache key, so the paging invalidates when the version changes.
+            $javascriptVersion = CM_Model_Language::getVersionJavascript();
+            $where .= " AND '{$javascriptVersion}' = '{$javascriptVersion}'";
         }
         $orderBy = 'k.name ASC';
         $join = 'LEFT JOIN `cm_languageValue` AS v ON k.id = v.languageKeyId AND v.languageId = ' . $this->_language->getId() . ' ';
         $groupBy = 'BINARY k.name';
-        $source = new CM_PagingSource_Sql_Deferred('k.name AS `key`, v.value, k.variables',
-            'cm_model_languagekey` as `k', $where, $orderBy, $join, $groupBy);
+        $source = new CM_PagingSource_Sql_Deferred('k.name AS `key`, v.value, k.variables', 'cm_model_languagekey` as `k', $where, $orderBy, $join, $groupBy);
         $source->enableCache(null, CM_Cache_Persistent::getInstance());
         parent::__construct($source);
     }
