4e4a08869c55a0405d6a1052a7bb700587e4305f
Michel Meyer <michel.meyer@cargomedia.ch>
2016-08-30 16:05:23 +0200

    CM_Mailer_Client extends Swift_Mailer

--- library/CM/Mailer/Client.php
+++ library/CM/Mailer/Client.php
@@ -1,21 +1,4 @@
 <?php
 
-class CM_Mailer_Client extends CM_Class_Abstract {
-
-    /** @var Swift_Transport */
-    private $_transport;
-
-    /**
-     * @param Swift_Transport $transport
-     */
-    public function __construct(Swift_Transport $transport) {
-        $this->_transport = $transport;
-    }
-
-    /**
-     * @return Swift_Transport
-     */
-    public function getTransport() {
-        return $this->_transport;
-    }
+class CM_Mailer_Client extends Swift_Mailer {
 }
