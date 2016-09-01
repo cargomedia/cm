27fc6c732a9e489a5f12816be44116421c8e0b3d
Michel Meyer <michel.meyer@cargomedia.ch>
2016-09-01 19:03:59 +0200

    Reproduce #2317

--- tests/library/CM/Log/Handler/StreamTest.php
+++ tests/library/CM/Log/Handler/StreamTest.php
@@ -30,9 +30,8 @@ class CM_Log_Handler_StreamTest extends CMTest_TestCase {
             $this->assertRegExp('/^\[[0-9T\:\-\+]+ - none - php none - INFO\] foo\n - extra: foo: bar\n - exception:(?:\s+) - message: foo.*$/s', $message);
         });
 
-
         $context = new CM_Log_Context();
-        $context->setExtra(['foo' => 'bar']);
+        $context->setExtra(['foo' => 'bar', 'bar' => ['bar1' => 10]]);
         $context->setException(new Exception('foo'));
         $record = new CM_Log_Record(CM_Log_Logger::INFO, 'foo', $context);
         $formatter = new CM_Log_Formatter_Text();
