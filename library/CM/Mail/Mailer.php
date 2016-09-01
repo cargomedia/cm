e6d8e39f96baa856ec97c2a218ddb61a292c80b9
Michel Meyer <michel.meyer@cargomedia.ch>
2016-09-01 16:30:00 +0200

    Log error/warning when all/some emails fail

--- library/CM/Mail/Mailer.php
+++ library/CM/Mail/Mailer.php
@@ -1,6 +1,8 @@
 <?php
 
-class CM_Mail_Mailer extends Swift_Mailer {
+class CM_Mail_Mailer extends Swift_Mailer implements CM_Service_ManagerAwareInterface {
+
+    use CM_Service_ManagerAwareTrait;
 
     public function __construct(Swift_Transport $transport) {
         CM_Mail_Message::register();
@@ -14,11 +16,23 @@ class CM_Mail_Mailer extends Swift_Mailer {
             throw new CM_Exception_Invalid('No recipient specified');
         }
         $numSent = parent::send($message, $failedRecipients);
-        if (0 === $numSent) {
-            throw new CM_Exception_Invalid('Failed to send email', null, [
-                'message'          => $message,
+        if ($failedRecipients) {
+            $context = new CM_Log_Context();
+            $context->setExtra([
+                'message'          => [
+                    'subject' => $message->getSubject(),
+                    'from'    => $message->getSender(),
+                    'to'      => $message->getTo(),
+                    'cc'      => $message->getCc(),
+                    'bcc'     => $message->getBcc(),
+                ],
                 'failedRecipients' => $failedRecipients,
             ]);
+            if (0 === $numSent) {
+                $this->getServiceManager()->getLogger()->error('Failed to send email to all recipients', $context);
+            } else {
+                $this->getServiceManager()->getLogger()->warning('Failed to send email to some recipients', $context);
+            }
         }
         return $numSent;
     }
