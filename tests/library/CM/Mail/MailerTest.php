14ed208f498d0c58fb1e22b127cec5f08b6d53b7
Michel Meyer <michel.meyer@cargomedia.ch>
2016-09-01 16:33:11 +0200

    Remove CM_Mail_MailerTest::testSendFailed
    Transport::send() can't be properly mocked, $failedRecipients is passed by reference

--- tests/library/CM/Mail/MailerTest.php
+++ tests/library/CM/Mail/MailerTest.php
@@ -37,23 +37,4 @@ class CM_Mail_MailerTest extends CMTest_TestCase {
         $this->assertSame('No recipient specified', $exception->getMessage());
         $this->assertSame(0, $sendMethod->getCallCount());
     }
-
-    public function testSendFailed() {
-        $transport = $this->mockInterface('Swift_Transport')->newInstance();
-        $transport->mockMethod('isStarted')->set(true);
-        $message = new Swift_Message('foo', 'content');
-        $message->setTo('foo@example.com');
-        $message->setCc('bar@example.com', 'bar');
-        $client = new CM_Mail_Mailer($transport);
-
-        $sendMethod = $transport->mockMethod('send')->set(0);
-        $exception = $this->catchException(function () use ($client, $message) {
-            $client->send($message);
-        });
-        $this->assertInstanceOf('CM_Exception_Invalid', $exception);
-        /** @var CM_Exception_Invalid $exception */
-        $this->assertSame('Failed to send email', $exception->getMessage());
-        $this->assertSame(['message', 'failedRecipients'], array_keys($exception->getMetaInfo()));
-        $this->assertEquals($message, $exception->getMetaInfo()['message']);
-    }
 }
