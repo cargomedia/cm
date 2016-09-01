c31a0c2a7384fbd5a8c803527107180f95133d77
Michel Meyer <michel.meyer@cargomedia.ch>
2016-09-01 14:53:26 +0200

    Move CM_Mail_WelcomeTest::testRenderTranslated into CM_Mail_MailableTest

--- tests/library/CM/Mail/MailableTest.php
+++ tests/library/CM/Mail/MailableTest.php
@@ -42,6 +42,27 @@ class CM_Mail_MailableTest extends CMTest_TestCase {
         $this->assertEquals('Hello (http://www.foo.bar)', $text);
     }
 
+    public function testRenderTranslated() {
+        $site = $this->getMockSite(null, null, [
+            'url' => 'http://www.foo.com',
+        ]);
+        $recipient = $this->getMockUser(null, $site);
+        $mail = new CM_Mail_ExampleMailable($recipient);
+        $language = CM_Model_Language::create('Test language', 'foo', true);
+        $language->setTranslation('Welcome to {$siteName}!', 'foo');
+
+        list($subject, $html, $text) = $mail->render();
+        $nodeList = new CM_Dom_NodeList($html);
+
+        $this->assertContains('foo', $nodeList->getText());
+
+        $nodeLink = $nodeList->find('a');
+        $this->assertSame(1, $nodeLink->count());
+        $this->assertSame('http://www.foo.com/example', $nodeLink->getAttribute('href'));
+        $this->assertSame('Example Page', $nodeLink->getText());
+        $this->assertContains('border-style:solid;', $nodeLink->getAttribute('style'));
+    }
+
     public function testSend() {
         $transport = $this->mockInterface('Swift_Transport')->newInstance();
         $sendMethod = $transport->mockMethod('send')->set(1);
