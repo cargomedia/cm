630f9e836b85db14407b04fde8bab52cfef2d9fb
Michel Meyer <michel.meyer@cargomedia.ch>
2016-09-02 13:44:43 +0200

    Add testSendWarning and testSendFailed

--- tests/library/CM/Mail/MailerTest.php
+++ tests/library/CM/Mail/MailerTest.php
@@ -17,7 +17,6 @@ class CM_Mail_MailerTest extends CMTest_TestCase {
         $this->assertSame(1, $sendMethod->getCallCount());
         $this->assertSame(2, $numSent);
         $this->assertSame([], $failedRecipients);
-
     }
 
     public function testSendNoRecipient() {
@@ -36,5 +35,83 @@ class CM_Mail_MailerTest extends CMTest_TestCase {
         /** @var CM_Exception_Invalid $exception */
         $this->assertSame('No recipient specified', $exception->getMessage());
         $this->assertSame(0, $sendMethod->getCallCount());
+    }
+
+    public function testSendWarning() {
+        $serviceManager = new CM_Service_Manager();
+        $logger = $this->mockObject('CM_Log_Logger');
+        $serviceManager->registerInstance('logger', $logger);
+        $transport = $this->mockInterface('Swift_Transport')->newInstance();
+        $transport->mockMethod('isStarted')->set(true);
+        $message = new Swift_Message('foo', 'content');
+        $message->setSender('foobar@example.com', 'Foobar');
+        $message->setTo('foo@example.com');
+        $message->setCc('bar@example.com', 'bar');
+
+        $client = new CM_Mail_Mailer($transport);
+        $client->setServiceManager($serviceManager);
+
+        $sendMethod = $transport->mockMethod('send')->set(1);
+        $errorMethod = $logger->mockMethod('error');
+        $warningMethod = $logger->mockMethod('warning')->set(function ($message, CM_Log_Context $context = null) {
+            $this->assertSame('Failed to send email to some recipients', $message);
+            $this->assertSame([
+                'message'          => [
+                    'subject' => 'foo',
+                    'from'    => ['foobar@example.com' => 'Foobar'],
+                    'to'      => ['foo@example.com' => null],
+                    'cc'      => ['bar@example.com' => 'bar'],
+                    'bcc'     => null,
+                ],
+                'failedRecipients' => ['foo@example.com'],
+            ], $context->getExtra());
+        });
+
+        $failedRecipients = ['foo@example.com'];
+        $client->send($message, $failedRecipients);
+
+        /** @var CM_Exception_Invalid $exception */
+        $this->assertSame(1, $sendMethod->getCallCount());
+        $this->assertSame(1, $warningMethod->getCallCount());
+        $this->assertSame(0, $errorMethod->getCallCount());
+    }
+
+    public function testSendFailed() {
+        $serviceManager = new CM_Service_Manager();
+        $logger = $this->mockObject('CM_Log_Logger');
+        $serviceManager->registerInstance('logger', $logger);
+        $transport = $this->mockInterface('Swift_Transport')->newInstance();
+        $transport->mockMethod('isStarted')->set(true);
+        $message = new Swift_Message('foo', 'content');
+        $message->setSender('foobar@example.com', 'Foobar');
+        $message->setTo('foo@example.com');
+        $message->setCc('bar@example.com', 'bar');
+
+        $client = new CM_Mail_Mailer($transport);
+        $client->setServiceManager($serviceManager);
+
+        $sendMethod = $transport->mockMethod('send')->set(0);
+        $warningMethod = $logger->mockMethod('warning');
+        $errorMethod = $logger->mockMethod('error')->set(function ($message, CM_Log_Context $context = null) {
+            $this->assertSame('Failed to send email to all recipients', $message);
+            $this->assertSame([
+                'message'          => [
+                    'subject' => 'foo',
+                    'from'    => ['foobar@example.com' => 'Foobar'],
+                    'to'      => ['foo@example.com' => null],
+                    'cc'      => ['bar@example.com' => 'bar'],
+                    'bcc'     => null,
+                ],
+                'failedRecipients' => ['foo@example.com', 'bar@example.com'],
+            ], $context->getExtra());
+        });
+
+        $failedRecipients = ['foo@example.com', 'bar@example.com'];
+        $client->send($message, $failedRecipients);
+
+        /** @var CM_Exception_Invalid $exception */
+        $this->assertSame(1, $sendMethod->getCallCount());
+        $this->assertSame(0, $warningMethod->getCallCount());
+        $this->assertSame(1, $errorMethod->getCallCount());
     }
 }
