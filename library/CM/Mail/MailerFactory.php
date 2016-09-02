d7485d5f05effc28d1660360a9cd0e5ad40fbf3d
Michel Meyer <michel.meyer@cargomedia.ch>
2016-09-02 18:55:22 +0200

    Support username/password with SMTP transport

--- library/CM/Mail/MailerFactory.php
+++ library/CM/Mail/MailerFactory.php
@@ -16,14 +16,22 @@ class CM_Mail_MailerFactory implements CM_Service_ManagerAwareInterface {
     /**
      * @param string|null $host
      * @param int|null    $port
+     * @param string|null $username
+     * @param string|null $password
      * @param string|null $security
      * @return CM_Mail_Mailer
      */
-    public function createSmtpMailer($host = null, $port = null, $security = null) {
+    public function createSmtpMailer($host = null, $port = null, $username = null, $password = null, $security = null) {
         $host = null !== $host ? (string) $host : 'localhost';
         $port = null !== $port ? (int) $port : 25;
         $security = null !== $security ? (string) $security : null;
         $transport = new Swift_SmtpTransport($host, $port, $security);
+        if (null !== $username) {
+            $transport->setUsername((string) $username);
+        }
+        if (null !== $password) {
+            $transport->setPassword((string) $password);
+        }
         return new CM_Mail_Mailer($transport);
     }
 }
