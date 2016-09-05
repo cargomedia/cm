561cb06853ad9f90ad3a36d10259758012431366
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2016-09-05 11:55:31 +0200

    Made sure the location aggregation doesn't get empty during updates

--- library/CM/Model/Location.php
+++ library/CM/Model/Location.php
@@ -335,8 +335,9 @@ class CM_Model_Location extends CM_Model_Abstract {
     }
 
     public static function createAggregation() {
-        CM_Db_Db::truncate('cm_tmp_location');
-        CM_Db_Db::exec('INSERT INTO `cm_tmp_location` (`level`,`id`,`1Id`,`2Id`,`3Id`,`4Id`,`name`, `abbreviation`, `nameFull`, `lat`,`lon`)
+        CM_Db_Db::exec('DROP TABLE IF EXISTS `cm_tmp_location_new`');
+        CM_Db_Db::exec('CREATE TABLE `cm_tmp_location_new` LIKE `cm_tmp_location`');
+        CM_Db_Db::exec('INSERT INTO `cm_tmp_location_new` (`level`,`id`,`1Id`,`2Id`,`3Id`,`4Id`,`name`, `abbreviation`, `nameFull`, `lat`,`lon`)
 			SELECT 1, `1`.`id`, `1`.`id`, NULL, NULL, NULL,
 					`1`.`name`, `1`.`abbreviation`, CONCAT_WS(" ", `1`.`name`, `1`.`abbreviation`), NULL, NULL
 			FROM `cm_model_location_country` AS `1`
@@ -359,8 +360,9 @@ class CM_Model_Location extends CM_Model_Abstract {
 			LEFT JOIN `cm_model_location_state` AS `2` ON(`3`.`stateId`=`2`.`id`)
 			LEFT JOIN `cm_model_location_country` AS `1` ON(`3`.`countryId`=`1`.`id`)');
 
-        CM_Db_Db::truncate('cm_tmp_location_coordinates');
-        CM_Db_Db::exec('INSERT INTO `cm_tmp_location_coordinates` (`level`,`id`,`coordinates`)
+        CM_Db_Db::exec('DROP TABLE IF EXISTS `cm_tmp_location_coordinates_new`');
+        CM_Db_Db::exec('CREATE TABLE `cm_tmp_location_coordinates_new` LIKE `cm_tmp_location_coordinates`');
+        CM_Db_Db::exec('INSERT INTO `cm_tmp_location_coordinates_new` (`level`,`id`,`coordinates`)
 			SELECT 3, `id`, POINT(lat, lon)
 			FROM `cm_model_location_city`
 			WHERE `lat` IS NOT NULL AND `lon` IS NOT NULL
@@ -368,6 +370,11 @@ class CM_Model_Location extends CM_Model_Abstract {
 			SELECT 4, `id`, POINT(lat, lon)
 			FROM `cm_model_location_zip`
 			WHERE `lat` IS NOT NULL AND `lon` IS NOT NULL');
+
+        CM_Db_Db::exec('RENAME TABLE `cm_tmp_location` TO `cm_tmp_location_old`, `cm_tmp_location_new` TO `cm_tmp_location`');
+        CM_Db_Db::exec('DROP TABLE `cm_tmp_location_old`');
+        CM_Db_Db::exec('RENAME TABLE `cm_tmp_location_coordinates` TO `cm_tmp_location_coordinates_old`, `cm_tmp_location_coordinates_new` TO `cm_tmp_location_coordinates`');
+        CM_Db_Db::exec('DROP TABLE `cm_tmp_location_coordinates_old`');
     }
 
     /**
