beaa4506d58fcadeae94dfdfeb1367e76c99202f
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-09-06 15:29:34 +0200

    check for key existence

--- library/CM/Paging/ModelAbstract.php
+++ library/CM/Paging/ModelAbstract.php
@@ -38,7 +38,7 @@ abstract class CM_Paging_ModelAbstract extends CM_Paging_Abstract {
             $this->_populateModelList($this->_getItemsRaw());
         }
         $index = serialize($itemRaw);
-        if (null === ($model = $this->_modelList[$index])) {
+        if (!array_key_exists($index, $this->_modelList) || null === ($model = $this->_modelList[$index])) {
             throw new CM_Exception_Nonexistent('Model itemRaw has no data', null, ['itemRaw' => CM_Util::var_line($itemRaw)]);
         }
         return $model;
