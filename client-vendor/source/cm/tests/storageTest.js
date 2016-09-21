7977c312d690d3b8f2b811aa769e49f592962235
Michel Meyer <michel.meyer@cargomedia.ch>
2016-09-21 12:35:09 +0200

    Test multiple storage adapters

--- client-vendor/source/cm/tests/storageTest.js
+++ client-vendor/source/cm/tests/storageTest.js
@@ -2,76 +2,83 @@ define(["cm/storage", "cm/adapter/memory"], function(PersistentStorage, AdapterM
 
   QUnit.module('cm/storage');
 
-  QUnit.test("Storage: sessionStorage adapter", function(assert) {
-    var adapter = window.sessionStorage;
-    var data = new PersistentStorage('foo', adapter);
+  QUnit.test("Storage: get/set/del", function(assert) {
+    var adapters = [
+      window.sessionStorage,
+      window.localStorage,
+      new AdapterMemory()
+    ];
 
-    data.set({
-      foo: 100
+    adapters.forEach(function(adapter) {
+      var data = new PersistentStorage('foo', adapter);
+
+      data.set({
+        foo: 100
+      });
+      assert.strictEqual(data.has('foo'), true);
+      assert.equal(data.get('foo'), 100);
+      assert.deepEqual(data.get(), {foo: 100});
+      assert.equal(adapter.getItem('foo'), '{\"foo\":100}');
+
+      data.set('bar', '10');
+      assert.deepEqual(data.get(), {foo: 100, bar: "10"});
+      assert.equal(adapter.getItem('foo'), '{\"foo\":100,\"bar\":"10"}');
+
+      data.set({
+        foo: 100,
+        bar: '100'
+      });
+      assert.deepEqual(data.get(), {foo: 100, bar: "100"});
+      assert.equal(adapter.getItem('foo'), '{\"foo\":100,\"bar\":"100"}');
+
+      data.remove('foobar');
+      assert.strictEqual(data.has('foobar'), false);
+      assert.strictEqual(data.get('foobar'), undefined);
+      assert.equal(adapter.getItem('foo'), '{\"foo\":100,\"bar\":"100"}');
+
+      data.clear();
+      assert.equal(adapter.getItem('foo'), '{}');
+
+      data.delete();
+      assert.equal(adapter.getItem('foo'), null);
+
+      var logger = {
+        callCount: 0,
+        warn: function(message, key, error) {
+          this.callCount++;
+          assert.equal('Failed to parse the `%s` PersistentStorage', message);
+          assert.equal('bar', key);
+          assert.equal("SyntaxError", error.name);
+        }
+      };
+
+      adapter.setItem('bar', '{\"foo\":100}');
+      data = new PersistentStorage('bar', adapter, logger);
+      assert.strictEqual(data.has('foo'), true);
+      assert.equal(data.get('foo'), 100);
+      assert.deepEqual(data.get(), {foo: 100});
+
+      adapter.setItem('bar', '{\"foo\":200}');
+      assert.equal(data.get('foo'), 200);
+      assert.deepEqual(data.get(), {foo: 200});
+      assert.equal(logger.callCount, 0);
+
+      adapter.setItem('bar', 'invalid json string');
+      assert.strictEqual(data.get('foo'), undefined);
+      assert.equal(logger.callCount, 1);
+
+      data.set('foo', 300);
+      assert.equal(data.get('foo'), 300);
+      assert.equal(logger.callCount, 2);
+
+      data.set('foo', 400);
+      assert.equal(data.get('foo'), 400);
+      assert.equal(logger.callCount, 2);
+
+      adapter.clear();
+      assert.equal(adapter.getItem('foo'), null);
+      assert.equal(adapter.getItem('bar'), null);
     });
-    assert.strictEqual(data.has('foo'), true);
-    assert.equal(data.get('foo'), 100);
-    assert.deepEqual(data.get(), {foo: 100});
-    assert.equal(adapter.getItem('foo'), '{\"foo\":100}');
-
-    data.set('bar', '10');
-    assert.deepEqual(data.get(), {foo: 100, bar: "10"});
-    assert.equal(adapter.getItem('foo'), '{\"foo\":100,\"bar\":"10"}');
-
-    data.set({
-      foo: 100,
-      bar: '100'
-    });
-    assert.deepEqual(data.get(), {foo: 100, bar: "100"});
-    assert.equal(adapter.getItem('foo'), '{\"foo\":100,\"bar\":"100"}');
-
-    data.remove('foobar');
-    assert.strictEqual(data.has('foobar'), false);
-    assert.strictEqual(data.get('foobar'), undefined);
-    assert.equal(adapter.getItem('foo'), '{\"foo\":100,\"bar\":"100"}');
-
-    data.clear();
-    assert.equal(adapter.getItem('foo'), '{}');
-
-    data.delete();
-    assert.equal(adapter.getItem('foo'), null);
-
-    var logger = {
-      callCount: 0,
-      warn: function(message, key, error) {
-        this.callCount++;
-        assert.equal('Failed to parse the `%s` PersistentStorage', message);
-        assert.equal('bar', key);
-        assert.equal("SyntaxError", error.name);
-      }
-    };
-
-    adapter.setItem('bar', '{\"foo\":100}');
-    data = new PersistentStorage('bar', adapter, logger);
-    assert.strictEqual(data.has('foo'), true);
-    assert.equal(data.get('foo'), 100);
-    assert.deepEqual(data.get(), {foo: 100});
-
-    adapter.setItem('bar', '{\"foo\":200}');
-    assert.equal(data.get('foo'), 200);
-    assert.deepEqual(data.get(), {foo: 200});
-    assert.equal(logger.callCount, 0);
-
-    adapter.setItem('bar', 'invalid json string');
-    assert.strictEqual(data.get('foo'), undefined);
-    assert.equal(logger.callCount, 1);
-
-    data.set('foo', 300);
-    assert.equal(data.get('foo'), 300);
-    assert.equal(logger.callCount, 2);
-
-    data.set('foo', 400);
-    assert.equal(data.get('foo'), 400);
-    assert.equal(logger.callCount, 2);
-
-    adapter.clear();
-    assert.equal(adapter.getItem('foo'), null);
-    assert.equal(adapter.getItem('bar'), null);
   });
 
   QUnit.test("Storage: not supported adapter", function(assert) {
