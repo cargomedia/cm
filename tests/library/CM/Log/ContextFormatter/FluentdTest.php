6d8005c44cee969319dd0da6eeb8908a08c1837c
Michel Meyer <michel.meyer@cargomedia.ch>
2016-09-27 18:51:36 +0200

    Introduce CM_Log_ContextFormatter_MongoDb

--- tests/library/CM/Log/ContextFormatter/FluentdTest.php
+++ tests/library/CM/Log/ContextFormatter/FluentdTest.php
@@ -2,6 +2,59 @@
 
 class CM_Log_ContextFormatter_FluentdTest extends CMTest_TestCase {
 
+    public function testFormatContext() {
+        $user = CMTest_TH::createUser();
+        $httpRequest = CM_Http_Request_Abstract::factory('post', '/foo');
+        $computerInfo = new CM_Log_Context_ComputerInfo('www.example.com', 'v7.0.1');
+        $context = new CM_Log_Context();
+        $context->setExtra([
+            'bar' => 1,
+            'a'   => new DateTime('2000-01-01'),
+            'b'   => (object) ['foo' => 1],
+            'c'   => new CM_Model_ContextFormatter_Fluentd_Mock(123, ['foo' => 'bar1']),
+            'd'   => new CM_Frontend_JsonSerializable(['foo' => 'bar2']),
+            'foo' => [
+                'a' => new DateTime('2000-01-02'),
+                'b' => (object) ['foo' => 2],
+                'c' => new CM_Model_ContextFormatter_Fluentd_Mock(456, ['foo' => 'bar3']),
+                'd' => new CM_Frontend_JsonSerializable(['foo' => 'bar4']),
+            ]
+        ]);
+        $context->setUser($user);
+        $context->setComputerInfo($computerInfo);
+        $context->setHttpRequest($httpRequest);
+
+        $contextFormatter = new CM_Log_ContextFormatter_Fluentd('appName');
+        $formattedContext = $contextFormatter->formatContext($context);
+
+        $this->assertSame([
+            'computerInfo' => [
+                'fqdn'       => 'www.example.com',
+                'phpVersion' => 'v7.0.1',
+            ],
+            'httpRequest'  => [
+                'uri'    => '/foo',
+                'method' => 'POST',
+                'query'  => [],
+            ],
+            'appName'      => [
+                'bar'      => 1,
+                'a'        => '2000-01-01T00:00:00+00:00',
+                'b'        => '[stdClass]',
+                'c'        => '[CM_Model_ContextFormatter_Fluentd_Mock:123]',
+                'd'        => '[CM_Frontend_JsonSerializable]',
+                'foo'      => [
+                    'a' => '2000-01-02T00:00:00+00:00',
+                    'b' => '[stdClass]',
+                    'c' => '[CM_Model_ContextFormatter_Fluentd_Mock:456]',
+                    'd' => '[CM_Frontend_JsonSerializable]',
+                ],
+                'user'     => $user->getId(),
+                'clientId' => $httpRequest->getClientId(),
+            ]
+        ], $formattedContext);
+    }
+
     public function testFormatAppContext() {
         $user = CMTest_TH::createUser();
         $httpRequest = CM_Http_Request_Abstract::factory('post', '/foo');
@@ -11,12 +64,12 @@ class CM_Log_ContextFormatter_FluentdTest extends CMTest_TestCase {
             'bar' => 1,
             'a'   => new DateTime('2000-01-01'),
             'b'   => (object) ['foo' => 1],
-            'c'   => new CM_ModelMock(123, ['foo' => 'bar1']),
+            'c'   => new CM_Model_ContextFormatter_Fluentd_Mock(123, ['foo' => 'bar1']),
             'd'   => new CM_Frontend_JsonSerializable(['foo' => 'bar2']),
             'foo' => [
                 'a' => new DateTime('2000-01-02'),
                 'b' => (object) ['foo' => 2],
-                'c' => new CM_ModelMock(456, ['foo' => 'bar3']),
+                'c' => new CM_Model_ContextFormatter_Fluentd_Mock(456, ['foo' => 'bar3']),
                 'd' => new CM_Frontend_JsonSerializable(['foo' => 'bar4']),
             ]
         ]);
@@ -32,12 +85,12 @@ class CM_Log_ContextFormatter_FluentdTest extends CMTest_TestCase {
                 'bar'      => 1,
                 'a'        => '2000-01-01T00:00:00+00:00',
                 'b'        => '[stdClass]',
-                'c'        => '[CM_ModelMock:123]',
+                'c'        => '[CM_Model_ContextFormatter_Fluentd_Mock:123]',
                 'd'        => '[CM_Frontend_JsonSerializable]',
                 'foo'      => [
                     'a' => '2000-01-02T00:00:00+00:00',
                     'b' => '[stdClass]',
-                    'c' => '[CM_ModelMock:456]',
+                    'c' => '[CM_Model_ContextFormatter_Fluentd_Mock:456]',
                     'd' => '[CM_Frontend_JsonSerializable]',
                 ],
                 'user'     => $user->getId(),
@@ -47,7 +100,7 @@ class CM_Log_ContextFormatter_FluentdTest extends CMTest_TestCase {
     }
 }
 
-class CM_ModelMock extends CM_Model_Abstract {
+class CM_Model_ContextFormatter_Fluentd_Mock extends CM_Model_Abstract {
 
     protected function _getData() {
         return [];
