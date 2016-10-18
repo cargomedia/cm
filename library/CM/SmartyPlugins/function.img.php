c2379fa962ef8b730d335b42ffe52b4609e2e50c
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2016-10-18 12:31:15 +0200

    Added support for absolute image paths and background previews

--- library/CM/SmartyPlugins/function.img.php
+++ library/CM/SmartyPlugins/function.img.php
@@ -5,15 +5,25 @@ function smarty_function_img(array $params, Smarty_Internal_Template $template) 
     $render = $template->smarty->getTemplateVars('render');
 
     $path = $params['path'];
-    $params = array_merge(array('width' => null, 'height' => null, 'title' => null, 'class' => null, 'site' => null), $params);
+    $params = array_merge(['width' => null, 'height' => null, 'title' => null, 'class' => null, 'site' => null], $params);
 
     if (!empty($params['static'])) {
         $url = $render->getUrlStatic('/img/' . $path, $params['site']);
-    } else {
+    } elseif (!preg_match('#(^/|://)#', $path)) {
         $url = $render->getUrlResource('layout', 'img/' . $path, null, $params['site']);
+    } else {
+        $url = $path;
     }
 
     $html = '<img src="' . $url . '"';
+    if (isset($params['background-image'])) {
+        $html .= ' style="background-image: url(' . CM_Util::htmlspecialchars($params['background-image']) . ')"';
+        $params['class'] = (string) $params['class'];
+        if ('' !== $params['class']) {
+            $params['class'] .= ' ';
+        }
+        $params['class'] .= 'background-cover';
+    }
     if ($params['class']) {
         $html .= ' class="' . $params['class'] . '"';
     }
