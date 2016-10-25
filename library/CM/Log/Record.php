b3c6fbc712c55b7b16c8480913021fc1d90545cf
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-10-25 10:29:51 +0200

    use structured data2

--- library/CM/Log/Record.php
+++ library/CM/Log/Record.php
@@ -30,7 +30,7 @@ class CM_Log_Record {
         $this->_level = $level;
         $this->_message = $message;
         $this->_context = $context;
-        $timeStruct = gettimeofday(true);
+        $timeStruct = gettimeofday();
         $this->_createdAt = DateTime::createFromFormat('U.u', $timeStruct['sec'] . '.' . $timeStruct['usec']);
     }
 
