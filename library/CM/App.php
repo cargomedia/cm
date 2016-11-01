89129747e9a6282772b3e751390fb6135d5aeb59
Michel Meyer <michel.meyer@cargomedia.ch>
2016-11-01 13:37:05 +0100

    Adjust app fill-caches cmd

--- library/CM/App.php
+++ library/CM/App.php
@@ -43,7 +43,6 @@ class CM_App implements CM_Service_ManagerAwareInterface {
 
         foreach ($siteList as $site) {
             $render = new CM_Frontend_Render(new CM_Frontend_Environment($site));
-            $assetList[] = new CM_Asset_Javascript_Internal($site, $debug);
             $assetList[] = new CM_Asset_Javascript_Bundle_Library($site, $debug);
             $assetList[] = new CM_Asset_Javascript_Bundle_Library($site, $debug, true);
             $assetList[] = new CM_Asset_Javascript_Bundle_Vendor_BeforeBody($site, $debug);
@@ -54,7 +53,8 @@ class CM_App implements CM_Service_ManagerAwareInterface {
             $assetList[] = new CM_Asset_Css_Library($render, $debug);
             /** @var CM_Model_Language $language */
             foreach ($languageList as $language) {
-                $assetList[] = new CM_Asset_Javascript_Bundle_Translations($site, $debug, $language);
+                $assetList[] = new CM_Asset_Javascript_Bundle_Translations($language, $site, $debug);
+                $assetList[] = new CM_Asset_Javascript_Bundle_Translations($language, $site, $debug, true);
             }
         }
 
