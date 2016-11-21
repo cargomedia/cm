c0b581f63ff343bbe5b48a679afa7f9b6409891b
Michel Meyer <michel.meyer@cargomedia.ch>
2016-11-21 10:58:32 +0100

    Use CM_ExceptionHandling_Formatter_Plain to format MetaInfo

--- tests/helpers/CMTest/library/CMTest/ExceptionWrapper.php
+++ tests/helpers/CMTest/library/CMTest/ExceptionWrapper.php
@@ -2,19 +2,10 @@
 
 class CMTest_ExceptionWrapper extends PHPUnit_Framework_ExceptionWrapper {
 
-    public function __construct($e) {
+    public function __construct(CM_Exception $e) {
         parent::__construct($e);
-
-        if ($e instanceof CM_Exception) {
-            $message = $e->getMessage();
-            $variableInspector = new CM_Debug_VariableInspector();
-            $metaInfo = Functional\map($e->getMetaInfo(), function ($value) use ($variableInspector) {
-                return $variableInspector->getDebugInfo($value);
-            });
-            foreach ($metaInfo as $key => $value) {
-                $message .= sprintf("\n - %s: %s", $key, $value);
-            }
-            $this->message = $message;
-        }
+        $formatter = new CM_ExceptionHandling_Formatter_Plain();
+        $serializedException = new CM_ExceptionHandling_SerializableException($e);
+        $this->message = get_class($e) . ': ' . $e->getMessage() . PHP_EOL . $formatter->getMetaInfo($serializedException);
     }
 }
