b2ade0bfaac05c5274b9d6b7f945db7820c26513
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-11-25 12:01:33 +0100

    fix migration2

--- resources/db/update/74.php
+++ resources/db/update/74.php
@@ -1,10 +1,3 @@
 <?php
 
-return;
-
-$tableInfo = CM_Db_Db::exec('SHOW TABLE STATUS LIKE  \'cm_user_online\' ')->fetch();
-
-if ($tableInfo['Engine'] !== 'InnoDB') {
-    CM_Db_Db::exec('ALTER TABLE `cm_user_online` ENGINE=InnoDB;');
-}
-
+CM_Model_LanguageKey::deleteByName('Select Photos');
