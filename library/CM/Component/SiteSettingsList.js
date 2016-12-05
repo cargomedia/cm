3524f3f657cd1c79a57ff5a2eab30b111b7f731e
Vladimir Finko <vladimir.finko@cargomedia.ch>
2016-12-05 16:19:07 +0100

    draft for settings list component

---
+++ library/CM/Component/SiteSettingsList.js
@@ -0,0 +1,9 @@
+/**
+ * @class CM_Component_SiteSettingsList
+ * @extends CM_Component_Abstract
+ */
+var CM_Component_SiteSettingsList = CM_Component_Abstract.extend({
+
+  /** @type String */
+  _class: 'CM_Component_SiteSettingsList'
+});
