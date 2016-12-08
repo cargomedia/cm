405bab2546b71f70c13595fbf9745acbe2a27a2b
vogdb <vozhdb@gmail.com>
2016-12-08 10:26:22 +0300

    Remove obsolete logic from CM_Form_Abstract

--- library/CM/Form/Abstract.js
+++ library/CM/Form/Abstract.js
@@ -14,9 +14,6 @@ var CM_Form_Abstract = CM_View_Abstract.extend({
   /** @type Object **/
   _actions: {},
 
-  /** @type Boolean **/
-  _stopErrorPropagation: false,
-
   initialize: function() {
     CM_View_Abstract.prototype.initialize.call(this);
 
@@ -59,8 +56,8 @@ var CM_Form_Abstract = CM_View_Abstract.extend({
             .then(function() {
               field.success();
             })
-            .catch(function(error) {
-              handler._handleSubmitError(error, handler.autosave, true);
+            .catch(CM_Exception_FormFieldValidation, function(error) {
+              this._displayValidationError(error);
             });
         }
       });
@@ -176,46 +173,36 @@ var CM_Form_Abstract = CM_View_Abstract.extend({
 
   /**
    * @param {String} [actionName]
-   * @param {Object} [options]
    * @return Promise
    */
-  submit: function(actionName, options) {
-    options = _.defaults(options || {}, {
-      handleErrors: true,
-      disableUI: true
-    });
-
+  submit: function(actionName) {
     return this
       .try(function() {
-        return this._submitOnly(actionName, options.disableUI);
+        return this._submitOnly(actionName);
       })
-      .catch(function(error) {
-        this._handleSubmitError(error, actionName, options.handleErrors);
+      .catch(CM_Exception_FormFieldValidation, function(error) {
+        this._displayValidationError(error);
       });
   },
 
   /**
    * @param {String} [actionName]
-   * @param {Boolean} [disableUI]
    * @return Promise
    */
-  _submitOnly: function(actionName, disableUI) {
+  _submitOnly: function(actionName) {
     var action = this._getAction(actionName);
     var data = this.getActionData(action.name);
     var errorListRequired = this._getErrorListRequired(action.name, data);
 
     this.resetErrors();
     if (_.size(errorListRequired)) {
-      var error = new CM_Exception_FormFieldRequired();
+      var error = new CM_Exception_FormFieldValidation();
       error.setErrorList(errorListRequired);
       return Promise.reject(error);
     }
 
     return this
       .try(function() {
-        if (disableUI) {
-          this.disable();
-        }
         this.trigger('submit', [data]);
         return cm.ajax('form', {viewInfoList: this.getViewInfoList(), actionName: action.name, data: data})
       })
@@ -223,6 +210,7 @@ var CM_Form_Abstract = CM_View_Abstract.extend({
         if (response.errors) {
           var error = new CM_Exception_FormFieldValidation();
           error.setErrorList(response.errors);
+          this.trigger('error error.' + actionName, error.message, error.name, error.isPublic);
           throw error;
         }
 
@@ -239,45 +227,7 @@ var CM_Form_Abstract = CM_View_Abstract.extend({
 
         this.trigger('success success.' + action.name, response.data);
         return response.data;
-      })
-      .finally(function() {
-        if (disableUI) {
-          this.enable();
-        }
       });
-  },
-
-  _handleSubmitError: function(error, actionName, displayErrors) {
-    if (error instanceof CM_Exception_FormFieldRequired) {
-      this._handleRequiredError(error, actionName, displayErrors);
-    } else if (error instanceof CM_Exception_FormFieldValidation) {
-      this._handleValidationError(error, actionName, displayErrors);
-    } else {
-      throw error;
-    }
-  },
-
-  _handleRequiredError: function(error, actionName, displayErrors) {
-    if (displayErrors) {
-      return this._displayValidationError(error);
-    }
-    throw error;
-  },
-
-  /**
-   * @param {CM_Exception_FormFieldValidation} error
-   * @param {String} actionName
-   * @param {Boolean} displayErrors
-   */
-  _handleValidationError: function(error, actionName, displayErrors) {
-    if (displayErrors) {
-      this._displayValidationError(error);
-    }
-    this._stopErrorPropagation = false;
-    this.trigger('error error.' + actionName, error.message, error.name, error.isPublic);
-    if (!this._stopErrorPropagation && !displayErrors) {
-      throw error;
-    }
   },
 
   /**
@@ -292,10 +242,6 @@ var CM_Form_Abstract = CM_View_Abstract.extend({
         this.error(error);
       }
     }
-  },
-
-  stopErrorPropagation: function() {
-    this._stopErrorPropagation = true;
   },
 
   reset: function() {
