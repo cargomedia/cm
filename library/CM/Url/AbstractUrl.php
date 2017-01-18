68e956c927dfe9b36ae019a260c025f69d198c6b
Michel Meyer <michel.meyer@cargomedia.ch>
2017-01-18 18:30:42 +0100

    Add AbstractUrl modifiers: normalize + sanitize

--- library/CM/Url/AbstractUrl.php
+++ library/CM/Url/AbstractUrl.php
@@ -2,10 +2,12 @@
 
 namespace CM\Url;
 
-use Psr\Http\Message\UriInterface;
-use League\Uri\Schemes\Generic\AbstractHierarchicalUri;
+use CM\Url\Modifiers\Sanitize;
+use League\Uri\Modifiers\Normalize;
+use League\Uri\Modifiers\Pipeline;
+use League\Uri\Schemes\Http;
 
-abstract class AbstractUrl extends AbstractHierarchicalUri implements UrlInterface {
+abstract class AbstractUrl extends Http implements UrlInterface {
 
     public function getRebaseUrl(UrlInterface $baseUrl) {
         $baseUrl = AbsoluteUrl::createFromString((string) $baseUrl);
@@ -15,10 +17,10 @@ abstract class AbstractUrl extends AbstractHierarchicalUri implements UrlInterfa
     }
 
     /**
-     * @param UriInterface $uri
+     * @param UrlInterface $uri
      * @return UrlInterface
      */
-    public function withRelativeComponentsFrom(UriInterface $uri) {
+    public function withRelativeComponentsFrom(UrlInterface $uri) {
         return $this
             ->withPath($uri->getPath())
             ->withQuery($uri->getQuery())
@@ -30,5 +32,27 @@ abstract class AbstractUrl extends AbstractHierarchicalUri implements UrlInterfa
      */
     public function isRelativeUrl() {
         return '' === $this->getScheme() && '' === $this->getHost();
+    }
+
+    /**
+     * @return Pipeline
+     */
+    protected static function _getPipeline() {
+        return new Pipeline([
+            new Normalize(),
+            new Sanitize(),
+        ]);
+    }
+
+    public static function createFromString($uri = '') {
+        return self::_getPipeline()->process(
+            parent::createFromString($uri)
+        );
+    }
+
+    public static function createFromComponents(array $components) {
+        return self::_getPipeline()->process(
+            parent::createFromComponents($components)
+        );
     }
 }
