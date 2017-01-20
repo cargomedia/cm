8e4e9ef587ba36e23492465bd41bf3d845dae6dd
Daniel Londero <daniel.londero@gmail.com>
2017-01-20 13:00:05 +0100

    Switch SVM repository for testing

--- tests/travis/install-php-svm.sh
+++ tests/travis/install-php-svm.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
-VERSION="0.1.9"
+# VERSION="0.1.9"
 
-curl -sL https://github.com/ianbarber/php-svm/archive/${VERSION}.tar.gz | tar -xzf -
-cd php-svm-${VERSION}
+# curl -sL https://github.com/ianbarber/php-svm/archive/${VERSION}.tar.gz | tar -xzf -
+git clone https://github.com/echobot/php-svm.git
+cd php-svm
 phpize
 ./configure
 make
