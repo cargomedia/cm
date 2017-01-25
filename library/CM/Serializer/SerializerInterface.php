13456a187375bc16add41b2bff2788fc6f42a7b0
Tomasz Durka <tomasz@durka.pl>
2017-01-25 13:31:24 +0100

    Serialize to, unserialize from strings

--- library/CM/Serializer/SerializerInterface.php
+++ library/CM/Serializer/SerializerInterface.php
@@ -4,12 +4,12 @@ interface CM_Serializer_SerializerInterface {
 
     /**
      * @param mixed $data
-     * @return mixed
+     * @return string
      */
     public function serialize($data);
 
     /**
-     * @param mixed $data
+     * @param string $data
      * @return mixed
      */
     public function unserialize($data);
