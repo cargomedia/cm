4199ba98ae05529e0b2e37421e6852d9d7db052a
Tomasz Durka <tomasz@durka.pl>
2017-01-25 13:32:16 +0100

    Add docu for JobQueue

--- library/CM/Gearman/JobQueue.php
+++ library/CM/Gearman/JobQueue.php
@@ -8,11 +8,18 @@ class CM_Gearman_JobQueue implements CM_Jobdistribution_QueueInterface {
     /** @var CM_Gearman_JobWorker */
     private $_worker;
 
+    /**
+     * @param CM_Gearman_Publisher $publisher
+     * @param CM_Gearman_JobWorker $worker
+     */
     public function __construct(CM_Gearman_Publisher $publisher, CM_Gearman_JobWorker $worker) {
         $this->_publisher = $publisher;
         $this->_worker = $worker;
     }
 
+    /**
+     * @param CM_Jobdistribution_Job_Abstract $job
+     */
     public function publish(CM_Jobdistribution_Job_Abstract $job) {
         $this->_publisher->queue($job);
     }
