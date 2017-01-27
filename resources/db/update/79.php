b10bfd052fb5b5eff2ce5a81e03ad8e37b7b7949
Alexis Peter <alexis.peter@cargomedia.ch>
2017-01-27 18:47:08 +0100

    remove file-deletion

--- resources/db/update/79.php
+++ resources/db/update/79.php
@@ -1,13 +1,5 @@
 <?php
 
-$filename = md5('app-maintenance-local');
-$file = new CM_File("clockwork/{$filename}.json", $this->getServiceManager()->getFilesystems()->getData());
-$file->delete();
-
-$filename = md5('search-maintenance');
-$file = new CM_File("clockwork/{$filename}.json", $this->getServiceManager()->getFilesystems()->getData());
-$file->delete();
-
 if (CM_Db_Db::existsTable('cm_svm')) {
     CM_Db_Db::exec('DROP TABLE cm_svm');
 }
