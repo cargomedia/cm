69c969d3ebdc518237620b80051bb68baf84b911
Alexis Peter <alexis.peter@cargomedia.ch>
2017-01-27 18:29:38 +0100

    removed svm tables

--- resources/db/update/79.php
+++ resources/db/update/79.php
@@ -7,3 +7,11 @@ $file->delete();
 $filename = md5('search-maintenance');
 $file = new CM_File("clockwork/{$filename}.json", $this->getServiceManager()->getFilesystems()->getData());
 $file->delete();
+
+if (CM_Db_Db::existsTable('cm_svm')) {
+    CM_Db_Db::exec('DROP TABLE cm_svm');
+}
+
+if (CM_Db_Db::existsTable('cm_svmtraining')) {
+    CM_Db_Db::exec('DROP TABLE cm_svmtraining');
+}
