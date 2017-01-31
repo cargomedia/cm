eb3076f712c4bfa284a495b2974bc8dc53886799
Michel Meyer <michel.meyer@cargomedia.ch>
2017-01-31 12:55:39 +0100

    Introduce Url::createWithParams() as a replacement for CM_Util::link()

--- tests/library/CM/Url/UrlTest.php
+++ tests/library/CM/Url/UrlTest.php
@@ -46,4 +46,30 @@ class UrlTest extends CMTest_TestCase {
         $url = $url->withSite($environment->getSite());
         $this->assertSame('http://www.example.com/foo/fr/path/to/something?param=1#fragment', (string) $url);
     }
+
+    public function testCreateWithParams() {
+        $url = Url::createWithParams('');
+        $this->assertSame(null, $url->getParams());
+        $this->assertSame('', $url->getQuery());
+        $this->assertSame('', $url->getFragment());
+        $this->assertSame('', (string) $url);
+
+        $url = Url::createWithParams('', []);
+        $this->assertSame([], $url->getParams());
+        $this->assertSame('', $url->getQuery());
+        $this->assertSame('', $url->getFragment());
+        $this->assertSame('', (string) $url);
+
+        $url = Url::createWithParams('/', ['foo' => 1]);
+        $this->assertSame(['foo' => '1'], $url->getParams());
+        $this->assertSame('foo=1', $url->getQuery());
+        $this->assertSame('', $url->getFragment());
+        $this->assertSame('/?foo=1', (string) $url);
+
+        $url = Url::createWithParams('/', ['foo' => 1], 'bar');
+        $this->assertSame(['foo' => '1'], $url->getParams());
+        $this->assertSame('foo=1', $url->getQuery());
+        $this->assertSame('bar', $url->getFragment());
+        $this->assertSame('/?foo=1#bar', (string) $url);
+    }
 }
