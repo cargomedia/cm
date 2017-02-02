fa9cd951042de79edb0478404ec58c9e8c177c66
Michel Meyer <michel.meyer@cargomedia.ch>
2017-02-02 17:42:59 +0100

    Fix "bin/cm migration add"

--- library/CM/Migration/Manager.php
+++ library/CM/Migration/Manager.php
@@ -31,7 +31,7 @@ class CM_Migration_Manager implements CM_Service_ManagerAwareInterface {
             $this->_getMigrationPathByModule(),
         ];
         foreach ($this->_modules as $moduleName) {
-            $paths[] = $this->_getMigrationPathByModule($moduleName);
+            $paths[] = self::getMigrationPathByModule($moduleName);
         }
         return $paths;
     }
@@ -40,7 +40,7 @@ class CM_Migration_Manager implements CM_Service_ManagerAwareInterface {
      * @param string|null $moduleName
      * @return string
      */
-    protected function _getMigrationPathByModule($moduleName = null) {
+    public static function getMigrationPathByModule($moduleName = null) {
         $modulePath = null !== $moduleName ? CM_Util::getModulePath((string) $moduleName) : DIR_ROOT;
         return join(DIRECTORY_SEPARATOR, [$modulePath, 'resources', 'migration']);
     }
