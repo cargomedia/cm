b9428ef63f08aa72f4b37d304c55a4c3f429e75f
vogdb <vozhdb@gmail.com>
2017-02-09 14:26:40 +0100

    remove CM_HtmlConverter

--- library/CM/FormField/Textarea.php
+++ library/CM/FormField/Textarea.php
@@ -1,10 +1,4 @@
 <?php
 
 class CM_FormField_Textarea extends CM_FormField_Text {
-
-    public function validate(CM_Frontend_Environment $environment, $userInput) {
-        $converter = new CM_HtmlConverter();
-        $userInput = $converter->convertHtmlToPlainText($userInput);
-        return parent::validate($environment, $userInput);
-    }
 }
