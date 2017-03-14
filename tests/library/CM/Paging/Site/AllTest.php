f9986eec056e1a8afa5f4b033b675e7ced75e211
Vladimir Finko <vladimir.finko@cargomedia.ch>
2017-03-14 17:04:30 +0100

    streamline all paging test

--- tests/library/CM/Paging/Site/AllTest.php
+++ tests/library/CM/Paging/Site/AllTest.php
@@ -7,11 +7,11 @@ class CM_Paging_Site_AllTest extends CMTest_TestCase {
     }
 
     public function testPaging() {
-        $this->assertSame(1, (new CM_Paging_Site_All())->getCount()); //mockSiteDefault
-        $this->getMockSite();
-        $this->getMockSite();
+        $defaultSite = (new CM_Site_SiteFactory())->getDefaultSite();
+        $this->assertEquals([$defaultSite], (new CM_Paging_Site_All())->getItems());
+        $site2 = $this->getMockSite();
+        $site3 = $this->getMockSite();
 
-        $this->assertSame(3, (new CM_Paging_Site_All())->getCount());
-        $this->assertInstanceOf('CM_Site_Abstract', (new CM_Paging_Site_All())->getItem(0));
+        $this->assertEquals([$defaultSite, $site2, $site3], (new CM_Paging_Site_All())->getItems());
     }
 }
