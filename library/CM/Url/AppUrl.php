38ef4d2532edfb239560840da9309257e1a4c676
Michel Meyer <michel.meyer@cargomedia.ch>
2017-05-08 15:45:23 +0200

    Move Url params to AppUrl

--- library/CM/Url/AppUrl.php
+++ library/CM/Url/AppUrl.php
@@ -2,10 +2,52 @@
 
 namespace CM\Url;
 
+use CM_Site_Abstract;
+use CM_Frontend_Environment;
+use CM_Model_Language;
+
 class AppUrl extends Url {
 
+    /** @var CM_Model_Language|null */
+    protected $_language = null;
+
+    /** @var CM_Site_Abstract|null */
+    protected $_site = null;
+
+    /** @var string|null */
+    protected $_deployVersion = null;
+
     /**
-     * @return null|string
+     * @param CM_Frontend_Environment $environment
+     * @return AppUrl
+     */
+    public function withEnvironment(CM_Frontend_Environment $environment) {
+        $url = clone $this;
+        if ($language = $environment->getLanguage()) {
+            $url = $url->withLanguage($language);
+        }
+        return $url->withSite($environment->getSite());
+    }
+
+    /**
+     * @param CM_Model_Language $language
+     * @return AppUrl
+     */
+    public function withLanguage(CM_Model_Language $language) {
+        $url = clone $this;
+        $url->_language = $language;
+        return $url;
+    }
+
+    /**
+     * @return CM_Model_Language|null
+     */
+    public function getLanguage() {
+        return $this->_language;
+    }
+
+    /**
+     * @return string|null
      */
     public function getLanguageSegment() {
         $language = $this->getLanguage();
@@ -13,11 +55,42 @@ class AppUrl extends Url {
     }
 
     /**
-     * @return null|string
+     * @param CM_Site_Abstract $site
+     * @return AppUrl
+     */
+    public function withSite(CM_Site_Abstract $site) {
+        $url = clone $this;
+        $url->_site = $site;
+        return $url->withBaseUrl($site->getUrl());
+    }
+
+    /**
+     * @return CM_Site_Abstract|null
+     */
+    public function getSite() {
+        return $this->_site;
+    }
+
+    /**
+     * @return string|null
      */
     public function getSiteSegment() {
         $site = $this->getSite();
         return $site ? 'site-' . $site->getId() : null;
+    }
+
+    /**
+     * @param string|null $deployVersion
+     */
+    public function setDeployVersion($deployVersion) {
+        $this->_deployVersion = $deployVersion;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDeployVersion() {
+        return $this->_deployVersion;
     }
 
     /**
@@ -26,5 +99,52 @@ class AppUrl extends Url {
     public function getDeployVersionSegment() {
         $version = $this->getDeployVersion();
         return $version ? 'version-' . $version : null;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSegments() {
+        $segments = [];
+        if ($prefix = $this->getPrefix()) {
+            $segments[] = $prefix;
+        }
+        return array_merge(
+            $segments,
+            $this->_getParameterSegments(),
+            explode('/', $this->path)
+        );
+    }
+
+    /**
+     * @return array
+     */
+    protected function _getParameterSegments() {
+        $segments = [];
+        if ($languageSegment = $this->getLanguageSegment()) {
+            $segments[] = $languageSegment;
+        }
+        if ($siteSegment = $this->getSiteSegment()) {
+            $segments[] = $siteSegment;
+        }
+        if ($versionSegment = $this->getDeployVersionSegment()) {
+            $segments[] = $versionSegment;
+        }
+        return $segments;
+    }
+
+    /**
+     * @param string                       $url
+     * @param CM_Frontend_Environment|null $environment
+     * @param string|null                  $deployVersion
+     * @return static
+     */
+    public static function createWithEnvironment($url, CM_Frontend_Environment $environment = null, $deployVersion = null) {
+        $url = new static($url);
+        $url->setDeployVersion($deployVersion);
+        if ($environment) {
+            $url = $url->withEnvironment($environment);
+        }
+        return $url;
     }
 }
--- tests/library/CM/Url/AppUrlTest.php
+++ tests/library/CM/Url/AppUrlTest.php
@@ -4,9 +4,7 @@ namespace CM\Test\Url;
 
 use CMTest_TH;
 use CMTest_TestCase;
-use CM\Url\BaseUrl;
 use CM\Url\AppUrl;
-use JsonSerializable;
 
 class AppUrlTest extends CMTest_TestCase {
 
@@ -44,5 +42,44 @@ class AppUrlTest extends CMTest_TestCase {
         $this->assertSame('prefix', $url->getPrefix());
         $this->assertSame($environment->getLanguage(), $url->getLanguage());
         $this->assertSame('http://www.example.com/prefix/language-de/site-44/bar?foobar=1', (string) $url);
+    }
+
+    public function testWithLanguage() {
+        $language = CMTest_TH::createLanguage('de');
+        $url = new AppUrl('/bar?foobar=1');
+        $urlWithLanguage = $url->withLanguage($language);
+
+        $this->assertNotEquals($url, $urlWithLanguage);
+        $this->assertSame(null, $url->getLanguage());
+        $this->assertSame('/bar?foobar=1', (string) $url);
+        $this->assertSame('/bar', $urlWithLanguage->getPath());
+        $this->assertSame($language, $urlWithLanguage->getLanguage());
+        $this->assertSame('/language-de/bar?foobar=1', (string) $urlWithLanguage);
+    }
+
+    public function testWithEnvironment() {
+        $url = new AppUrl('/bar?foobar=1');
+
+        $environment = $this->createEnvironment();
+        $urlWithEnvironment = $url->withEnvironment($environment);
+        $this->assertSame(null, $url->getLanguage());
+        $this->assertSame('/bar?foobar=1', (string) $url);
+        $this->assertSame($environment->getSite(), $urlWithEnvironment->getSite());
+        $this->assertSame(null, $urlWithEnvironment->getLanguage());
+        $this->assertSame('/bar', $urlWithEnvironment->getPath());
+        $this->assertSame('http://www.example.com/site-42/bar?foobar=1', (string) $urlWithEnvironment);
+
+        $environment = $this->createEnvironment(null, null, 'de');
+        $urlWithEnvironmentAndLanguage = $url->withEnvironment($environment);
+        $this->assertSame($environment->getSite(), $urlWithEnvironmentAndLanguage->getSite());
+        $this->assertSame($environment->getLanguage(), $urlWithEnvironmentAndLanguage->getLanguage());
+        $this->assertSame('/bar', $urlWithEnvironmentAndLanguage->getPath());
+        $this->assertSame('http://www.example.com/language-de/site-43/bar?foobar=1', (string) $urlWithEnvironmentAndLanguage);
+
+        $urlWithEnvironmentPreserved = $urlWithEnvironmentAndLanguage->withPath('/baz');
+        $this->assertSame($environment->getSite(), $urlWithEnvironmentPreserved->getSite());
+        $this->assertSame($environment->getLanguage(), $urlWithEnvironmentPreserved->getLanguage());
+        $this->assertSame('/baz', $urlWithEnvironmentPreserved->getPath());
+        $this->assertSame('http://www.example.com/language-de/site-43/baz?foobar=1', (string) $urlWithEnvironmentPreserved);
     }
 }
