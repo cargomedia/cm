83436ab9fdbb1ddc589c901f928b7a07e2f3edb8
Michel Meyer <michel.meyer@cargomedia.ch>
2017-05-22 12:16:34 +0200

    Run tests by chunk

--- codeship/test.sh
+++ codeship/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
 set -e
 
 ./app-wait-services.sh
-./bin/phpunit $@
+
+export PHPUNIT_TEST_RANGE=$1
+./bin/phpunit  --bootstrap ./tests/bootstrap.php --configuration ./phpunit.xml  ./tests/helpers/CMTest/library/CMTest/SplittedUnitTest.php
