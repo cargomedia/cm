506c14ad5f3c6f8918bd43cfdc4c653d7d9b927d
Michel Meyer <michel.meyer@cargomedia.ch>
2017-06-14 12:25:09 +0200

    Install deps in test script (should be moved to the cm-app image)

--- ci/all.sh
+++ ci/all.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+apt-get update && apt-get install -y net-tools netcat curl redis-tools mysql-client
+
 set -e
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
