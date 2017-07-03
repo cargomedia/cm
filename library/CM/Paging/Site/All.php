695ec7e196ce821a57a1f31bc35f52cefb9490ec
Vladimir Finko <vladimir.finko@cargomedia.ch>
2017-07-03 16:24:49 +0200

    fetch models without additional cache

--- library/CM/Paging/Site/All.php
+++ library/CM/Paging/Site/All.php
@@ -3,12 +3,12 @@
 class CM_Paging_Site_All extends CM_Paging_Abstract {
 
     public function __construct() {
-        $source = new CM_PagingSource_MongoDb('cm_site', [], ['_type' => 1]);
+        $source = new CM_PagingSource_MongoDb('cm_site');
         $source->enableCache();
         parent::__construct($source);
     }
 
     protected function _processItem($item) {
-        return CM_Site_Abstract::factoryFromType($item['_id'], $item['_type']);
+        return CM_Site_Abstract::factoryFromModelData($item);
     }
 }
