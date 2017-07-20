f19ffc12f5d6b087975cf5d045469d562aa12b33
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2017-07-20 12:34:07 +0200

    Verified recipient address before sending emails

--- library/CM/Mail/SendJob.php
+++ library/CM/Mail/SendJob.php
@@ -3,9 +3,19 @@
 class CM_Mail_SendJob extends CM_Jobdistribution_Job_Abstract {
 
     protected function _execute(CM_Params $params) {
-        CM_Service_Manager::getInstance()->getMailer()->send($params->getMailMessage('message'));
-        if ($params->has('recipient') && $params->has('mailType')) {
-            $recipient = $params->getUser('recipient');
+        $message = $params->getMailMessage('message');
+        $recipient = $params->has('recipient') ? $params->getUser('recipient') : null;
+        if (!$recipient || !$recipient->getEmailVerified()) {
+            /** @var CM_Service_EmailVerification_ClientInterface $emailVerificationClient */
+            $emailVerificationClient = CM_Service_Manager::getInstance()->get('email-verification', CM_Service_EmailVerification_ClientInterface::class);
+            foreach ($message->getTo() as $address => $name) {
+                if (!$emailVerificationClient->isValid($address)) {
+                    return;
+                }
+            }
+        }
+        CM_Service_Manager::getInstance()->getMailer()->send($message);
+        if ($recipient && $params->has('mailType')) {
             $mailType = $params->getInt('mailType');
             $action = new CM_Action_Email(CM_Action_Abstract::SEND, $recipient, $mailType);
             $action->prepare($recipient);
