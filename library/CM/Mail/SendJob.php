1d6e100438018e632dce113bea4d2df286e90157
Sébastien Fauvel <sebastien.fauvel@cargomedia.ch>
2017-07-20 14:21:58 +0200

    Coding style

--- library/CM/Mail/SendJob.php
+++ library/CM/Mail/SendJob.php
@@ -5,6 +5,7 @@ class CM_Mail_SendJob extends CM_Jobdistribution_Job_Abstract {
     protected function _execute(CM_Params $params) {
         $message = $params->getMailMessage('message');
         $recipient = $params->has('recipient') ? $params->getUser('recipient') : null;
+        $mailType = $params->has('mailType') ? $params->getInt('mailType') : null;
         if (!$recipient || !$recipient->getEmailVerified()) {
             /** @var CM_Service_EmailVerification_ClientInterface $emailVerificationClient */
             $emailVerificationClient = CM_Service_Manager::getInstance()->get('email-verification', CM_Service_EmailVerification_ClientInterface::class);
@@ -15,8 +16,7 @@ class CM_Mail_SendJob extends CM_Jobdistribution_Job_Abstract {
             }
         }
         CM_Service_Manager::getInstance()->getMailer()->send($message);
-        if ($recipient && $params->has('mailType')) {
-            $mailType = $params->getInt('mailType');
+        if ($recipient && isset($mailType)) {
             $action = new CM_Action_Email(CM_Action_Abstract::SEND, $recipient, $mailType);
             $action->prepare($recipient);
             $action->notify($recipient);
